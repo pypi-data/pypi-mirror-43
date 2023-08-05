9bc96583b5e04d11c4368720957db7f43711c6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:29 +0000

    Add package robotframework-jsd/0.1.2/robotframework-jsd-0.1.2.tar.gz

---
+++ packages/robotframework-jsd/robotframework-jsd-0.1.2.tar.gz/robotframework-jsd-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+version_file = join(dirname(abspath(__file__)), 'JsdLibrary', 'version.py')
+
+with open(version_file) as f:
+    code = compile(f.read(), version_file, 'exec')
+    exec code
+
+setup(
+    name='robotframework-jsd',
+    version=VERSION,
+    description='JSD validator for robotframework',
+    author='mangobowl',
+    author_email='mangobowl@163.com',
+    license='BSD License',
+    keywords='robotframework testing test automation json json-schema',
+    packages=['JsdLibrary'],
+    install_requires=[
+        'robotframework',
+        'jsonschema'
+    ],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing'
+    ],
+)
