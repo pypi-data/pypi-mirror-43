bae58d8e16138712cb41eb3215640d678cbdff8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package edc-registration/0.2.23/edc_registration-0.2.23-py3-none-any.whl

---
+++ packages/edc-registration/edc_registration-0.2.23-py3-none-any.whl/edc_registration/admin.py
@@ -0,0 +1,61 @@
+from django.contrib import admin
+from edc_model_admin import audit_fieldset_tuple, audit_fields, SimpleHistoryAdmin
+
+from .admin_mixins import RegisteredSubjectModelAdminMixin
+from .admin_site import edc_registration_admin
+from .models import RegisteredSubject
+
+
+@admin.register(RegisteredSubject, site=edc_registration_admin)
+class RegisteredSubjectAdmin(RegisteredSubjectModelAdminMixin, SimpleHistoryAdmin):
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "subject_identifier",
+                    "sid",
+                    "first_name",
+                    "last_name",
+                    "initials",
+                    "dob",
+                    "gender",
+                    "subject_type",
+                    "registration_status",
+                    "identity",
+                    "screening_identifier",
+                    "screening_datetime",
+                    "registration_datetime",
+                    "randomization_datetime",
+                    "consent_datetime",
+                )
+            },
+        ),
+        audit_fieldset_tuple,
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        fields = super().get_readonly_fields(request, obj=obj)
+        fields = (
+            fields
+            + (
+                "subject_identifier",
+                "sid",
+                "first_name",
+                "last_name",
+                "initials",
+                "dob",
+                "gender",
+                "subject_type",
+                "registration_status",
+                "identity",
+                "screening_identifier",
+                "screening_datetime",
+                "registration_datetime",
+                "randomization_datetime",
+                "consent_datetime",
+            )
+            + audit_fields
+        )
+        return fields
