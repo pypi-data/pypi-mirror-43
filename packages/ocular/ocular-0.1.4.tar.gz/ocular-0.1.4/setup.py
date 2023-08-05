b2c7ab21698b1f1ef66efe0ef62afe1973a7b88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:26 +0000

    Add package ocular/0.1.4/ocular-0.1.4.tar.gz

---
+++ packages/ocular/ocular-0.1.4.tar.gz/ocular-0.1.4/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+def read_readme():
+    with open('README.md') as f:
+        return f.read()
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='ocular',
+      version='0.1.4',
+      description="A tool for observing Kubernetes pods' statuses in real time.",
+      long_description=read_readme(),
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mourad@polyaxon.com',
+      author='Mourad Mourafiq',
+      author_email='mourad@polyaxon.com',
+      url='https://github.com/polyaxon/ocular',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'aws',
+          'azure',
+          'google cloud storage',
+          'k8s',
+          'kubernetes',
+          'docker'
+      ],
+      install_requires=[
+          "hestia>=0.2.4",
+          "kubernetes>=8.0.1",
+          "six==1.12.0"
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[
+          "pytest",
+      ],
+      cmdclass={'test': PyTest})
