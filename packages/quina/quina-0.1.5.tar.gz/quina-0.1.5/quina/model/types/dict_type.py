5393d69da7fd475be29cba3e02177ea995a5c4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:48 +0000

    Add package quina/0.1.5/quina-0.1.5.tar.gz

---
+++ packages/quina/quina-0.1.5.tar.gz/quina-0.1.5/quina/model/types/dict_type.py
@@ -0,0 +1,37 @@
+from ..base import ValueInterface
+from ..base import ChangedValueEntity
+from ..base import AbstractItem, AbstractProperty
+
+
+class DictTypeMixin(ValueInterface):
+    @property
+    def value(self) -> dict:
+        return self.get_value()
+
+    @value.setter
+    def value(self, value):
+        self.set_value(value)
+
+
+class DictTypeProperty(AbstractProperty, DictTypeMixin):
+    pass
+
+
+class DictPropertyInterface(ChangedValueEntity):
+    @property
+    def dict(self) -> DictTypeProperty:
+        return self.attach(creator=DictTypeProperty, args=(
+            self.get_dict_value, self.set_dict_value, self.changed
+        ))
+
+    def get_dict_value(self) -> dict:
+        return self.get_value()
+
+    def set_dict_value(self, value: dict):
+        if not isinstance(value, dict):
+            raise ValueError(f'UnSupport Set Value Type {type(value)} to Type dict')
+        return self.set_value(value)
+
+
+class DictItem(AbstractItem, DictPropertyInterface):
+    pass
