fe9bdd206cedb21c55a9244fcc5b7cf4f77df1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package luigi-daisy/0.0.dev0/luigi-daisy-0.0.dev0.tar.gz

---
+++ packages/luigi-daisy/luigi-daisy-0.0.dev0.tar.gz/luigi-daisy-0.0.dev0/daisy/__init__.py
@@ -0,0 +1,11 @@
+from luigi import *
+from luigi.util import *
+
+from . import task
+# This will overwrite luigi.Task
+from .task import *
+
+from . import formatted_target
+from .formatted_target import *
+
+
