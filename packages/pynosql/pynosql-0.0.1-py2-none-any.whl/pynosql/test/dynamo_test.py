1df9e59e5e919986b295626905727f6e7deb30f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:15 +0000

    Add package pynosql/0.0.1/pynosql-0.0.1-py2-none-any.whl

---
+++ packages/pynosql/pynosql-0.0.1-py2-none-any.whl/pynosql/test/dynamo_test.py
@@ -0,0 +1,175 @@
+import unittest
+from boto3.dynamodb.conditions import Key
+from pynosql.base.model import Model
+from pynosql.clients.aws_mock import AWSMockResponses
+from pynosql.providers.dynamo import DynamoDBProvider
+from pynosql.credentials.aws import AWSCredentials
+from pynosql.clients.aws import AWSClient
+
+
+class TestModel(Model):
+
+    BASE = {
+        'test1': None,
+        'test2': None,
+        'test3': None,
+        'test4': None
+    }
+
+    def __init__(self):
+        super(TestModel, self).__init__(self.BASE)
+
+
+class TestSetup(unittest.TestCase):
+    """ Credentials Test """
+
+    def setUp(self):
+        """ Create Mock Client and Provider
+
+        :return:
+        """
+        self.credentials = AWSCredentials(
+            'AKIAIOSFODNN7EXAMPLE',
+            'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        )
+
+    def test_basic_get_item(self):
+        """ Test basic get_item from dynamodb table """
+        expected = {
+            'test1': 'value1',
+            'test2': 'value2',
+            'test3': 'value3',
+            'test4': 'value4'
+        }
+        key = {
+            'test1': 'value1',
+            'test2': 'value2'
+        }
+        dynamo = DynamoDBProvider(
+            AWSClient(self.credentials,
+                      'us-west-2',
+                      mock=AWSMockResponses.get_item_response)
+        )
+        response = dynamo.get_record(TestModel(), 'TestTable', Key=key)
+
+        self.assertDictEqual(response, expected)
+
+    def test_basic_query(self):
+        """ Test basic query from DynamoDB table """
+        expected = [
+            {
+                'test1': 'value1',
+                'test2': 'value2',
+                'test3': 'value3',
+                'test4': 'value4',
+            },
+            {
+                'test1': 'value5',
+                'test2': 'value6',
+                'test3': 'value7',
+                'test4': 'value8',
+            },
+            {
+                'test1': 'value9',
+                'test2': 'value10',
+                'test3': 'value11',
+                'test4': 'value12',
+            },
+            {
+                'test1': 'value13',
+                'test2': 'value14',
+                'test3': 'value15',
+                'test4': 'value16',
+            },
+        ]
+        dynamo = DynamoDBProvider(
+            AWSClient(self.credentials,
+                      'us-west-2',
+                      mock=AWSMockResponses.query_response)
+        )
+        response = dynamo.get_records(
+            TestModel(), 'TestTable',
+            KeyConditionExpression=Key('test1').eq('value1'),
+            IndexName='TestIndex'
+        )
+
+        self.assertListEqual(response, expected)
+
+    def test_basic_scan(self):
+        """ Test basic scan from DynamoDB table """
+        expected = [
+            {
+                'test1': 'value1',
+                'test2': 'value2',
+                'test3': 'value3',
+                'test4': 'value4',
+            },
+            {
+                'test1': 'value5',
+                'test2': 'value6',
+                'test3': 'value7',
+                'test4': 'value8',
+            },
+            {
+                'test1': 'value9',
+                'test2': 'value10',
+                'test3': 'value11',
+                'test4': 'value12',
+            },
+            {
+                'test1': 'value13',
+                'test2': 'value14',
+                'test3': 'value15',
+                'test4': 'value16',
+            },
+        ]
+        dynamo = DynamoDBProvider(
+            AWSClient(self.credentials,
+                      'us-west-2',
+                      mock=AWSMockResponses.scan_response)
+        )
+        response = dynamo.scan_records(
+            TestModel(), 'TestTable',
+            FilterExpression=Key('test1').eq('value1'),
+            IndexName='TestIndex'
+        )
+
+        self.assertListEqual(response, expected)
+
+    def test_basic_put_item(self):
+        """ Test basic put_item into dynamodb table """
+        expected = {
+            'test1': 'value1',
+            'test2': 'value2',
+            'test3': 'value3',
+            'test4': 'value4'
+        }
+        dynamo = DynamoDBProvider(
+            AWSClient(self.credentials,
+                      'us-west-2',
+                      mock=AWSMockResponses.put_item_response)
+        )
+        response = dynamo.put_record(TestModel(), 'TestTable', Item=expected)
+
+        self.assertDictEqual(response, expected)
+
+    def test_basic_delete_item(self):
+        """ Test basic delete_item from dynamodb table """
+        expected = {
+            'test1': 'value1',
+            'test2': 'value2',
+            'test3': 'value3',
+            'test4': 'value4'
+        }
+        key = {
+            'test1': 'value1',
+            'test2': 'value2'
+        }
+        dynamo = DynamoDBProvider(
+            AWSClient(self.credentials,
+                      'us-west-2',
+                      mock=AWSMockResponses.delete_item_response)
+        )
+        response = dynamo.delete_record(TestModel(), 'TestTable', Key=key)
+
+        self.assertDictEqual(response, expected)
