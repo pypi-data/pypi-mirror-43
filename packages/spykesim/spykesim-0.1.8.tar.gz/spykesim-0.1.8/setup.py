0575625852826afe13fceec12321392ee0c99922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package spykesim/0.1.8/spykesim-0.1.8.tar.gz

---
+++ packages/spykesim/spykesim-0.1.8.tar.gz/spykesim-0.1.8/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+try:
+    from Cython.Distutils.extension import Extension
+    from Cython.Distutils import build_ext
+except ImportError:
+    from setuptools import Extension
+    USING_CYTHON = False
+else:
+    USING_CYTHON = True
+print("using cython: ", USING_CYTHON)
+ext = 'pyx' if USING_CYTHON else 'c'
+sources = glob('spykesim/*.%s' % (ext,))
+extensions = [
+    Extension(source.split('.')[0].replace(os.path.sep, '.'),
+              sources=[source],
+    )
+for source in sources]
+cmdclass = {'build_ext': build_ext} if USING_CYTHON else {}
+
+import numpy
+setup(
+    name='spykesim',
+    version='0.1.8',
+    description='Python module that offers functions for measuring the similarity between two segmented multi-neuronal spiking activities.',
+    long_description=readme,
+    author='Keita Watanabe',
+    author_email='keitaw09@gmail.com',
+    install_requires=['scipy', 'joblib', 'tqdm'],
+    url='https://github.com/KeitaW/spikesim',
+    license=license,
+    ext_modules=extensions,
+    cmdclass=cmdclass,
+    include_dirs = [numpy.get_include()],
+)
+
