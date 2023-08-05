d966bba44e88809cf109c9dc7990b03a6b358a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:07 +0000

    Add package rpnChilada/7.2.3/rpnChilada-7.2.3.tar.gz

---
+++ packages/rpnChilada/rpnChilada-7.2.3.tar.gz/rpnChilada-7.2.3/buildRPN.bat
@@ -0,0 +1,12 @@
+rem TCC script for building the RPN wheel
+
+del dist\*.whl
+del dist\*.gz
+
+rem python makeUnits.py
+rem python makeHelp.py
+
+python setup.py sdist bdist_wheel
+
+del /sxyz /nt rpnChilada.egg-info
+
