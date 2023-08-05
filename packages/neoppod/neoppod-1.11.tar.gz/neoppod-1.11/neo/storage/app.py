8d36ec232246b6fb00e21828b8980f3ac4938959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:09 +0000

    Add package neoppod/1.11/neoppod-1.11.tar.gz

---
+++ packages/neoppod/neoppod-1.11.tar.gz/neoppod-1.11/neo/storage/app.py
@@ -0,0 +1,336 @@
+#
+# Copyright (C) 2006-2019  Nexedi SA
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+from collections import deque
+
+from neo.lib import logging
+from neo.lib.app import BaseApplication, buildOptionParser
+from neo.lib.protocol import CellStates, ClusterStates, NodeTypes, Packets
+from neo.lib.connection import ListeningConnection
+from neo.lib.exception import StoppedOperation, PrimaryFailure
+from neo.lib.pt import PartitionTable
+from neo.lib.util import dump
+from neo.lib.bootstrap import BootstrapManager
+from .checker import Checker
+from .database import buildDatabaseManager, DATABASE_MANAGER_DICT
+from .handlers import identification, initialization, master
+from .replicator import Replicator
+from .transactions import TransactionManager
+
+from neo.lib.debug import register as registerLiveDebugger
+
+option_defaults = {
+  'adapter': 'MySQL',
+  'wait': 0,
+}
+assert option_defaults['adapter'] in DATABASE_MANAGER_DICT
+
+@buildOptionParser
+class Application(BaseApplication):
+    """The storage node application."""
+
+    checker = replicator = tm = None
+
+    @classmethod
+    def _buildOptionParser(cls):
+        parser = cls.option_parser
+        parser.description = "NEO Storage node"
+        cls.addCommonServerOptions('storage', '127.0.0.1')
+
+        _ = parser.group('storage')
+        _('a', 'adapter', choices=sorted(DATABASE_MANAGER_DICT),
+            help="database adapter to use")
+        _('d', 'database', required=True,
+            help="database connections string")
+        _.float('w', 'wait',
+            help="seconds to wait for backend to be available,"
+                 " before erroring-out (-1 = infinite)")
+        _.bool('disable-drop-partitions',
+            help="do not delete data of discarded cells, which is useful for"
+                 " big databases because the current implementation is"
+                 " inefficient (this option should disappear in the future)")
+
+        _ = parser.group('database creation')
+        _.int('u', 'uuid',
+            help="specify an UUID to use for this process. Previously"
+                 " assigned UUID takes precedence (i.e. you should"
+                 " always use reset with this switch)")
+        _('e', 'engine', help="database engine (MySQL only)")
+        _.bool('dedup',
+            help="enable deduplication of data"
+                 " when setting up a new storage node")
+        # TODO: Forbid using "reset" along with any unneeded argument.
+        #       "reset" is too dangerous to let user a chance of accidentally
+        #       letting it slip through in a long option list.
+        #       It should even be forbidden in configuration files.
+        _.bool('reset',
+            help="remove an existing database if any, and exit")
+
+        parser.set_defaults(**option_defaults)
+
+    def __init__(self, config):
+        super(Application, self).__init__(
+            config.get('ssl'), config.get('dynamic_master_list'))
+        # set the cluster name
+        self.name = config['cluster']
+
+        self.dm = buildDatabaseManager(config['adapter'],
+            (config['database'], config.get('engine'), config['wait']),
+        )
+        self.disable_drop_partitions = config.get('disable_drop_partitions',
+                                                  False)
+
+        # load master nodes
+        for master_address in config['masters']:
+            self.nm.createMaster(address=master_address)
+
+        # set the bind address
+        self.server = config['bind']
+        logging.debug('IP address is %s, port is %d', *self.server)
+
+        # The partition table is initialized after getting the number of
+        # partitions.
+        self.pt = None
+
+        self.listening_conn = None
+        self.master_conn = None
+        self.master_node = None
+
+        # operation related data
+        self.operational = False
+
+        self.dm.setup(reset=config.get('reset', False),
+                      dedup=config.get('dedup', False))
+        self.loadConfiguration()
+        self.devpath = self.dm.getTopologyPath()
+
+        # force node uuid from command line argument, for testing purpose only
+        if 'uuid' in config:
+            self.uuid = config['uuid']
+            logging.node(self.name, self.uuid)
+
+        registerLiveDebugger(on_log=self.log)
+
+    def close(self):
+        self.listening_conn = None
+        self.dm.close()
+        super(Application, self).close()
+
+    def _poll(self):
+        self.em.poll(1)
+
+    def log(self):
+        self.em.log()
+        self.nm.log()
+        if self.tm:
+            self.tm.log()
+        if self.pt is not None:
+            self.pt.log()
+
+    def loadConfiguration(self):
+        """Load persistent configuration data from the database.
+        If data is not present, generate it."""
+
+        dm = self.dm
+
+        # check cluster name
+        name = dm.getName()
+        if name is None:
+            dm.setName(self.name)
+        elif name != self.name:
+            raise RuntimeError('name %r does not match with the database: %r'
+                               % (self.name, name))
+
+        # load configuration
+        self.uuid = dm.getUUID()
+        logging.node(self.name, self.uuid)
+        num_partitions = dm.getNumPartitions()
+        num_replicas = dm.getNumReplicas()
+        ptid = dm.getPTID()
+
+        # check partition table configuration
+        if num_partitions is not None and num_replicas is not None:
+            if num_partitions <= 0:
+                raise RuntimeError, 'partitions must be more than zero'
+            # create a partition table
+            self.pt = PartitionTable(num_partitions, num_replicas)
+
+        logging.info('Configuration loaded:')
+        logging.info('PTID      : %s', dump(ptid))
+        logging.info('Name      : %s', self.name)
+        logging.info('Partitions: %s', num_partitions)
+        logging.info('Replicas  : %s', num_replicas)
+
+    def loadPartitionTable(self):
+        """Load a partition table from the database."""
+        self.pt.clear()
+        ptid = self.dm.getPTID()
+        if ptid is None:
+            return
+        cell_list = []
+        for offset, uuid, state in self.dm.getPartitionTable():
+            # register unknown nodes
+            if self.nm.getByUUID(uuid) is None:
+                self.nm.createStorage(uuid=uuid)
+            cell_list.append((offset, uuid, CellStates[state]))
+        self.pt.update(ptid, cell_list, self.nm)
+
+    def run(self):
+        try:
+            self._run()
+        except Exception:
+            logging.exception('Pre-mortem data:')
+            self.log()
+            logging.flush()
+            raise
+
+    def _run(self):
+        """Make sure that the status is sane and start a loop."""
+        if len(self.name) == 0:
+            raise RuntimeError, 'cluster name must be non-empty'
+
+        # Make a listening port
+        handler = identification.IdentificationHandler(self)
+        self.listening_conn = ListeningConnection(self, handler, self.server)
+        self.server = self.listening_conn.getAddress()
+
+        # Connect to a primary master node, verify data, and
+        # start the operation. This cycle will be executed permanently,
+        # until the user explicitly requests a shutdown.
+        self.operational = False
+        while True:
+            self.cluster_state = None
+            if self.master_node is None:
+                # look for the primary master
+                self.connectToPrimary()
+            self.checker = Checker(self)
+            self.replicator = Replicator(self)
+            self.tm = TransactionManager(self)
+            try:
+                self.initialize()
+                self.doOperation()
+                raise RuntimeError, 'should not reach here'
+            except StoppedOperation, msg:
+                logging.error('operation stopped: %s', msg)
+            except PrimaryFailure, msg:
+                logging.error('primary master is down: %s', msg)
+            finally:
+                self.operational = False
+            # When not ready, we reject any incoming connection so for
+            # consistency, we also close any connection except that to the
+            # master. This includes connections to other storage nodes and any
+            # replication is aborted, whether we are feeding or out-of-date.
+            for conn in self.em.getConnectionList():
+                if conn not in (self.listening_conn, self.master_conn):
+                    conn.close()
+            del self.checker, self.replicator, self.tm
+
+    def connectToPrimary(self):
+        """Find a primary master node, and connect to it.
+
+        If a primary master node is not elected or ready, repeat
+        the attempt of a connection periodically.
+
+        Note that I do not accept any connection from non-master nodes
+        at this stage."""
+        pt = self.pt
+
+        # search, find, connect and identify to the primary master
+        bootstrap = BootstrapManager(self, NodeTypes.STORAGE, self.server,
+                                     self.devpath)
+        self.master_node, self.master_conn, num_partitions, num_replicas = \
+            bootstrap.getPrimaryConnection()
+        self.dm.setUUID(self.uuid)
+
+        # Reload a partition table from the database. This is necessary
+        # when a previous primary master died while sending a partition
+        # table, because the table might be incomplete.
+        if pt is not None:
+            self.loadPartitionTable()
+            if num_partitions != pt.getPartitions():
+                raise RuntimeError('the number of partitions is inconsistent')
+
+        if pt is None or pt.getReplicas() != num_replicas:
+            # changing number of replicas is not an issue
+            self.dm.setNumPartitions(num_partitions)
+            self.dm.setNumReplicas(num_replicas)
+            self.pt = PartitionTable(num_partitions, num_replicas)
+            self.loadPartitionTable()
+
+    def initialize(self):
+        logging.debug('initializing...')
+        _poll = self._poll
+        self.master_conn.setHandler(initialization.InitializationHandler(self))
+        while not self.operational:
+            _poll()
+        self.master_conn.send(Packets.NotifyReady())
+
+    def doOperation(self):
+        """Handle everything, including replications and transactions."""
+        logging.info('doing operation')
+
+        poll = self._poll
+        _poll = self.em._poll
+        isIdle = self.em.isIdle
+
+        self.master_conn.setHandler(master.MasterOperationHandler(self))
+        self.replicator.populate()
+
+        # Forget all unfinished data.
+        self.dm.dropUnfinishedData()
+
+        self.task_queue = task_queue = deque()
+        try:
+            self.dm.doOperation(self)
+            while True:
+                while task_queue:
+                    try:
+                        while isIdle():
+                            next(task_queue[-1]) or task_queue.rotate()
+                            _poll(0)
+                        break
+                    except StopIteration:
+                        task_queue.pop()
+                poll()
+        finally:
+            del self.task_queue
+
+    def changeClusterState(self, state):
+        self.cluster_state = state
+        if state == ClusterStates.STOPPING_BACKUP:
+            self.replicator.stop()
+
+    def newTask(self, iterator):
+        self.task_queue.appendleft(iterator)
+
+    def closeClient(self, connection):
+        if connection is not self.replicator.getCurrentConnection() and \
+           connection not in self.checker.conn_dict:
+            connection.closeClient()
+
+    def shutdown(self, erase=False):
+        """Close all connections and exit"""
+        for c in self.em.getConnectionList():
+            try:
+                c.close()
+            except PrimaryFailure:
+                pass
+        # clear database to avoid polluting the cluster at restart
+        if erase:
+            self.dm.erase()
+        logging.info("Application has been asked to shut down")
+        sys.exit()
---
+++ packages/neoppod/neoppod-1.11.tar.gz/neoppod-1.11/neo/tests/threaded/testImporter.py
@@ -0,0 +1,289 @@
+#
+# Copyright (C) 2014-2019  Nexedi SA
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from cPickle import Pickler, Unpickler
+from cStringIO import StringIO
+from itertools import izip_longest
+import os, random, shutil, time, unittest
+import transaction, ZODB
+from neo.client.exception import NEOPrimaryMasterLost
+from neo.lib import logging
+from neo.lib.util import u64
+from neo.storage.database import getAdapterKlass, importer, manager
+from neo.storage.database.importer import Repickler, TransactionRecord
+from .. import expectedFailure, getTempDirectory, random_tree, Patch
+from . import NEOCluster, NEOThreadedTest
+from ZODB import serialize
+from ZODB.FileStorage import FileStorage
+
+class Equal:
+
+    _recurse = {}
+
+    def __hash__(self):
+        return 1
+
+    def __eq__(self, other):
+        return type(self) is type(other) and self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return "<%s(%s)>" % (self.__class__.__name__,
+            ", ".join("%s=%r" % k for k in self.__dict__.iteritems()))
+
+class Reduce(Equal, object):
+
+    state = None
+
+    def __init__(self, *args):
+        self.args = args
+        self._l = []
+        self._d = []
+
+    def append(self, item):
+        self._l.append(item)
+
+    def extend(self, item):
+        self._l.extend(item)
+
+    def __setitem__(self, *args):
+        self._d.append(args)
+
+    def __setstate__(self, state):
+        self.state = state
+
+    def __reduce__(self):
+        r = self.__class__, self.args, self.state, iter(self._l), iter(self._d)
+        return r[:5 if self._d else
+                  4 if self._l else
+                  3 if self.state is not None else
+                  2]
+
+class Obj(Equal):
+
+    state = None
+
+    def __getinitargs__(self):
+        return self.args
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __getstate__(self):
+        return self.state
+
+    def __setstate__(self, state):
+        self.state = state
+
+class NewObj(Obj, object):
+
+    def __init__(self):
+        pass # __getinitargs__ only work with old-style classes
+
+class DummyRepickler(Repickler):
+
+    def __init__(self):
+        Repickler.__init__(self, None)
+
+    _changed = True
+
+    def __setattr__(self, name, value):
+        if name != "_changed":
+            self.__dict__[name] = value
+
+
+class ImporterTests(NEOThreadedTest):
+
+    def testRepickler(self):
+        r2 = Obj("foo")
+        r2.__setstate__("bar")
+        r2 = Reduce(r2)
+        r3 = Reduce(1, 2)
+        r3.__setstate__(NewObj())
+        r4 = Reduce()
+        r4.args = r2.args
+        r4.__setstate__("bar")
+        r4.extend("!!!")
+        r5 = Reduce()
+        r5.append("!!!")
+        r5["foo"] = "bar"
+        state = {r2: r3, r4: r5}
+        p = StringIO()
+        Pickler(p, 1).dump(Obj).dump(state)
+        p = p.getvalue()
+        r = DummyRepickler()(p)
+        load = Unpickler(StringIO(r)).load
+        self.assertIs(Obj, load())
+        self.assertDictEqual(state, load())
+
+    def _importFromFileStorage(self, multi=(),
+                               root_filter=None, sub_filter=None):
+        import_hash = '1d4ff03730fe6bcbf235e3739fbe5f5b'
+        txn_size = 10
+        tree = random_tree.generateTree(random.Random(0))
+        i = len(tree) // 3
+        assert i > txn_size
+        before_tree = tree[:i]
+        after_tree = tree[i:]
+        fs_dir = os.path.join(getTempDirectory(), self.id())
+        shutil.rmtree(fs_dir, 1) # for --loop
+        os.mkdir(fs_dir)
+        iter_list = []
+        db_list = []
+        # Setup several FileStorage databases.
+        for i, db in enumerate(('root',) + multi):
+            fs_path = os.path.join(fs_dir, '%s.fs' % db)
+            c = ZODB.DB(FileStorage(fs_path)).open()
+            r = c.root()['tree'] = random_tree.Node()
+            transaction.commit()
+            iter_list.append(random_tree.importTree(r, before_tree, txn_size,
+                sub_filter(db) if i else root_filter))
+            db_list.append((db, r, {
+                "storage": "<filestorage>\npath %s\n</filestorage>" % fs_path
+                }))
+        # Populate FileStorage databases.
+        for i, iter_list in enumerate(izip_longest(*iter_list)):
+            for r in iter_list:
+                if r:
+                    transaction.commit()
+        # Get oids of mount points and close.
+        zodb = []
+        importer = {'zodb': zodb}
+        for db, r, cfg in db_list:
+            if db == 'root':
+                if multi:
+                    for x in multi:
+                        cfg['_%s' % x] = str(u64(r[x]._p_oid))
+                else:
+                    h = random_tree.hashTree(r)
+                    h()
+                    self.assertEqual(import_hash, h.hexdigest())
+                    importer['writeback'] = 'true'
+            else:
+                cfg["oid"] = str(u64(r[db]._p_oid))
+                db = '_%s' % db
+            r._p_jar.db().close()
+            zodb.append((db, cfg))
+        del db_list, iter_list
+        #del zodb[0][1][zodb.pop()[0]]
+        # Start NEO cluster with transparent import.
+        with NEOCluster(importer=importer) as cluster:
+            # Suspend import for a while, so that import
+            # is finished in the middle of the below 'for' loop.
+            # Use a slightly different main loop for storage so that it
+            # does not import data too fast and we test read/write access
+            # by the client during the import.
+            dm = cluster.storage.dm
+            def doOperation(app):
+                del dm.doOperation
+                try:
+                    while True:
+                        if app.task_queue:
+                            app.task_queue[-1].next()
+                        app._poll()
+                except StopIteration:
+                    app.task_queue.pop()
+            dm.doOperation = doOperation
+            cluster.start()
+            t, c = cluster.getTransaction()
+            r = c.root()['tree']
+            # Test retrieving of an object from ZODB when next serial is in NEO.
+            r._p_changed = 1
+            t.commit()
+            t.begin()
+            storage = c.db().storage
+            storage._cache.clear()
+            storage.loadBefore(r._p_oid, r._p_serial)
+            ##
+            self.assertRaisesRegexp(NotImplementedError, " getObjectHistory$",
+                                    c.db().history, r._p_oid)
+            h = random_tree.hashTree(r)
+            h(30)
+            logging.info("start migration")
+            dm.doOperation(cluster.storage)
+            # Adjust if needed. Must remain > 0.
+            self.assertEqual(22, h())
+            self.assertEqual(import_hash, h.hexdigest())
+            # New writes after the switch to NEO.
+            last_import = -1
+            for i, r in enumerate(random_tree.importTree(
+                    r, after_tree, txn_size)):
+                t.commit()
+                if cluster.storage.dm._import:
+                    last_import = i
+            self.tic()
+            # Same as above. We want last_import smaller enough compared to i
+            assert i < last_import * 3 < 2 * i, (last_import, i)
+            self.assertFalse(cluster.storage.dm._import)
+            storage._cache.clear()
+            def finalCheck(r):
+                h = random_tree.hashTree(r)
+                self.assertEqual(93, h())
+                self.assertEqual('6bf0f0cb2d6c1aae9e52c412ef0e25b6',
+                                 h.hexdigest())
+            finalCheck(r)
+            if dm._writeback:
+                dm.commit()
+                dm._writeback.wait()
+        if dm._writeback:
+            db = ZODB.DB(FileStorage(fs_path, read_only=True))
+            finalCheck(db.open().root()['tree'])
+            db.close()
+
+    @unittest.skipUnless(importer.FORK, 'no os.fork')
+    def test1(self):
+        self._importFromFileStorage()
+
+    def testThreadedWriteback(self):
+        # Also check reconnection to the underlying DB for relevant backends.
+        tid_list = []
+        def __init__(orig, tr, db, tid):
+            orig(tr, db, tid)
+            tid_list.append(tid)
+        def fetchObject(orig, db, *args):
+            if len(tid_list) == 5:
+                if isinstance(db, getAdapterKlass('MySQL')):
+                    from neo.tests.storage.testStorageMySQL import ServerGone
+                    with ServerGone(db):
+                        orig(db, *args)
+                    self.fail()
+                else:
+                    tid_list.append(None)
+                    p.revert()
+            return orig(db, *args)
+        def sleep(orig, seconds):
+            self.assertEqual(len(tid_list), 5)
+            p.revert()
+        with Patch(importer, FORK=False), \
+             Patch(TransactionRecord, __init__=__init__), \
+             Patch(manager.DatabaseManager, fetchObject=fetchObject), \
+             Patch(time, sleep=sleep) as p:
+            self._importFromFileStorage()
+            self.assertFalse(p.applied)
+        self.assertEqual(len(tid_list), 11)
+
+    def testMerge(self):
+        multi = 1, 2, 3
+        self._importFromFileStorage(multi,
+            (lambda path: path[0] not in multi or len(path) == 1),
+            (lambda db: lambda path: path[0] in (db, 4)))
+
+    if getattr(serialize, '_protocol', 1) > 1:
+        # XXX: With ZODB5, we should at least keep a working test that does not
+        #      merge several DB.
+        testMerge = expectedFailure(NEOPrimaryMasterLost)(testMerge)
+
+if __name__ == "__main__":
+    unittest.main()
