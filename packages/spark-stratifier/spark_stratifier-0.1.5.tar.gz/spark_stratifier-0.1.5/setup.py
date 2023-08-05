29cc71dde4134148135464f1db5a2214d8bd4e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package spark-stratifier/0.1.5/spark_stratifier-0.1.5.tar.gz

---
+++ packages/spark-stratifier/spark_stratifier-0.1.5.tar.gz/spark_stratifier-0.1.5/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '0.1.5'
+
+setup(
+    name='spark_stratifier',
+    version=version,
+    description='Stratified Cross Validator for Spark',
+    author='Justin Suen',
+    author_email='justin.suen@hackerrank.com',
+    url='https://github.com/interviewstreet/spark-stratifier',
+    license='MIT',
+    packages=['spark_stratifier'],
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').read().split()
+)
