dafb02bd60b76dc111aaf330e29b6961bf58a97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:52 +0000

    Add package aqua-python/0.5/aqua-python-0.5.tar.gz

---
+++ packages/aqua-python/aqua-python-0.5.tar.gz/aqua-python-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='aqua-python',
+        version='0.5',
+        packages=['formatter',],
+        entry_points={'console_scripts': ['aqua = formatter.aqua:main']},
+        author='Alexander J. Zerphy',
+        author_email='ajz5109@psu.edu',
+        license='GPL-3.0',
+        description='A simple formatter for Python code that adheres to the PEP8 standards',
+        long_description=open('README.txt').read(),
+    )
