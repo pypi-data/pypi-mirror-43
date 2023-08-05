88e369c40a59fad5d2854c240797122337a8e1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package e-fonenana-frontend/20190305.1/e-fonenana-frontend-20190305.1.tar.gz

---
+++ packages/e-fonenana-frontend/e-fonenana-frontend-20190305.1.tar.gz/e-fonenana-frontend-20190305.1/hass_frontend/service_worker.js
@@ -0,0 +1,5 @@
+importScripts("/frontend_latest/precache-manifest.7d9ca0a61184ead1e55b734079f59ed8.js", "/frontend_latest/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/frontend_latest/workbox-v3.6.3"});
+/* global importScripts */
+importScripts("/static/service-worker-hass.js");
+
---
+++ packages/e-fonenana-frontend/e-fonenana-frontend-20190305.1.tar.gz/e-fonenana-frontend-20190305.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="e-fonenana-frontend",
+    version="20190305.1",
+    description="The E-Fonenana frontend",
+    url="https://gitlab.com/teamdown/smart-home/frontend",
+    author="The E-Fonenana Authors",
+    author_email="nyandrianinamamy@gmail.com",
+    license="Apache License 2.0",
+    packages=find_packages(
+        include=[
+            "hass_frontend",
+            "hass_frontend_es5",
+            "hass_frontend.*",
+            "hass_frontend_es5.*",
+        ]
+    ),
+    install_requires=["user-agents==1.1.0"],
+    include_package_data=True,
+    zip_safe=False,
+    download_url="https://github.com/teamdown/e-fonenana-frontend/archive/20190305.1.tar.gz",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Other Audience",
+        "Topic :: Other/Nonlisted Topic",
+        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
