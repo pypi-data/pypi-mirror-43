7d376933a451e8892e7e0b0bff623775793b2c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package djenga/0.9.6/djenga-0.9.6.tar.gz

---
+++ packages/djenga/djenga-0.9.6.tar.gz/djenga-0.9.6/djenga/celery/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from djenga.celery.utils import update_progress
+
+
+__all__ = [
+    'update_progress',
+]
---
+++ packages/djenga/djenga-0.9.6.tar.gz/djenga-0.9.6/djenga/views/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from djenga.views.csv_views import CsvView
+from djenga.views.csv_views import ZippedCsvView
+
+__all__ = [
+    'CsvView',
+    'ZippedCsvView',
+]
