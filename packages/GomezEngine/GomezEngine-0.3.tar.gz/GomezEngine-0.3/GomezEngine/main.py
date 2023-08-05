ee6813996aa8c9a4e2507de32ab0f95540c5b2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:26 +0000

    Add package GomezEngine/0.3/GomezEngine-0.3.tar.gz

---
+++ packages/GomezEngine/GomezEngine-0.3.tar.gz/GomezEngine-0.3/GomezEngine/main.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+import json
+import warnings
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import datetime as dt
+
+import urllib.request
+
+from collections import defaultdict
+
+import numpy as np
+
+from tqdm.autonotebook import tqdm
+
+
+from osgeo import gdal
+
+gdal.UseExceptions()
+
+
+def retrieve_field(k, band, url0, roi=None, urlcloud=None, cld_thresh=20):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        if roi is not None:
+            if roi.find("http://") >= 0:
+                prefix = "/vsicurl/"
+            else:
+                prefix = ""
+            kwds = {"cutlineDSName": f"{prefix:s}{roi}", "cropToCutline": True}
+        else:
+            kwds = {}
+        g1 = gdal.Warp("", f"/vsicurl/{url0:s}", format="MEM", **kwds)
+        if urlcloud is not None:
+            height = g1.RasterYSize
+            width = g1.RasterXSize
+            g = gdal.Warp(
+                "",
+                f"/vsicurl/{urlcloud:s}",
+                format="MEM",
+                height=height,
+                width=width,
+                **kwds,
+            )
+            cld = g.ReadAsArray()
+            if np.sum(cld < cld_thresh) == 0:
+                # No clear pixels
+                return k, band, None
+        data1 = g1.ReadAsArray() * 1.0
+        data1[data1 < -9990] = np.nan
+        if urlcloud is not None:
+            data1[cld > cld_thresh] = np.nan
+        if np.isnan(np.nanmean(data1)):
+            return k, band, None
+        return k, band, data1
+
+
+class DataStorage(object):
+    def __init__(self, url, max_workers=10):
+        try:
+            remote_file = urllib.request.urlopen(url)
+        except urllib.request.HTTPError as e:
+            raise ValueError("Can't connect. Reason " + f"{e.msg:s}")
+        except urllib.request.URLError as e:
+            raise ValueError("Can't connect. Reason " + f"{e.reason:s}")
+        try:
+            tmp_db = json.loads(remote_file.read())
+        except json.JSONDecodeError:
+            raise IOError("Remote json file appears dodgy")
+        dates = [
+            (k, dt.datetime.strptime(k, "%Y-%m-%d").date())
+            for k in tmp_db.keys()
+        ]
+        self.data_db = {kk: tmp_db[k] for (k, kk) in dates}
+        self.max_workers = max_workers
+
+
+class DataStorageSentinel2(DataStorage):
+    def __init__(self, url):
+        DataStorage.__init__(self, url, max_workers=10)
+        self.valid_bands = [
+            "B02",
+            "B03",
+            "B04",
+            "B05",
+            "B06",
+            "B07",
+            "B08",
+            "B8A",
+            "B11",
+            "B12",
+            "CLD",
+            "AOT",
+            "TCWV",
+        ]
+
+
+    def extract_band(self, bands, dates=None, roi=None,
+                     use_cloud_mask=True, cld_thresh=20):
+        if not isinstance(bands, list):
+            bands = [bands]
+        assert all([band in self.valid_bands for band in bands])
+
+        sel_dates = list(self.data_db.keys()) if dates is None else dates
+        if not isinstance(sel_dates, list):
+            sel_dates = [sel_dates]
+        not_present = list(
+            (set(sel_dates).difference(set(self.data_db.keys())))
+        )
+
+        if not_present:
+            raise ValueError(f"{str(not_present):s} not present in DB!")
+        with ThreadPoolExecutor(max_workers=self.max_workers) as ex:
+            futures = []
+            for band in bands:
+                band_loc = self.valid_bands.index(band)
+                for k in sel_dates:
+                    if use_cloud_mask:
+                        cld_file = [
+                            s
+                            for s in self.data_db[k]
+                            if s.endswith("_CLD.vrt")
+                        ][0]
+                    else:
+                        cld_file = None
+                    futures.append(
+                        ex.submit(
+                            retrieve_field,
+                            k,
+                            band,
+                            self.data_db[k][band_loc],
+                            roi,
+                            urlcloud=cld_file,
+                            cld_thresh=cld_thresh,
+                        )
+                    )
+            kwargs = {
+                "total": len(futures),
+                "unit": "it",
+                "unit_scale": True,
+                "leave": True,
+            }
+            analysis_data = defaultdict(dict)
+            # Print out the progress as tasks complete
+            for result in tqdm(as_completed(futures), **kwargs):
+                the_date, the_band, the_data = result.result()
+                analysis_data[the_band][the_date] = the_data
+        clean_data = {}
+        for band in bands:
+            clean_data[band] = {
+                k: v for k, v in analysis_data[band].items() if v is not None
+            }
+        return clean_data
---
+++ packages/GomezEngine/GomezEngine-0.3.tar.gz/GomezEngine-0.3/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""Setup script for building GomezErzEngine"""
+import os
+import codecs
+import re
+from os import path
+from setuptools import setup
+
+# Global variables for this extension:
+name = "GomezEngine"  # name of the generated python extension (.so)
+description = "The GomezErzEngine"
+long_description = "A bunch of convenience functions to play around with " + \
+                   "stacks of S2 data"
+
+this_directory = path.abspath(path.dirname(__file__))
+
+def read(filename):
+    with open(os.path.join(this_directory, filename), "rb") as f:
+        return f.read().decode("utf-8")
+
+
+if os.path.exists("README.md"):
+    long_description = read("README.md")
+
+def read(*parts):
+    with codecs.open(os.path.join(this_directory, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+author = "J Gomez-Dans/NCEO & University College London"
+author_email = "j.gomez-dans@ucl.ac.uk"
+url = "http://github.com/jgomezdans/GomezEngine"
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    "Topic :: Scientific/Engineering :: Atmospheric Science",
+    "Topic :: Scientific/Engineering :: Physics",
+    "Topic :: Scientific/Engineering :: GIS",
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Developers',
+    'Environment :: Console'
+]
+
+
+
+
+setup(
+    name=name,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=author,
+    url=url,
+    author_email=author_email,
+    classifiers=classifiers,
+    install_requires=[
+        "numpy",
+        "gdal>=2.3",
+        "scipy",
+        "pytest",
+        "tqdm"
+    ],
+    version=find_version("GomezEngine", "__init__.py"),
+    packages=["GomezEngine"],
+    zip_safe=False # Apparently needed for conda
+)
