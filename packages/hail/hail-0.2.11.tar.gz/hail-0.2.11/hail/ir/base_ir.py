10a291d57606a2472642e68a89fc2b3aba953612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:47 +0000

    Add package hail/0.2.11/hail-0.2.11.tar.gz

---
+++ packages/hail/hail-0.2.11.tar.gz/hail-0.2.11/hail/ir/base_ir.py
@@ -0,0 +1,135 @@
+import abc
+from .renderer import Renderer
+from hail.utils.java import Env
+
+
+class BaseIR(object):
+    def __init__(self):
+        super().__init__()
+        self._type = None
+
+    def __str__(self):
+        r = Renderer(stop_at_jir = False)
+        return r(self)
+
+    @abc.abstractmethod
+    def parse(self, code, ref_map, ir_map):
+        return
+
+    @abc.abstractproperty
+    def typ(self):
+        return
+
+
+class IR(BaseIR):
+    def __init__(self, *children):
+        super().__init__()
+        self._aggregations = None
+        self.children = children
+
+    @property
+    def aggregations(self):
+        if self._aggregations is None:
+            self._aggregations = [agg for child in self.children for agg in child.aggregations]
+        return self._aggregations
+
+    @property
+    def is_nested_field(self):
+        return False
+
+    def search(self, criteria):
+        others = [node for child in self.children if isinstance(child, IR) for node in child.search(criteria)]
+        if criteria(self):
+            return others + [self]
+        return others
+
+    def copy(self, *args):
+        raise NotImplementedError("IR has no copy method defined.")
+
+    def map_ir(self, f):
+        new_children = []
+        for child in self.children:
+            if isinstance(child, IR):
+                new_children.append(f(child))
+            else:
+                new_children.append(child)
+
+        return self.copy(*new_children)
+
+    @property
+    def bound_variables(self):
+        return {v for child in self.children for v in child.bound_variables}
+
+    @property
+    def typ(self):
+        if self._type is None:
+            self._compute_type({}, None)
+            assert self._type is not None, self
+        return self._type
+
+    @abc.abstractmethod
+    def _compute_type(self, env, agg_env):
+        raise NotImplementedError(self)
+
+    def parse(self, code, ref_map={}, ir_map={}):
+        return Env.hail().expr.ir.IRParser.parse_value_ir(
+            code,
+            {k: t._parsable_string() for k, t in ref_map.items()},
+            ir_map)
+
+
+class TableIR(BaseIR):
+    def __init__(self):
+        super().__init__()
+
+    @abc.abstractmethod
+    def _compute_type(self):
+        raise NotImplementedError(self)
+
+    @property
+    def typ(self):
+        if self._type is None:
+            self._compute_type()
+            assert self._type is not None, self
+        return self._type
+
+    def parse(self, code, ref_map={}, ir_map={}):
+        return Env.hail().expr.ir.IRParser.parse_table_ir(code, ref_map, ir_map)
+
+
+class MatrixIR(BaseIR):
+    def __init__(self):
+        super().__init__()
+
+    @abc.abstractmethod
+    def _compute_type(self):
+        raise NotImplementedError(self)
+
+    @property
+    def typ(self):
+        if self._type is None:
+            self._compute_type()
+            assert self._type is not None, self
+        return self._type
+
+    def parse(self, code, ref_map={}, ir_map={}):
+        return Env.hail().expr.ir.IRParser.parse_matrix_ir(code, ref_map, ir_map)
+
+
+class BlockMatrixIR(BaseIR):
+    def __init__(self):
+        super().__init__()
+
+    @abc.abstractmethod
+    def _compute_type(self):
+        raise NotImplementedError(self)
+
+    @property
+    def typ(self):
+        if self._type is None:
+            self._compute_type()
+            assert self._type is not None, self
+        return self._type
+
+    def parse(self, code, ref_map={}, ir_map={}):
+        return Env.hail().expr.ir.IRParser.parse_blockmatrix_ir(code, ref_map, ir_map)
---
+++ packages/hail/hail-0.2.11.tar.gz/hail-0.2.11/hail/ir/matrix_ir.py
@@ -0,0 +1,485 @@
+import json
+import hail as hl
+from hail.ir.base_ir import *
+from hail.utils.java import escape_str, escape_id, parsable_strings, dump_json
+
+class MatrixAggregateRowsByKey(MatrixIR):
+    def __init__(self, child, entry_expr, row_expr):
+        super().__init__()
+        self.child = child
+        self.entry_expr = entry_expr
+        self.row_expr = row_expr
+
+    def render(self, r):
+        return f'(MatrixAggregateRowsByKey {r(self.child)} {r(self.entry_expr)} {r(self.row_expr)})'
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.entry_expr._compute_type(child_typ.col_env(), child_typ.entry_env())
+        self.row_expr._compute_type(child_typ.global_env(), child_typ.row_env())
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            child_typ.row_key_type._concat(self.row_expr.typ),
+            child_typ.row_key,
+            self.entry_expr.typ)
+
+
+class MatrixRead(MatrixIR):
+    def __init__(self, reader, drop_cols=False, drop_rows=False):
+        super().__init__()
+        self.reader = reader
+        self.drop_cols = drop_cols
+        self.drop_rows = drop_rows
+
+    def render(self, r):
+        return f'(MatrixRead None {self.drop_cols} {self.drop_rows} "{r(self.reader)}")'
+
+    def _compute_type(self):
+        self._type = Env.backend().matrix_type(self)
+
+
+class MatrixFilterRows(MatrixIR):
+    def __init__(self, child, pred):
+        super().__init__()
+        self.child = child
+        self.pred = pred
+
+    def render(self, r):
+        return '(MatrixFilterRows {} {})'.format(r(self.child), r(self.pred))
+
+    def _compute_type(self):
+        self.pred._compute_type(self.child.typ.row_env(), None)
+        self._type = self.child.typ
+
+class MatrixChooseCols(MatrixIR):
+    def __init__(self, child, old_indices):
+        super().__init__()
+        self.child = child
+        self.old_indices = old_indices
+
+    def render(self, r):
+        return '(MatrixChooseCols ({}) {})'.format(
+            ' '.join([str(i) for i in self.old_indices]), r(self.child))
+
+    def _compute_type(self):
+        self._type = self.child.typ
+
+class MatrixMapCols(MatrixIR):
+    def __init__(self, child, new_col, new_key):
+        super().__init__()
+        self.child = child
+        self.new_col = new_col
+        self.new_key = new_key
+
+    def render(self, r):
+        return '(MatrixMapCols {} {} {})'.format(
+            '(' + ' '.join(f'"{escape_str(f)}"' for f in self.new_key) + ')' if self.new_key is not None else 'None',
+            r(self.child), r(self.new_col))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.new_col._compute_type(child_typ.col_env(), child_typ.entry_env())
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            self.new_col.typ,
+            self.new_key if self.new_key is not None else child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            child_typ.entry_type)
+
+class MatrixUnionCols(MatrixIR):
+    def __init__(self, left, right):
+        super().__init__()
+        self.left = left
+        self.right = right
+
+    def render(self, r):
+        return f'(MatrixUnionCols {r(self.left)} {r(self.right)})'
+
+    def _compute_type(self):
+        self.right.typ # force
+        self._type = self.left.typ
+
+class MatrixMapEntries(MatrixIR):
+    def __init__(self, child, new_entry):
+        super().__init__()
+        self.child = child
+        self.new_entry = new_entry
+
+    def render(self, r):
+        return '(MatrixMapEntries {} {})'.format(r(self.child), r(self.new_entry))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.new_entry._compute_type(child_typ.entry_env(), None)
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            self.new_entry.typ)
+
+
+class MatrixFilterEntries(MatrixIR):
+    def __init__(self, child, pred):
+        super().__init__()
+        self.child = child
+        self.pred = pred
+
+    def render(self, r):
+        return '(MatrixFilterEntries {} {})'.format(r(self.child), r(self.pred))
+
+    def _compute_type(self):
+        self.pred._compute_type(self.child.typ.entry_env(), None)
+        self._type = self.child.typ
+
+class MatrixKeyRowsBy(MatrixIR):
+    def __init__(self, child, keys, is_sorted=False):
+        super().__init__()
+        self.child = child
+        self.keys = keys
+        self.is_sorted = is_sorted
+
+    def render(self, r):
+        return '(MatrixKeyRowsBy ({}) {} {})'.format(
+            ' '.join([escape_id(x) for x in self.keys]),
+            self.is_sorted,
+            r(self.child))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            child_typ.row_type,
+            self.keys,
+            child_typ.entry_type)
+
+
+class MatrixMapRows(MatrixIR):
+    def __init__(self, child, new_row):
+        super().__init__()
+        self.child = child
+        self.new_row = new_row
+
+    def render(self, r):
+        return '(MatrixMapRows {} {})'.format(r(self.child), r(self.new_row))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.new_row._compute_type(child_typ.row_env(), child_typ.entry_env())
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            self.new_row.typ,
+            child_typ.row_key,
+            child_typ.entry_type)
+
+class MatrixMapGlobals(MatrixIR):
+    def __init__(self, child, new_global):
+        super().__init__()
+        self.child = child
+        self.new_global = new_global
+
+    def render(self, r):
+        return f'(MatrixMapGlobals {r(self.child)} {r(self.new_global)})'
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.new_global._compute_type(child_typ.global_env(), None)
+        self._type = hl.tmatrix(
+            self.new_global.typ,
+            child_typ.col_type,
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            child_typ.entry_type)
+
+
+class MatrixFilterCols(MatrixIR):
+    def __init__(self, child, pred):
+        super().__init__()
+        self.child = child
+        self.pred = pred
+
+    def render(self, r):
+        return f'(MatrixFilterCols {r(self.child)} {r(self.pred)})'
+
+    def _compute_type(self):
+        self.pred._compute_type(self.child.typ.col_env(), None)
+        self._type = self.child.typ
+
+class MatrixCollectColsByKey(MatrixIR):
+    def __init__(self, child):
+        super().__init__()
+        self.child = child
+
+    def render(self, r):
+        return f'(MatrixCollectColsByKey {r(self.child)})'
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_key_type._concat(
+                hl.tstruct(**{f: hl.tarray(t) for f, t in child_typ.col_value_type.items()})),
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            hl.tstruct(**{f: hl.tarray(t) for f, t in child_typ.entry_type.items()}))
+
+class MatrixAggregateColsByKey(MatrixIR):
+    def __init__(self, child, entry_expr, col_expr):
+        super().__init__()
+        self.child = child
+        self.entry_expr = entry_expr
+        self.col_expr = col_expr
+
+    def render(self, r):
+        return '(MatrixAggregateColsByKey {} {} {})'.format(r(self.child), r(self.entry_expr), r(self.col_expr))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self.entry_expr._compute_type(child_typ.row_env(), child_typ.entry_env())
+        self.col_expr._compute_type(child_typ.global_env(), child_typ.col_env())
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_key_type._concat(self.col_expr.typ),
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            self.entry_expr.typ)
+            
+
+class MatrixExplodeRows(MatrixIR):
+    def __init__(self, child, path):
+        super().__init__()
+        self.child = child
+        self.path = path
+
+    def render(self, r):
+        return '(MatrixExplodeRows ({}) {})'.format(
+            ' '.join([escape_id(id) for id in self.path]),
+            r(self.child))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        a = child_typ.row_type._index_path(self.path)
+        new_row_type = child_typ.row_type._insert(self.path, a.element_type)
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            new_row_type,
+            child_typ.row_key,
+            child_typ.entry_type)
+            
+
+class MatrixRepartition(MatrixIR):
+    def __init__(self, child, n, strategy):
+        super().__init__()
+        self.child = child
+        self.n = n
+        self.strategy = strategy
+
+    def render(self, r):
+        return f'(MatrixRepartition {r(self.child)} {self.n} {self.strategy})'
+
+    def _compute_type(self):
+        self._type = self.child.typ
+
+
+class MatrixUnionRows(MatrixIR):
+    def __init__(self, *children):
+        super().__init__()
+        self.children = children
+
+    def render(self, r):
+        return '(MatrixUnionRows {})'.format(' '.join(map(r, self.children)))
+
+    def _compute_type(self):
+        for c in self.children:
+            c.typ # force
+        self._type = self.children[0].typ
+
+class MatrixDistinctByRow(MatrixIR):
+    def __init__(self, child):
+        super().__init__()
+        self.child = child
+
+    def render(self, r):
+        return f'(MatrixDistinctByRow {r(self.child)})'
+
+    def _compute_type(self):
+        self._type = self.child.typ
+
+
+class MatrixRowsHead(MatrixIR):
+    def __init__(self, child, n):
+        super().__init__()
+        self.child = child
+        self.n = n
+
+    def render(self, r):
+        return f'(MatrixRowsHead {self.n} {r(self.child)})'
+
+    def _compute_type(self):
+        self._type = self.child.typ
+
+
+class MatrixExplodeCols(MatrixIR):
+    def __init__(self, child, path):
+        super().__init__()
+        self.child = child
+        self.path = path
+
+    def render(self, r):
+        return '(MatrixExplodeCols ({}) {})'.format(
+            ' '.join([escape_id(id) for id in self.path]),
+            r(self.child))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        a = child_typ.col_type._index_path(self.path)
+        new_col_type = child_typ.col_type._insert(self.path, a.element_type)
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            new_col_type,
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            child_typ.entry_type)
+
+
+class CastTableToMatrix(MatrixIR):
+    def __init__(self, child, entries_field_name, cols_field_name, col_key):
+        super().__init__()
+        self.child = child
+        self.entries_field_name = entries_field_name
+        self.cols_field_name = cols_field_name
+        self.col_key = col_key
+
+    def render(self, r):
+        return '(CastTableToMatrix {} {} ({}) {})'.format(
+           escape_str(self.entries_field_name),
+           escape_str(self.cols_field_name),
+           ' '.join([escape_id(id) for id in self.col_key]),
+           r(self.child))
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self._type = hl.tmatrix(
+            child_typ.global_type._drop_fields([self.cols_field_name]),
+            child_typ.global_type[self.cols_field_name].element_type,
+            self.col_key,
+            child_typ.row_type._drop_fields([self.entries_field_name]),
+            child_typ.row_key,
+            child_typ.row_type[self.entries_field_name].element_type)
+
+
+class MatrixAnnotateRowsTable(MatrixIR):
+    def __init__(self, child, table, root):
+        super().__init__()
+        self.child = child
+        self.table = table
+        self.root = root
+
+    def render(self, r):
+        return f'(MatrixAnnotateRowsTable "{self.root}" {r(self.child)} {r(self.table)})'
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type,
+            child_typ.col_key,
+            child_typ.row_type._insert_field(self.root, self.table.typ.value_type),
+            child_typ.row_key,
+            child_typ.entry_type)
+
+class MatrixAnnotateColsTable(MatrixIR):
+    def __init__(self, child, table, root):
+        super().__init__()
+        self.child = child
+        self.table = table
+        self.root = root
+
+    def render(self, r):
+        return f'(MatrixAnnotateColsTable "{self.root}" {r(self.child)} {r(self.table)})'
+
+    def _compute_type(self):
+        child_typ = self.child.typ
+        self._type = hl.tmatrix(
+            child_typ.global_type,
+            child_typ.col_type._insert_field(self.root, self.table.typ.value_type),
+            child_typ.col_key,
+            child_typ.row_type,
+            child_typ.row_key,
+            child_typ.entry_type)
+
+
+class MatrixToMatrixApply(MatrixIR):
+    def __init__(self, child, config):
+        super().__init__()
+        self.child = child
+        self.config = config
+
+    def render(self, r):
+        return f'(MatrixToMatrixApply {dump_json(self.config)} {r(self.child)})'
+
+    def _compute_type(self):
+        name = self.config['name']
+        child_typ = self.child.typ
+        if (name == 'MatrixFilterPartitions'
+            or name == 'MatrixFilterIntervals'):
+            self._type = child_typ
+        else:
+            assert name == 'WindowByLocus', name
+            self._type = hl.tmatrix(
+                child_typ.global_type,
+                child_typ.col_type,
+                child_typ.col_key,
+                child_typ.row_type._insert_field('prev_rows', hl.tarray(child_typ.row_type)),
+                child_typ.row_key,
+                child_typ.entry_type._insert_field('prev_entries', hl.tarray(child_typ.entry_type)))
+
+class MatrixRename(MatrixIR):
+    def __init__(self, child, global_map, col_map, row_map, entry_map):
+        super().__init__()
+        self.child = child
+        self.global_map = global_map
+        self.col_map = col_map
+        self.row_map = row_map
+        self.entry_map = entry_map
+
+    def render(self, r):
+        return f'(MatrixRename ' \
+               f'{parsable_strings(self.global_map.keys())} ' \
+               f'{parsable_strings(self.global_map.values())} ' \
+               f'{parsable_strings(self.col_map.keys())} ' \
+               f'{parsable_strings(self.col_map.values())} ' \
+               f'{parsable_strings(self.row_map.keys())} ' \
+               f'{parsable_strings(self.row_map.values())} ' \
+               f'{parsable_strings(self.entry_map.keys())} ' \
+               f'{parsable_strings(self.entry_map.values())} ' \
+               f'{r(self.child)})'
+
+    def _compute_type(self):
+        self._type = self.child.typ._rename(self.global_map, self.col_map, self.row_map, self.entry_map)
+
+
+class JavaMatrix(MatrixIR):
+    def __init__(self, jir):
+        super().__init__()
+        self._jir = jir
+
+    def render(self, r):
+        return f'(JavaMatrix {r.add_jir(self._jir)})'
+
+    def _compute_type(self):
+        self._type = hl.tmatrix._from_java(self._jir.typ())
