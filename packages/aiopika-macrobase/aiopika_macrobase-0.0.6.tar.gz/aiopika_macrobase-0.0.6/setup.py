6f93c2c938636876ab74140e1195861877b9fe8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package aiopika-macrobase/0.0.6/aiopika_macrobase-0.0.6.tar.gz

---
+++ packages/aiopika-macrobase/aiopika_macrobase-0.0.6.tar.gz/aiopika_macrobase-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiopika_macrobase',
+    version='0.0.6',
+    packages=find_packages(),
+    url='https://github.com/mbcores/aiopika-macrobase',
+    license='MIT',
+    author='Alexey Shagaleev',
+    author_email='alexey.shagaleev@yandex.ru',
+    description='Aio-pika driver for macrobase framework',
+    install_requires=[
+        'macrobase-driver>=0.0.8',
+        'aio-pika==5.2.2',
+        'uvloop==0.12.1',
+        'python-rapidjson==0.7.0',
+        'structlog==19.1.0'
+    ]
+)
