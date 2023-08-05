29ea3752ba122bada8a44b9d18a9d2cef14d479b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:04 +0000

    Add package masonite-api-pagination/0.2.0/masonite-api-pagination-0.2.0.tar.gz

---
+++ packages/masonite-api-pagination/masonite-api-pagination-0.2.0.tar.gz/masonite-api-pagination-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="masonite-api-pagination",
+    author="Raphael Schubert",
+    author_email="rfswdp@gmail.com",
+    description="Simple yet powerful masonite-api pagination",
+    url="https://github.com/rfschubert/masonite_api_pagination",
+    version='0.2.0',
+    packages=[
+        'masonite_api_pagination'
+    ],
+    install_requires=[
+        'masonite',
+        'masonite-api'
+    ],
+    include_package_data=True,
+)
