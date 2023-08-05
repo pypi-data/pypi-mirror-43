cb7a95b66fcd5454262f89db990ebab963d12478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:22 +0000

    Add package pyalice/0.2/pyalice-0.2.tar.gz

---
+++ packages/pyalice/pyalice-0.2.tar.gz/pyalice-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='pyalice',
+      version='0.2',
+      description='Yandex.Dialogs non-official package.',
+      long_description='Processing requests and sending responses for Yandex.Dialogs API.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='yandex dialogs alice skills',
+      url='https://github.com/Jornau/alice',
+      author='Alexander Surkov',
+      author_email='jornau@yandex.ru',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'requests'
+      ],
+      zip_safe=False)
