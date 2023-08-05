1bfbbf4242a9eed62556adae2cdd284135cf29a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:19 +0000

    Add package dataclass_structor/0.0.5/dataclass_structor-0.0.5-py3-none-any.whl

---
+++ packages/dataclass_structor/dataclass_structor-0.0.5-py3-none-any.whl/dataclass_structor/__init__.py
@@ -0,0 +1,7 @@
+"""Typed object structure/destructure."""
+from .structure import structure
+from .unstructure import unstructure
+
+__version__ = "0.0.5"
+
+__all__ = [str(structure), str(unstructure)]
