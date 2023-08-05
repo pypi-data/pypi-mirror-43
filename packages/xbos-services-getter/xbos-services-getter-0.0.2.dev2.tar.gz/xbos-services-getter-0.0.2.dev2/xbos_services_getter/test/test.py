212dcd5293162faed66efdd35e527f7dcfc616b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:34 +0000

    Add package xbos-services-getter/0.0.2.dev2/xbos-services-getter-0.0.2.dev2.tar.gz

---
+++ packages/xbos-services-getter/xbos-services-getter-0.0.2.dev2.tar.gz/xbos-services-getter-0.0.2.dev2/xbos_services_getter/test/test.py
@@ -0,0 +1,26 @@
+
+import datetime
+import pytz
+
+import xbos_services_utils3 as utils
+
+import os
+
+import xbos_services_getter
+
+all_buildings = utils.get_buildings()
+
+# Comfortband Test
+
+for iter_bldg in all_buildings:
+	for iter_zone in utils.get_zones(iter_bldg):
+
+		start = datetime.datetime(year=2018, month=1, day=1, hour=0, minute=0).replace(tzinfo=pytz.utc)
+		end = start + datetime.timedelta(days=7)
+		window = "15m"
+
+		temperature_band_stub = xbos_services_getter.get_temperature_band_stub()
+
+		comfortband = xbos_services_getter.get_comfortband(temperature_band_stub, iter_bldg, iter_zone, start, end, window)
+
+
