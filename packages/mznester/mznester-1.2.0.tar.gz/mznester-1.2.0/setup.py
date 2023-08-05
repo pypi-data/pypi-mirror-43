0f11d27b84641377aaa8db0c4a59861fe35f9e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package mznester/1.2.0/mznester-1.2.0.tar.gz

---
+++ packages/mznester/mznester-1.2.0.tar.gz/mznester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'mznester',
+    version         = '1.2.0',
+    py_modules      = ['mznester'],
+    author          = 'mzpython',
+    author_email    = 'medzed2010@gmail.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested list',
+)
