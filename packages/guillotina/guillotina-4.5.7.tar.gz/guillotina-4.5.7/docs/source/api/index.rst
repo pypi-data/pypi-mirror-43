798f8e6f705b6b2ad625674fe4676411c53e93ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:43 +0000

    Add package guillotina/4.5.7/guillotina-4.5.7.tar.gz

---
+++ packages/guillotina/guillotina-4.5.7.tar.gz/guillotina-4.5.7/docs/source/api/index.rst
@@ -0,0 +1,21 @@
+Programming API Reference
+=========================
+
+This is not a complete reference but a reference of modules
+which seem most useful.
+
+.. toctree::
+   :maxdepth: 2
+
+   configure
+   content
+   contentapi
+   request
+   response
+   fields
+   component
+   utils
+   factory
+
+
+.. consider adding schema, security, transactions, auth, blob, directives
---
+++ packages/guillotina/guillotina-4.5.7.tar.gz/guillotina-4.5.7/guillotina/tests/test_guillo.py
@@ -0,0 +1,56 @@
+# flake8: noqa
+
+import json
+
+
+async def test_get_the_root(guillotina):
+    response, status = await guillotina('GET', '/')
+    assert response['static_directory'] == ['static', 'module_static', 'jsapp_static']
+    assert response['databases'] == ['db', 'db-custom']
+    assert response['static_file'] == ['favicon.ico']
+
+
+async def test_get_db(guillotina):
+    response, status = await guillotina('GET', '/db')
+    assert response['containers'] == []
+
+
+async def test_get_container(container_requester):
+    async with container_requester as requester:
+        response, status = await requester('GET', '/db/guillotina')
+        assert response['@type'] == 'Container'
+
+
+async def test_get_content(container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            'POST',
+            '/db/guillotina',
+            data=json.dumps({
+                '@type': 'Item',
+                'id': 'hello',
+                'title': 'Hola'
+            }))
+        assert status == 201
+        response, status = await requester(
+            'GET',
+            '/db/guillotina/hello')
+        assert status == 200
+
+
+async def test_content_paths_are_correct(container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            'POST',
+            '/db/guillotina',
+            data=json.dumps({
+                '@type': 'Item',
+                'id': 'hello',
+                'title': 'Hola'
+            }))
+        assert status == 201
+        response, status = await requester(
+            'GET',
+            '/db/guillotina/hello')
+        assert status == 200
+        assert '/db/guillotina/hello' in response['@id']
