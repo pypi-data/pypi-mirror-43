14775402a038ec36ba559cec61577e96835b8fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package twaml/0.2.1/twaml-0.2.1.tar.gz

---
+++ packages/twaml/twaml-0.2.1.tar.gz/twaml-0.2.1/twaml/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""tW analysis machine learning: twaml
+
+This python package contains modules for handling different machine
+learning requirements for the ATLAS Full Run II tW analysis.
+
+"""
+
+from .data import dataset
