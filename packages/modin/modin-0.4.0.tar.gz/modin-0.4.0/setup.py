c34c93529edbc66f1c66d887b765bc68ecbc443b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:51 +0000

    Add package modin/0.4.0/modin-0.4.0.tar.gz

---
+++ packages/modin/modin-0.4.0.tar.gz/modin-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="modin",
+    version="0.4.0",
+    description="Modin: Make your pandas code run faster by changing one line of code.",
+    packages=find_packages(),
+    url="https://github.com/modin-project/modin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pandas==0.24.1", "ray==0.6.2", "numpy<=1.15.0", "typing"],
+    extras_require={
+        # can be installed by pip install modin[dask]
+        "dask": ["dask==1.0.0", "distributed==1.25.0"],
+        # can be install by pip install modin[out_of_core]
+        "out_of_core": ["psutil==5.4.8"],
+    },
+)
