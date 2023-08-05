6e36f5f3d79081b6b887667e4ffe7107fba46054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:28 +0000

    Add package frapi/0.0.5/frapi-0.0.5.tar.gz

---
+++ packages/frapi/frapi-0.0.5.tar.gz/frapi-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from frapi import __version__, __author__, __email__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    'numpy',
+    'grpcio',
+    'tensorflow',
+    'tensorflow-serving-api'
+]
+
+setuptools.setup(
+    name="frapi",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description="Package for Face Recognition API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BIG-CHENG/FaceRec",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
