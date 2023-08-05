1f63ecde2ff7b33fb1d081b38f2b792bfb5e6a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:13 +0000

    Add package vishwakarma/0.1.1/vishwakarma-0.1.1.tar.gz

---
+++ packages/vishwakarma/vishwakarma-0.1.1.tar.gz/vishwakarma-0.1.1/vishwakarma/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+    Python visualization library for Probabilistic Graphical Models, Discrete & Continuous Distributions, and a lot more!
+
+    :copyright: (c) 2018 by Diagram AI.
+    :license: see LICENSE for more details.
+'''
+
+__title__ = 'vishwakarma'
+__version__ = '0.1.1'
+__author__ = 'diagram-ai'
+
+from .pgmplot import pgmplot
+from .pdfplot import pdfplot
+from .pmfplot import pmfplot
