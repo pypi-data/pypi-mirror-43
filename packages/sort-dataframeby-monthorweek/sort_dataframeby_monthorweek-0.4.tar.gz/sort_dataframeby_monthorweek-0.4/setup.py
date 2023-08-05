cf2e595fd4daee459ed419d5631d05b2a70f4708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:17 +0000

    Add package sort-dataframeby-monthorweek/0.4/sort_dataframeby_monthorweek-0.4.tar.gz

---
+++ packages/sort-dataframeby-monthorweek/sort_dataframeby_monthorweek-0.4.tar.gz/sort_dataframeby_monthorweek-0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sort_dataframeby_monthorweek",
+    version="0.4",
+    author="Yeddu Dinesh Babu",
+    author_email="ydineshy225@gmail.com",
+    description="Sort a dataframe by month name or by weekday name in chronological order",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ydineshy225/sort-dataframeby-monthorweek",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
