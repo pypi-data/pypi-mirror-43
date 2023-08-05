be1bd83c3bbdcb29fb3fcf74688dbedbb4f504b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package pushdata-io/0.1.0/pushdata-io-0.1.0.tar.gz

---
+++ packages/pushdata-io/pushdata-io-0.1.0.tar.gz/pushdata-io-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pushdata-io",
+    version="0.1.0",
+    author="Ragnar Lonn",
+    author_email="hello@pushdata.io",
+    description="Python client library for pushdata.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pushdata-io/Python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
