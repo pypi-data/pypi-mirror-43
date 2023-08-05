a33494bc224aa0e78c40a5cd61c5ca897439af1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:33 +0000

    Add package kfwx/1.0.0/kfwx-1.0.0.tar.gz

---
+++ packages/kfwx/kfwx-1.0.0.tar.gz/kfwx-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kfwx",
+    version="1.0.0",
+    author="Tiffany Nguyen",
+    description="A data analysis toolkit for the KFW lab (MacOS X)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiffkwin/KFW",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
