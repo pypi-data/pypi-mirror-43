a3f3ce43abf1b93c67873c0335fc01e8c4f1a624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package keepTrace/1.0.2/keepTrace-1.0.2.tar.gz

---
+++ packages/keepTrace/keepTrace-1.0.2.tar.gz/keepTrace-1.0.2/setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os.path
+
+setup(
+    version = "1.0.2",
+    name = "keepTrace",
+    author = "Jason Dixon",
+    py_modules = ["keepTrace"],
+    author_email = "jason.dixon.email@gmail.com",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/internetimagery/keepTrace",
+    description = "Pickle traceback support. Featuring debuggable restored tracebacks.",
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.md")).read())
