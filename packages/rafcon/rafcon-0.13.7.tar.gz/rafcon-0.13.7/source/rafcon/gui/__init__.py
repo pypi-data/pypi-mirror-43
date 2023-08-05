20483dea8f46662b209d8434af0f0d2e72a7ddea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package rafcon/0.13.7/rafcon-0.13.7.tar.gz

---
+++ packages/rafcon/rafcon-0.13.7.tar.gz/rafcon-0.13.7/source/rafcon/gui/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2016-2017 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License v1.0 which
+# accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Rico Belder <rico.belder@dlr.de>
+# Sebastian Brunner <sebastian.brunner@dlr.de>
+
+import rafcon.gui.interface
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('PangoCairo', '1.0')
+gi.require_version('GtkSource', '3.0')
