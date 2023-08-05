4045ae36747c37bc13078f4a413221aeb1662262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:24 +0000

    Add package itk-core/4.13.2/itk_core-4.13.2-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/itk-core/itk_core-4.13.2-cp37-cp37m-macosx_10_9_x86_64.whl/itk/Configuration/ITKIOGIPLConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('GiplImageIO', 'itk::GiplImageIO', 'itkGiplImageIO', True),
+  ('GiplImageIOFactory', 'itk::GiplImageIOFactory', 'itkGiplImageIOFactory', True),
+)
