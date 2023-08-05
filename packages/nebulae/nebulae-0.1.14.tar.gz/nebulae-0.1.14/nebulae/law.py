64ef2b009e2a4cfbb7abb2f28419f5bd12658398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:29 +0000

    Add package nebulae/0.1.14/nebulae-0.1.14.tar.gz

---
+++ packages/nebulae/nebulae-0.1.14.tar.gz/nebulae-0.1.14/nebulae/law.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''
+law
+Created by Seria at 02/02/2019 12:50 AM
+Email: zzqsummerai@yeah.net
+
+                    _ooOoo_
+                  o888888888o
+                 o88`_ . _`88o
+                 (|  0   0  |)
+                 O \   。   / O
+              _____/`-----‘\_____
+            .’   \||  _ _  ||/   `.
+            |  _ |||   |   ||| _  |
+            |  |  \\       //  |  |
+            |  |    \-----/    |  |
+             \ .\ ___/- -\___ /. /
+         ,--- /   ___\<|>/___   \ ---,
+         | |:    \    \ /    /    :| |
+         `\--\_    -. ___ .-    _/--/‘
+   ===========  \__  NOBUG  __/  ===========
+   
+'''
+# -*- coding:utf-8 -*-
+class Law(object):
+    CORE = 'tensorflow'
+    FIELD_SEP = '|'
+    CHAR_SEP = '\t'
+    FRAME_KEY = '_MAX_FRAMES'
