73dc827d3ae84e35f4d2077b6aba8dd39f3d98a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:13 +0000

    Add package PostgreSQL-Audit/0.11.0/PostgreSQL-Audit-0.11.0.tar.gz

---
+++ packages/PostgreSQL-Audit/PostgreSQL-Audit-0.11.0.tar.gz/PostgreSQL-Audit-0.11.0/postgresql_audit/templates/operators.sql
@@ -0,0 +1,19 @@
+-- http://coussej.github.io/2016/05/24/A-Minus-Operator-For-PostgreSQLs-JSONB/
+DROP FUNCTION IF EXISTS jsonb_subtract(jsonb,jsonb) CASCADE;
+CREATE FUNCTION jsonb_subtract(arg1 jsonb, arg2 jsonb)
+RETURNS jsonb AS $$
+SELECT
+  COALESCE(json_object_agg(key, value), '{}')::jsonb
+FROM
+  jsonb_each(arg1)
+WHERE
+  (arg1 -> key) <> (arg2 -> key) OR (arg2 -> key) IS NULL
+$$ LANGUAGE SQL;
+
+DROP OPERATOR IF EXISTS - (jsonb, jsonb);
+
+CREATE OPERATOR - (
+  LEFTARG = jsonb,
+  RIGHTARG = jsonb,
+  PROCEDURE = jsonb_subtract
+);
