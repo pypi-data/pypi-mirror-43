872246e6e90820dab1dbaa0aec67f1930619e92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:24 +0000

    Add package pyfidelimax/1.0.4/pyfidelimax-1.0.4.tar.gz

---
+++ packages/pyfidelimax/pyfidelimax-1.0.4.tar.gz/pyfidelimax-1.0.4/pyfidelimax/__init__.py
@@ -0,0 +1,3 @@
+from .auth import GetToken, LojaPertencentesPrograma,\
+    CadastrarConsumidor, AtualizarConsumidor, ExtratoConsumidor, ConsultaConsumidor,\
+    RetornaDadosCliente,ResgataPremio,PontuaConsumidor,ListaProdutos
