a20a7f199c419496fc80fa19a6b1ac620848bd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:44 +0000

    Add package xontrib-thefuck/0.3.2/xontrib-thefuck-0.3.2.tar.gz

---
+++ packages/xontrib-thefuck/xontrib-thefuck-0.3.2.tar.gz/xontrib-thefuck-0.3.2/xontrib/thefuck.py
@@ -0,0 +1,8 @@
+from builtins import aliases, __xonsh__.history, evalx
+
+
+def _new_command(args, stdin=None):
+    last_command = __xonsh__.history[-1].cmd.strip()
+    evalx('thefuck {}'.format(last_command))
+
+aliases['fuck'] = _new_command
