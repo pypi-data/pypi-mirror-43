a80502b1c41f3ee45726543427401199189f3631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package shellypython/0.0.4b1/shellypython-0.0.4b1-py2.py3-none-any.whl

---
+++ packages/shellypython/shellypython-0.0.4b1-py2.py3-none-any.whl/shellypython/helpers.py
@@ -0,0 +1,35 @@
+import requests
+from .const import REQUEST_TIMEOUT
+from .exception import (
+    ShellyNetworkException, ShellyUnreachableException
+)
+
+
+def Get_item_safe(lst, idx, default):
+    try:
+        if isinstance(lst, dict):
+            return lst[idx]
+        if isinstance(lst, list):
+            return idx if idx in lst else default
+        else:
+            return default
+    except KeyError:
+        return default
+
+
+def Call_shelly_api(url, username=None, password=None):
+    """Call shelly Api and get RAW result"""
+    from requests.exceptions import RequestException
+    try:
+        r = requests.get(url, timeout=REQUEST_TIMEOUT)
+        if (r.status_code != 200):
+            raise ShellyUnreachableException("Invalid status code %s" % r.status_code)
+        return r.text
+    except RequestException:
+        raise ShellyNetworkException(
+            message="Shelly not responding at address %s" % url)
+
+
+def Rssi_to_percentage(rssi=0):
+    """Conversion from RSSI to Percent"""
+    return min(max(2 * (0 if rssi is None else rssi + 100), 0), 100)
