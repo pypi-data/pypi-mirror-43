b4f2b7d6a4687fb3563a99bd28bb4a2e4c6ea5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package other-semantic-release/5.0.0/other_semantic_release-5.0.0-py3-none-any.whl

---
+++ packages/other-semantic-release/other_semantic_release-5.0.0-py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,20 @@
+"""Semantic Release
+"""
+__version__ = '5.0.0'
+
+from .errors import (ImproperConfigurationError, SemanticReleaseBaseError,
+                     UnknownCommitMessageStyleError)
+
+__all__ = ['ImproperConfigurationError', 'SemanticReleaseBaseError',
+           'UnknownCommitMessageStyleError', 'setup_hook']
+
+
+def setup_hook(argv: list):
+    """
+    A hook to be used in setup.py to enable `python setup.py publish`.
+
+    :param argv: sys.argv
+    """
+    if len(argv) > 1 and argv[1] in ['version', 'publish', 'changelog']:
+        from .cli import main
+        main()
