0c5e1213fba6278bd4761d78ef55e657ba7d7997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package SolarUtils/0.2.4/SolarUtils-0.2.4-cp27-cp27m-win_amd64.whl

---
+++ packages/SolarUtils/SolarUtils-0.2.4-cp27-cp27m-win_amd64.whl/solar_utils/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+This is the solar utilities package.
+
+2013 SunPower Corp.
+Confidential & Proprietary
+Do Not Distribute
+"""
+
+from solar_utils.core import solposAM, spectrl2
+
+__version__ = '0.2.4'
+__release__ = 'Beetroot'
+__author__ = 'Mark Mikofski'
+__email__ = 'mark.mikofski@sunpowercorp.com'
+__url__ = 'https://github.com/SunPower/SolarUtils'
+__all__ = ['solposAM', 'spectrl2']
