817abcdd94c432dcd4c4aa40fa105c8385bd74e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:23 +0000

    Add package dollar-ref/0.1.2/dollar-ref-0.1.2.tar.gz

---
+++ packages/dollar-ref/dollar-ref-0.1.2.tar.gz/dollar-ref-0.1.2/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    README = f.read()
+
+
+setup(
+    name='dollar-ref',
+    version='0.1.2',
+
+    description='JSON Reference Resolution',
+    long_description=README,
+
+    url='https://github.com/bagrat/dollar-ref',
+
+    author='Bagrat Aznauryan',
+    author_email='contact@bagrat.io',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Environment :: Console',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Topic :: Utilities'
+    ],
+
+    keywords='json schema ref reference openapi yaml resolve',
+
+    packages=['dollar_ref'],
+
+    install_requires=[
+        'PyYAML',
+        'termcolor'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'dref=dollar_ref.console:main'
+        ]
+    }
+)
