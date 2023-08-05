2c514a815d5bb6dd6d12cbe654d22516c45f607e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:43 +0000

    Add package data-worker/0.1.4/data_worker-0.1.4.tar.gz

---
+++ packages/data-worker/data_worker-0.1.4.tar.gz/data_worker-0.1.4/data_worker/utils.py
@@ -0,0 +1,12 @@
+column_rename = {
+    'date': 'timestamp',
+    '1. open': 'open',
+    '2. high': 'high',
+    '3. low': 'low',
+    '4. close': 'close',
+    '5. volume': 'volume',
+    '5. adjusted close': 'adjusted_close',
+    '6. volume': 'volume',
+    '7. dividend amount': 'dividend',
+    '8. split coefficient': 'split_coeff'
+}
