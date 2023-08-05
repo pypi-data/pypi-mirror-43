52e8c49408e188e476e977d24358019bc2e99ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:40 +0000

    Add package keysightoscilloscopeacquire/1.0.0/keysightoscilloscopeacquire-1.0.0-py3-none-any.whl

---
+++ packages/keysightoscilloscopeacquire/keysightoscilloscopeacquire-1.0.0-py3-none-any.whl/keyoscacquire/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Andreas Svela // 2019
+"""
+
+__version__ = '1.0.0'
+
+
+# Default options
+from keyoscacquire.default_options import *
+
+from keyoscacquire.oscacq import Oscilloscope
