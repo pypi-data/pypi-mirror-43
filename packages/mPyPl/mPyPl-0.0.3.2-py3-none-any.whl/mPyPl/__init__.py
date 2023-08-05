585c9a6059c8fd43f692d6942152dfb070724173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package mPyPl/0.0.3.2/mPyPl-0.0.3.2-py3-none-any.whl

---
+++ packages/mPyPl/mPyPl-0.0.3.2-py3-none-any.whl/mPyPl/__init__.py
@@ -0,0 +1,15 @@
+# mPyPl - Monadic Pipeline Library for Python
+# http://github.com/shwars/mPyPl
+
+__version__ = '0.0.3.2'
+
+from .mdict import *
+from .core import *
+from .funcs import *
+from .video import  *
+from .keras import *
+from .sink import *
+from .multiclass_datastream import *
+from .xmlstream import *
+from .utils.pipeutils import *
+from .utils.coreutils import *
