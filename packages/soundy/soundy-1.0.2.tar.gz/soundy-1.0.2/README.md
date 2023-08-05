ad92ee1a5cee219c8e24e614de42364c207e3826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:32 +0000

    Add package soundy/1.0.2/soundy-1.0.2.tar.gz

---
+++ packages/soundy/soundy-1.0.2.tar.gz/soundy-1.0.2/README.md
@@ -0,0 +1,21 @@
+# Soundy
+A light-weight SoundCloud package to download and scrape tracks.
+
+## Installation 
+you can install Soundy from [PyPI](https://pypi.org/project/soundy/):
+
+    pip install soundy
+
+soundy is tested on python 3.7
+
+## Usage 
+Soundy is a command line application, You can download a specific track using the following command
+       
+    python -m soundy --url track-url-here --path path-here
+
+the default download path is the directory which you run the command from 
+
+You can also use Soundy in your own Python code, by importing from the `soundy` package:
+
+    >>> from soundy import soundy
+    >>> soundy.download_track(url = "url", path = r"path")
---
+++ packages/soundy/soundy-1.0.2.tar.gz/soundy-1.0.2/soundy/Notes.py
@@ -0,0 +1,30 @@
+
+# Use OOP
+
+#make at as webapp to show all of your skills(front-back)
+
+#indicator of how many downloads he have done, and after 20 ask him to donate and when he does stop showing the warning
+
+#make the codes locked and to unlock them for edit you need authintaction from the website(admin)
+
+# Have the __init__ directory change to input of the user from the settings, if not made make it download to Desktop.
+
+# need to make a check to find if the link is sc or a Blank! and if it's valid and accesabile.
+
+# divide code into 2 scripts ths one, and another one to run the CMD sys.args
+
+# when it comes to list/user downloading use a loop to make each track an instance of class and download it seperatly ; it's a classmethod.
+
+# a kind of history using sqllite3?
+
+# node-id3 is a ID3-Tag library written in JavaScript without other dependencies.
+
+# Don't know what to listen to? check out POSEIDON.
+
+# 2 regex functions; one to check if it's a valid SC URL or if valid at all and tells the user to check his connection and the second to check if the valid SC link is list/user.
+
+# when able to make a script to download and embedd jpgs to track we should make it a subclass as the only difference between them is the format and the have the download func in common and if had different attrs you can use super().__init(common1,common2,common3)...cory schafer OOP4
+
+# Add id3 Stamp on the tracks(downloaded by,site) 
+
+# private tracks have No ID < byut have SpecialID check it later
