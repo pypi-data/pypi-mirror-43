e50628219a3c2cdaf25a3b77def8636df7a6414b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:05 +0000

    Add package fulfil_client/0.13.0/fulfil_client-0.13.0.tar.gz

---
+++ packages/fulfil_client/fulfil_client-0.13.0.tar.gz/fulfil_client-0.13.0/fulfil_client/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Fulfil.IO Inc.'
+__email__ = 'hello@fulfil.io'
+__version__ = '0.13.0'
+
+# flake8: noqa
+
+from .client import Client, Model, ServerError, SessionAuth, APIKeyAuth, \
+    BearerAuth, ClientError
