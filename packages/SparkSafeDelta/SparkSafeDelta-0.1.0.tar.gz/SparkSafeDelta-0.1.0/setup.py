53f6edf1ec7a675948bf6e079216fc399b08772e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package SparkSafeDelta/0.1.0/SparkSafeDelta-0.1.0.tar.gz

---
+++ packages/SparkSafeDelta/SparkSafeDelta-0.1.0.tar.gz/SparkSafeDelta-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='SparkSafeDelta',
+    version='0.1.0',
+    author='Aleksandrs Krivickis',
+    author_email='aleksandrs.krivickis@gmail.com',
+    packages=['sparksafedelta', 'sparksafedelta.tests'],
+    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/SparkSafeDelta/',
+    license='LICENSE.txt',
+    description='Tool that allows to automatically update schema of DataBricks Delta in case of Changes in data structure',
+    long_description=open('README.txt').read(),
+)
