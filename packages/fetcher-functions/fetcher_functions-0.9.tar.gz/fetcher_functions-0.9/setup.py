db05aceea256e5768d7be1c6cd84aaa09229e15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package fetcher-functions/0.9/fetcher_functions-0.9.tar.gz

---
+++ packages/fetcher-functions/fetcher_functions-0.9.tar.gz/fetcher_functions-0.9/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="fetcher_functions",
+    version="0.9",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["fetcher_functions"],
+    install_requires=["requests-html", "pandas"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
