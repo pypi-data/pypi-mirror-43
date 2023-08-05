8ba91384909ffb93aa8fc72c96b544dacefdfb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:47 +0000

    Add package pydriosm/1.0.3/pydriosm-1.0.3-py3-none-any.whl

---
+++ packages/pydriosm/pydriosm-1.0.3-py3-none-any.whl/pydriosm/download_BBBike.py
@@ -0,0 +1,164 @@
+""" BBBike downloads http://download.bbbike.org/osm/bbbike/ """
+
+import os
+import re
+import urllib.parse
+import urllib.request
+
+import bs4
+import fuzzywuzzy.process
+import pandas as pd
+
+from pydriosm.utils import cd_dat, cd_dat_bbbike, confirmed, load_pickle, save_pickle
+
+
+#
+def get_bbbike_subregion_index(update=False):
+    """
+    :param update: [bool]
+    :return: [pandas.DataFrame]
+    """
+    home_url = 'http://download.bbbike.org/osm/bbbike/'
+    path_to_file = cd_dat("BBBike-subregion-index.pickle")
+    if os.path.isfile(path_to_file) and not update:
+        bbbike_subregion_index = load_pickle(path_to_file)
+    else:
+        try:
+            bbbike_subregion_index = pd.read_html(home_url, header=0, parse_dates=['Last Modified'])[0].drop(0)
+            bbbike_subregion_index.Name = bbbike_subregion_index.Name.map(lambda x: x.strip('/'))
+            save_pickle(bbbike_subregion_index, path_to_file)
+        except Exception as e:
+            print("Failed to get BBBike directory index. {}".format(e))
+            bbbike_subregion_index = None
+    return bbbike_subregion_index
+
+
+#
+def get_bbbike_subregion_downloads_index(subregion_name, update=False, verbose=True):
+    """
+    :param subregion_name: [str]
+    :param update: [bool]
+    :param verbose: [bool]
+    :return: 
+    """
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name_ = fuzzywuzzy.process.extractOne(subregion_name, bbbike_subregion_index.Name, score_cutoff=10)[0]
+
+    if verbose:
+        if subregion_name != subregion_name_:
+            print("\"{}\" is not found. \n".format(subregion_name))
+        print("Trying to get downloads index for \"{}\" ... ".format(subregion_name_), end="")
+
+    path_to_file = cd_dat_bbbike(subregion_name_, subregion_name_ + "-url-index.pickle")
+    if os.path.isfile(path_to_file) and not update:
+        subregion_downloads_index = load_pickle(path_to_file)
+        if verbose:
+            print("Done.")
+    else:
+        try:
+            url = 'https://download.bbbike.org/osm/bbbike/{}/'.format(subregion_name_)
+
+            source = urllib.request.urlopen(url)
+            source_soup = bs4.BeautifulSoup(source, 'lxml')
+            download_links_class = source_soup.find_all(name='a', attrs={'class': ['download_link', 'small']})
+
+            def parse_dlc(dlc):
+                dlc_href = dlc.get('href')  # URL
+                filename, download_url = dlc_href.strip('./'), urllib.parse.urljoin(url, dlc_href)
+                if not dlc.has_attr('title'):
+                    file_format, file_size, last_update = 'Poly', None, None
+                else:
+                    if len(dlc.contents) < 3:
+                        file_format, file_size = 'Txt', None
+                    else:
+                        file_format, file_size, _ = dlc.contents  # File type and size
+                        file_format, file_size = file_format.strip(), file_size.text
+                    last_update = pd.to_datetime(dlc.get('title'))  # Date and time
+                parsed_dat = [filename, download_url, file_format, file_size, last_update]
+                return parsed_dat
+
+            parsed_dlc = [parse_dlc(x) for x in download_links_class]
+
+            col_names = ['File_name', 'Download_URL', 'File_format', 'File_size', 'Last_update']
+            subregion_downloads_index = pd.DataFrame(parsed_dlc, columns=col_names)
+
+            if verbose:
+                print("Done.")
+            save_pickle(subregion_downloads_index, path_to_file)
+
+        except Exception as e:
+            print("Failed. \"{}\"".format(subregion_name_, e))
+            subregion_downloads_index = None
+
+    return subregion_downloads_index
+
+
+#
+def get_bbbike_downloads_dictionary(update=False):
+    path_to_file = cd_dat("BBBike-downloads-dictionary.pickle")
+    if os.path.isfile(path_to_file) and not update:
+        downloads_dictionary = load_pickle(path_to_file)
+    else:
+        bbbike_subregion_index = get_bbbike_subregion_index(update=update)
+        downloads_index = [get_bbbike_subregion_downloads_index(s, update, verbose=False)
+                           for s in bbbike_subregion_index.Name]
+        downloads_dictionary = dict(zip(bbbike_subregion_index.Name, downloads_index))
+        save_pickle(downloads_dictionary, path_to_file)
+    return downloads_dictionary
+
+
+#
+def download_bbbike_subregion_osm_file(subregion_name, file_format=".osm.pbf",
+                                       update=False, download_confirmation_required=True):
+    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update=False)
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name_ = fuzzywuzzy.process.extractOne(subregion_name, bbbike_subregion_index.Name, score_cutoff=10)[0]
+
+    subregion_downloads = bbbike_downloads_dict[subregion_name_]
+
+    available_formats = [re.sub('{}|CHECKSUM'.format(subregion_name_), '', f) for f in subregion_downloads.File_name]
+
+    file_fmt, _ = fuzzywuzzy.process.extractOne(file_format, available_formats, score_cutoff=10)
+    filename, _, idx = fuzzywuzzy.process.extractOne(file_fmt, subregion_downloads.File_name)
+    url, path_to_file = subregion_downloads.Download_URL[idx], cd_dat_bbbike(subregion_name_, filename)
+
+    if os.path.isfile(path_to_file) and not update:
+        print("\"{}\" is already available for \"{}\".".format(filename, subregion_name_))
+    else:
+        if confirmed(prompt="Confirm to download \"{}\"?".format(filename),
+                     resp=False, confirmation_required=download_confirmation_required):
+            try:
+                urllib.request.urlretrieve(url, path_to_file)
+                print("\"{}\" successfully downloaded.".format(filename))
+                print("File has been saved to \"{}\"".format(path_to_file))
+            except Exception as e:
+                print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
+
+
+#
+def download_bbbike_subregion_osm_all_files(subregion_name, download_confirmation_required=True):
+    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update=False)
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name_ = fuzzywuzzy.process.extractOne(subregion_name, bbbike_subregion_index.Name, score_cutoff=10)[0]
+
+    subregion_downloads = bbbike_downloads_dict[subregion_name_]
+
+    if confirmed(prompt="Confirm to download all available BBBike files for \"{}\"?".format(subregion_name_),
+                 resp=False, confirmation_required=download_confirmation_required):
+        for url, filename in zip(subregion_downloads.Download_URL, subregion_downloads.File_name):
+            try:
+                print("\"{}\" successfully downloaded.".format(filename))
+                urllib.request.urlretrieve(url, cd_dat_bbbike(subregion_name_, filename))
+            except Exception as e:
+                print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
+        print("All the downloaded files have been saved to \"{}\".".format(cd_dat_bbbike(subregion_name_)))
+    else:
+        for url, filename in zip(subregion_downloads.Download_URL, subregion_downloads.File_name):
+            if confirmed(prompt="Download \"{}\"?".format(filename), resp=False,
+                         confirmation_required=download_confirmation_required):
+                try:
+                    print("\"{}\" successfully downloaded.".format(filename))
+                    urllib.request.urlretrieve(url, cd_dat_bbbike(subregion_name_, filename))
+                    print("\"{}\" saved to \"{}\".".format(filename, cd_dat_bbbike(subregion_name_)))
+                except Exception as e:
+                    print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
---
+++ packages/pydriosm/pydriosm-1.0.3-py3-none-any.whl/pydriosm/dump_GeoFabrik.py
@@ -0,0 +1,88 @@
+# Download a bunch of OSM data extracts and import them to local PostgreSQL
+
+import gc
+import rapidjson
+
+import ogr
+import pandas as pd
+
+from pydriosm.download_GeoFabrik import get_region_subregion_index, remove_subregion_osm_file
+from pydriosm.osm_psql import OSM
+from pydriosm.read_GeoFabrik import justify_subregion_input, read_osm_pbf
+from pydriosm.utils import confirmed
+
+
+# Dump data extracts to PostgreSQL
+def psql_osm_extracts(update=False, file_size_limit=120, rm_raw_file=True):
+    """
+    :param update: [bool] False (default)
+    :param file_size_limit: [int] 100 (default)
+    :param rm_raw_file: [bool] True (default)
+    :return:
+    """
+    if confirmed("To dump GeoFabrik OSM data extracts to PostgreSQL?"):
+        subregion_names = get_region_subregion_index("GeoFabrik-no-subregion-list")
+
+        osmdb = OSM()
+        osmdb.connect_db(database_name='osm_extracts')
+
+        err_subregion_names = []
+        for subregion_name in subregion_names:
+            try:
+                subregion_filename, path_to_osm_pbf = justify_subregion_input(subregion_name)
+
+                subregion_osm_pbf = read_osm_pbf(
+                    subregion_name, update=update, download_confirmation_required=False,
+                    file_size_limit=file_size_limit,
+                    pickle_it=False, rm_raw_file=False)
+
+                if subregion_osm_pbf is not None:
+                    osmdb.dump_osm_pbf_data(subregion_osm_pbf, table_name=subregion_name)
+                    del subregion_osm_pbf
+                    gc.collect()
+
+                else:
+                    raw_osm_pbf = ogr.Open(path_to_osm_pbf)
+                    layer_count = raw_osm_pbf.GetLayerCount()
+                    print("\nParsing and importing \"{}\" feature-wisely to PostgreSQL ... ".format(subregion_name))
+                    for i in range(layer_count):
+                        lyr = raw_osm_pbf.GetLayerByIndex(i)  # Hold the i-th layer
+                        layer_name = lyr.GetName()
+                        print("                         {} ... ".format(layer_name), end="")
+                        try:
+                            # Reference: https://gdal.org/python/osgeo.ogr.Feature-class.html
+                            f, counter = lyr.GetNextFeature(), 1
+                            # Loop through all other available features
+                            while f is not None:
+                                feat = rapidjson.loads(f.ExportToJson())  # Get features from the i-th layer
+                                feat_data = pd.DataFrame.from_dict(feat, orient='index')
+                                if counter == 1:
+                                    osmdb.dump_osm_pbf_data_by_layer(feat_data.T, layer_name, subregion_name,
+                                                                     if_exists='replace')
+                                else:
+                                    osmdb.dump_osm_pbf_data_by_layer(feat_data.T, layer_name, subregion_name,
+                                                                     if_exists='append')
+                                del feat, feat_data  # f.Destroy()
+                                f = lyr.GetNextFeature()
+                                counter += 1
+                            del f
+                            print("Done. Total amount of features: {}".format(counter - 1))
+                        except Exception as e:
+                            print("Failed. {}".format(e))
+                        del lyr
+                    raw_osm_pbf.Release()
+                    del raw_osm_pbf, layer_count
+                    gc.collect()
+
+                if rm_raw_file:
+                    remove_subregion_osm_file(path_to_osm_pbf)
+
+            except Exception as e:
+                print(e)
+                err_subregion_names.append(subregion_name)
+
+        if len(err_subregion_names) == 0:
+            print("\nMission accomplished.\n")
+        else:
+            print("\nErrors occurred when parsing data of the following subregion(s):")
+            print(*err_subregion_names, sep='\n')
