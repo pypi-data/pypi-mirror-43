411683810381164bf341a353338694f8ddfad62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package ncbitax/0.1.2/ncbitax-0.1.2.tar.gz

---
+++ packages/ncbitax/ncbitax-0.1.2.tar.gz/ncbitax-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='ncbitax',
+    version='0.1.2',
+    description='Python API for working with NCBI taxonomy db',
+    entry_points={
+        'console_scripts': [
+            'ncbitax = ncbitax.main:main',
+            ]
+    },
+    long_description=readme,
+    author='Simon Ye',
+    author_email='mail@yesimon.com',
+    url='https://github.com/yesimon/ncbitax',
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
