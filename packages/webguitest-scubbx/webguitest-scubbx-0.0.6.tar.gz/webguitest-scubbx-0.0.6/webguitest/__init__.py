a1abd8eadaa2ce185e5b1f93d22a693adb6c3a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package webguitest-scubbx/0.0.6/webguitest-scubbx-0.0.6.tar.gz

---
+++ packages/webguitest-scubbx/webguitest-scubbx-0.0.6.tar.gz/webguitest-scubbx-0.0.6/webguitest/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+name = "webguitest"
+
+"""
+----------------------------------------------------------------------
+This is the WebGuiTest module.
+
+The functions available are:
+
+  openBrowser():
+  
+  clickGraphic():
+  
+  clickName():
+  
+  getValueFromName():
+  
+  enterValueToName():
+  
+  
+----------------------------------------------------------------------
+"""
+
+
+from webguitest.openBrowser import *
+from webguitest.clickGraphic import *
+from webguitest.clickName import *
+from webguitest.getValueFromName import *
+from webguitest.enterValueToName import *
+from webguitest.setValueOfName import *
+from webguitest.clickTextElement import *
+from webguitest.enterValueToGraphic import *
+from webguitest.enterValue import *
+from webguitest.checkGraphic import *
