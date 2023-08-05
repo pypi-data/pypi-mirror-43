0a9053a86420419f34eb0707d54dbc1a088500d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:27 +0000

    Add package qtpyvcp/0.2.1/qtpyvcp-0.2.1.tar.gz

---
+++ packages/qtpyvcp/qtpyvcp-0.2.1.tar.gz/qtpyvcp-0.2.1/examples/mini/mini.py
@@ -0,0 +1,6 @@
+from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
+
+class MiniVCP(VCPMainWindow):
+    """Main window class for the Mini example VCP."""
+    def __init__(self, *args, **kwargs):
+        super(MiniVCP, self).__init__(*args, **kwargs)
