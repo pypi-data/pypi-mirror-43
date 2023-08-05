f858ffedbaa94c844008716a1b321517badc4156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:50 +0000

    Add package wrds-tools/0.0.2/wrds_tools-0.0.2.tar.gz

---
+++ packages/wrds-tools/wrds_tools-0.0.2.tar.gz/wrds_tools-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='wrds_tools',
+                 version='0.0.2',
+                 description='Various tools to create a connection to the WRDS service and download commonly used ' +
+                             'data.',
+                 author='Julian Barg',
+                 author_email='barg.julian@gmail.com',
+                 packages=['wrds_tools'],
+                 install_requires=['pandas', 'wrds']
+                 )
