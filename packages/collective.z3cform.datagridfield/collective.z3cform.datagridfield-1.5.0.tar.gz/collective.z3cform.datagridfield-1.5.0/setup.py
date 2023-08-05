f7482a1a57888aeb661f12809041e3669ca667dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:44 +0000

    Add package collective.z3cform.datagridfield/1.5.0/collective.z3cform.datagridfield-1.5.0.tar.gz

---
+++ packages/collective.z3cform.datagridfield/collective.z3cform.datagridfield-1.5.0.tar.gz/collective.z3cform.datagridfield-1.5.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    # zest.releaser needs version here for now
+    version='1.5.0',
+
+    # thanks to this bug
+    # https://github.com/pypa/setuptools/issues/1136
+    # we need one line in here:
+    package_dir={"": "src"}
+
+    # see setup.cfg
+)
