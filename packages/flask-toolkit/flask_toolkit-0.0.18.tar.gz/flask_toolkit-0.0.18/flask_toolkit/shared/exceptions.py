c68e7327fc36255969c388498432fc3b7ba95be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package flask-toolkit/0.0.18/flask_toolkit-0.0.18.tar.gz

---
+++ packages/flask-toolkit/flask_toolkit-0.0.18.tar.gz/flask_toolkit-0.0.18/flask_toolkit/shared/exceptions.py
@@ -0,0 +1,14 @@
+class ObjectDoesNotExistException(Exception):
+    pass
+
+
+class ForbiddenException(Exception):
+    pass
+
+
+class BadRequestException(Exception):
+    pass
+
+
+class InvalidDomainConditions(Exception):
+    pass
