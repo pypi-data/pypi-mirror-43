aec027d7c7900e6ed1b49d3cd804dd8bc097aa06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:15 +0000

    Add package qtile-plasma/1.5.4/qtile-plasma-1.5.4.tar.gz

---
+++ packages/qtile-plasma/qtile-plasma-1.5.4.tar.gz/qtile-plasma-1.5.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='qtile-plasma',
+    packages=['plasma'],
+    version='1.5.4',
+    description='A flexible, tree-based layout for Qtile',
+    author='numirias',
+    author_email='numirias@users.noreply.github.com',
+    url='https://github.com/numirias/qtile-plasma',
+    license='MIT',
+    python_requires='>=3',
+    install_requires=['xcffib', 'qtile'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Desktop Environment :: Window Managers',
+    ],
+)
