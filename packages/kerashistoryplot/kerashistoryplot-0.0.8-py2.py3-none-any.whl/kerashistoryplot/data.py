42b4cc3cae0527024da43d003b31b49b5cf5b42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:34 +0000

    Add package kerashistoryplot/0.0.8/kerashistoryplot-0.0.8-py2.py3-none-any.whl

---
+++ packages/kerashistoryplot/kerashistoryplot-0.0.8-py2.py3-none-any.whl/kerashistoryplot/data.py
@@ -0,0 +1,37 @@
+def get_metrics(history):
+    plot_metrics = [
+        k for k in history.keys()
+        if k not in ['epoch', 'batches'] and not k.startswith('val_')
+    ]
+    return plot_metrics
+
+
+def get_metric_vs_epoch(history, metric, batches=True):
+    data = []
+    val_metric = f'val_{metric}'
+    for key in [metric, val_metric]:
+        if key in history:
+            data.append(
+                {
+                    'x': history['epoch'],
+                    'y': history[key],
+                    'label': key
+                }
+            )
+    if batches:
+        batch_data = _get_batch_metric_vs_epoch(history, metric)
+        if batch_data:
+            data.append(batch_data)
+    return data
+
+
+def _get_batch_metric_vs_epoch(history, metric):
+    if not history.get('batches') or metric not in history['batches'][0]:
+        return
+    epoch_value = [
+        (epoch, value)
+        for epoch, batch in zip(history['epoch'], history['batches'])
+        for value in batch[metric]
+    ]
+    epoch, value = zip(*epoch_value)
+    return {'x': list(epoch), 'y': list(value), 'label': f'batch_{metric}'}
