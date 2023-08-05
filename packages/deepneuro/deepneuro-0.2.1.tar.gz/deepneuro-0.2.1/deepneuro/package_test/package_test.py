92e143412f380a55041847acd0e9fa20fb23580e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:58 +0000

    Add package deepneuro/0.2.1/deepneuro-0.2.1.tar.gz

---
+++ packages/deepneuro/deepneuro-0.2.1.tar.gz/deepneuro-0.2.1/deepneuro/package_test/package_test.py
@@ -0,0 +1,25 @@
+"""A test module for automatic documentation purposes
+"""
+
+
+def test_function():
+
+    """Some test documentation for automatic documentation.
+
+    Parameters
+    ----------
+    in: str
+        Description.
+
+    Returns
+    -------
+    out: str
+        Description
+        
+    """
+
+    return
+
+
+if __name__ == '__main__':
+    test_function()
