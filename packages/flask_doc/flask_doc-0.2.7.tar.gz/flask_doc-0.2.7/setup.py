d979b88d8ce2011b5c3a87569ed5b06bd8f79253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:19 +0000

    Add package flask_doc/0.2.7/flask_doc-0.2.7.tar.gz

---
+++ packages/flask_doc/flask_doc-0.2.7.tar.gz/flask_doc-0.2.7/setup.py
@@ -0,0 +1,23 @@
+# coding=utf8
+__author__ = 'liming'
+
+from setuptools import setup
+
+setup(name='flask_doc',
+      version='0.2.7',
+      description='Write API document when you coding, Test your API when you press last word immediately',
+      url='https://github.com/ipconfiger/flask-apidoc',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['flask_doc'],
+      package_data = {
+            "flask_doc": ["*.html", "*.gif"],
+      },
+      install_requires=[
+          'flask','markdown'
+      ],
+      entry_points = {
+        'console_scripts': ['fkapidoc=flask_doc.generator:main'],
+      },
+      zip_safe=False)
