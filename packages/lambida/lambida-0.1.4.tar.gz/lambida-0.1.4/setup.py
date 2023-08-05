f5a8d5de7e9a0ffd61ea42ef9ae6b399ecd3cca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:45 +0000

    Add package lambida/0.1.4/lambida-0.1.4.tar.gz

---
+++ packages/lambida/lambida-0.1.4.tar.gz/lambida-0.1.4/setup.py
@@ -0,0 +1,22 @@
+"""An AWS utility package"""
+import setuptools
+import lambida
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='lambida',  
+     version='0.1.04',
+     author="AMARO",
+     author_email="data@amaro.com",
+     description="An AWS utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url=None,
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
