a66f7ef56a3af19ecdcd97c2fb81e2c11fae9d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package sense-core/0.2.6/sense_core-0.2.6-py3-none-any.whl

---
+++ packages/sense-core/sense_core-0.2.6-py3-none-any.whl/sense_core/queue.py
@@ -0,0 +1,48 @@
+from .queue0 import *
+from .log import log_exception, log_info
+from .utils import sleep
+from pika.exceptions import *
+
+
+class RabbitProducer(RabbitProducer0):
+
+    def send_safely(self, topic, message):
+        try:
+            self.send(topic, message)
+            return True
+        except Exception as ex:
+            log_exception(str(ex))
+            self.close_safely()
+            return False
+
+    def close_safely(self):
+        try:
+            self.close_connection()
+        except Exception as ex:
+            log_exception(str(ex))
+
+
+class RabbitConsumer(RabbitConsumer0):
+
+    def _close_safely(self):
+        try:
+            self.close_connection()
+        except Exception as ex:
+            log_exception(str(ex))
+
+    def execute_safely(self, caller, prefetch_count=2):
+        while True:
+            try:
+                self.execute(caller, prefetch_count)
+            except ConnectionClosed as ex:
+                log_info("rabbit ConnectionClosed reset")
+                self._close_safely()
+                sleep(2)
+            except ChannelClosed as ex:
+                log_info("rabbit ChannelClosed reset")
+                self._close_safely()
+                sleep(2)
+            except Exception as ex:
+                log_exception(ex)
+                self._close_safely()
+                raise ex
