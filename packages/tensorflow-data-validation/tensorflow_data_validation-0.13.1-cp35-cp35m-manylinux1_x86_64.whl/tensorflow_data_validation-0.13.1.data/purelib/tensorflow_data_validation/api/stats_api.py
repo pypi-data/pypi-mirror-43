89719c91bfac12e871daa4a9b87fb6cf0ade683f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package tensorflow-data-validation/0.13.1/tensorflow_data_validation-0.13.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-data-validation/tensorflow_data_validation-0.13.1-cp35-cp35m-manylinux1_x86_64.whl/tensorflow_data_validation-0.13.1.data/purelib/tensorflow_data_validation/api/stats_api.py
@@ -0,0 +1,123 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+"""TensorFlow Data Validation Statistics API.
+
+The Statistics API for TF Data Validation consists of a single beam.PTransform,
+GenerateStatistics, that computes a set of statistics on an input set of
+examples in a single pass over the examples.
+
+GenerateStatistics applies a set of statistics generator each of which
+computes a specific type of statistic.  Specifically, we have five default
+generators:
+  1) CommonStatsGenerator, which computes the common statistics for all
+     the features.
+  2) NumericStatsGenerator, which computes the numeric statistics for features
+     of numeric type (INT or FLOAT).
+  3) StringStatsGenerator, which computes the common string statistics for
+     features of string type.
+  4) TopKStatsGenerator, which computes the top-k values for features
+     of string type.
+  5) UniqueStatsGenerator, which computes the number of unique values for
+     features of string type.
+
+Additional generators can be implemented and added to the default set to
+compute additional custom statistics.
+
+The stats generators process a batch of examples at a time. All the stats
+generators are run together in the same pass. At the end, their
+outputs are combined and converted to a DatasetFeatureStatisticsList proto
+(https://github.com/tensorflow/metadata/blob/master/tensorflow_metadata/proto/v0/statistics.proto).  # pylint: disable=line-too-long
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+from __future__ import print_function
+
+import random
+import apache_beam as beam
+from tensorflow_data_validation import types
+from tensorflow_data_validation.statistics import stats_impl
+from tensorflow_data_validation.statistics import stats_options
+from tensorflow_data_validation.types_compat import Generator
+
+from tensorflow_metadata.proto.v0 import statistics_pb2
+
+
+# TODO(b/112146483): Test the Stats API with unicode input.
+@beam.typehints.with_input_types(types.BeamExample)
+@beam.typehints.with_output_types(statistics_pb2.DatasetFeatureStatisticsList)
+class GenerateStatistics(beam.PTransform):
+  """API for generating data statistics.
+
+  Example:
+
+  ```python
+    with beam.Pipeline(runner=...) as p:
+      _ = (p
+           | 'ReadData' >> beam.io.ReadFromTFRecord(data_location)
+           | 'DecodeData' >> beam.Map(TFExampleDecoder().decode)
+           | 'GenerateStatistics' >> GenerateStatistics()
+           | 'WriteStatsOutput' >> beam.io.WriteToTFRecord(
+               output_path, shard_name_template='',
+               coder=beam.coders.ProtoCoder(
+                   statistics_pb2.DatasetFeatureStatisticsList)))
+  ```
+  """
+
+  def __init__(
+      self,
+      options = stats_options.StatsOptions()
+  ):
+    """Initializes the transform.
+
+    Args:
+      options: `tfdv.StatsOptions` for generating data statistics.
+
+    Raises:
+      TypeError: If options is not of the expected type.
+    """
+    if not isinstance(options, stats_options.StatsOptions):
+      raise TypeError('options is of type %s, should be a StatsOptions.' %
+                      type(options).__name__)
+    self._options = options
+
+  def expand(self, dataset):
+    # Sample input data if sample_count option is provided.
+    # TODO(b/117229955): Consider providing an option to write the sample
+    # to a file.
+    if self._options.sample_count is not None:
+      # beam.combiners.Sample.FixedSizeGlobally returns a
+      # PCollection[List[types.Example]], which we then flatten to get a
+      # PCollection[types.Example].
+      dataset |= ('SampleExamples(%s)' % self._options.sample_count >>
+                  beam.combiners.Sample.FixedSizeGlobally(
+                      self._options.sample_count)
+                  | 'FlattenExamples' >> beam.FlatMap(lambda lst: lst))
+    elif self._options.sample_rate is not None:
+      dataset |= ('SampleExamplesAtRate(%s)' % self._options.sample_rate >>
+                  beam.FlatMap(_sample_at_rate,
+                               sample_rate=self._options.sample_rate))
+
+    return (dataset | 'RunStatsGenerators' >>
+            stats_impl.GenerateStatisticsImpl(self._options))
+
+
+def _sample_at_rate(example, sample_rate
+                   ):
+  """Sample examples at input sampling rate."""
+  # TODO(pachristopher): Revisit this to decide if we need to fix a seed
+  # or add an optional seed argument.
+  if random.random() <= sample_rate:
+    yield example
---
+++ packages/tensorflow-data-validation/tensorflow_data_validation-0.13.1-cp35-cp35m-manylinux1_x86_64.whl/tensorflow_data_validation-0.13.1.data/purelib/tensorflow_data_validation/coders/csv_decoder_test.py
@@ -0,0 +1,426 @@
+# coding=utf-8
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for CSV decoder."""
+
+from __future__ import absolute_import
+from __future__ import division
+
+from __future__ import print_function
+
+import sys
+from absl.testing import absltest
+import apache_beam as beam
+from apache_beam.testing import util
+import numpy as np
+from tensorflow_data_validation.coders import csv_decoder
+from tensorflow_data_validation.utils import test_util
+
+from google.protobuf import text_format
+from tensorflow_metadata.proto.v0 import schema_pb2
+
+
+class CSVDecoderTest(absltest.TestCase):
+  """Tests for CSV decoder."""
+
+  def test_csv_decoder(self):
+    input_lines = ['1,2.0,hello',
+                   '5,12.34,world']
+    column_names = ['int_feature', 'float_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer),
+         'float_feature': np.array([2.0], dtype=np.float32),
+         'str_feature': np.array([b'hello'], dtype=np.object)},
+        {'int_feature': np.array([5], dtype=np.integer),
+         'float_feature': np.array([12.34], dtype=np.float32),
+         'str_feature': np.array([b'world'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_schema(self):
+    input_lines = ['1,1,2.0,hello',
+                   '5,5,12.34,world']
+    column_names = ['int_feature_parsed_as_float', 'int_feature',
+                    'float_feature', 'str_feature']
+    schema = text_format.Parse(
+        """
+        feature { name: "int_feature_parsed_as_float" type: FLOAT }
+        feature { name: "int_feature" type: INT }
+        feature { name: "float_feature" type: FLOAT }
+        feature { name: "str_feature" type: BYTES }
+        """, schema_pb2.Schema())
+    expected_result = [
+        {'int_feature_parsed_as_float': np.array([1], dtype=np.float32),
+         'int_feature': np.array([1], dtype=np.integer),
+         'float_feature': np.array([2.0], dtype=np.float32),
+         'str_feature': np.array([b'hello'], dtype=np.object)},
+        {'int_feature_parsed_as_float': np.array([5], dtype=np.float32),
+         'int_feature': np.array([5], dtype=np.integer),
+         'float_feature': np.array([12.34], dtype=np.float32),
+         'str_feature': np.array([b'world'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names, schema=schema,
+                                      infer_type_from_schema=True))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_missing_values(self):
+    input_lines = ['1,,hello',
+                   ',12.34,']
+    column_names = ['int_feature', 'float_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1.0], dtype=np.float32),
+         'float_feature': None,
+         'str_feature': np.array([b'hello'], dtype=np.object)},
+        {'int_feature': None,
+         'float_feature': np.array([12.34], dtype=np.float32),
+         'str_feature': None}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_int_and_float_in_same_column(self):
+    input_lines = ['2,1.5',
+                   '1.5,2']
+    column_names = ['float_feature1', 'float_feature2']
+    expected_result = [
+        {'float_feature1': np.array([2.0], dtype=np.float32),
+         'float_feature2': np.array([1.5], dtype=np.float32)},
+        {'float_feature1': np.array([1.5], dtype=np.float32),
+         'float_feature2': np.array([2.0], dtype=np.float32)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_int_and_string_in_same_column(self):
+    input_lines = ['2,abc',
+                   'abc,2']
+    column_names = ['str_feature1', 'str_feature2']
+    expected_result = [
+        {'str_feature1': np.array([b'2'], dtype=np.object),
+         'str_feature2': np.array([b'abc'], dtype=np.object)},
+        {'str_feature1': np.array([b'abc'], dtype=np.object),
+         'str_feature2': np.array([b'2'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_float_and_string_in_same_column(self):
+    input_lines = ['2.3,abc',
+                   'abc,2.3']
+    column_names = ['str_feature1', 'str_feature2']
+    expected_result = [
+        {'str_feature1': np.array([b'2.3'], dtype=np.object),
+         'str_feature2': np.array([b'abc'], dtype=np.object)},
+        {'str_feature1': np.array([b'abc'], dtype=np.object),
+         'str_feature2': np.array([b'2.3'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_unicode(self):
+    input_lines = [u'1,שקרכלשהו,22.34,text field']
+    column_names = ['int_feature', 'unicode_feature',
+                    'float_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer),
+         'unicode_feature': np.array([u'שקרכלשהו'.encode('utf-8')],
+                                     dtype=np.object),
+         'float_feature': np.array([22.34], dtype=np.float32),
+         'str_feature': np.array([b'text field'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_csv_record_with_quotes(self):
+    input_lines = ['1,"ab,cd,ef"',
+                   '5,"wx,xy,yz"']
+    column_names = ['int_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer),
+         'str_feature': np.array([b'ab,cd,ef'], dtype=np.object)},
+        {'int_feature': np.array([5], dtype=np.integer),
+         'str_feature': np.array([b'wx,xy,yz'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_space_delimiter(self):
+    input_lines = ['1 "ab,cd,ef"',
+                   '5 "wx,xy,yz"']
+    column_names = ['int_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer),
+         'str_feature': np.array([b'ab,cd,ef'], dtype=np.object)},
+        {'int_feature': np.array([5], dtype=np.integer),
+         'str_feature': np.array([b'wx,xy,yz'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names,
+                                      delimiter=' '))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_with_tab_delimiter(self):
+    input_lines = ['1\t"this is a \ttext"',
+                   '5\t']
+    column_names = ['int_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer),
+         'str_feature': np.array([b'this is a \ttext'], dtype=np.object)},
+        {'int_feature': np.array([5], dtype=np.integer),
+         'str_feature': None}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names,
+                                      delimiter='\t'))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_negative_values(self):
+    input_lines = ['-34', '45']
+    column_names = ['feature']
+    expected_result = [
+        {'feature': np.array([-34], dtype=np.int64)},
+        {'feature': np.array([45], dtype=np.int64)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_int64_max(self):
+    input_lines = ['34', str(sys.maxsize)]
+    column_names = ['feature']
+    expected_result = [
+        {'feature': np.array([34], dtype=np.int64)},
+        {'feature': np.array([sys.maxsize], dtype=np.int64)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_large_int_categorical_pos(self):
+    input_lines = ['34', str(sys.maxsize+1)]
+    column_names = ['feature']
+    expected_result = [{
+        'feature': np.array([b'34'], dtype=np.object)
+    },
+                       {
+                           'feature':
+                               np.array([str(sys.maxsize + 1).encode('utf-8')],
+                                        dtype=np.object)
+                       }]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_large_int_categorical_neg(self):
+    input_lines = ['34', str(-(sys.maxsize+2))]
+    column_names = ['feature']
+    expected_result = [{
+        'feature': np.array([b'34'], dtype=np.object)
+    },
+                       {
+                           'feature':
+                               np.array(
+                                   [str(-(sys.maxsize + 2)).encode('utf-8')],
+                                   dtype=np.object)
+                       }]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_large_int_categorical_pos_and_neg(self):
+    input_lines = [str(sys.maxsize+1), str(-(sys.maxsize+2))]
+    column_names = ['feature']
+    expected_result = [{
+        'feature':
+            np.array([str(sys.maxsize + 1).encode('utf-8')], dtype=np.object)
+    },
+                       {
+                           'feature':
+                               np.array(
+                                   [str(-(sys.maxsize + 2)).encode('utf-8')],
+                                   dtype=np.object)
+                       }]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_empty_row(self):
+    input_lines = [',,',
+                   '1,2.0,hello']
+    column_names = ['int_feature', 'float_feature', 'str_feature']
+    expected_result = [
+        {'int_feature': None,
+         'float_feature': None,
+         'str_feature': None},
+        {'int_feature': np.array([1.0], dtype=np.float32),
+         'float_feature': np.array([2.0], dtype=np.float32),
+         'str_feature': np.array([b'hello'], dtype=np.object)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_skip_blank_line(self):
+    input_lines = ['',
+                   '1,2']
+    column_names = ['int_feature1', 'int_feature2']
+    expected_result = [
+        {'int_feature1': np.array([1], dtype=np.integer),
+         'int_feature2': np.array([2], dtype=np.integer)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_consider_blank_line(self):
+    input_lines = ['',
+                   '1,2']
+    column_names = ['float_feature1', 'float_feature2']
+    expected_result = [
+        {'float_feature1': None,
+         'float_feature2': None},
+        {'float_feature1': np.array([1.0], dtype=np.float32),
+         'float_feature2': np.array([2.0], dtype=np.float32)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names,
+                                      skip_blank_lines=False))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_skip_blank_line_single_column(self):
+    input_lines = ['',
+                   '1']
+    column_names = ['int_feature']
+    expected_result = [
+        {'int_feature': np.array([1], dtype=np.integer)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_consider_blank_line_single_column(self):
+    input_lines = ['',
+                   '1']
+    column_names = ['float_feature']
+    expected_result = [
+        {'float_feature': None},
+        {'float_feature': np.array([1.0], dtype=np.float32)}]
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=column_names,
+                                      skip_blank_lines=False))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_empty_csv(self):
+    input_lines = []
+    expected_result = []
+
+    with beam.Pipeline() as p:
+      result = (p | beam.Create(input_lines) |
+                csv_decoder.DecodeCSV(column_names=[]))
+      util.assert_that(
+          result,
+          test_util.make_example_dict_equal_fn(self, expected_result))
+
+  def test_csv_decoder_invalid_row(self):
+    input_lines = ['1,2.0,hello',
+                   '5,12.34']
+    column_names = ['int_feature', 'float_feature', 'str_feature']
+
+    with self.assertRaisesRegexp(
+        ValueError, '.*Columns do not match specified csv headers.*'):
+      with beam.Pipeline() as p:
+        result = (p | beam.Create(input_lines) |
+                  csv_decoder.DecodeCSV(column_names=column_names))
+        util.assert_that(
+            result,
+            test_util.make_example_dict_equal_fn(self, None))
+
+
+if __name__ == '__main__':
+  absltest.main()
