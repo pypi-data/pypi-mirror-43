edb108bc257670980236acecec14081d13ba891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package atsge/0.1.10/atsge-0.1.10.tar.gz

---
+++ packages/atsge/atsge-0.1.10.tar.gz/atsge-0.1.10/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def get_long_description():
+    with open("README.md", 'r') as fh:
+        long_description = fh.read()
+    return long_description
+
+setuptools.setup(
+    name="atsge",
+    version="0.1.10",
+    author="Shane Breeze",
+    author_email="sdb15@ic.ac.uk",
+    scripts=[],
+    description="SGE batch job submission with AlphaTwirl",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shane-breeze/atsge",
+    packages=setuptools.find_packages(),
+    download_url="https://github.com/shane-breeze/atsge/archive/0.1.10.tar.gz",
+    install_requires=["alphatwirl>=0.20.1"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Development Status :: 3 - Alpha",
+    ],
+)
