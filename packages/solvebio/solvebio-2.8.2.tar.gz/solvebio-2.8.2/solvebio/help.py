b28c293d7d3b2eda24e6591557fe0fe84e8dd86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package solvebio/2.8.2/solvebio-2.8.2.tar.gz

---
+++ packages/solvebio/solvebio-2.8.2.tar.gz/solvebio-2.8.2/solvebio/help.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
+
+try:
+    import webbrowser
+except ImportError:
+    webbrowser = None
+
+
+def open_help(path):
+    url = urljoin('https://www.solvebio.com/', path)
+    try:
+        webbrowser.open(url)
+    except webbrowser.Error:
+        print('The SolveBio Python client was unable to open the following '
+              'URL: %s' % url)
