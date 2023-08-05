bd81383fd4ba102f71bc652d0e3f69243af44b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:00 +0000

    Add package screen-assembly/1.0.1/screen_assembly-1.0.1.tar.gz

---
+++ packages/screen-assembly/screen_assembly-1.0.1.tar.gz/screen_assembly-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="screen_assembly",
+    version="1.0.1",
+    license='MIT',
+    author="Liam McIntyre",
+    author_email="shimbalama@gmail.com",
+    description="screens for presence of genes of interest (GOI) in bacterial assemblies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shimbalama/screen_assembly.git",
+    packages=setuptools.find_packages(),
+    scripts = ['bin/screen_assembly3.py'],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='screen assemblies bacteria gene',
+    install_requires=['pandas'],
+    dependency_links=['https://github.com/Cibiv/IQ-TREE.git'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
