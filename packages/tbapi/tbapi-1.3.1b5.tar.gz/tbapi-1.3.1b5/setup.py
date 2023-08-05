46e11a0f9edfede04799a9672198d92e273c1d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:29 +0000

    Add package tbapi/1.3.1b5/tbapi-1.3.1b5.tar.gz

---
+++ packages/tbapi/tbapi-1.3.1b5.tar.gz/tbapi-1.3.1b5/setup.py
@@ -0,0 +1,14 @@
+# A file used to generate upload packages for TBApi.  Not meant for home-use.
+
+from setuptools import setup
+
+setup(name='tbapi',
+      version='1.3.1b5',
+      description='A Python Library for connection to The Blue Alliance API v2 and v3 | Created by Plasma Robotics, Team 2403',
+      url='https://github.com/PlasmaRobotics2403/TBApi',
+      author='Plasma Robotics | FRC Team 2403',
+      author_email='plasma2403@gmail.com',
+      license='MIT',
+      packages=['tbapi', 'tbapi.v2', 'tbapi.v3'],
+      install_requires=['requests','datetime','numpy','SQLiteHelper'],
+      zip_safe=False)
