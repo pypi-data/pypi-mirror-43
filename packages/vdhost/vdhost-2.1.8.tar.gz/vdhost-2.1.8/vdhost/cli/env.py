92c6349791b25623789e5ec8d95b9b301055e477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:56 +0000

    Add package vdhost/2.1.8/vdhost-2.1.8.tar.gz

---
+++ packages/vdhost/vdhost-2.1.8.tar.gz/vdhost-2.1.8/vdhost/cli/env.py
@@ -0,0 +1,18 @@
+import os
+
+"""
+Any environment variables used by vdhost.
+"""
+
+# the base API url
+if 'VDHOST_API_URL' in os.environ:
+    BASE_API_URL = os.environ['VDHOST_API_URL']
+    print('vdhost is using development URL: {}'.format(BASE_API_URL))
+else:
+    BASE_API_URL = 'https://api.vectordash.com'
+    print('vdhost is using production URL: {}'.format(BASE_API_URL))
+
+
+# the Vectordash installation path
+BASE_INSTALL_PATH = '/var/vectordash'
+
