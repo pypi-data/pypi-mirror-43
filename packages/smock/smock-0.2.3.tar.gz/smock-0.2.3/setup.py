b1d6c5314f3c6bc7eee572e82d4046cc08b81e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:50 +0000

    Add package smock/0.2.3/smock-0.2.3.tar.gz

---
+++ packages/smock/smock-0.2.3.tar.gz/smock-0.2.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+  name='smock',
+  packages=['smock'],  # this must be the same as the name above
+  version='0.2.3',
+  description='Serverboards Mock library',
+  author='David Moreno',
+  author_email='dmoreno@serverboards.io',
+  url='https://github.com/serverboards/smock',
+  download_url='https://github.com/serverboards/smock/archive/0.1.tar.gz',
+  keywords=['testing', 'mock', 'mocking'],
+  install_requires=["PyYAML"],
+  classifiers=[],
+  license="Apache2",
+  long_description=README,
+  long_description_content_type="text/markdown",
+)
+
+# to upload to pypi:
+# python3 setup.py sdist
+# twine upload dist/LATEST.tgz
