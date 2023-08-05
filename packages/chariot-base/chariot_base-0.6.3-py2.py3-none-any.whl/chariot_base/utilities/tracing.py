8c7fdff62ad62800c02092a0002ddab7241769a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package chariot-base/0.6.3/chariot_base-0.6.3-py2.py3-none-any.whl

---
+++ packages/chariot-base/chariot_base-0.6.3-py2.py3-none-any.whl/chariot_base/utilities/tracing.py
@@ -0,0 +1,43 @@
+from jaeger_client import Config
+from opentracing import Format
+
+
+class Tracer(object):
+    def __init__(self, options):
+        self.name = options['service']
+        self.host = options['host']
+        self.port = options['port']
+        self.tracer = None
+
+    def init_tracer(self):
+        config = Config(
+            config={
+                'sampler': {
+                    'type': 'const',
+                    'param': 1,
+                },
+                'local_agent': {
+                    'reporting_host': self.host,
+                    'reporting_port': self.port
+                },
+                'logging': False,
+            },            
+            service_name=self.name,
+        )
+
+        self.tracer = config.initialize_tracer()
+
+    def inject(self, span, msg):
+        d = {}
+        self.tracer.inject(span.context, Format.TEXT_MAP, d)
+        msg['uber-trace-id'] = d['uber-trace-id']
+        return msg
+
+    def extract(self, msg):
+        d = {
+            'uber-trace-id': msg['uber-trace-id']
+        }
+        return self.tracer.extract(Format.TEXT_MAP, d)
+
+    def close(self):
+        self.tracer.close()
