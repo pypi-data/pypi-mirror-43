e1ca768ee1be62058a6531db781aaab95ba59a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package mongobase/0.3.1a0/mongobase-0.3.1a0.tar.gz

---
+++ packages/mongobase/mongobase-0.3.1a0.tar.gz/mongobase-0.3.1a0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import pymongo
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="mongobase",
+    version="0.3.1a0",
+    author="Kazuki Otsuka",
+    author_email="otsuka.kazuki@googlemail.com",
+    description="A lightweight Pythonic OR Mapper for MongoDB.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kazukiotsuka/mongobase",
+    license="MIT",
+    keywords=["mongodb", "mongo", "pymongo", "orm", "or mapper"],
+    packages=setuptools.find_packages(),
+    install_requires=["pymongo>=3.6.0"],
+    python_requires='~=3.6',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Software Development :: Libraries"
+    ],
+)
