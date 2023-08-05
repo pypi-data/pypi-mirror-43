8cea70232cc0db0e7c581fddeaeadba3c6795dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package distributions123/0.1/distributions123-0.1.tar.gz

---
+++ packages/distributions123/distributions123-0.1.tar.gz/distributions123-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions123',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions123'],
+      zip_safe=False)
