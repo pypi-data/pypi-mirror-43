1ba5957e4c643fce7b13557a4b6572afa130d470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package asu/0.2.0/asu-0.2.0.tar.gz

---
+++ packages/asu/asu-0.2.0.tar.gz/asu-0.2.0/setup.py
@@ -0,0 +1,21 @@
+import io
+
+from setuptools import find_packages, setup
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="asu",
+    version="0.2.0",
+    url="https://github.com/aparcar/attendedsysupgrade-server",
+    license="",
+    maintainer="Paul Spooren",
+    maintainer_email="mail@aparcar.org",
+    description="Create sysupgrade images for OpenWrt on demand",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask", "pyodbc", "pyyaml"],
+)
