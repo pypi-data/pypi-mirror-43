e7d872204cc1cbd192003b74ed4f9a817011760d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:56 +0000

    Add package SPEMM/1.1.1/SPEMM-1.1.1-py2.py3-none-any.whl

---
+++ packages/SPEMM/SPEMM-1.1.1-py2.py3-none-any.whl/SPEMM/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import main
+
+def mainfir():
+    if __name__ == "__main__":
+        print("daorule main()")
+        main.main(none, none, none , none, none, none, none)
