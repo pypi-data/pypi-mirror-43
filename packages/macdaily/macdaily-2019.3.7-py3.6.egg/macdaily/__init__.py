cff1aa3d8b3980c42d69c3540e9d8c08c5302201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:37 +0000

    Add package macdaily/2019.3.7/macdaily-2019.3.7-py3.6.egg

---
+++ packages/macdaily/macdaily-2019.3.7-py3.6.egg/macdaily/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import importlib
+import platform
+import sys
+
+import tbtrim
+
+from macdaily.util.const.macro import DEVMODE
+from macdaily.util.const.term import purple, red, reset
+from macdaily.util.error import Error, UnsupportedOS
+from macdaily.util.tools.misc import predicate
+
+# set up sys.excepthook
+if DEVMODE:
+    print(f'macdaily: {purple}info{reset}: devmode enabled', file=sys.stderr)
+else:
+    tbtrim.set_trim_rule(predicate, strict=False,
+                         target=(Error, KeyboardInterrupt))
+
+# check platform
+if platform.system() != 'Darwin':
+    raise UnsupportedOS('macdaily: error: script runs only on macOS')
+
+# check dependencies
+if sys.version_info[:2] <= (3, 4):
+    def test_import(module):
+        try:
+            importlib.import_module(module)
+        except ImportError:
+            print(f'macdaily: {red}error{reset}: broken dependency', file=sys.stderr)
+            raise
+    test_import('pathlib2')
+    test_import('subprocess32')
