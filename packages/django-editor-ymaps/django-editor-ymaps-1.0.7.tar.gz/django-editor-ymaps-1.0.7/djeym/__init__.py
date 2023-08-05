d9bd8e223297ae61035ad9151a6add04646ad1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package django-editor-ymaps/1.0.7/django-editor-ymaps-1.0.7.tar.gz

---
+++ packages/django-editor-ymaps/django-editor-ymaps-1.0.7.tar.gz/django-editor-ymaps-1.0.7/djeym/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+"""
+$$$$$___$$$$$$__$$$$$__$$__$$__$$___$$
+$$__$$______$$__$$______$$$$___$$$_$$$
+$$__$$______$$__$$$$_____$$____$$_$_$$
+$$__$$__$$__$$__$$_______$$____$$___$$
+$$$$$____$$$$___$$$$$____$$____$$___$$
+"""
+
+__title__ = 'DjEYM (django-editor-ymaps)'
+__version__ = '1.0.7'
+__author__ = 'genkosta'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2014 genkosta'
+
+default_app_config = 'djeym.apps.DjeymConfig'
