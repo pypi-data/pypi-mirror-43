b97fe8db52984f6895b69b82c8b62765f439b6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:38 +0000

    Add package visualml/0.1b2/visualml-0.1b2.tar.gz

---
+++ packages/visualml/visualml-0.1b2.tar.gz/visualml-0.1b2/visualml/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for VisualML."""
+from visualml import *
+
+__author__ = """Fernando Marcos Wittmann"""
+__email__ = 'fernando.wittmann@gmail.com'
+__version__ = '0.1.0'
