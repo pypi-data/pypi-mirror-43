538b2d1b4b00d1f203c5f5d0888a3fc8ee8edf74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package Flask-Sawtooth/0.2.6/Flask-Sawtooth-0.2.6.tar.gz

---
+++ packages/Flask-Sawtooth/Flask-Sawtooth-0.2.6.tar.gz/Flask-Sawtooth-0.2.6/README.md
@@ -0,0 +1,10 @@
+# Flask-Sawtooth
+A Sawtooth utlity library as a Flask extension.
+
+## Installation
+```
+pip3 install flask-sawtooth
+```
+
+## Usage
+TODO
---
+++ packages/Flask-Sawtooth/Flask-Sawtooth-0.2.6.tar.gz/Flask-Sawtooth-0.2.6/setup.py
@@ -0,0 +1,41 @@
+"""
+Flask-Sawtooth
+-------------
+
+A Sawtooth utlity library as a Flask extension.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Sawtooth',
+    version='0.2.6',
+    license='GPLv2',
+    author='Thomas Veale',
+    author_email='tveale@mymakecents.com',
+    description='A Sawtooth utlity library as a Flask extension.',
+    long_description=__doc__,
+    url='https://github.com/MakeCents-NYC/flask-sawtooth',
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    packages=['flask_sawtooth'],
+    install_requires=[
+        'Flask',
+        'sawtooth_sdk>=1.0, <1.1',
+        'sawtooth_signing>=1.0, <1.1',
+        'requests',
+        'cbor2'
+    ],
+    tests_require=[
+
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
