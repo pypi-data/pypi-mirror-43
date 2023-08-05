99ff10e6b32b0c70159d09a360f14dc8653a490d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:08 +0000

    Add package quickdone/0.0.9/quickdone-0.0.9.tar.gz

---
+++ packages/quickdone/quickdone-0.0.9.tar.gz/quickdone-0.0.9/quickdone/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from quickdone.quickdone import format_path,excel_to_csv
+from quickdone.quickdone import fp,etc
---
+++ packages/quickdone/quickdone-0.0.9.tar.gz/quickdone-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quickdone",
+    version="0.0.9",
+    author="quickdone",
+    author_email="donequick@outlook.com",
+    description="A handy toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donequick/quickdone",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
