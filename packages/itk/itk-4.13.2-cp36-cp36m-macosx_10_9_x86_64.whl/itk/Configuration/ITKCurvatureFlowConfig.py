1a649c0dda029f47a95ceaf1939d12d44edc333f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package itk/4.13.2/itk-4.13.2-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/itk/itk-4.13.2-cp36-cp36m-macosx_10_9_x86_64.whl/itk/Configuration/ITKCurvatureFlowConfig.py
@@ -0,0 +1,9 @@
+depends = ('ITKPyBase', 'ITKImageFilterBase', 'ITKFiniteDifference', )
+templates = (
+  ('CurvatureFlowImageFilter', 'itk::CurvatureFlowImageFilter', 'itkCurvatureFlowImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('CurvatureFlowImageFilter', 'itk::CurvatureFlowImageFilter', 'itkCurvatureFlowImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('MinMaxCurvatureFlowImageFilter', 'itk::MinMaxCurvatureFlowImageFilter', 'itkMinMaxCurvatureFlowImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('MinMaxCurvatureFlowImageFilter', 'itk::MinMaxCurvatureFlowImageFilter', 'itkMinMaxCurvatureFlowImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('BinaryMinMaxCurvatureFlowImageFilter', 'itk::BinaryMinMaxCurvatureFlowImageFilter', 'itkBinaryMinMaxCurvatureFlowImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('BinaryMinMaxCurvatureFlowImageFilter', 'itk::BinaryMinMaxCurvatureFlowImageFilter', 'itkBinaryMinMaxCurvatureFlowImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+)
