6527379b38d55c25d50e3a96b203684954e3f31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:24 +0000

    Add package okra/1.0.dev23/okra-1.0.dev23.tar.gz

---
+++ packages/okra/okra-1.0.dev23.tar.gz/okra-1.0.dev23/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='okra',
+    version='1.0dev23',
+    packages=["okra", "okra/protobuf"],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'google-cloud-storage==1.14.0',
+        'protobuf==3.6.1',
+        'sqlalchemy==1.2.17',
+        'redis==3.2.0',
+    ],
+    scripts=['bin/okra'],
+    setup_requires=['pytest-runner'],
+    test_requires=[
+        'pytest',
+    ],
+    test_suite='pytest',
+    zip_safe=False
+)
