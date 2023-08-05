14869bfe6903aa9b57a871db0f1c429ca4bb4c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package fast-autocomplete/0.3.0/fast-autocomplete-0.3.0.tar.gz

---
+++ packages/fast-autocomplete/fast-autocomplete-0.3.0.tar.gz/fast-autocomplete-0.3.0/fast_autocomplete/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+__version__ = '0.3.0'
+import sys
+pyversion = float(sys.version[:3])
+if pyversion < 3.6:
+    sys.exit('fast-autocomplete requires Python 3.6 or later.')
+
+from fast_autocomplete.dwg import AutoComplete
+from fast_autocomplete.draw import DrawGraphMixin
+from fast_autocomplete.demo import demo
+from fast_autocomplete.normalize import normalize_node_name, remove_any_special_character
