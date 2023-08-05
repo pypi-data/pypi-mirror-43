cf53afad6fb1528cdde9da30dbe17ddd994c64c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:38 +0000

    Add package burst/2.1.7/burst-2.1.7.tar.gz

---
+++ packages/burst/burst-2.1.7.tar.gz/burst-2.1.7/burst/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '2.1.7'
+
+from .burst import Burst
+from .blueprint import Blueprint
+from .share.log import logger
+from .share.utils import safe_call, safe_func
