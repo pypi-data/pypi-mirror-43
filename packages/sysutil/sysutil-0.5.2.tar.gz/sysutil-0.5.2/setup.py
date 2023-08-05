183d0a2dcfb38765b1367bc11d11e7f76120f270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package sysutil/0.5.2/sysutil-0.5.2.tar.gz

---
+++ packages/sysutil/sysutil-0.5.2.tar.gz/sysutil-0.5.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(
+  name = 'sysutil',
+  packages = find_packages(), # this must be the same as the name above
+  version = '0.5.2',  
+  description = 'A lib for common methods used in project',
+  author = 'Vivekanand Pandey',
+  author_email = 'vivekanandpandey98@gmail.com',
+  url = 'https://github.com/vmartian/sysutil.git',
+  download_url = 'https://github.com/vmartian/sysutil/archive/master.zip',
+  keywords = ['common','system','library'], 
+  classifiers = [],
+)
