dd66868dda0e3d993a3d0f650bb2c9f7f69d7785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:28 +0000

    Add package tatt/0.976/tatt-0.976.tar.gz

---
+++ packages/tatt/tatt-0.976.tar.gz/tatt-0.976/tatt/vendors/vendor.py
@@ -0,0 +1,73 @@
+import abc
+import os
+from pathlib import PurePath
+from typing import List, Union
+
+from tatt import exceptions
+
+
+class TranscriberBaseClass:
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, filepath):
+        self._setup()
+        if ' ' in filepath:
+            raise exceptions.FormatError('Please don\'t put any spaces in the filename.')
+        self.filepath = PurePath(filepath)
+        self.basename = str(os.path.basename(self.filepath))
+
+    @property
+    @abc.abstractmethod
+    def no_config_error_message(self):
+        """
+        This must be defined as a class attribute, to be printed when raising
+        such an error.
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def transcript_type(self):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def cost_per_15_seconds(self):
+        """This must be defined as a class attribute."""
+        pass
+
+    @classmethod
+    def _setup(cls):
+        if not cls.check_for_config():
+            raise exceptions.ConfigError(cls.no_config_error_message)
+
+    @staticmethod
+    @abc.abstractmethod
+    def check_for_config() -> bool:
+        pass
+
+    @abc.abstractmethod
+    def transcribe(self) -> str:
+        """
+        This should do any required logic, 
+        then call self._request_transcription.
+        It should return the job_name.
+        """
+        pass
+
+    @abc.abstractmethod
+    def _request_transcription(self) -> str:
+        """Returns the job_name"""
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def retrieve_transcript(transcription_job_name: str) -> Union[str, dict]:
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def get_transcription_jobs() -> List[dict]:
+        pass
+
