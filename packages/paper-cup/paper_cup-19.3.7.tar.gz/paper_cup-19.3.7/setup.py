b9e947e8a9c09a019b8cda19c8282eceebd0e0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:35 +0000

    Add package paper-cup/19.3.7/paper_cup-19.3.7.tar.gz

---
+++ packages/paper-cup/paper_cup-19.3.7.tar.gz/paper_cup-19.3.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+setup(name='paper_cup',
+      version='19.3.7',
+      license='MIT',
+      homepage='https://github.com/LUXEYS/paper_cup',
+      author='Luxeys',
+      author_email='bryan@luxeys.com',
+      description='Micro-service communication system powered with paper cup engine!',
+      packages=['paper_cup'],
+      install_requires=[
+          'boto3',
+      ],
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],)
