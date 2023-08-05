e06f7de44e60239ac7e4f4187fded1b9ab60dddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package MEArec/1.1.6/MEArec-1.1.6.tar.gz

---
+++ packages/MEArec/MEArec-1.1.6.tar.gz/MEArec-1.1.6/MEArec/__init__.py
@@ -0,0 +1,6 @@
+from MEArec.tools import load_templates, load_recordings, save_recording_generator, save_template_generator, \
+    get_default_config
+from MEArec.generators import gen_recordings, gen_templates, RecordingGenerator, TemplateGenerator, SpikeTrainGenerator
+
+
+__version__ = '1.1.6'
