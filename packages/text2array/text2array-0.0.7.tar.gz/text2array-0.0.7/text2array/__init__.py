0e2db70a3c126c64494cd01a70ebcafb1d831e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:45 +0000

    Add package text2array/0.0.7/text2array-0.0.7.tar.gz

---
+++ packages/text2array/text2array-0.0.7.tar.gz/text2array-0.0.7/text2array/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'Sample',
+    'Batch',
+    'Vocab',
+    'BatchIterator',
+    'ShuffleIterator',
+]
+__version__ = '0.0.7'
+
+from .batches import Batch
+from .samples import Sample
+from .iterators import BatchIterator, ShuffleIterator
+from .vocab import Vocab
