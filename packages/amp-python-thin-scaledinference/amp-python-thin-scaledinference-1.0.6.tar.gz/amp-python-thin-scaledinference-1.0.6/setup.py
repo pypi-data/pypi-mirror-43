2c96d7923f9bafbd1b5dc25ed4884596371096e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package amp-python-thin-scaledinference/1.0.6/amp-python-thin-scaledinference-1.0.6.tar.gz

---
+++ packages/amp-python-thin-scaledinference/amp-python-thin-scaledinference-1.0.6.tar.gz/amp-python-thin-scaledinference-1.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amp-python-thin-scaledinference",
+    version="1.0.6",
+    author="Scaled Inference",
+    author_email="dev.opera@scaledinference.com",
+    description="Amp Python Thin Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ScaledInference/amp-python-thin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
