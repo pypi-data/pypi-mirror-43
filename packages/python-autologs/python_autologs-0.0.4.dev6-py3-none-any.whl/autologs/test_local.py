81fb877e838a5d04de93d86879011017ab610b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:51 +0000

    Add package python-autologs/0.0.4.dev6/python_autologs-0.0.4.dev6-py3-none-any.whl

---
+++ packages/python-autologs/python_autologs-0.0.4.dev6-py3-none-any.whl/autologs/test_local.py
@@ -0,0 +1,19 @@
+from .autologs import generate_logs
+import logging
+import pytest
+
+
+@generate_logs()
+def full_log(name, version='1', **kwargs):
+    """
+    Get version by name
+    """
+    return
+
+
+def test1():
+    full_log(name='test1')
+
+
+def test2():
+    full_log()
