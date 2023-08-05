6b763cd283ef4693fe722d134bbd57942533a3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package eazyserver/0.2.1/eazyserver-0.2.1.tar.gz

---
+++ packages/eazyserver/eazyserver-0.2.1.tar.gz/eazyserver-0.2.1/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import os
+import glob
+
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('AUTHORS.rst') as authors_file:
+    authors = authors_file.read()
+
+with open('CONTRIBUTING.rst') as contributing_file:
+    contributing = contributing_file.read()
+
+requirements = ['Click>=6.0', ]
+with open('requirements_dev.txt') as f:
+    requirements = f.read().splitlines()
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Ashutosh Mishra",
+    author_email='ashutoshdtu@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A simple python web framework for creating RESTful and JSON-RPC services",
+    entry_points={
+        'console_scripts': [
+            'eazyserver=eazyserver.cli:cli',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='eazyserver',
+    name='eazyserver',
+    packages=find_packages("src", include=['eazyserver']),
+    package_dir={"": "src"},
+    py_modules=[os.path.splitext(os.path.basename(i))[0] for i in glob.glob("src/*.py")],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ashutoshdtu/eazyserver',
+    version=version,
+    zip_safe=False,
+)
