df0a45d38ccc23066a39c845f169cdf722652996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:04 +0000

    Add package MatricesM/0.9a4.post1/MatricesM-0.9a4.post1.tar.gz

---
+++ packages/MatricesM/MatricesM-0.9a4.post1.tar.gz/MatricesM-0.9a4.post1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MatricesM",
+    version="0.9.alpha4.post1",
+    author="MathStuff members",
+    author_email="business@semihmumcu.com",
+	license="MIT",
+    description="A Python 3 library of matrices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MathStuff/Matrices",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+	python_requires='>=3',
+)
