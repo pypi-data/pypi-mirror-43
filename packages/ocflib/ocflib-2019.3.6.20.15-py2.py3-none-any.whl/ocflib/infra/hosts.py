25ce3c8815847db2cef170a9c4747421242c0218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:12 +0000

    Add package ocflib/2019.3.6.20.15/ocflib-2019.3.6.20.15-py2.py3-none-any.whl

---
+++ packages/ocflib/ocflib-2019.3.6.20.15-py2.py3-none-any.whl/ocflib/infra/hosts.py
@@ -0,0 +1,52 @@
+"""Methods for working with OCF hosts."""
+import ldap3
+
+import ocflib.infra.ldap as ldap
+from ocflib.infra.ldap import OCF_LDAP_HOSTS
+
+HOST_TYPES_WITH_IPV6 = frozenset({
+    'desktop',
+    'dhcp',
+    'server',
+    'staffvm',
+    'switch',
+    'vip',
+    'wifi',
+})
+
+
+def hosts_by_filter(ldap_filter):
+    """Return a list of hosts satisfying the LDAP filter.
+
+    The list returned contains a dictionary of LDAP attributes for each host.
+    """
+
+    with ldap.ldap_ocf() as c:
+        c.search(
+            OCF_LDAP_HOSTS,
+            ldap_filter,
+            attributes=ldap3.ALL_ATTRIBUTES,
+        )
+
+        return [entry['attributes'] for entry in c.response]
+
+
+def hostname_from_domain(fqdn):
+    """Extracts the hostname from its canonical domain.
+
+    >>> hostname_from_domain('death.ocf.berkeley.edu')
+    'death'
+    """
+    return fqdn.split('.')[0]
+
+
+def type_of_host(hostname):
+    """Returns the type of a host as specified in LDAP.
+
+    >>> type_of_host('eruption')
+    'desktop'
+    >>> type_of_host('supernova')
+    'server'
+    """
+    hosts = hosts_by_filter('(cn={})'.format(hostname))
+    return hosts[0]['type'] if hosts else None
