18c680bb18c0243f94af0a22071735833f6f08ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package energy-demand/0.8.9/energy_demand-0.8.9.tar.gz

---
+++ packages/energy-demand/energy_demand-0.8.9.tar.gz/energy_demand-0.8.9/tests/travis_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+#
+# This script is taken from Scikit-Learn (http://scikit-learn.org/)
+#
+# THIS SCRIPT IS SUPPOSED TO BE AN EXAMPLE. MODIFY IT ACCORDING TO YOUR NEEDS!
+
+set -e
+
+if [[ "$DISTRIB" == "conda" ]]; then
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
+
+    # Use the miniconda installer for faster download / install of conda
+    # itself
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+        -O miniconda.sh
+    chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+    export PATH=$HOME/miniconda/bin:$PATH
+
+    conda update --yes conda -c conda-forge
+    conda config --add channels conda-forge
+
+    # Configure the conda environment using consistent .environment.yml
+    conda env create -f .environment.yml -n testenv
+    source activate testenv
+
+    if [[ "$PYTHON_VERSION" == "3.5" ]]; then
+        # Pin libgcc as possible root cause of fiona/shapely shared library import errors
+        conda install --yes libgcc-ng==7.2.0
+    fi
+fi
+
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install coverage coveralls
+fi
+
+# Pip install by default
+pip install -r requirements.txt
+python setup.py develop
