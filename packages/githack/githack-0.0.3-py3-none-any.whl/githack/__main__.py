4d4154302600444a253ce167364a583f842597ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:49 +0000

    Add package githack/0.0.3/githack-0.0.3-py3-none-any.whl

---
+++ packages/githack/githack-0.0.3-py3-none-any.whl/githack/__main__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import logging
+
+try:
+    from scanner import Scanner
+except ModuleNotFoundError:
+    from .scanner import Scanner
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description='.git/ leakage exploit',
+        epilog='OwenChia <https://github.com/OwenChia/githack>')
+    parser.add_argument('URI', type=str, help="target uri to exploit (eg. http://example.com/.git)")
+    parser.add_argument('-o', '--output', default='site', type=str,
+                        help="output dir, all the file will download to this directory")
+    parser.add_argument('--level', type=str,
+                        choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        default='INFO', help="log level (default: INFO)")
+
+    args = parser.parse_args()
+    _level = getattr(logging, args.level)
+    logging.basicConfig(level=_level)
+    return args
+
+
+def main():
+    args = parse_args()
+    scanner = Scanner(args.URI, args.output)
+    scanner.crawl()
+    scanner.restore()
+
+
+if __name__ == '__main__':
+    main()
