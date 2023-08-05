0462b2721f77bcdf6780d5e8393bf36248e5c178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:14 +0000

    Add package getl/0.3/getl-0.3.tar.gz

---
+++ packages/getl/getl-0.3.tar.gz/getl-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='getl',
+  version='0.3',
+  description='',
+  long_description=open('README.rst').read(),
+  url='https://github.com/gregology/getl',
+  author='Greg Clarke',
+  author_email='greg@gho.st',
+  license='MIT',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python'
+  ],
+  python_requires='>=3',
+  keywords='google, takeout, data',
+  packages=find_packages(),
+  package_data={
+    'getl': []
+  }
+)
