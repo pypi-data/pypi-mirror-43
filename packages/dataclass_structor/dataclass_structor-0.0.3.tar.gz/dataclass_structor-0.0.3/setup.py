6a5d3d0cd9084b808f0ddf88c6ca071a54e5b254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:16 +0000

    Add package dataclass_structor/0.0.3/dataclass_structor-0.0.3.tar.gz

---
+++ packages/dataclass_structor/dataclass_structor-0.0.3.tar.gz/dataclass_structor-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from _version import VERSION
+
+with open('README.md') as readme:
+    README = readme.read()
+
+setup(
+    name='dataclass_structor',
+    version=VERSION,
+    description='A type aware structor/destructor for python value objects.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hockeybuggy/dataclass_structor',
+    author='Douglas Anderson',
+    author_email='hockeybuggy@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    py_modules=['dataclasses']
+)
