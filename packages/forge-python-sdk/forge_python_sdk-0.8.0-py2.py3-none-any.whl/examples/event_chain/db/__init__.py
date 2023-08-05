b9886d7561326418c8649f6fe5f96fee0ee65837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package forge-python-sdk/0.8.0/forge_python_sdk-0.8.0-py2.py3-none-any.whl

---
+++ packages/forge-python-sdk/forge_python_sdk-0.8.0-py2.py3-none-any.whl/examples/event_chain/db/__init__.py
@@ -0,0 +1,12 @@
+from event_chain.config import config
+import logging
+from event_chain.db import utils
+logger = logging.getLogger('ec-db-init')
+
+if __name__ == '__main__':
+    db = config.db_path
+    logger.info('Connecting to db {}'.format(db))
+    conn = utils.create_connection()
+    utils.init_db(conn)
+    conn.close()
+
---
+++ packages/forge-python-sdk/forge_python_sdk-0.8.0-py2.py3-none-any.whl/examples/event_chain/server/__init__.py
@@ -0,0 +1,5 @@
+from event_chain.server import server
+
+
+if __name__ == "__main__":
+    server.forgeSdk.server.start()
