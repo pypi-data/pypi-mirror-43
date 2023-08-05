58513df1855ad10e153669e2bb8e88230616bd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package coreir/1.0.2/coreir-1.0.2.tar.gz

---
+++ packages/coreir/coreir-1.0.2.tar.gz/coreir-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='coreir',
+    version='1.0.2',
+    description='Python bindings for CoreIR',
+    packages=["coreir"],
+    license='BSD License',
+    url='https://github.com/leonardt/pycoreir',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["bit_vector >= 0.39a, <=0.42a0"]
+)
