cc462e52060898e9d60b7482dba4b392c9b0b166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:21 +0000

    Add package fyne/0.3.0/fyne-0.3.0.tar.gz

---
+++ packages/fyne/fyne-0.3.0.tar.gz/fyne-0.3.0/src/fyne/__init__.py
@@ -0,0 +1,10 @@
+"""The Python module for option pricing with affine processes
+
+Please see below the documentation for the implemented affine modules.
+
+* :mod:`fyne.blackscholes`
+* :mod:`fyne.heston`
+
+"""
+
+__version__ = '0.3.0'
---
+++ packages/fyne/fyne-0.3.0.tar.gz/fyne-0.3.0/src/fyne/common.py
@@ -0,0 +1,29 @@
+import cmath
+
+from numba import njit
+
+
+def _assert_no_arbitrage(underlying_price, option_price, strike):
+    no_arb_low_bound = max(0., underlying_price - strike)
+    no_arb_upper_bound = underlying_price
+
+    if option_price <= no_arb_low_bound:
+        raise ValueError("Warning: Option price below no-arbitrage bounds")
+    elif option_price >= no_arb_upper_bound:
+        raise ValueError("Warning: Option price above no-arbitrage bounds")
+
+
+@njit
+def _lipton_integrand(u, k, v, psi_1, psi_2):
+    return cmath.exp((0.5 - u*1j)*k + psi_1 + psi_2*v).real/(u**2 + 0.25)
+
+
+@njit
+def _delta_integrand(u, k, v, psi_1, psi_2):
+    return (cmath.exp(-u*k*1j + psi_1 + v*psi_2)/(u*1j)).real
+
+
+@njit
+def _vega_integrand(u, k, v, psi_1, psi_2):
+    return (psi_2*cmath.exp((0.5 - u*1j)*k + psi_1 + psi_2*v)
+            ).real/(u**2 + 0.25)
