803fc3f0a1b5aecd7c132062b420f783891aadd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package twitter-fire-scraper/0.0.7/twitter-fire-scraper-0.0.7.tar.gz

---
+++ packages/twitter-fire-scraper/twitter-fire-scraper-0.0.7.tar.gz/twitter-fire-scraper-0.0.7/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+
+with open("../README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="twitter-fire-scraper",
+    version="0.0.7",
+
+    author="Henry Post",
+    author_email="HenryFBP@gmail.com",
+
+    description="A tool to scrape data about fires from Twitter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/raaraa/IPRO497-Analytics-Team/tree/master/coding/twitter-fire-scraper",
+
+    package_data = {
+        b'twitter-fire-scraper': [
+            'data/*.yml',
+            'templates/*.html'
+        ]
+    },
+
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "click",
+        "itsdangerous",
+        "nltk",
+        "oauthlib",
+        "requests",
+        "requests-oauthlib",
+        "six",
+        "textblob",
+        "tweepy",
+        "Flask",
+        "Jinja2",
+        "MarkupSafe",
+        "Werkzeug",
+        "tmdbsimple",
+        "colorama",
+        "pymongo",
+        "typing",
+        "pyyaml",
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
