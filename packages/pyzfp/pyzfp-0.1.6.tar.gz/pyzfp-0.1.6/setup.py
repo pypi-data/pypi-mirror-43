bf928d3f35c3450c9217e10421aa155a3fc38731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:28 +0000

    Add package pyzfp/0.1.6/pyzfp-0.1.6.tar.gz

---
+++ packages/pyzfp/pyzfp-0.1.6.tar.gz/pyzfp-0.1.6/setup.py
@@ -0,0 +1,125 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
+from distutils import log as distutils_logger
+from distutils.errors import DistutilsSetupError
+
+import os, subprocess
+import glob
+import numpy
+import setuptools
+
+class lazy_cythonize(list):
+    def __init__(self, callback):
+        self._list, self.callback = None, callback
+
+    def c_list(self):
+        if self._list is None:
+            self._list = self.callback()
+        return self._list
+
+    def __iter__(self):
+        for e in self.c_list():
+            yield e
+
+    def __getitem__(self, ii):
+        return self.c_list()[ii]
+
+    def __len__(self):
+        return len(self.c_list())
+
+
+def extensions():
+    from Cython.Build import cythonize
+    ext = Extension("pyzfp",
+              sources=["pyzfp.pyx"],
+              include_dirs=['zfp-0.5.3/include',
+                            numpy.get_include()],
+              libraries=["zfp"],  # Unix-like specific,
+              library_dirs=["zfp-0.5.3/lib"],
+              extra_compile_args=['-fopenmp'],
+             extra_link_args=['-fopenmp', '-Wl,-rpath,/usr/local/lib']
+              )
+    return cythonize([ext])
+
+class specialized_build_ext(build_ext, object):
+    """
+    Specialized builder for testlib library
+
+    """
+    special_extension = "pyzfp"
+
+    def build_extension(self, ext):
+
+        if ext.name!=self.special_extension:
+            # Handle unspecial extensions with the parent class' method
+            super(specialized_build_ext, self).build_extension(ext)
+        else:
+            # Handle special extension
+            sources = ext.sources
+            if sources is None or not isinstance(sources, (list, tuple)):
+                raise DistutilsSetupError(
+                       "in 'ext_modules' option (extension '%s'), "
+                       "'sources' must be present and must be "
+                       "a list of source filenames" % ext.name)
+            sources = list(sources)
+
+            if len(sources)>1:
+                sources_path = os.path.commonpath(sources)
+            else:
+                sources_path = os.path.dirname(sources[0])
+            sources_path = os.path.realpath(sources_path)
+            if not sources_path.endswith(os.path.sep):
+                sources_path+= os.path.sep
+
+            if not os.path.exists(sources_path) or not os.path.isdir(sources_path):
+                raise DistutilsSetupError(
+                       "in 'extensions' option (extension '%s'), "
+                       "the supplied 'sources' base dir "
+                       "must exist" % ext.name)
+
+            output_dir = os.path.realpath(os.path.join(sources_path,'..','lib'))
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            output_lib = 'libtestlib.a'
+
+            command = 'make'
+            distutils_logger.info('Will execute the following command in with subprocess.Popen: \n{0}'.format(command))
+
+
+            make_process = subprocess.Popen(command,
+                                            cwd=sources_path,
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE,
+                                            shell=True)
+            stdout, stderr = make_process.communicate()
+            distutils_logger.debug(stdout)
+            #if stderr:
+            #    raise DistutilsSetupError('An ERROR occured while running the '
+            #                              'Makefile for the {0} library. '
+            #                              'Error status: {1}'.format(output_lib, stderr))
+            # After making the library build the c library's python interface with the parent build_extension method
+            super(specialized_build_ext, self).build_extension(ext)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+configuration = {
+    'name': 'pyzfp',
+    'packages': setuptools.find_packages(),
+    'setup_requires': ['cython>=0.17'],
+    'ext_modules': lazy_cythonize(extensions),
+    'version': "0.1.6",
+    'cmdclass': {'build_ext': specialized_build_ext},
+    'description': "A python wrapper for the ZFP compression libary",
+    'long_description': long_description,
+    'url': 'https://github.com/navjotk/pyzfp',
+    'author': "Navjot Kukreja",
+    'author_email': 'navjotk@gmail.com',
+    'license': 'MIT',
+}
+
+
+setup(**configuration)
