8081743fcb9babffd579940e3d5464d54a275a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:18 +0000

    Add package frapi/0.1.0/frapi-0.1.0.tar.gz

---
+++ packages/frapi/frapi-0.1.0.tar.gz/frapi-0.1.0/frapi/frapi_image.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# Frapi image utility, support normalization/whitening
+# BIG CHENG, init 2019/03/07
+# BIG CHENG, bug-fixed: scipy.misc.imread need Pillow installed !!!
+
+from __future__ import print_function
+
+import numpy as np
+from scipy import misc
+
+image_size = 160
+
+def prewhiten(x):
+  mean = np.mean(x)
+  std = np.std(x)
+  std_adj = np.maximum(std, 1.0/np.sqrt(x.size))
+  y = np.multiply(np.subtract(x, mean), 1/std_adj)
+  return y  
+
+"""
+def crop(image, random_crop, image_size):
+  if image.shape[1]>image_size:
+    sz1 = int(image.shape[1]//2)
+    sz2 = int(image_size//2)
+    if random_crop:
+        diff = sz1-sz2
+        (h, v) = (np.random.randint(-diff, diff+1), np.random.randint(-diff, diff+1))
+    else:
+        (h, v) = (0,0)
+    image = image[(sz1-sz2+v):(sz1+sz2+v),(sz1-sz2+h):(sz1+sz2+h),:]
+  return image
+  
+def flip(image, random_flip):
+  if random_flip and np.random.choice([True, False]):
+    image = np.fliplr(image)
+  return image
+"""
+
+def to_rgb(img):
+  #print img
+  w, h = img.shape[0], img.shape[1]
+  ret = np.empty((w, h, 3), dtype=np.uint8)
+  ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img[:, :, 0]
+  #print ret
+  return ret
+
+def load_data(image_paths, do_random_crop, do_random_flip, image_size, do_prewhiten=True):
+
+  nrof_samples = len(image_paths)
+  images = np.zeros((nrof_samples, image_size, image_size, 3))
+  for i in range(nrof_samples):
+    img = misc.imread(image_paths[i])
+    #print img.shape
+    #if img.ndim == 2:
+    #    img = to_rgb(img)
+    if img.shape[2] == 2:
+        img = to_rgb(img) 
+    if do_prewhiten:
+        img = prewhiten(img)
+    #img = crop(img, do_random_crop, image_size)
+    #img = flip(img, do_random_flip)
+
+    
+    if img.shape[2] == 4: ## if image has alpha layer
+        img = img [:, :, :3] 
+    images[i,:,:,:] = img
+
+  return images
+
+
+def file2img(path):
+  images = load_data([path], False, False, image_size)
+  return images[0].astype(np.float32)          
+
+
+def files2imgs(paths):
+  images = load_data(paths, False, False, image_size)
+  return images.astype(np.float32)
+
+
+## naive local test
+if __name__ == "__main__":
+  
+  def utest_img():
+
+    img = file2img("./coco1.png")
+    #print(img)
+    print(img.shape)
+
+  def utest_imgs():
+
+    paths = ["./coco1.png"]
+    print (paths[0])
+
+    imgs = files2imgs(paths)
+    #print(imgs)
+    print(imgs.shape)
+
+  utest_img()
+  utest_imgs()
+
+
+
