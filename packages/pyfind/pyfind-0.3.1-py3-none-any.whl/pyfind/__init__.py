e0d15e31538b78bb52fc67a61e946f77469a23ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package pyfind/0.3.1/pyfind-0.3.1-py3-none-any.whl

---
+++ packages/pyfind/pyfind-0.3.1-py3-none-any.whl/pyfind/__init__.py
@@ -0,0 +1,17 @@
+# __title__ = 'pyfind'
+# __author__ = 'Alexander Angelidis'
+# __license__ = 'GNU AFFERO GENERAL PUBLIC LICENSE'
+# __copyright__ = 'Copyright 2019 Alexander Angelidis'
+# __ver_major__ = 0
+# __ver_minor__ = 2
+# __ver_patch__ = 5
+# __ver_sub__ = ''
+# __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
+#                               __ver_patch__, __ver_sub__)
+
+
+class pyfind(object):
+    def __init__(self):
+        pass
+    #Imported methods
+    from ._find import find
