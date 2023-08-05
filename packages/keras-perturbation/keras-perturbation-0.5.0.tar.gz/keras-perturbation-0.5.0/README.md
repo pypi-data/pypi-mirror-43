467717a0fd80ddaff1106a00d90e03d8835cfd67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:16 +0000

    Add package keras-perturbation/0.5.0/keras-perturbation-0.5.0.tar.gz

---
+++ packages/keras-perturbation/keras-perturbation-0.5.0.tar.gz/keras-perturbation-0.5.0/README.md
@@ -0,0 +1,17 @@
+# Keras Perturbation
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-perturbation.svg)](https://travis-ci.org/CyberZHG/keras-perturbation)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-perturbation/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-perturbation)
+[![PyPI](https://img.shields.io/pypi/pyversions/keras-perturbation.svg)](https://pypi.org/project/keras-perturbation/)
+
+A demonstration of perturbation of data.
+
+## Install
+
+```bash
+pip install keras-perturbation
+```
+
+## Usage
+
+See [demo](https://github.com/CyberZHG/keras-perturbation/tree/master/demo).
---
+++ packages/keras-perturbation/keras-perturbation-0.5.0.tar.gz/keras-perturbation-0.5.0/keras_perturbation/perturbation.py
@@ -0,0 +1,54 @@
+import keras
+import keras.backend as K
+from .constraint import Clip
+
+
+class Perturbation(keras.layers.Layer):
+
+    def __init__(self,
+                 max_variable_shape,
+                 initializer='zeros',
+                 constraint=Clip(min_value=-1e5, max_value=1e5),
+                 **kwargs):
+        self.variable_len = len(max_variable_shape)
+        self.max_variable_shape = list(max_variable_shape)
+        self.initializer = keras.initializers.get(initializer)
+        self.constraint = keras.constraints.get(constraint)
+        self.supports_masking = True
+        self.perturbation = None
+        super(Perturbation, self).__init__(**kwargs)
+
+    def get_config(self):
+        config = {
+            'max_variable_shape': self.max_variable_shape,
+            'initializer': keras.initializers.serialize(self.initializer),
+            'constraint': keras.constraints.serialize(self.constraint),
+        }
+        base_config = super(Perturbation, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def build(self, input_shape):
+        self.perturbation = self.add_weight(shape=tuple(self.max_variable_shape) + input_shape[self.variable_len:],
+                                            name='{}_W'.format(self.name),
+                                            initializer=self.initializer,
+                                            constraint=self.constraint,
+                                            dtype=K.floatx())
+        super(Perturbation, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        current_shape = K.shape(inputs)[:self.variable_len]
+        return inputs + self.get_perturbation(current_shape)
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
+
+    def compute_output_shape(self, input_shape):
+        return tuple([None] * self.variable_len) + input_shape[self.variable_len:]
+
+    def get_perturbation(self, shape):
+        key = tuple(slice(None, shape[i], None) for i in range(self.variable_len))
+        return self.perturbation[key]
+
+    def get_perturbation_values(self, shape):
+        key = tuple(slice(None, x, None) for x in shape)
+        return self.perturbation[key].eval(session=K.get_session())
