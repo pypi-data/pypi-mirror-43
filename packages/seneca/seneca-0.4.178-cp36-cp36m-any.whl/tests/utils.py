3dfb808a15831440c8d25830e2e0044f23fcac57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:22 +0000

    Add package seneca/0.4.178/seneca-0.4.178-cp36-cp36m-any.whl

---
+++ packages/seneca/seneca-0.4.178-cp36-cp36m-any.whl/tests/utils.py
@@ -0,0 +1,32 @@
+import sys, redis
+from contextlib import contextmanager
+from io import StringIO
+from unittest import TestCase
+from seneca.engine.interface import SenecaInterface
+from seneca.constants.config import get_redis_port, MASTER_DB, DB_OFFSET, get_redis_password
+
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return(recur_fibo(n-1) + recur_fibo(n-2))
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+class TestInterface(TestCase):
+
+    def setUp(self):
+        self.si = SenecaInterface(False, port=get_redis_port(), password=get_redis_password())
+        self.si.r.flushall()
+        print('\n{}'.format('#' * 128))
+        print(self.id)
+        print('{}\n'.format('#' * 128))
