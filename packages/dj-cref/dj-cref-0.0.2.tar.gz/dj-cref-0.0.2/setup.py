2152d101fa4af3757e1e8da5f3ce690d767f9a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package dj-cref/0.0.2/dj-cref-0.0.2.tar.gz

---
+++ packages/dj-cref/dj-cref-0.0.2.tar.gz/dj-cref-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="dj-cref",
+    version="0.0.2",
+    description="The django generic view output unit",
+    license="MIT",
+    author="Kazuma Murata",
+    author_email="mrt014kzm@gmail.com",
+    packages=find_packages(exclude=['venv/']),
+    install_requires=install_requirements,
+    keywords="django,generic,output",
+    url="https://github.com/Canon11/dj-cref",
+    entry_points={
+        "console_scripts": [
+            "dj-cref=command.main:main",
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ]
+)
