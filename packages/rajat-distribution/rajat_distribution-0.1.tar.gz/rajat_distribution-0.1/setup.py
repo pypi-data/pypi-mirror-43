7cfaaa84adea55a4a168cc3bd4e0f8840dacd78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:41 +0000

    Add package rajat-distribution/0.1/rajat_distribution-0.1.tar.gz

---
+++ packages/rajat-distribution/rajat_distribution-0.1.tar.gz/rajat_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rajat_distribution',
+      version='0.1',
+      description='distributions',
+      packages=['rajat_distribution'],
+      zip_safe=False)
