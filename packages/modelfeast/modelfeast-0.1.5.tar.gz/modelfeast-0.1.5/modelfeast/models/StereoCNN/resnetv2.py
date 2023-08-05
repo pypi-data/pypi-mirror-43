e0798e9881ce3a31c0457bf539ca73a7a2fb111b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:03 +0000

    Add package modelfeast/0.1.5/modelfeast-0.1.5.tar.gz

---
+++ packages/modelfeast/modelfeast-0.1.5.tar.gz/modelfeast-0.1.5/modelfeast/models/StereoCNN/resnetv2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: zcy
+# @Date:   2019-02-14 19:29:27
+# @Last Modified by:   zcy
+# @Last Modified time: 2019-03-05 17:56:49
+
+
+from .Resnetv2_module import *
+
+
+__all__ = [
+    'resnet18v2_3d', 'resnet34v2_3d', 'resnet50v2_3d', 'resnet101v2_3d',
+    'resnet152v2_3d', 'resnet200v2_3d'
+]
+
+
+def resnet18v2_3d(**kwargs):
+    """Constructs a ResNet-18 model.
+    """
+    model = PreActivationResNet(PreActivationBasicBlock, [2, 2, 2, 2], **kwargs)
+    return model
+
+
+def resnet34v2_3d(**kwargs):
+    """Constructs a ResNet-34 model.
+    """
+    model = PreActivationResNet(PreActivationBasicBlock, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def resnet50v2_3d(**kwargs):
+    """Constructs a ResNet-50 model.
+    """
+    model = PreActivationResNet(PreActivationBottleneck, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def resnet101v2_3d(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = PreActivationResNet(PreActivationBottleneck, [3, 4, 23, 3],
+                                **kwargs)
+    return model
+
+
+def resnet152v2_3d(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = PreActivationResNet(PreActivationBottleneck, [3, 8, 36, 3],
+                                **kwargs)
+    return model
+
+
+def resnet200v2_3d(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = PreActivationResNet(PreActivationBottleneck, [3, 24, 36, 3],
+                                **kwargs)
+    return model
+
---
+++ packages/modelfeast/modelfeast-0.1.5.tar.gz/modelfeast-0.1.5/modelfeast/models/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: zcy
+# @Date:   2019-01-31 21:15:37
+# @Last Modified by:   zcy
+# @Last Modified time: 2019-03-05 17:58:30
+
+''' image classifiers models '''
+from .classifiers.densenet import *
+from .classifiers.resnet import *
+from .classifiers.xception import *
+from .classifiers.inception import *
+from .classifiers.vgg import *
+from .classifiers.squeezenet import *
+from .classifiers.inceptionresnetv2 import *
+from .classifiers.resnext import *
+
+from .StereoCNN.resnetv2 import *
+from .StereoCNN.resnext import *
+from .StereoCNN.densenet import *
+from .StereoCNN.resnet import *
+from .StereoCNN.wideresnet import *
+from .StereoCNN.i3d import *
+
+__all__ = [ 
+            'vgg11',  'vgg13', 'vgg16', 'vgg19' ,
+            'squeezenet', 'squeezenet1_0', 'squeezenet1_1',
+            'inception', 'inceptionv3', 
+            'inceptionresnetv2',
+            'xception',
+            'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
+            'resnext', 'resnext101_32x4d', 'resnext101_64x4d',
+            'densenet121', 'densenet169', 'densenet201', 'densenet161',
+
+
+            'resnet18v2_3d', 'resnet34v2_3d', 'resnet50v2_3d', 
+            'resnet101v2_3d', 'resnet152v2_3d', 'resnet200v2_3d',
+
+            'resnext50_3d', 'resnext101_3d', 'resnext152_3d',
+
+            'densenet121_3d', 'densenet169_3d', 'densenet201_3d', 'densenet264_3d',
+
+            'resnet10_3d', 'resnet18_3d', 'resnet34_3d', 
+            'resnet101_3d', 'resnet152_3d', 'resnet200_3d', 
+
+            'wideresnet50_3d',
+
+            'i3d50', 'i3d101', 'i3d152',           
+            ]
