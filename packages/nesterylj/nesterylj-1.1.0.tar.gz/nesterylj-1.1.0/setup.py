f778639313ac84d44325f78025470e85205847e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package nesterylj/1.1.0/nesterylj-1.1.0.tar.gz

---
+++ packages/nesterylj/nesterylj-1.1.0.tar.gz/nesterylj-1.1.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name = 'nesterylj',version = '1.1.0',py_modules = ['nester'],author = 'ylj',author_email = 'hfpython@headfirstlabs.com',url = 'http://www.headfirstlabs.com',description = 'A simple printer of nested lists')
