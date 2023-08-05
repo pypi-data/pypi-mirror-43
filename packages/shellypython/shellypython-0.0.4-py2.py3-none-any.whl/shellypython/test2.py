4045a1fcf66880ff5adac47dc99707e9ff3b72e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:54 +0000

    Add package shellypython/0.0.4/shellypython-0.0.4-py2.py3-none-any.whl

---
+++ packages/shellypython/shellypython-0.0.4-py2.py3-none-any.whl/shellypython/test2.py
@@ -0,0 +1,21 @@
+from .shelly import (Shelly, Wifi_sta)
+import logging
+import json
+from .helpers import (Call_shelly_api, Get_item_safe, Rssi_to_percentage)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+print(Rssi_to_percentage(None))
+print(Rssi_to_percentage(-100))
+print(Rssi_to_percentage(-70))
+x = Shelly("192.168.1.82").update_data()
+print(len(x.rollers))
+print(x.rollers[0].__dict__)
+x = Shelly("192.168.1.81").update_data()
+print(len(x.relays))
+print(x.relays[0].__dict__)
+# print(x)
+# print(type(x))
+# x.update_data()
+# print (x.__dict__)
