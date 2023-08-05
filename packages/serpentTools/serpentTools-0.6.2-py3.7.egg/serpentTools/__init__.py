d5e42bc8c0b2ad21a9c23366bc473d856ab1f068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:20 +0000

    Add package serpentTools/0.6.2/serpentTools-0.6.2-py3.7.egg

---
+++ packages/serpentTools/serpentTools-0.6.2-py3.7.egg/serpentTools/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from serpentTools.parsers import *
+from serpentTools.data import *
+from serpentTools.samplers import *
+from serpentTools.seed import *
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
