f23bb61b6bafc7220e9761fbab9cb0127b649929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:43 +0000

    Add package git2net/0.0.1/git2net-0.0.1-py3-none-any.whl

---
+++ packages/git2net/git2net-0.0.1-py3-none-any.whl/git2net/__init__.py
@@ -0,0 +1,10 @@
+"""
+An OpenSource Python package for the extraction of fine-grained
+and time-stamped co-editing networks from git repositories.
+"""
+
+__author__ = "Christoph Gote"
+__email__ = "cgote@ethz.ch"
+__version__ = "0.0.1"
+
+from .git2net import hello_world
