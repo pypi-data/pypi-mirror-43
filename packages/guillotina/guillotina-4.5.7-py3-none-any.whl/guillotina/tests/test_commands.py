5ed530b0d197b29b340ea1330e503b63919a9aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:41 +0000

    Add package guillotina/4.5.7/guillotina-4.5.7-py3-none-any.whl

---
+++ packages/guillotina/guillotina-4.5.7-py3-none-any.whl/guillotina/tests/test_commands.py
@@ -0,0 +1,72 @@
+import json
+import os
+from tempfile import mkstemp
+
+import pytest
+from guillotina import testing
+from guillotina.commands import get_settings
+from guillotina.commands.run import RunCommand
+
+
+DATABASE = os.environ.get('DATABASE', 'DUMMY')
+DB_SCHEMA = os.environ.get('DB_SCHEMA', 'public')
+
+
+def test_run_command(command_arguments):
+    _, filepath = mkstemp(suffix='.py')
+    _, filepath2 = mkstemp()
+    with open(filepath, 'w') as fi:
+        fi.write(f'''
+async def run(app):
+    with open("{filepath2}", 'w') as fi:
+        fi.write("foobar")
+''')
+
+    command_arguments.script = filepath
+    command = RunCommand(command_arguments)
+    settings = testing.get_settings()
+    command.run_command(settings=settings)
+    with open(filepath2) as fi:
+        assert fi.read() == 'foobar'
+
+
+@pytest.mark.skipif(DATABASE != 'postgres', reason="Cockroach does not have cascade support")
+@pytest.mark.skipif(DB_SCHEMA != 'public', reason="Fixture 'container_command' does not support 'db_schema'")
+def test_run_command_with_container(command_arguments, container_command):
+    _, filepath = mkstemp(suffix='.py')
+    _, filepath2 = mkstemp()
+    with open(filepath, 'w') as fi:
+        fi.write(f'''
+async def run(container):
+    with open("{filepath2}", 'w') as fi:
+        fi.write('foobar')
+''')
+
+    command_arguments.script = filepath
+    command = RunCommand(command_arguments)
+    command.run_command(settings=container_command['settings'])
+    with open(filepath2) as fi:
+        assert fi.read() == 'foobar'
+
+
+def test_get_settings():
+    settings = get_settings('doesnotexist.json', [
+        'foobar=foobar',
+        'foo.bar=foobar'
+    ])
+    assert settings['foobar'] == 'foobar'
+    assert settings['foo']['bar'] == 'foobar'
+
+
+def test_get_settings_with_environment_variables():
+    os.environ.update({
+        'G_foobar': 'foobar',
+        'G_foo__bar': 'foobar',
+        'G_foo__bar1__bar2': json.dumps({
+            'foo': 'bar'
+        })
+    })
+    settings = get_settings('doesnotexist.json')
+    assert settings['foobar'] == 'foobar'
+    assert settings['foo']['bar'] == 'foobar'
+    assert settings['foo']['bar1']['bar2'] == {'foo': 'bar'}
