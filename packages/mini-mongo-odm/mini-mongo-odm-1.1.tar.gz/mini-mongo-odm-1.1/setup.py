1e44eb638c1afde1c6e0d843ff6486fc22f70715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package mini-mongo-odm/1.1/mini-mongo-odm-1.1.tar.gz

---
+++ packages/mini-mongo-odm/mini-mongo-odm-1.1.tar.gz/mini-mongo-odm-1.1/setup.py
@@ -0,0 +1,17 @@
+# encoding=utf8
+
+from setuptools import setup
+
+setup(name='mini-mongo-odm',
+      version='1.1',
+      description='Easy MongoDB Object Document Mapper for Flask',
+      url='https://github.com/solo-framework/mini-mongo-odm',
+      author='Andrey Filippov',
+      author_email='a.fiwork@gmail.com',
+      license='MIT',
+      packages=['mini_mongo_odm'],
+      install_requires=[
+          'mongoengine',
+          'flask-mongoengine'
+      ],
+      zip_safe=False)
