fce80daed03d9f73ebdb9912f9a8325759d40a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:24 +0000

    Add package dnsdb/0.2.1/dnsdb-0.2.1.tar.gz

---
+++ packages/dnsdb/dnsdb-0.2.1.tar.gz/dnsdb-0.2.1/dnsdb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'dnsdb'
+__version__ = '0.2.0'
+__author__ = 'Gabriel Iovino'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (C) 2019 Gabriel Iovino'
+
+from .dnsdb import Dnsdb
