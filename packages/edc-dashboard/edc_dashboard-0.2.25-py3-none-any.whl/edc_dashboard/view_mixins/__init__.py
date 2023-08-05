da91863513d4f79d81a32c7144b424522e667083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package edc-dashboard/0.2.25/edc_dashboard-0.2.25-py3-none-any.whl

---
+++ packages/edc-dashboard/edc_dashboard-0.2.25-py3-none-any.whl/edc_dashboard/view_mixins/__init__.py
@@ -0,0 +1,10 @@
+from .administration_view_mixin import AdministrationViewMixin
+from .listboard import (
+    QueryStringViewMixin,
+    ListboardFilterViewMixin,
+    SearchFormViewMixin,
+    SearchListboardMixin,
+    SiteQuerysetViewMixin,
+)
+from .template_request_context_mixin import TemplateRequestContextMixin
+from .url_request_context_mixin import UrlRequestContextMixin
