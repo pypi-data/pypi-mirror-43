f5a192b10b7b4f03bd401275773d1afd8b96b8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package argus-toolbelt/1.7.2/argus_toolbelt-1.7.2-py3-none-any.whl

---
+++ packages/argus-toolbelt/argus_toolbelt-1.7.2-py3-none-any.whl/argus_cli/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "1.7.2"
+
+from .plugin import register_command, register_provider, run
---
+++ packages/argus-toolbelt/argus_toolbelt-1.7.2-py3-none-any.whl/argus_plugins/api_provider.py
@@ -0,0 +1,14 @@
+from api_generator import load
+from argus_cli import register_provider
+from argus_cli.settings import settings
+
+
+# TODO: Debug isn't used, but rather look at settings._get_debug_mode.
+#       This should be fixed.
+@register_provider()
+def argus_api(apikey: str = None, debug: bool = False) -> None:
+    """Argus CLI provider"""
+    if apikey:
+        settings["api"]["api_key"] = apikey
+
+    load(settings["api"]["api_url"], settings["api"]["definitions"])
