8648271cb6958ce9289c716eb2d5dd0118adeb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package sz-calculator/0.0.2/sz-calculator-0.0.2.tar.gz

---
+++ packages/sz-calculator/sz-calculator-0.0.2.tar.gz/sz-calculator-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Demo Calculator
+
+The purpose of this module is to demo how to build and publish python package
---
+++ packages/sz-calculator/sz-calculator-0.0.2.tar.gz/sz-calculator-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md"), "r") as f:
+    README = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="sz-calculator",
+    version="0.0.2",
+    description="Cool Calculator",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stonezhong/pythondemos/tree/master/sz_calculator",
+    author="Stone Zhong",
+    author_email="stonezhong@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["sz_calculator"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "sz-calculator=reader.__main__:main",
+        ]
+    },
+)
