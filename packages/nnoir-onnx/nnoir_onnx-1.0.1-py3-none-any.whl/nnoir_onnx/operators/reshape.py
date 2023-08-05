2e5ae79fd788f10c89ce3494a5c98a709d88ccd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package nnoir-onnx/1.0.1/nnoir_onnx-1.0.1-py3-none-any.whl

---
+++ packages/nnoir-onnx/nnoir_onnx-1.0.1-py3-none-any.whl/nnoir_onnx/operators/reshape.py
@@ -0,0 +1,28 @@
+from nnoir.functions import *
+from .utils import *
+
+
+class OpReshape(Op):
+
+    def __init__(self, node):
+        super(OpReshape, self).__init__(node)
+
+    def get_dummy_output(self, env):
+        [x, shape] = self.node.input
+        x = env[x]
+        shape = env[shape]
+        for i in shape:
+            if shape[i] == 0:
+                shape[i] = x.shape[i]
+        return x.reshape(shape)
+
+    def to_function(self, env, constants):
+        [x, _] = self.node.input
+        [y] = self.node.output
+        return [
+            Reshape(
+                [x],
+                list(self.node.output),
+                shape=list(map(int, env[y].shape))
+            )
+        ]
