9f8a729da0eea1ef74441e2ab26a1b8f55a02f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package rucio-clients/1.19.3/rucio-clients-1.19.3.tar.gz

---
+++ packages/rucio-clients/rucio-clients-1.19.3.tar.gz/rucio-clients-1.19.3/lib/rucio/tests/test_counter.py
@@ -0,0 +1,121 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013
+# - Martin Barisits, <martin.barisits@cern.ch>, 2014
+# - Hannes Hansen, <hannes.jakob.hansen@cern.ch>, 2018
+# - Cedric Serfon,<cedric.serfon@cern.ch>, 2019
+
+from nose.tools import assert_equal
+
+from rucio.core import account_counter, rse_counter
+from rucio.core.account import get_usage
+from rucio.core.rse import get_rse
+from rucio.daemons.abacus.rse import rse_update
+from rucio.daemons.abacus.account import account_update
+
+
+class TestCoreRSECounter():
+
+    def test_inc_dec_get_counter(self):
+        """ RSE COUNTER (CORE): Increase, decrease and get counter """
+        rse_id = get_rse('MOCK').id
+        rse_update(once=True)
+        rse_counter.del_counter(rse_id=rse_id)
+        rse_counter.add_counter(rse_id=rse_id)
+        cnt = rse_counter.get_counter(rse_id=rse_id)
+        del cnt['updated_at']
+        assert_equal(cnt, {'files': 0, 'bytes': 0})
+
+        count, sum = 0, 0
+        for i in range(10):
+            rse_counter.increase(rse_id=rse_id, files=1, bytes=2.147e+9)
+            rse_update(once=True)
+            count += 1
+            sum += 2.147e+9
+            cnt = rse_counter.get_counter(rse_id=rse_id)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(4):
+            rse_counter.decrease(rse_id=rse_id, files=1, bytes=2.147e+9)
+            rse_update(once=True)
+            count -= 1
+            sum -= 2.147e+9
+            cnt = rse_counter.get_counter(rse_id=rse_id)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(5):
+            rse_counter.increase(rse_id=rse_id, files=1, bytes=2.147e+9)
+            rse_update(once=True)
+            count += 1
+            sum += 2.147e+9
+            cnt = rse_counter.get_counter(rse_id=rse_id)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(8):
+            rse_counter.decrease(rse_id=rse_id, files=1, bytes=2.147e+9)
+            rse_update(once=True)
+            count -= 1
+            sum -= 2.147e+9
+            cnt = rse_counter.get_counter(rse_id=rse_id)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+
+class TestCoreAccountCounter():
+
+    def test_inc_dec_get_counter(self):
+        """ACCOUNT COUNTER (CORE): Increase, decrease and get counter """
+        account_update(once=True)
+        rse_id = get_rse('MOCK').id
+        account = 'jdoe'
+        account_counter.del_counter(rse_id=rse_id, account=account)
+        account_counter.add_counter(rse_id=rse_id, account=account)
+        cnt = get_usage(rse_id=rse_id, account=account)
+        del cnt['updated_at']
+        assert_equal(cnt, {'files': 0, 'bytes': 0})
+
+        count, sum = 0, 0
+        for i in range(10):
+            account_counter.increase(rse_id=rse_id, account=account, files=1, bytes=2.147e+9)
+            account_update(once=True)
+            count += 1
+            sum += 2.147e+9
+            cnt = get_usage(rse_id=rse_id, account=account)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(4):
+            account_counter.decrease(rse_id=rse_id, account=account, files=1, bytes=2.147e+9)
+            account_update(once=True)
+            count -= 1
+            sum -= 2.147e+9
+            cnt = get_usage(rse_id=rse_id, account=account)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(5):
+            account_counter.increase(rse_id=rse_id, account=account, files=1, bytes=2.147e+9)
+            account_update(once=True)
+            count += 1
+            sum += 2.147e+9
+            cnt = get_usage(rse_id=rse_id, account=account)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
+
+        for i in range(8):
+            account_counter.decrease(rse_id=rse_id, account=account, files=1, bytes=2.147e+9)
+            account_update(once=True)
+            count -= 1
+            sum -= 2.147e+9
+            cnt = get_usage(rse_id=rse_id, account=account)
+            del cnt['updated_at']
+            assert_equal(cnt, {'files': count, 'bytes': sum})
