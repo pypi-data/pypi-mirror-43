8bf32b54fd19d96b969b2230738296e666f2875f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:15 +0000

    Add package mastercard-small-business-decision-enhancer/1.0.0/mastercard_small_business_decision_enhancer-1.0.0.tar.gz

---
+++ packages/mastercard-small-business-decision-enhancer/mastercard_small_business_decision_enhancer-1.0.0.tar.gz/mastercard_small_business_decision_enhancer-1.0.0/mastercardsmallbusinessdecisionenhancer/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardsmallbusinessdecisionenhancer.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardsmallbusinessdecisionenhancer.merchantperformance import *
