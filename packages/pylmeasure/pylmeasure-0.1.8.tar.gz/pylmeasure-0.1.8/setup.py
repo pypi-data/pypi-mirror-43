d744bef7a5f70773a8ce2c077448efee996637ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:47 +0000

    Add package pylmeasure/0.1.8/pylmeasure-0.1.8.tar.gz

---
+++ packages/pylmeasure/pylmeasure-0.1.8.tar.gz/pylmeasure-0.1.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylmeasure",
+    version="0.1.8",
+    author="Ajayrama Kumaraswamy, Justas Birgiolas",
+    author_email="justas@asu.edu",
+    description="A Python wrapper for L-Measure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justasb/pylmeasure",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
