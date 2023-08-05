f51a9332eb8d57794be875365ee5b6b84d904389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:17 +0000

    Add package pyVT4002/0.2/pyVT4002-0.2.tar.gz

---
+++ packages/pyVT4002/pyVT4002-0.2.tar.gz/pyVT4002-0.2/VT4002/__init__.py
@@ -0,0 +1,22 @@
+import serial
+
+
+READ_ACTUAL_VAL = "I"
+
+class Connection:
+    def __init__(self, port='/dev/ttyS4', baudrate=9600, chamber_address="00"):
+        self.port=port
+        self.baudrate=baudrate
+        self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+    def serial_read(self):
+        try:
+            return self.ser.readline().decode()
+        except UnicodeDecodeError:
+            print("read line error. (pyVT4002)")
+            return ""
+    def serial_write(self, data):
+        self.ser.write(data.encode('utf-8'))
+
+    def get_temp(self):
+        self.ser.write(("$"+chamber_address+data+READ_ACTUAL_VAL).encode('utf-8'))
+        return self.serial_read()
