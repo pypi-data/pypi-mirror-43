03f3ca72c2642d4ca8ed22b065a4b41b06cf1b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package food/0.0.5/food-0.0.5.tar.gz

---
+++ packages/food/food-0.0.5.tar.gz/food-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='food',
+	version='0.0.5',
+	author='mat',
+	author_email='fake@email.lol',
+	description='yum',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://repl.it/@mat1',
+	packages=setuptools.find_packages(),
+	install_requires='',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
