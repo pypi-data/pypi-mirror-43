9ab40e53d192be799987ab01d184f40eeb95096d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:25 +0000

    Add package py3-dependency-graph/0.0.5/py3_dependency_graph-0.0.5.tar.gz

---
+++ packages/py3-dependency-graph/py3_dependency_graph-0.0.5.tar.gz/py3_dependency_graph-0.0.5/README.md
@@ -0,0 +1,14 @@
+
+# Table of Contents
+
+1.  [What is this tool for?](#org68708aa)
+
+
+
+<a id="org68708aa"></a>
+
+# What is this tool for?
+
+  This is just a simple python static dependency generate tool, It can help you generate the dependency graph
+find circular import.
+
---
+++ packages/py3-dependency-graph/py3_dependency_graph-0.0.5.tar.gz/py3_dependency_graph-0.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+
+setuptools.setup(
+  name="py3_dependency_graph",
+  version="0.0.5",
+  author="Cha Chen",
+  author_email="chencha92111@gmail.com",
+  description="A simple jedi based python3 dependency analysis tool",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/jam-world/py3_dependency_graph",
+  packages=setuptools.find_packages(),
+  scripts=['bin/simple_dependency_graph'],
+  install_requires=[
+    'GitPython', 'jedi', 'networkx'
+  ],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
