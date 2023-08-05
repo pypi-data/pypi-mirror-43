56e6adf284ec52e22a8501c3cd25fc7967bd400a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:17 +0000

    Add package gatilegrid/0.1.12/gatilegrid-0.1.12.tar.gz

---
+++ packages/gatilegrid/gatilegrid-0.1.12.tar.gz/gatilegrid-0.1.12/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# HACK for `nose.collector` to work on python 2.7.3 and earlier
+import multiprocessing
+import os
+from setuptools import setup
+
+
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+
+setup(name='gatilegrid',
+      version='0.1.12',
+      description='Popular tile grids and grids API for web mapping applications',
+      keywords='',
+      author='Loic Gasser',
+      author_email='loicgasser4@gmail.com',
+      license='MIT',
+      url='https://github.com/geoadmin/lib-gatilegrid',
+      packages=['gatilegrid'],
+      package_dir={'gatilegrid': 'gatilegrid'},
+      include_package_data=True,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: GIS',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+      zip_safe=False,
+      long_description=README,
+      test_suite='nose.collector',
+      install_requires=['future'],
+      )
