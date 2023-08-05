9fa039c8eee871f5a65581bfbc612754cfb8b9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:16 +0000

    Add package clearpasspy/1.1.2/clearpasspy-1.1.2.tar.gz

---
+++ packages/clearpasspy/clearpasspy-1.1.2.tar.gz/clearpasspy-1.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clearpasspy",
+    version="1.1.2",
+    author="zemerick1",
+    author_email="zemerick@emerickcc.com",
+    description="ClearPass API Python Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zemerick1/clearpasspy",
+    packages=setuptools.find_packages(),
+    py_modules=['clearpasspy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests==2.21.0',
+    ]
+)
