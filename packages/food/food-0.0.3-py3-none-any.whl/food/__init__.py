4b0028fae3a2428903fec6f204dee98460581ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package food/0.0.3/food-0.0.3-py3-none-any.whl

---
+++ packages/food/food-0.0.3-py3-none-any.whl/food/__init__.py
@@ -0,0 +1,10 @@
+class Cookie():
+	def __init__(self):
+		deliciousness = 9001
+		exists = True
+	def eat(self):
+		self.exists = False
+		self.deliciousness = False
+		print('yum')
+	def __str__(self):
+		return '🍪'
