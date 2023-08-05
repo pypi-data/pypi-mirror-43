2d9b23f3b2f9b5408c1d13090154dfcc25e6f27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:44 +0000

    Add package librecell-lib/0.0.1/librecell-lib-0.0.1.tar.gz

---
+++ packages/librecell-lib/librecell-lib-0.0.1.tar.gz/librecell-lib-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(name='librecell-lib',
+      version='0.0.1',
+      description='CMOS standard cell characterization kit.',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='cmos cell characterization vlsi asic',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+          'Development Status :: 3 - Alpha',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Programming Language :: Python :: 3'
+      ],
+      url='https://codeberg.org/tok/librecell',
+      author='T. Kramer',
+      author_email='dont@spam.me',
+      license='AGPL',
+      entry_points={
+          'console_scripts': [
+              'libertyviz = lclib.liberty.visualize:main_plot_timing',
+              'libertymerge = lclib.liberty.merge:main',
+              'lcsize = lclib.transistor_sizing.width_opt:main',
+              'lctime = lclib.characterization.standalone:main'
+          ]
+      },
+      install_requires=[
+          # 'lclayout-util',
+          'numpy',  # BSD
+          'sympy',  # BSD
+          'matplotlib',
+          'networkx',  # BSD
+          'pyspice',  # GPLv3
+          'scipy',  # BSD
+          'liberty-parser',  # LGPL
+      ],
+      zip_safe=False)
