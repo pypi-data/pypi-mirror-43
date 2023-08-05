b15d36072a1f54cd27185233dc2493a0fdddb3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package cppyy-cling/6.15.2.3/cppyy_cling-6.15.2.3-py2.py3-none-macosx_10_11_x86_64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.15.2.3-py2.py3-none-macosx_10_11_x86_64.whl/cppyy_backend/_rootcling.py
@@ -0,0 +1,16 @@
+import os, sys, subprocess
+
+MYHOME = os.path.dirname(__file__)
+
+def main():
+    os.environ['LD_LIBRARY_PATH'] = os.path.join(MYHOME, 'lib')
+    rootcling = os.path.join(MYHOME, 'bin', 'rootcling')
+    if 'win32' in sys.platform:
+        rootcling += '.exe'
+    if not os.path.exists(rootcling):
+        raise RuntimeError("rootcling not installed in standard location")
+    return subprocess.call([rootcling] + sys.argv[1:])
+
+
+if __name__ == "__main__":
+    sys.exit(main())
