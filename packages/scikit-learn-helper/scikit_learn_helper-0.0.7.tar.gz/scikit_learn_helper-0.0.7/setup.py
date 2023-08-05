ff20edce709338a3c56d591a11b18951d733e401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:10 +0000

    Add package scikit-learn-helper/0.0.7/scikit_learn_helper-0.0.7.tar.gz

---
+++ packages/scikit-learn-helper/scikit_learn_helper-0.0.7.tar.gz/scikit_learn_helper-0.0.7/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="scikit_learn_helper",
+    version="0.0.7",
+    author="Andres Arrieche",
+    author_email="andres.arrieche.7@gmail.com",
+    description="Helper code to make easier working with sklearn",
+    packages=["."],
+    download_url="https://pypi.org/project/scikit-learn-helper/#files",
+    install_requires=["scikit-learn"],
+    license="Apache",
+    classifiers=["Programming Language :: Python", "License :: OSI Approved :: Apache Software License"]
+)
