bef66de4e5162214d20e2352975ac9c2e25dc3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package pyhachi/0.2.1/pyhachi-0.2.1.tar.gz

---
+++ packages/pyhachi/pyhachi-0.2.1.tar.gz/pyhachi-0.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info<(3,):
+    sys.exit("Sorry, Python 3 is required for Caver")
+
+with open("requirements.txt", "r") as f:
+    reqs = [l for l in f.read().splitlines() if l]
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+setup(
+    name="pyhachi",
+    version="0.2.1",
+    description="spam filter",
+    long_description=long_description,
+    author='Guokr Inc.',
+    author_email='jinyang.zhou@guokr.com',
+    # url="https://github.com/guokr/Caver",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+    ),
+    entry_points={
+        'console_scripts': [
+            # 'trickster_train=trickster::train',
+        ]
+    },
+    install_requires=reqs
+)
