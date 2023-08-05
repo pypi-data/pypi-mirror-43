45ea5c7bca14b85aa01b38bca4106b86bad1e905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package nester-eslamturki/1.0.0/nester-eslamturki-1.0.0.tar.gz

---
+++ packages/nester-eslamturki/nester-eslamturki-1.0.0.tar.gz/nester-eslamturki-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-eslamturki',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='eslamturki',
+    author_email='eslam.turki@gmail.com',
+    url='https://github.com/pypa/sampleproject',
+    description='A simple printer of nested lists',
+    )
