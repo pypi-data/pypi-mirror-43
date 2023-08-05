c7a804981170886e7f8444c52b1160f1ff4db2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package librecell/0.0.0/librecell-0.0.0.tar.gz

---
+++ packages/librecell/librecell-0.0.0.tar.gz/librecell-0.0.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(name='librecell',
+      version='0.0.0',
+      description='CMOS standard cell generator.',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='cmos cell generator layout klayout vlsi asic',
+      classifiers=[
+          # 'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Development Status :: 3 - Alpha',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Programming Language :: Python :: 3'
+      ],
+      url='https://codeberg.org/tok/librecell',
+      author='T. Kramer',
+      author_email='dont@spam.me',
+      license='OHL-S',  # ???
+      entry_points={
+          'console_scripts': [
+              'librecell = librecell.standalone:main',
+              #'drc_cleaner = librecell.drc_cleaner.standalone:main',
+              'libertyviz = librecell.liberty.visualize:main_plot_timing',
+              'libertymerge = librecell.liberty.merge:main',
+              'librecell_size = librecell.transistor_sizing.width_opt:main',
+              'librecell_characterize = librecell.characterization.standalone:main'
+          ]
+      },
+      install_requires=[
+          'klayout',  # GPLv3
+          'numpy',  # BSD
+          'sympy',  # BSD
+          'networkx',  # BSD
+          'pyparsing',  # MIT
+          'lark-parser',  # MIT
+          'pyspice',  # GPLv3
+          'scipy',  # BSD
+          'liberty-parser',  # LGPL
+          'pysmt',  # Apache-2.0
+          'pulp' # MIT
+      ],
+      zip_safe=False)
