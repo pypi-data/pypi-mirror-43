cf8600aa92ccd5c3bd6ab3d6f93af6b8b9aa31e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:37 +0000

    Add package sycopath/0.1/sycopath-0.1.tar.gz

---
+++ packages/sycopath/sycopath-0.1.tar.gz/sycopath-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sycopath',
+      version='0.1',
+      description='a file system path helper',
+      url='http://github.com/amol9',
+      author='Amol Umrale',
+      author_email='babaiscool@gmail.com',
+      license='MIT',
+      packages=['sycopath'],
+      zip_safe=False)
+
