b012185dba45dd7d4636a4b9809b811993c0efec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package cognite-model-hosting/0.1.0a12/cognite-model-hosting-0.1.0a12.tar.gz

---
+++ packages/cognite-model-hosting/cognite-model-hosting-0.1.0a12.tar.gz/cognite-model-hosting-0.1.0a12/setup.py
@@ -0,0 +1,18 @@
+import re
+
+from setuptools import setup
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"', open("cognite/model_hosting/_cognite_model_hosting_common/version.py").read(), re.M
+).group(1)
+
+setup(
+    name="cognite-model-hosting",
+    version=version,
+    description="Utilities Cognite's model hosting environment",
+    url="",  # TODO
+    author="Nils Barlaug",
+    author_email="nils.barlaug@cognite.com",
+    install_requires=["pandas", "marshmallow==3.0.0rc4", "requests>=2.21.0,<3.0.0"],
+    python_requires=">=3.5",
+)
