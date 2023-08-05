0e3bed8a47ac2741e50030211d8498c5a5e65efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:22 +0000

    Add package masonite/2.1.20/masonite-2.1.20.tar.gz

---
+++ packages/masonite/masonite-2.1.20.tar.gz/masonite-2.1.20/masonite/commands/CommandCommand.py
@@ -0,0 +1,15 @@
+"""Creates New Command Command."""
+from masonite.commands import BaseScaffoldCommand
+
+
+class CommandCommand(BaseScaffoldCommand):
+    """
+    Creates a new command.
+
+    command
+        {name : Name of the command you would like to create}
+    """
+
+    scaffold_name = 'Command'
+    template = '/masonite/snippets/scaffold/command'
+    base_directory = 'app/commands/'
