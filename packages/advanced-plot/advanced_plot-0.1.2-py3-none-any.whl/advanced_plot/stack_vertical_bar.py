04a3fd7402d86bd532f8567de040ec5c1e7dc128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:31 +0000

    Add package advanced-plot/0.1.2/advanced_plot-0.1.2-py3-none-any.whl

---
+++ packages/advanced-plot/advanced_plot-0.1.2-py3-none-any.whl/advanced_plot/stack_vertical_bar.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def stack_vertical_bar(data: pd.DataFrame, label: str, value: str, ax=None, fontsize=20):
+    if ax is None:
+        fig = plt.figure(figsize=(16, 2))
+        ax = fig.add_subplot(1, 1, 1)
+
+    labels = getattr(data, label)
+    values = getattr(data, value)
+
+    sum_ = values.sum()
+    before_rate = 0
+
+    for l, v in zip(labels, values):
+        rate = v / sum_
+        x = before_rate + rate
+
+        ax.barh(0, x, left=before_rate)
+        text_x = before_rate + rate / 2
+        text = f"{l}\n{value} ({rate:.1%})"
+        ax.text(text_x, 0, text, fontsize=fontsize, horizontalalignment='center', verticalalignment='center')
+        before_rate = x
+    ax.grid(False)
+    ax.set_xlim(0, 1)
+    ax.tick_params(axis='both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    ax.set_facecolor('white')
+    [spine.set_visible(False) for spine in ax.spines.values()]
+
+    return ax
---
+++ packages/advanced-plot/advanced_plot-0.1.2-py3-none-any.whl/advanced_plot/vertical_bar.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+def vertical_bar(data: pd.DataFrame, label: str, value: str, ax=None, ratio=True, fontsize=14):
+    if ax is None:
+        n = data.shape[0]
+        fig = plt.figure(figsize=(16, 1 * n))
+        ax = fig.add_subplot(1, 1, 1)
+
+    labels = getattr(data, label)
+    values = getattr(data, value)
+
+    max_ = values.max()
+    sum_ = values.sum()
+    base = sum_ # TODO: max version for base
+
+    sns.barplot(x=value, y=label, data=data.reset_index(), ax=ax, order=labels)
+    margin = ax.get_xlim()[1]  / 100 
+
+    for i, (l, v) in enumerate(zip(labels, values)):
+        text = f'{v}'
+        if ratio:
+            v_ratio = v / base
+            text = f'{text} ({v_ratio:.1%})'
+        ax.text(v + margin, i, text, color='black', ha="left", va='center', fontsize=fontsize)
+
+    [spine.set_visible(False) for spine in ax.spines.values()]
+    ax.tick_params(bottom=False, left=False, labelbottom=False)
+    ax.tick_params(axis='y', labelsize='x-large')
+    ax.set_facecolor('white')
+
+    return ax
