629b9a77d08a56a3ff4133b66f2b22cf65212b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package indy-plenum-dev/1.6.716/indy-plenum-dev-1.6.716.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.6.716.tar.gz/indy-plenum-dev-1.6.716/plenum/test/test_ledger_manager.py
@@ -0,0 +1,13 @@
+from plenum.common.ledger_manager import LedgerManager
+from plenum.test.testable import spyable
+
+ledger_manager_spyables = [LedgerManager.startCatchUpProcess,
+                           LedgerManager.catchupCompleted,
+                           LedgerManager.processConsistencyProof,
+                           LedgerManager.processCatchupRep]
+
+
+@spyable(methods=ledger_manager_spyables)
+class TestLedgerManager(LedgerManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
