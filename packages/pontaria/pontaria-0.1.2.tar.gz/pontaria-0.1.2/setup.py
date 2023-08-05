db6d77178e040361e5cdfe598084a601ca23be06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:04 +0000

    Add package pontaria/0.1.2/pontaria-0.1.2.tar.gz

---
+++ packages/pontaria/pontaria-0.1.2.tar.gz/pontaria-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pontaria",
+    version="0.1.2",
+    author="Henrique Cunha",
+    author_email="henrique.cunha@birdie.ai",
+    description="A time monitoring utility for Birdie's tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=['auth', 'date_utils'],
+    include_package_data=True,
+    install_requires=['arrow', 'gspread',
+                      'google-api-python-client', 'google-auth-httplib2', 'google-auth-oauthlib'],
+    entry_points={
+        'console_scripts': [
+            'pontaria = app:main'
+        ]
+    }
+)
