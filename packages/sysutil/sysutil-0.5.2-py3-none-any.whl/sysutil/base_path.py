1b5360006baf2627a0f9fafbb2c7127992cca770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package sysutil/0.5.2/sysutil-0.5.2-py3-none-any.whl

---
+++ packages/sysutil/sysutil-0.5.2-py3-none-any.whl/sysutil/base_path.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import inspect
+import pathlib
+def get_base_path(base_file='constants.py',file_path=None):
+    if file_path is None:
+       file_path = os.getcwd()
+    path_arr = file_path.split('/')
+    base_path = '/'
+    for path in path_arr:
+        if path != '':
+           base_path += "{}/".format(path)
+           file = pathlib.Path(base_path+"/"+base_file)
+           if file.exists():
+              return base_path
+    return 
+
