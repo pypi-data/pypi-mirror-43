f794ed16316762780c30a0d38dce8c0c708e5e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package wpilib/2019.2.2/wpilib-2019.2.2.tar.gz

---
+++ packages/wpilib/wpilib-2019.2.2.tar.gz/wpilib-2019.2.2/wpilib/drive/__init__.py
@@ -0,0 +1,5 @@
+from .differentialdrive import DifferentialDrive
+from .killoughdrive import KilloughDrive
+from .mecanumdrive import MecanumDrive
+from .robotdrivebase import RobotDriveBase
+from .vector2d import Vector2d
