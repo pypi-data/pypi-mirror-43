cac963b9fc794ef42c816c9ea21007f6ee14e04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package requests-filecache/0.0.3/requests-filecache-0.0.3.tar.gz

---
+++ packages/requests-filecache/requests-filecache-0.0.3.tar.gz/requests-filecache-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='requests-filecache',
+    version='0.0.3',
+    packages=['requests_filecache'],
+    url='https://github.com/strizhechenko/requests-filecache',
+    license='MIT',
+    author='Oleg Strizhechenko',
+    author_email='oleg.strizhechenko@gmail.com',
+    description='Saves fetched HTML into files in current directory.',
+    install_requires=['requests']
+)
