6bf001f2a8e8daa4817fe4fd0590c997ea6fe0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:14 +0000

    Add package django-mdpages/0.2/django-mdpages-0.2.tar.gz

---
+++ packages/django-mdpages/django-mdpages-0.2.tar.gz/django-mdpages-0.2/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-mdpages',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A simple Django app to generate pages from files.',
+    long_description=README,
+    url='https://github.com/marcanuy/django-mdpages/',
+    author='Marcelo Canina',
+    author_email='me@marcanuy.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords='django static',
+    project_urls={
+        #'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Source': 'https://github.com/marcanuy/django-mdpages/',
+        'Tracker': 'https://github.com/marcanuy/django-mdpages/issues',
+    },
+    install_requires=[
+        'Django>=2.1',
+        'Markdown'
+    ],
+
+)
