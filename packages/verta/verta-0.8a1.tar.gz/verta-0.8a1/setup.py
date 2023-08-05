b85680bffe2172b420f2dbdaaca781d5bcf0aa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:00 +0000

    Add package verta/0.8a1/verta-0.8a1.tar.gz

---
+++ packages/verta/verta-0.8a1.tar.gz/verta-0.8a1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="verta",
+    version="0.8a1",
+    maintainer="Michael Liu",
+    maintainer_email="miliu@verta.ai",
+    description="Python client for interfacing with ModelDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.verta.ai/",
+    packages=[
+        "verta",
+        "verta._protos.public.modeldb",
+    ],
+    install_requires=[
+        "googleapis-common-protos~=1.5.6",
+        "grpcio~=1.17.1",
+        "protobuf~=3.6.1",
+        "requests~=2.21.0",
+    ],
+)
