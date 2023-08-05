fe629bb86d9511aee3a177102def22872de10bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:40 +0000

    Add package tagtomarkdown/0.2.7/tagtomarkdown-0.2.7.tar.gz

---
+++ packages/tagtomarkdown/tagtomarkdown-0.2.7.tar.gz/tagtomarkdown-0.2.7/__init__.py
@@ -0,0 +1,15 @@
+# Christians first attempt to make a Python3 package
+import pkg_resources
+
+name = 'tagtomarkdown'
+
+class ToolSet(object):
+  pass
+
+try_pack = ToolSet()
+#named_objects =  {}
+for ep in pkg_resources.iter_entry_points(group='markdown.extensions'):
+  #print('ep.name: %s' % ep.name)
+  setattr(try_pack, ep.name, ep.load())
+  #print('ep.name: %s, ep.load(): %s' % (ep.name, ep.load()))
+  #named_objects.update({ep.name, ep.load()})
