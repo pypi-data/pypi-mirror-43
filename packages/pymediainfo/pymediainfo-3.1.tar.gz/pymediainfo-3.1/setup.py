8a84ccdc2936feeb2287b0f17899a44b3cbc5891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:13 +0000

    Add package pymediainfo/3.1/pymediainfo-3.1.tar.gz

---
+++ packages/pymediainfo/pymediainfo-3.1.tar.gz/pymediainfo-3.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name='pymediainfo',
+    author='Louis Sautier',
+    author_email='sautier.louis@gmail.com',
+    url='https://github.com/sbraz/pymediainfo',
+    description="""A Python wrapper for the mediainfo library.""",
+    long_description=long_description,
+    packages=find_packages(),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    install_requires=["setuptools"],
+    tests_require=["pytest", "pytest-runner"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
