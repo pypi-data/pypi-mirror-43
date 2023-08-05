e05cbe486c4d1d3af834c1b78f79f7454f7b3499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:42 +0000

    Add package shell-scripting/0.0.4/shell-scripting-0.0.4.tar.gz

---
+++ packages/shell-scripting/shell-scripting-0.0.4.tar.gz/shell-scripting-0.0.4/shell_scripting/__init__.py
@@ -0,0 +1,15 @@
+from subprocess import run as subprocess_run
+
+
+def run(command, **kwargs):
+    subprocess_run(command, shell=True, **kwargs)
+
+
+def shell(command, **kwargs):
+    return subprocess_run(command, shell=True, capture_output=True, text=True, **kwargs)
+
+
+def shell_list(command, **kwargs):
+    return subprocess_run(
+        command, shell=True, capture_output=True, text=True, **kwargs
+    ).stdout.split("\n")
