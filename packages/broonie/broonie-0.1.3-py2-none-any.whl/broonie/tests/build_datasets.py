84b2a8829637765adb494917ff8260cada763120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package broonie/0.1.3/broonie-0.1.3-py2-none-any.whl

---
+++ packages/broonie/broonie-0.1.3-py2-none-any.whl/broonie/tests/build_datasets.py
@@ -0,0 +1,17 @@
+#
+# Construct the data sets given the sizes n,p,k,and w
+#
+import time as tm
+from construct_simulated_dataset import SimulatedDataSet
+
+metadata = 'simulatedNew'
+n=5000
+p=1000
+k=10
+w=1
+
+simdat = SimulatedDataSet(metadata,n,p,k,w)
+simdat.constructDataObjects()
+simdat.outputSimulationParams()
+simdat.writeObjectsToDisk()
+
