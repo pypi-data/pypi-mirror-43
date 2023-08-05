8b18442af2bd29f1ebed0e94b1780815fe86014f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package pyn5/0.1.0/pyn5-0.1.0-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pyn5/pyn5-0.1.0-cp36-cp36m-manylinux1_x86_64.whl/pyn5/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyn5."""
+
+__author__ = """William Hunter Patton"""
+__email__ = "pattonw@hhmi.org"
+__version__ = "0.1.0"
+
+from .python_wrappers import *
