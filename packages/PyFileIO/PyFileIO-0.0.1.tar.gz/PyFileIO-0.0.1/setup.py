a4c68f7418ad59ae4a1f14544dc1c742ef66a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:44 +0000

    Add package PyFileIO/0.0.1/PyFileIO-0.0.1.tar.gz

---
+++ packages/PyFileIO/PyFileIO-0.0.1.tar.gz/PyFileIO-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from setuptools.command.install import install
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyFileIO",
+    version="0.0.1",
+    author="Matthew Knight James",
+    author_email="mattkjames7@gmail.com",
+    description="Some very basic  routines for file IO in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattkjames7/PyFileIO",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+    install_requires=[
+		'numpy',
+		'RecarrayTools',
+	],
+)
+
+
+
