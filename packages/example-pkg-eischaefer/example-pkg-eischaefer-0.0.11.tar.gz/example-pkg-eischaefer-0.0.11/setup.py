d5f92a3ccaab31679fc030dc983ba5fe48080833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package example-pkg-eischaefer/0.0.11/example-pkg-eischaefer-0.0.11.tar.gz

---
+++ packages/example-pkg-eischaefer/example-pkg-eischaefer-0.0.11.tar.gz/example-pkg-eischaefer-0.0.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-eischaefer",
+    version="0.0.11",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    install_requires=["scipy>=0.19", "numpy>=1.13.3", "bottleneck>=1.2.1",
+                      "psutil>=3.3.0", "gdal>=2.1.3"],
+    # install_requires=["scipy", "numpy", "gdal"],
+)
