702ba2c8ae9f4b1e8cf7a21a550d982b82217b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:24 +0000

    Add package qsh/0.7.3/qsh-0.7.3.tar.gz

---
+++ packages/qsh/qsh-0.7.3.tar.gz/qsh-0.7.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='qsh',
+    version='0.7.3',
+    url='https://github.com/nethask/qsh',
+    author='Artyom Knyazev',
+    author_email='nethask@gmail.com',
+    license='MIT',
+    install_requires=['python-dateutil'],
+)
