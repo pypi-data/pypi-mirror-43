4cb8da4b75425d6b4e4c3d18849f22d5005505d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package json_viewer/0.1.3/json_viewer-0.1.3.tar.gz

---
+++ packages/json_viewer/json_viewer-0.1.3.tar.gz/json_viewer-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name='json_viewer',
+        version='0.1.3',
+        scripts=['json_viewer'],
+        author="arfan",
+        author_email="abdul.arfan@gmail.com",
+        description="A Python GUI json viewer",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/arfan/json-viewer",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'PyQt5',
+            'PyQt5-sip'
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+    )
