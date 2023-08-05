2048495fdf0e817506f8738f66c474378ea63e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package rn2903/0.2.0/rn2903-0.2.0.tar.gz

---
+++ packages/rn2903/rn2903-0.2.0.tar.gz/rn2903-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+
+setup(name='rn2903',
+
+      version='0.2.0',
+
+      description='Uses Python 3 to Interface with a RN2903 LoRa Module Connected via USB',
+
+      url='https://github.com/ANRGUSC/rn2903-lora-library',
+
+      author='Ryan Winters',
+
+      author_email='ryanwint@usc.edu',
+
+      license='MIT',
+
+      packages=['rn2903'],
+
+      zip_safe=False)
