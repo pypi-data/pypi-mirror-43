26f34495156a3833debf26fc7a124018a554a11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package ringcentral-bot-framework/0.2.3/ringcentral_bot_framework-0.2.3.tar.gz

---
+++ packages/ringcentral-bot-framework/ringcentral_bot_framework-0.2.3.tar.gz/ringcentral_bot_framework-0.2.3/ringcentral_bot_framework/core/bot_oauth.py
@@ -0,0 +1,42 @@
+"""
+bot auth
+"""
+import time
+from .common import result, debug
+from pydash.predicates import is_number, is_string
+from pydash import get
+
+def initBotAuthHandler(conf, Bot, dbAction):
+  def botAuth(event):
+    bot = Bot()
+    code = get(event, 'queryStringParameters.code[0]') or get(event, 'queryStringParameters.code')
+    if is_string(code):
+      bot.auth(code)
+    else:
+      bot.authPrivateBot(event['body'])
+
+    bot.renewWebHooks(event)
+    conf.botAuthAction(bot, dbAction)
+    return result('Bot added')
+
+  def renewBot (event):
+    """
+    for self call renewbot async
+    """
+    debug('self tringgering renew bot')
+    if is_number(event['wait']):
+      time.sleep(event['wait'])
+
+    bot = Bot()
+    bot.id = event['botId']
+    bot.token = event['token']
+    bot.rc.token = bot.token
+    bot.writeToDb({
+      'id': bot.id,
+      'token': bot.token,
+      'data': bot.data
+    })
+    bot.renewWebHooks(event)
+    return result('Bot renew done')
+
+  return botAuth, renewBot
