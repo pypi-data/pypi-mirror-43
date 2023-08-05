328c4362e7211cf5262dc6bc88bde7ebfe1fe553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package dtoolbioimage/0.1.5/dtoolbioimage-0.1.5.tar.gz

---
+++ packages/dtoolbioimage/dtoolbioimage-0.1.5.tar.gz/dtoolbioimage-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='dtoolbioimage',
+      version='0.1.5',
+      description='dtool bioimaging utilties',
+      packages=['dtoolbioimage', 'dtoolbioimage.util'],
+      url='https://github.com/JIC-Image-Analysis/dtoolbioimage',
+      author='Matthew Hartley',
+      author_email='Matthew.Hartley@jic.ac.uk',
+      license='MIT',
+      install_requires=[
+	  "click",
+	  "parse",
+          "imageio",
+          "dtoolcore",
+          "ipywidgets",
+          "scipy"
+      ],
+      entry_points='''
+        [console_scripts]
+        convert_image_dataset=dtoolbioimage.convert:cli
+      ''',
+      zip_safe=False)
