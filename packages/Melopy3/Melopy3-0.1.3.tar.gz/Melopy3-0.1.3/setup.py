73a1a49970820d470ced1c748ab9be578e57a123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package Melopy3/0.1.3/Melopy3-0.1.3.tar.gz

---
+++ packages/Melopy3/Melopy3-0.1.3.tar.gz/Melopy3-0.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+config = {
+    'name': 'Melopy3',
+    'author': 'Aldermann',
+    'author_email': 'vietanisme@gmail.com',
+    'description': 'Python music library',
+    'packages': ['melopy'],
+    'version': '0.1.3',
+    'url': 'https://github.com/aldermann/Melopy3',
+    'license': 'LICENSE.txt',
+    'classifiers': []
+}
+
+setup(**config)
+
+# Licensed under The MIT License (MIT)
+# See LICENSE file for more
