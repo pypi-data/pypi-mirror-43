3a5ab527e66babd5f8497be4cae4764db90a04e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:31 +0000

    Add package wild-nlp/0.0.2/wild-nlp-0.0.2.tar.gz

---
+++ packages/wild-nlp/wild-nlp-0.0.2.tar.gz/wild-nlp-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="wild-nlp",
+    version="0.0.2",
+    author="Dominika Basaj, Barbara Rychalska, Alicja Gosiewska, Adam Słucki",
+    author_email="adam.slucki@gmail.com",
+    description=("Text aspects for nlp models"),
+    license="BSD",
+    url="https://github.com/MI2DataLab/WildNLP",
+    packages=['wildnlp'],
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ]
+)
