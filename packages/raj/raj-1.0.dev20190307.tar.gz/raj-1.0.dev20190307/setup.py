126fabd6590dfa73309cf03db7ca8aaab6ff8644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:43 +0000

    Add package raj/1.0.dev20190307/raj-1.0.dev20190307.tar.gz

---
+++ packages/raj/raj-1.0.dev20190307.tar.gz/raj-1.0.dev20190307/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup,find_packages
+import os
+
+setup(
+    name='raj',
+    version='1.0',
+    description='A sample Python project',
+    long_description='long_description',
+    packages=['raj'],
+    #packages= find_packages(),
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    #url='https://gitlab.caratred.com/bhanuchander008/application',
+
+    #packages=find_packages(),
+    install_requires=[
+        'flask',
+
+    ],
+classifiers=[
+
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+],
+
+)
