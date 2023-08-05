c12724af17cb63118b2b22eb94b8d5c78ab33967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:22 +0000

    Add package csgo-menu-maker/0.2.2/csgo-menu-maker-0.2.2.tar.gz

---
+++ packages/csgo-menu-maker/csgo-menu-maker-0.2.2.tar.gz/csgo-menu-maker-0.2.2/csgomenumaker/param/name.py
@@ -0,0 +1,18 @@
+from .string import String
+
+
+class Name(String):
+    """
+    Convenience class to generate a name key with only one argument!
+
+    Fancy.
+    """
+
+    def __init__(self, name):
+        String.__init__(
+            self,
+            "name",
+            default=name,
+            description="The component's name.",
+            nodoc=True
+        )
---
+++ packages/csgo-menu-maker/csgo-menu-maker-0.2.2.tar.gz/csgo-menu-maker-0.2.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csgo-menu-maker",
+    version="0.2.2",
+    author="Citrus",
+    author_email="address@example.com",
+    description="Make cool-looking menus in CSGO consoles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/citrusCS/csgo-menu-maker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
