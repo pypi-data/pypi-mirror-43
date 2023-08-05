4287c77e840e2b0661687113ad918f4073de3a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package mlh/0.0.1/mlh-0.0.1.tar.gz

---
+++ packages/mlh/mlh-0.0.1.tar.gz/mlh-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlh",
+    version = '0.0.1',
+    author="Devendra Kumar Sahu",
+    author_email="devsahu99@gmail.com",
+    description="This package provides helper utilities for machine learning tasks. One major utility is calculation of weight of evidence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devsahu99/mlh",
+    packages=['mlh'],
+    install_requires=[
+        'pandas','scipy','IPython','matplotlib'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
