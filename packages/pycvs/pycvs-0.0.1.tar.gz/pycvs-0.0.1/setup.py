891af0efb7830836588c6975e52da87686062ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package pycvs/0.0.1/pycvs-0.0.1.tar.gz

---
+++ packages/pycvs/pycvs-0.0.1.tar.gz/pycvs-0.0.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# The MIT License (MIT)
+# Copyright (c) 2016 Gerson Carlos
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import setuptools
+from distutils.core import setup
+
+setup(name='pycvs',
+      version='0.0.1',
+      description='A Python client for CVS projects',
+      author='Gerson Carlos',
+      author_email='gerson.mtg@gmail.com',
+      url='https://github.com/gerson23/pycvs',
+      license='MIT',
+      scripts=['src/scripts/pycvs'],
+      package_dir={'': 'src/python'},
+      packages=['pycvs'],
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Unix'
+      ],
+      install_requires=['pexpect', 'colorama']
+      )
