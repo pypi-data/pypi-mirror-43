3847adff4d2ffe18b0aae278a843b8fb9d7f3614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:49 +0000

    Add package Eto-stubs/0.0.4/Eto-stubs-0.0.4.tar.gz

---
+++ packages/Eto-stubs/Eto-stubs-0.0.4.tar.gz/Eto-stubs-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools, os
+
+long_description = '''
+# Eto-stubs
+Stubs for Eto
+'''
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for f in files:
+            stubs.append(os.path.relpath(os.path.join(root, f), package))
+    return {package: stubs}
+
+setuptools.setup(
+    name="Eto-stubs",
+    version="0.0.4",
+	package_data=find_stubs("Eto-stubs"),
+    packages=['Eto-stubs'],
+    author="Robert McNeel & Associates",
+    author_email="steve@mcneel.com",
+    description="Stubs for Eto",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcneel/pythonstubs",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ],
+)
