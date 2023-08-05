d5f724a7b633a01164c94c55f7d24cb094d7cbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package nlp-model-gen/0.1.5/nlp_model_gen-0.1.5-py3-none-any.whl

---
+++ packages/nlp-model-gen/nlp_model_gen-0.1.5-py3-none-any.whl/nlp_model_gen-0.1.5.data/scripts/install.sh
@@ -0,0 +1,5 @@
+#!python
+
+import subprocess
+
+subprocess.run("python3 -m spacy download es_core_news_md".split())
---
+++ packages/nlp-model-gen/nlp_model_gen-0.1.5-py3-none-any.whl/nlp_model_gen-0.1.5.data/scripts/install_deps.sh
@@ -0,0 +1,8 @@
+#!python
+
+pip3 install spacy
+pip3 install pymongo
+pip3 install termcolor
+pip3 install schema
+pip3 install terminaltables
+python3 -m spacy download en
