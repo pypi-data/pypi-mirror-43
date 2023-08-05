a5ae103e2201f917b789e87d3effe319e98c882b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:41 +0000

    Add package ociopath/0.1/ociopath-0.1.tar.gz

---
+++ packages/ociopath/ociopath-0.1.tar.gz/ociopath-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ociopath',
+      version='0.1',
+      description='a url path helper',
+      url='http://github.com/amol9',
+      author='Amol Umrale',
+      author_email='babaiscool@gmail.com',
+      license='MIT',
+      packages=['ociopath'],
+      zip_safe=False)
+
