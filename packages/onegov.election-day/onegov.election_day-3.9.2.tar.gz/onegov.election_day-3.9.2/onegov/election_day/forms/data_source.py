c79a717c46180c045c9c313bf50a381d95d50370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package onegov.election-day/3.9.2/onegov.election_day-3.9.2.tar.gz

---
+++ packages/onegov.election-day/onegov.election_day-3.9.2.tar.gz/onegov.election_day-3.9.2/onegov/election_day/forms/data_source.py
@@ -0,0 +1,90 @@
+from onegov.election_day import _
+from onegov.election_day.models.data_source import UPLOAD_TYPE_LABELS
+from onegov.form import Form
+from wtforms import RadioField
+from wtforms import StringField
+from wtforms.validators import InputRequired
+
+
+class DataSourceForm(Form):
+
+    name = StringField(
+        label=_("Name"),
+        validators=[
+            InputRequired()
+        ],
+    )
+
+    upload_type = RadioField(
+        _("Type"),
+        choices=list(UPLOAD_TYPE_LABELS),
+        validators=[
+            InputRequired()
+        ],
+        default='vote'
+    )
+
+    def update_model(self, model):
+        model.name = self.name.data
+        model.type = self.upload_type.data
+
+    def apply_model(self, model):
+        self.name.data = model.name
+        self.upload_type.data = model.type
+
+
+class DataSourceItemForm(Form):
+
+    item = RadioField(
+        label="",
+        choices=[],
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    number = StringField(
+        label="'SortGeschaeft'",
+        validators=[
+            InputRequired()
+        ],
+        render_kw=dict(force_simple=True)
+    )
+
+    district = StringField(
+        label="'SortWahlkreis'",
+        render_kw=dict(force_simple=True)
+    )
+
+    callout = ''
+
+    def populate(self, source):
+        self.type = source.type
+        self.item.label.text = dict(UPLOAD_TYPE_LABELS).get(self.type)
+        self.item.choices = [
+            (item.id, item.title) for item in source.query_candidates()
+        ]
+        self.callout = ''
+        if not self.item.choices:
+            if self.type == 'vote':
+                self.callout = _("No votes yet.")
+            else:
+                self.callout = _("No elections yet.")
+
+    def update_model(self, model):
+        if self.type == 'vote':
+            model.vote_id = self.item.data
+        else:
+            model.election_id = self.item.data
+
+        model.district = self.district.data
+        model.number = self.number.data
+
+    def apply_model(self, model):
+        if self.type == 'vote':
+            self.item.data = model.vote_id
+        else:
+            self.item.data = model.election_id
+
+        self.district.data = model.district
+        self.number.data = model.number
