aaa3da32f204db88a16939ef5914d9030ce4db3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:36 +0000

    Add package setupextras/0.1.5/setupextras-0.1.5-py3-none-any.whl

---
+++ packages/setupextras/setupextras-0.1.5-py3-none-any.whl/setupextras/__init__.py
@@ -0,0 +1,24 @@
+
+# =========================================
+#       IMPORTS / EXPORTS
+# --------------------------------------
+
+import sys
+
+from os import path
+
+CURRENT_PATH = path.abspath(path.dirname(__file__))
+ROOT_PATH = path.abspath(path.join(CURRENT_PATH, '..'))
+
+try:
+    try:
+        sys.path.remove(CURRENT_PATH)
+    except:
+        pass
+
+    sys.path.index(ROOT_PATH)
+
+except ValueError:
+    sys.path.insert(0, ROOT_PATH)
+
+from setupextras.extras import *
