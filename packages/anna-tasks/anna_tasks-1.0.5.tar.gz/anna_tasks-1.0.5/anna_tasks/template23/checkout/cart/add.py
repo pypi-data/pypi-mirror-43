34605c50325efff8b60fd28f2e3ee8ec040e1fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:14 +0000

    Add package anna-tasks/1.0.5/anna_tasks-1.0.5.tar.gz

---
+++ packages/anna-tasks/anna_tasks-1.0.5.tar.gz/anna_tasks-1.0.5/anna_tasks/template23/checkout/cart/add.py
@@ -0,0 +1,26 @@
+events = [
+	{
+		"type": "click",
+		"target": ".action-search"
+	},
+	{
+		"type": "send_keys",
+		"target": "#search",
+		"value": "didi watch"
+	},
+	{
+		"type": "click",
+		"target": ".product-item-link"
+	},
+	{
+		"type": "click",
+		"target": "#product-addtocart-button"
+	},
+	{
+		"type": "click",
+		"target": "[title='Close (Esc)']"
+	}
+]
+
+assertions = [
+]
