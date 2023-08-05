f7261928723d8103d7f2cf66ccb9872fde77803a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:54 +0000

    Add package structured-markdown/0.1/structured_markdown-0.1.tar.gz

---
+++ packages/structured-markdown/structured_markdown-0.1.tar.gz/structured_markdown-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+
+setuptools.setup(
+    name="structured_markdown",
+    version=0.1,
+    description="Add an extra dimension to your markdown documents",
+    long_description=long_description,
+    url="https://github.com/Structured-Markdown/structured_markdown",
+    author="Structured-Markdown",
+    author_email="isaacimagine@gmail.com",
+    license="MIT",
+    packages=["structured_markdown"],
+    zip_safe=False,
+)
