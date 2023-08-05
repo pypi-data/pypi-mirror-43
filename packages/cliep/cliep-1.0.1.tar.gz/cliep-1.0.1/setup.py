4e6e28599c95b72166ce5fa99202ca076b3eb35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package cliep/1.0.1/cliep-1.0.1.tar.gz

---
+++ packages/cliep/cliep-1.0.1.tar.gz/cliep-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='cliep',
+    version='1.0.1',
+    author='Harald Sorenson',
+    author_email='haraldesorenson@gmail.com',
+    description='Simple CLI entrypoint and argument parsing.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    url='https://github.com/hsorenson/cliep',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ]
+)
