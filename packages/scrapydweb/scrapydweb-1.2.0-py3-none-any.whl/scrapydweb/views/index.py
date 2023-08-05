03cfa7308d215f03a490ca3f78806b4cf6e19a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:51 +0000

    Add package scrapydweb/1.2.0/scrapydweb-1.2.0-py3-none-any.whl

---
+++ packages/scrapydweb/scrapydweb-1.2.0-py3-none-any.whl/scrapydweb/views/index.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from flask import redirect, url_for
+
+from ..myview import MyView
+
+
+class IndexView(MyView):
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+    def dispatch_request(self, **kwargs):
+        if self.SCRAPYD_SERVERS_AMOUNT == 1:
+            if self.IS_MOBILE and not self.IS_IPAD:
+                return redirect(url_for('jobs', node=self.node, ui='mobile'))
+            else:
+                return redirect(url_for('jobs', node=self.node, ui=self.UI))
+        else:
+            if self.USE_MOBILEUI or (self.IS_MOBILE and not self.IS_IPAD):
+                return redirect(url_for('jobs', node=self.node, ui='mobile'))
+            else:
+                return redirect(url_for('servers', node=self.node, ui=self.UI))
