b19e7ddfdb5dde0f27cb1646307fa61b55f4495f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:06 +0000

    Add package doopl/12.8.0.11/doopl-12.8.0.11-cp35-cp35m-win_amd64.whl

---
+++ packages/doopl/doopl-12.8.0.11-cp35-cp35m-win_amd64.whl/doopl-12.8.0.11.data/purelib/doopl/version.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------
+# Source file provided under Apache License, Version 2.0, January 2004,
+# http://www.apache.org/licenses/
+# (c) Copyright IBM Corp. 2018
+# --------------------------------------------------------------------------
+
+# gendoc: ignore
+# This file is generated !
+opl_version_major = 12
+opl_version_minor = 8
+opl_version_micro = 0
+opl_version_string = '12.8.0'
