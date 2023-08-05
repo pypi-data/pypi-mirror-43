56241d78eceeeabb0aa2463363ff2a1a6f9427e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:31 +0000

    Add package jpndlpy/0.1.0/jpndlpy-0.1.0-py3-none-any.whl

---
+++ packages/jpndlpy/jpndlpy-0.1.0-py3-none-any.whl/jpndlpy/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+'''
+Created on 2019/03/10
+@author: shimakaze-git
+'''
+
+__version__ = '0.1.0'
+__license__ = 'BSD 2-Clause "Simplified" License (BSD-2-Clause)'
+__author__ = 'shimakaze-git'
+__author_email__ = 'shimakaze.soft@gmail.com'
+__url__ = 'https://github.com/shimakaze-git/jpndlpy'
+
+from .client import JapanNdlClient
