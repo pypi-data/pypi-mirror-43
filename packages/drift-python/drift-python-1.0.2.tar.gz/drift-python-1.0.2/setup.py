f034a767cce77bca9b0b780043c4e6b5cc823248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package drift-python/1.0.2/drift-python-1.0.2.tar.gz

---
+++ packages/drift-python/drift-python-1.0.2.tar.gz/drift-python-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from drift import __version__
+
+setup(name='drift-python',
+      version=__version__,
+      description="A Simple Drift API Wrapper.",
+      long_description=open('README.md').read(),
+      author='Drift Engineering',
+      author_email='driftintegrations@drift.com',
+      packages=['drift'],
+      url='https://github.com/Driftt/drift-python',
+      include_package_data=True,
+      zip_safe=False,
+      license='MIT',
+      install_requires=[
+          'requests==2.20.0',
+      ])
