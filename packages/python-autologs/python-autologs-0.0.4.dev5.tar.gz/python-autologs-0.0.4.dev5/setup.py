a143b8485464d8ee8b8bf771444461c8f7f1a5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:47 +0000

    Add package python-autologs/0.0.4.dev5/python-autologs-0.0.4.dev5.tar.gz

---
+++ packages/python-autologs/python-autologs-0.0.4.dev5.tar.gz/python-autologs-0.0.4.dev5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-autologs",
+    version="0.0.4-dev5",
+    author="Meni Yakove",
+    author_email="myakove@gmail.com",
+    description="Auto generate logs for functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/myakove/python-autologs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
