03d3929563274f1d47e8b82473057087a160fad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package Dataviewer/0.2.3/Dataviewer-0.2.3.tar.gz

---
+++ packages/Dataviewer/Dataviewer-0.2.3.tar.gz/Dataviewer-0.2.3/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="Dataviewer",
+    version="0.2.3",
+    description="Dataviewer for project files",
+    license="MIT",
+    author="kdd",
+    author_email="kobededecker@gmail.com",
+    url="https://gitlab.com/kobededecker/dataviewer",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[r'bin\viewer.bat', r'bin\viewer.py'],
+    install_requires=['flask', 'flask_cors'],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
