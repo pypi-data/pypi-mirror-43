9fe32b3923a3ee52f9dbbd014bfbfa63585ea4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package qcportal/0.5.2/qcportal-0.5.2.tar.gz

---
+++ packages/qcportal/qcportal-0.5.2.tar.gz/qcportal-0.5.2/qcportal/collections/__init__.py
@@ -0,0 +1,9 @@
+"""
+Python init for QCPortal collections
+"""
+
+from .collection_utils import collection_factory
+from .dataset import Dataset
+from .reaction_dataset import ReactionDataset
+from .generic import Generic
+from .openffworkflow import OpenFFWorkflow
