7372b645293993d677923f7fc2becae4eb4e20ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package ahmad-taj-pkg-printer/0.0.2/ahmad_taj_pkg_printer-0.0.2.tar.gz

---
+++ packages/ahmad-taj-pkg-printer/ahmad_taj_pkg_printer-0.0.2.tar.gz/ahmad_taj_pkg_printer-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ahmad_taj_pkg_printer",
+    version="0.0.2",
+    author="Ahmad Taj",
+    author_email="Ahmadtaj77@gmail.com",
+    description="A small printer package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ahmadtaj1754",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
