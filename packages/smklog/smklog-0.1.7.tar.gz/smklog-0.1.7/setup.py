1c3e14f4c1ae9e2fdf38b4dba6f1920373177961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:20 +0000

    Add package smklog/0.1.7/smklog-0.1.7.tar.gz

---
+++ packages/smklog/smklog-0.1.7.tar.gz/smklog-0.1.7/setup.py
@@ -0,0 +1,34 @@
+from os import path
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "smklog"
+VERSION = "0.1.7"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["arrow"]
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Samarkand logger",
+    author="David Chen",
+    author_email="david.chen@samarkand.global",
+    url="https://gitlab.com/samarkand-util/smklog",
+    keywords=["logger"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
