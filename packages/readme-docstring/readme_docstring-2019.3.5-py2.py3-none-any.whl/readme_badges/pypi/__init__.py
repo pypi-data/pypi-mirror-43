022d8fe22da7b46212784d774c7b04d898cea775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:03 +0000

    Add package readme-docstring/2019.3.5/readme_docstring-2019.3.5-py2.py3-none-any.whl

---
+++ packages/readme-docstring/readme_docstring-2019.3.5-py2.py3-none-any.whl/readme_badges/pypi/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import public
+import shields
+import setupcfg
+
+
+def name():
+    if os.path.exists("setup.cfg"):
+        return setupcfg.get("metadata", "name")
+    return os.popen("python setup.py --name").popen().strip()
+
+
+@public.add
+def pyversions():
+    """python versions badge"""
+    if os.path.exists("setup.py"):
+        return shields.pypi.Pyversions(name(), longCache=True)
+
+
+@public.add
+def v():
+    """pypi project version badge"""
+    if os.path.exists("setup.py"):
+        return shields.pypi.V(name(), maxAge=3600)
