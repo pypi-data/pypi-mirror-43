d79d437e3f5d33133c357c1aa39f7a796991f917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:51 +0000

    Add package biobb-adapters/0.1.4/biobb_adapters-0.1.4-py3-none-any.whl

---
+++ packages/biobb-adapters/biobb_adapters-0.1.4-py3-none-any.whl/biobb_adapters/pycompss/biobb_io/api/pdb_variants_pc.py
@@ -0,0 +1,14 @@
+import traceback
+from pycompss.api.task import task
+from pycompss.api.constraint import constraint
+from pycompss.api.parameter import FILE_IN, FILE_OUT
+from biobb_common.tools import file_utils as fu
+from biobb_md.mmb_api import pdb_variants
+
+@task(output_mutations_list_txt=FILE_OUT)
+def pdb_variants_pc(output_mutations_list_txt, properties, **kwargs):
+    try:
+        pdb_variants.MmbPdbVariants(output_mutations_list_txt=output_mutations_list_txt, properties=properties, **kwargs).launch()
+    except Exception:
+        traceback.print_exc()
+        fu.write_failed_output(output_mutations_list_txt)
