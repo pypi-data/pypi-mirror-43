a8c720313c8a5280c180c448cac5db39b610741b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package apimaticcalculatorpython1/1.0.2/apimaticcalculatorpython1-1.0.2.tar.gz

---
+++ packages/apimaticcalculatorpython1/apimaticcalculatorpython1-1.0.2.tar.gz/apimaticcalculatorpython1-1.0.2/apimaticcalculatorpython1/configuration.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+    apimaticcalculatorpython1
+
+    This file was automatically generated for testing by APIMATIC v2.0 ( https://apimatic.io ).
+"""
+
+from apimaticcalculatorpython1.api_helper import APIHelper
+
+
+class Configuration(object):
+
+    """A class used for configuring the SDK by a user.
+
+    This class need not be instantiated and all properties and methods
+    are accessible without instance creation.
+
+    """
+
+    # Set the array parameter serialization method
+    # (allowed: indexed, unindexed, plain, csv, tsv, psv)
+    array_serialization = "indexed"
+
+    # The base Uri for API calls
+    base_uri = 'http://examples.apimatic.io/apps/calculator'
+
+    # sd
+    # TODO: Set an appropriate value
+    gfdsfkl = None
+
+    dsfsdf = 'ds'
+
+    # The username to use with basic authentication
+    # TODO: Set an appropriate value
+    basic_auth_user_name = None
+
+    # The password to use with basic authentication
+    # TODO: Set an appropriate value
+    basic_auth_password = None
+
