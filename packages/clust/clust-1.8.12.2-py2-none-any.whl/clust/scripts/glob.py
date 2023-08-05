6e73f7f3f6664df7ac52897dce8dff52395321ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package clust/1.8.12.2/clust-1.8.12.2-py2-none-any.whl

---
+++ packages/clust/clust-1.8.12.2-py2-none-any.whl/clust/scripts/glob.py
@@ -0,0 +1,38 @@
+logfile = ''
+tmpfile = 'tmp'
+object_label_upper = 'Gene'
+object_label_lower = 'gene'
+outputwidth = 73
+version = 'v1.8.12.2'
+email = 'basel.abu-jamous@sensynehealth.com'
+maxgenesinsetforpdist = 10000
+
+
+def set_logfile(val):
+    global logfile
+    logfile = val
+
+
+def set_tmpfile(val):
+    global tmpfile
+    tmpfile = val
+
+
+def set_object_label_upper(val):
+    global object_label_upper
+    object_label_upper = val
+
+
+def set_object_label_lower(val):
+    global object_label_lower
+    object_label_lower = val
+
+
+def set_outputwith(val):
+    global outputwidth
+    outputwidth = val
+
+
+def set_version(val):
+    global version
+    version = val
