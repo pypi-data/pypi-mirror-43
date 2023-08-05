97965fa560e7ebdb36f6b821e9eef1f0f2bafe92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:27 +0000

    Add package tpro/0.13/tpro-0.13-py3-none-any.whl

---
+++ packages/tpro/tpro-0.13-py3-none-any.whl/transcript_processing/converters/amazon.py
@@ -0,0 +1,117 @@
+import json
+from typing import Dict, Optional
+
+from ..converter import TranscriptConverter
+from .. import helpers
+
+
+
+class AmazonConverter(TranscriptConverter):
+
+    name = 'amazon'
+
+    def __init__(self, json_data):
+        super().__init__(json_data)
+
+    def get_word_objects(self, json_data) -> list:
+        return json_data['results']['items']
+
+    def get_speaker_segments(self) -> Optional[Dict[float, str]]:
+        try:
+            segments = self.json_data['results']['speaker_labels']['segments']
+        except KeyError:
+            return None
+        else:
+            segment_dict = {}
+            for segment in segments:
+                word_level_segment = segment['items']
+                for word in word_level_segment:
+                    start_time = float(word['start_time'])
+                    speaker_label = word['speaker_label']
+                    speaker_id = ''
+                    for char in speaker_label:
+                        if char.isnumeric():
+                            speaker_id += char
+                    segment_dict[start_time] = int(speaker_id)
+            return segment_dict
+
+    @staticmethod
+    def get_word_start(word_object):
+        return float(word_object['start_time'])
+
+    @staticmethod
+    def get_word_end(word_object):
+        return float(word_object['end_time'])
+
+    @staticmethod
+    def get_word_confidence(word_object):
+        return float(word_object['alternatives'][0]['confidence'])
+
+    @staticmethod
+    def get_word_word(word_object) -> str:
+        word_word = word_object['alternatives'][0]['content']
+        if word_word == 'i':
+            # weird Amazon quirk
+            word_word = 'I'
+        return word_word
+
+    @classmethod
+    def get_speaker_id(cls, word_object, speaker_segments=None):
+        if speaker_segments is None:
+            return None
+        else:
+            word_start = cls.get_word_start(word_object)
+            return speaker_segments[word_start]
+
+    def convert_words(self, word_objects, words, tagged_words=None):
+        converted_words = []
+        speaker_segments = self.get_speaker_segments()
+
+        punc_before = False
+        punc_after = False
+
+        for i, w in enumerate(word_objects):
+            if w['type'] == 'punctuation':
+                continue
+            next_word_punc_after = None
+            word_obj = self.get_word_object(
+                    w, 
+                    i,
+                    tagged_words,
+                    word_objects,
+                    speaker_segments,
+                    )
+
+            if word_obj.next_word:
+                next_word = self.get_word_word(word_obj.next_word)
+                next_word_type = word_obj.next_word['type']
+                if next_word in ['.', ',']:
+                    punc_after = next_word
+                elif next_word_punc_after:
+                    punc_after = next_word_punc_after
+                    next_word_punc_after = None
+
+            if word_obj.word.lower() == 'you' and next_word == 'know':
+                prev_word = word_objects[i - 1]
+                if prev_word['type'] != 'punctuation':
+                    converted_words[-1]['punc_after'] = ','
+                if next_word_type != 'punctuation':
+                    next_word_punc_after = ','
+
+            converted_words.append({
+                'start': word_obj.start,
+                'end': word_obj.end,
+                'confidence': word_obj.confidence,
+                'word': word_obj.word,
+                'always_capitalized': self.check_if_always_capitalized(
+                    word_obj.word, 
+                    i,
+                    tagged_words),
+                'punc_after': punc_after,
+                'punc_before': punc_before,
+                'speaker_id': word_obj.speaker_id,
+            })
+
+            punc_after = False
+
+        return converted_words
---
+++ packages/tpro/tpro-0.13-py3-none-any.whl/transcript_processing/tpro.py
@@ -0,0 +1,37 @@
+import json
+
+import click
+
+from .converters import services
+from . import outputs
+from . import helpers
+
+output_choices =  [k for k, v in 
+                   outputs.__dict__.items()
+                   if callable(v)]
+
+@click.command()
+@click.option('-p', '--print-output', is_flag=True, default=True,
+        help='pretty print the transcript, breaks pipeability')
+@click.argument('transcript_data_path', type=click.File('r'))
+@click.argument('output_path', type=click.Path(writable=True, dir_okay=False))
+@click.argument('input_format', type=click.Choice(services.keys()))
+@click.argument('output_format', type=click.Choice(output_choices))
+def cli(print_output,
+        transcript_data_path,
+        output_path,
+        input_format,
+        output_format):
+
+    json_data = json.load(transcript_data_path)
+    service = services[input_format]
+
+    converter = service(json_data)
+    converter.convert()
+    converter.save(output_path, output_format)
+
+    if print_output:
+        with open(output_path) as fin:
+            click.echo(fin.read())
+
+    click.echo(f'☝☝☝ There\'s your transcript, which was saved to {output_path}.')
