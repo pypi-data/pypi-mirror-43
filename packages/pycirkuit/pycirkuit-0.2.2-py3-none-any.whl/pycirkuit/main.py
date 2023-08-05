8267092182fc049a1a8b12d3e72609752e91f115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:01 +0000

    Add package pycirkuit/0.2.2/pycirkuit-0.2.2-py3-none-any.whl

---
+++ packages/pycirkuit/pycirkuit-0.2.2-py3-none-any.whl/pycirkuit/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# coding: utf-8
+"""
+Main program entry point/function
+"""
+# Copyright (C) 2018-2019 Orestes Mas
+# This file is part of PyCirkuit.
+#
+# PyCirkuit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCirkuit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCirkuit.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# Main entry point
+def main():
+    # Third-party imports
+    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtCore import QCoreApplication, QTranslator, QLocale,  QLibraryInfo
+    
+    # Local application imports
+    from pycirkuit.ui.mainwindow import MainWindow
+    
+    # Resources for translation
+    from pycirkuit.resources import resources_rc
+
+    import sys
+    app = QApplication(sys.argv)
+
+    # First try to load the Qt-provided translations (used in some standard dialog strings)
+    qtTranslator = QTranslator()
+    filename = "qtbase_" + QLocale.system().name().split("_")[0]
+    if qtTranslator.load(filename, QLibraryInfo.location(QLibraryInfo.TranslationsPath), suffix=".qm"):
+        app.installTranslator(qtTranslator)
+    # Then load PyCirkuit translations
+    pycirkuitTranslator = QTranslator()
+    if pycirkuitTranslator.load(QLocale(), "pycirkuit", ".", ":/translations", ".qm"):
+        app.installTranslator(pycirkuitTranslator)
+        
+    # These two next values are passed to every instance of QSettings everywhere in the app
+    QCoreApplication.setOrganizationName("PyCirkuit")
+    QCoreApplication.setApplicationName("pycirkuit")
+    my_mainWindow = MainWindow()
+    my_mainWindow.show()
+    sys.exit(app.exec_())
+    
+    
+if __name__ == "__main__":
+    main()
