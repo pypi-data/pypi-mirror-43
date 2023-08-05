3ecb607cc7e869fef576f922836fdcc7ad845ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package pyNlple/0.6.6/pyNlple-0.6.6.tar.gz

---
+++ packages/pyNlple/pyNlple-0.6.6.tar.gz/pyNlple-0.6.6/setup.py
@@ -0,0 +1,30 @@
+﻿from setuptools import setup, find_packages
+from pynlple import __version__
+
+setup(
+    name='pyNlple',
+    packages=find_packages(exclude=['scripts']),
+    include_package_data=True,
+    version=__version__,
+    description='NLP procedures in python brought to you by YouScan.',
+    author='Paul Khudan',
+    author_email='pk@youscan.io',
+    company='YouScan Limited',
+    url='https://github.com/YouScan/pyNlple',
+    install_requires=['ipython',
+                      'numpy',
+                      'scikit-learn',
+                      'requests>=2.9.1',
+                      'pandas>=0.19.0',
+                      'gensim>=2.1.0',
+                      'elasticsearch>=2.0.0,<3.0.0',
+                      'pymorphy2',
+                      'emoji'],
+
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='pynlple.tests',
+
+)
