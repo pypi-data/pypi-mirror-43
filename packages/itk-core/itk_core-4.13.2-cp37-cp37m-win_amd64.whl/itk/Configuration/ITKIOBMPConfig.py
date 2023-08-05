e15ca11a539b5d4bbb801e6d3da6525480e361d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package itk-core/4.13.2/itk_core-4.13.2-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-core/itk_core-4.13.2-cp37-cp37m-win_amd64.whl/itk/Configuration/ITKIOBMPConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('BMPImageIO', 'itk::BMPImageIO', 'itkBMPImageIO', True),
+  ('BMPImageIOFactory', 'itk::BMPImageIOFactory', 'itkBMPImageIOFactory', True),
+)
