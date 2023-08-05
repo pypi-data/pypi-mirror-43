bba3755eaa17493907dfb854d13841e89c1eb08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:19 +0000

    Add package remedian/0.1.2/remedian-0.1.2.tar.gz

---
+++ packages/remedian/remedian-0.1.2.tar.gz/remedian-0.1.2/setup.py
@@ -0,0 +1,37 @@
+"""Setup remedian."""
+from setuptools import setup, find_packages
+from os import path
+import io
+
+here = path.abspath(path.dirname(__file__))
+
+# Get long description from README file
+with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='remedian',
+      version='0.1.2',
+      description='Remedian: robust averaging of large data sets',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/sappelhoff/remedian',
+      author='Stefan Appelhoff',
+      author_email='stefan.appelhoff@mailbox.org',
+      license='MIT',
+      classifiers=[
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research'
+      ],
+      keywords='remedian median memory efficient big data',
+      packages=find_packages(),
+      install_requires=['numpy>=1.14.1'],
+      python_requires='>=2.7',
+      extras_require={
+        'test': ['pytest>=3.6', 'pytest-cov']
+      },
+      project_urls={
+        'Bug Reports': 'https://github.com/sappelhoff/remedian/issues',
+        'Source': 'https://github.com/sappelhoff/remedian'
+      })
