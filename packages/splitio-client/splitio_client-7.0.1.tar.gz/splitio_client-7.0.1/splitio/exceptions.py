04cbf197745b05d2cb1e0427ca0d15194b35133b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:57 +0000

    Add package splitio-client/7.0.1/splitio_client-7.0.1.tar.gz

---
+++ packages/splitio-client/splitio_client-7.0.1.tar.gz/splitio_client-7.0.1/splitio/exceptions.py
@@ -0,0 +1,10 @@
+"""This module contains everything related to split.io exceptions"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+class TimeoutException(Exception):
+    pass
+
+
+class NetworkingException(Exception):
+    pass
