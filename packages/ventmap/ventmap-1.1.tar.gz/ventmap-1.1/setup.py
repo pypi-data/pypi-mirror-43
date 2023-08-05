bb9173dd9e0480f7cd0d7095e633501fcebd676e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package ventmap/1.1/ventmap-1.1.tar.gz

---
+++ packages/ventmap/ventmap-1.1.tar.gz/ventmap-1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='ventmap',
+      version="1.1",
+      description='Ventilator Multi-Analytic Platform for analysis of ventilator waveform data',
+      packages=find_packages(exclude=["*tests*"]),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'scipy',
+      ],
+      entry_points={
+          'console_scripts': [
+              'clear_null_bytes=ventmap.clear_null_bytes:main',
+              'cut_breath_section=ventmap.cut_breath_section:main',
+              'breath_meta=ventmap.breath_meta:main',
+          ]
+      },
+      include_package_data=True,
+      )
