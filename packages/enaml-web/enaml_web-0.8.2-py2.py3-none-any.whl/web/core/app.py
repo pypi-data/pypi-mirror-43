182716d36cb856296abf6002fa7cef89ef929917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:15 +0000

    Add package enaml-web/0.8.2/enaml_web-0.8.2-py2.py3-none-any.whl

---
+++ packages/enaml-web/enaml_web-0.8.2-py2.py3-none-any.whl/web/core/app.py
@@ -0,0 +1,38 @@
+"""
+Copyright (c) 2017, Jairus Martin.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE.text, distributed with this software.
+
+Created on Apr 17, 2017
+
+@author: jrm
+"""
+import logging
+from atom.api import Value, Instance
+from enaml.application import Application, ProxyResolver
+from web.impl import lxml_components
+
+
+class WebApplication(Application):
+    """ Base enaml web application that uses the widgets defined in 
+    `web.components.html`
+
+    """
+    #: Logger
+    logger = Instance(logging.Logger)
+
+    #: Database
+    database = Value()
+    
+    def _default_logger(self):
+        return logging.getLogger('web')
+    
+    def __init__(self, *args, **kwargs):
+        """ Initialize a WebApplication.
+
+        """
+        super(WebApplication, self).__init__(*args, **kwargs)
+        self.resolver = ProxyResolver(factories=lxml_components.FACTORIES)
+        
---
+++ packages/enaml-web/enaml_web-0.8.2-py2.py3-none-any.whl/web/impl/lxml_md.py
@@ -0,0 +1,49 @@
+"""
+Copyright (c) 2017, Jairus Martin.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE.text, distributed with this software.
+
+Created on Aug 2, 2017
+
+@author: jrm
+"""
+import markdown
+from .lxml_raw import RawComponent
+from web.components.md import ProxyMarkdown
+
+
+class MarkdownComponent(RawComponent, ProxyMarkdown):
+    """ A block for rendering Markdown source. """
+    
+    def _refresh_source(self):
+        d = self.declaration
+        md = d.source
+
+        #: Parse md and put in a root node
+        source = markdown.markdown(
+            md,
+            extensions=d.extensions,
+            extension_configs=d.extension_configs
+        )
+        #: Parse source to html
+        super(MarkdownComponent, self).set_source(source)
+
+    def set_source(self, source):
+        self._refresh_source()
+
+    def set_safe_mode(self, mode):
+        self._refresh_source()
+
+    def set_output_format(self, format):
+        self._refresh_source()
+
+    def set_tab_length(self, length):
+        self._refresh_source()
+
+    def set_extensions(self, extensions):
+        self._refresh_source()
+
+    def set_extension_configs(self, config):
+        self._refresh_source()
