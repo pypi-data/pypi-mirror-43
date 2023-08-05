f4e19f43840c5a9bed8931fc606dd9310cd917aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:37 +0000

    Add package altered-states/1.0.0/altered.states-1.0.0-py2.7.egg

---
+++ packages/altered-states/altered.states-1.0.0-py2.7.egg/altered/meta.py
@@ -0,0 +1,19 @@
+name='altered.states'
+version='1.0.0'
+licence='ISC'
+author='Jacob Oscarson'
+author_email='jacob@414soft.com'
+description = 'Monkey patching for Humans'
+long_description = """
+=================================
+Python monkey-patching for Humans
+=================================
+
+*Altered States* tries to take the concept of "Python for Humans"
+(http://python-for-humans.heroku.com/) to the world of monkey
+patching.
+
+Read more here:
+https://github.com/Plexical/altered.states/blob/master/README.rst
+"""
+url = 'https://github.com/Plexical/altered.states'
