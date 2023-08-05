c2e76d48768609f6cd1a09097bfaed3cab484911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package edc-appointment/0.2.41/edc_appointment-0.2.41-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.2.41-py3-none-any.whl/edc_appointment/views/home_view.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.views.generic.base import TemplateView
+from edc_base.view_mixins import EdcBaseViewMixin
+
+
+class HomeView(EdcBaseViewMixin, TemplateView):
+
+    template_name = f"edc_appointment/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
