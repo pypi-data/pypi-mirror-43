3a1b976a3980bc21422b31e3281d33ac06eb9f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package macdaily/2019.3.8/macdaily-2019.3.8-cp36-none-macosx_10_14_x86_64.whl

---
+++ packages/macdaily/macdaily-2019.3.8-cp36-none-macosx_10_14_x86_64.whl/macdaily/util/tools/deco.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import functools
+import os
+import platform
+import queue
+import signal
+import sys
+
+from macdaily.util.compat import multiprocessing, threading
+from macdaily.util.const.term import red, reset, yellow
+from macdaily.util.error import ChildExit, TimeExpired, UnsupportedOS
+from macdaily.util.tools.misc import kill
+from macdaily.util.tools.print import print_term
+
+# error-not-raised flag
+ERR_FLAG = True
+# func-not-called flag
+FUNC_FLAG = True
+# timeout interval
+TIMEOUT = int(os.environ.get('TIMEOUT', '60'))
+
+
+def beholder(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if platform.system() != 'Darwin':
+            print_term('macdaily: error: script runs only on macOS', os.devnull)
+            raise UnsupportedOS
+
+        def _finale(epilogue):
+            global FUNC_FLAG
+            if FUNC_FLAG:
+                FUNC_FLAG = False
+                sys.stdout.write(reset)
+                sys.stderr.write(reset)
+                kill(os.getpid(), signal.SIGSTOP)
+            return epilogue
+
+        def _funeral(last_words):
+            global ERR_FLAG
+            ERR_FLAG = False
+            # sys.tracebacklimit = 0
+            sys.stdout.write(reset)
+            sys.stderr.write(reset)
+            kill(os.getpid(), signal.SIGKILL)
+            print(last_words, file=sys.stderr)
+
+        try:
+            return _finale(func(*args, **kwargs))
+        except KeyboardInterrupt:
+            if ERR_FLAG:
+                _funeral(f'macdaily: {red}error{reset}: operation interrupted')
+            raise
+        except Exception:
+            if ERR_FLAG:
+                _funeral(f'macdaily: {red}error{reset}: operation failed')
+            raise
+    return wrapper
+
+
+def retry(default=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if sys.stdin.isatty():  # pylint: disable=no-else-return
+                return func(*args, **kwargs)
+            else:
+                QUEUE = multiprocessing.Queue(1)
+                kwargs['queue'] = QUEUE
+                for _ in range(3):
+                    proc = multiprocessing.Process(target=func, args=args, kwargs=kwargs)
+                    timer = threading.Timer(TIMEOUT, function=proc.kill)
+                    timer.start()
+                    proc.start()
+                    proc.join()
+                    timer.cancel()
+                    if proc.exitcode == 0:
+                        break
+                    if proc.exitcode != 9:
+                        print_term(f'macdaily: {yellow}misc{reset}: function {func.__qualname__!r} '
+                                   f'exits with exit status {proc.exitcode} on child process', os.devnull)
+                        raise ChildExit
+                else:
+                    print_term(f'macdaily: {red}misc{reset}: function {func.__qualname__!r} '
+                               f'retry timeout after {TIMEOUT} seconds', os.devnull)
+                    raise TimeExpired
+                try:
+                    return QUEUE.get(block=False)
+                except queue.Empty:
+                    return default
+        return wrapper
+    return decorator
---
+++ packages/macdaily/macdaily-2019.3.8-cp36-none-macosx_10_14_x86_64.whl/macdaily/util/tools/get.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import getpass
+import os
+import sys
+
+from macdaily.util.compat import subprocess
+from macdaily.util.const.macro import PASS, USER
+from macdaily.util.const.term import reset
+from macdaily.util.tools.deco import retry
+
+
+@retry('N')
+def get_input(confirm, prompt='Input: ', *, prefix='', suffix='', queue=None):
+    if sys.stdin.isatty():
+        try:
+            return input(f'{prompt}{suffix}')
+        except KeyboardInterrupt:
+            print(reset)
+            raise
+    try:
+        subprocess.check_call(['osascript', confirm, f'{prefix}{prompt}'],
+                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        RETURN = 'N'
+    else:
+        RETURN = 'Y'
+    finally:
+        if queue is not None:
+            queue.put(RETURN)
+    return RETURN
+
+
+@retry(PASS)
+def get_pass(askpass, queue=None):
+    SUDO_PASSWORD = os.environ.get('SUDO_PASSWORD')
+    if SUDO_PASSWORD is not None:
+        if queue is not None:
+            queue.put(SUDO_PASSWORD)
+        return SUDO_PASSWORD
+    if sys.stdin.isatty():
+        try:
+            return getpass.getpass(prompt='Password:')
+        except KeyboardInterrupt:
+            print(reset)
+            raise
+    RETURN = subprocess.check_output([askpass, f'🔑 Enter your password for {USER}.'],  # pylint: disable=E1101
+                                     stderr=subprocess.DEVNULL).strip().decode()
+    if queue is not None:
+        queue.put(RETURN)
+    return RETURN
