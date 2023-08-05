de9a9250c41747012d069b4212cd2bd19abf8b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package jhTAlib/20190307.0/jhTAlib-20190307.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20190307.0.tar.gz/jhTAlib-20190307.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jhTAlib',
+    version='20190307.0',
+    description='Technical Analysis Library Time-Series',
+    long_description='Technical Analysis Library Time-Series',
+    keywords=['Technical', 'Analysis', 'Library', 'Time-Series'],
+    url='https://github.com/joosthoeks/jhTAlib',
+    author='Joost Hoeks',
+    author_email='joosthoeks@gmail.com',
+    license='GNU',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False
+)
+
