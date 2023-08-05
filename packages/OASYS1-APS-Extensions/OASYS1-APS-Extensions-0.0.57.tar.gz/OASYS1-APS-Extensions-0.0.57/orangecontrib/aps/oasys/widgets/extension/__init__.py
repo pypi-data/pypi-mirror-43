cb3ec925c65af38b3b64ad65484e974d8523c6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:08 +0000

    Add package OASYS1-APS-Extensions/0.0.57/OASYS1-APS-Extensions-0.0.57.tar.gz

---
+++ packages/OASYS1-APS-Extensions/OASYS1-APS-Extensions-0.0.57.tar.gz/OASYS1-APS-Extensions-0.0.57/orangecontrib/aps/oasys/widgets/extension/__init__.py
@@ -0,0 +1,9 @@
+NAME = "APS OASYS Extension"
+
+DESCRIPTION = "Widgets for APS Oasys Extension."
+
+BACKGROUND = "#FAAC58"#"#c9901e"
+
+ICON = "icons/extension.png"
+
+PRIORITY = 3
---
+++ packages/OASYS1-APS-Extensions/OASYS1-APS-Extensions-0.0.57.tar.gz/OASYS1-APS-Extensions-0.0.57/orangecontrib/aps/srw/widgets/extension/__init__.py
@@ -0,0 +1,9 @@
+NAME = "APS SRW Extension"
+
+DESCRIPTION = "Widgets for APS SRW Extension."
+
+BACKGROUND = "#b8bcdb"#"#798a8e"#
+
+ICON = "icons/extension.png"
+
+PRIORITY = 205
