d955512790537df0bf406593e32989b626557e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:38 +0000

    Add package anna-tasks/1.0.2/anna_tasks-1.0.2.tar.gz

---
+++ packages/anna-tasks/anna_tasks-1.0.2.tar.gz/anna_tasks-1.0.2/anna_tasks/test/config.py
@@ -0,0 +1,14 @@
+url = "http://annahub.se:8000/test/"
+sequence = (
+	"send_keys",
+	"submit",
+	"click",
+	"hover",
+	"wait",
+	"switch_to",
+	"iframe.send_keys",
+	"iframe.submit",
+	"iframe.click",
+	"iframe.hover",
+	"iframe.wait"
+)
