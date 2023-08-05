791d113f04a8b45016b76ca247973e8f23e3a090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package form-designer/0.14.0/form_designer-0.14.0-py2.py3-none-any.whl

---
+++ packages/form-designer/form_designer-0.14.0-py2.py3-none-any.whl/form_designer/default_field_types.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals
+
+from functools import partial
+
+from django import forms
+from django.apps import apps
+from django.utils.translation import ugettext_lazy as _
+
+
+FIELD_TYPES = [
+    ("text", _("text"), forms.CharField),
+    ("email", _("e-mail address"), forms.EmailField),
+    ("longtext", _("long text"), partial(forms.CharField, widget=forms.Textarea)),
+    ("checkbox", _("checkbox"), partial(forms.BooleanField, required=False)),
+    ("select", _("select"), partial(forms.ChoiceField, required=False)),
+    ("radio", _("radio"), partial(forms.ChoiceField, widget=forms.RadioSelect)),
+    (
+        "multiple-select",
+        _("multiple select"),
+        partial(forms.MultipleChoiceField, widget=forms.CheckboxSelectMultiple),
+    ),
+    ("hidden", _("hidden"), partial(forms.CharField, widget=forms.HiddenInput)),
+]
+
+# Add recaptcha field if available
+if apps.is_installed("captcha"):  # pragma: no cover
+    try:
+        from captcha.fields import ReCaptchaField
+        from captcha.widgets import ReCaptchaV2Checkbox
+    except ImportError:
+        pass
+    else:
+        FIELD_TYPES.append(
+            (
+                "recaptcha",
+                _("recaptcha"),
+                partial(ReCaptchaField, widget=ReCaptchaV2Checkbox),
+            )
+        )
