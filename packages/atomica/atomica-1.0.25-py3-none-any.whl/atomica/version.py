bb564f18843a3f7fd1b4c978622469c0d7023fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package atomica/1.0.25/atomica-1.0.25-py3-none-any.whl

---
+++ packages/atomica/atomica-1.0.25-py3-none-any.whl/atomica/version.py
@@ -0,0 +1,12 @@
+"""
+Atomica version file.
+
+Standard location for module version number and date.
+"""
+
+import sciris as sc
+
+version = "1.0.25"
+versiondate = "2019-02-16"
+gitinfo = sc.gitinfo(__file__, verbose=False)
+
