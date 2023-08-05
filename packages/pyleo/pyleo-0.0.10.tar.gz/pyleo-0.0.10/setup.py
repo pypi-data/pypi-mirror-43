ae820aeae541c2a58e1e7e511d98923f620d956c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:19 +0000

    Add package pyleo/0.0.10/pyleo-0.0.10.tar.gz

---
+++ packages/pyleo/pyleo-0.0.10.tar.gz/pyleo-0.0.10/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from pyleo import constants
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='pyleo',
+    version=constants.PYLEO_VERSION,
+    description='',
+    author='Andrew Dorokhin',
+    author_email='andrew@dorokhin.moscow',
+    url='http://github.com/dorokhin/pyleo',
+    packages=['pyleo',
+              'pyleo.abstractions'
+              ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=[],
+    python_requires='>=3.4',
+    classifiers=[
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: 3.5",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: 3.7",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Development Status :: 1 - Planning",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development :: Libraries"],
+    package_data={
+        '': ['*.md', '*.txt', '*.json']
+    },
+    keywords='pyleo api client',
+    license='MIT'
+)
