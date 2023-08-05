03eea5ffb3f080ca24e0217191165326095dbfac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package bouchon/0.1.2/bouchon-0.1.2.tar.gz

---
+++ packages/bouchon/bouchon-0.1.2.tar.gz/bouchon-0.1.2/bouchon/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+__version__ = "0.1.0"
+
+
+@app.route("/")
+@app.route("/<path:path>")
+def show_subpath(path=""):
+    return jsonify(
+        {
+            "request": {
+                "method": request.method,
+                "path": request.path,
+                "full_path": request.full_path,
+                "headers": dict(sorted(request.headers.items())),
+            },
+            "system": {"env": dict(sorted(os.environ.items()))},
+        }
+    )
