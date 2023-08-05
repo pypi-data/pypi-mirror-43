5258aa0bec8f6de8fe37840bbca02497aa931dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package ProtParCon/2.0/ProtParCon-2.0-py3-none-any.whl

---
+++ packages/ProtParCon/ProtParCon-2.0-py3-none-any.whl/ProtParCon/__init__.py
@@ -0,0 +1,6 @@
+from ProtParCon.imc import imc, detect
+from ProtParCon.asr import asr
+from ProtParCon.aut import aut
+from ProtParCon.mlt import mlt
+from ProtParCon.msa import msa
+from ProtParCon.sim import sim
