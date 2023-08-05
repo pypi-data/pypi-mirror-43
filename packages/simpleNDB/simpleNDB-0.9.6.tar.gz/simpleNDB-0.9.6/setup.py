27f5b0115745bfd7c3da29b283abe0e17a636274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:33 +0000

    Add package simpleNDB/0.9.6/simpleNDB-0.9.6.tar.gz

---
+++ packages/simpleNDB/simpleNDB-0.9.6.tar.gz/simpleNDB-0.9.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpleNDB",
+    version= "0.9.6",
+    author="Paul Harwood",
+    author_email="runette@gmail.com",
+    description="A very simple package to make Google Cloud Clients look more like appengine.api and NDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/runette/simpleNDB",
+    install_requires=[
+        "grpcio",
+        "google-cloud-datastore",
+        "google-cloud-storage",
+        "Pillow",
+        "attrdict"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
