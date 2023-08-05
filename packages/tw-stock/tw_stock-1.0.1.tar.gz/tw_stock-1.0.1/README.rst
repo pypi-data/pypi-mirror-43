7182dac7c1acea44238a8fcc3053a758dcd9eec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package tw-stock/1.0.1/tw_stock-1.0.1.tar.gz

---
+++ packages/tw-stock/tw_stock-1.0.1.tar.gz/tw_stock-1.0.1/README.rst
@@ -0,0 +1,8 @@
+Get Taiwan's stock info in real-time
+
+Install:
+  pip install tw-stock
+
+Example:
+  from tw_stock.stock import get_realtime
+  print(get_realtime(2330))
---
+++ packages/tw-stock/tw_stock-1.0.1.tar.gz/tw_stock-1.0.1/setup.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import codecs
+import os
+import sys
+from setuptools import setup, find_packages
+
+with open('README.rst', 'rb') as fp:
+    readme = fp.read()
+
+VERSION = "1.0.1"
+
+LICENSE = "MIT"
+
+setup(
+    name='tw_stock',
+    version=VERSION,
+    description=(
+        'Get Taiwan\'s stock info in real-time'
+    ),
+    long_description=readme,
+    author='Jasper',
+    author_email='jiunway@gmail.com',
+    maintainer='Jasper',
+    maintainer_email='jiunway@gmail.com',
+    license=LICENSE,
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/jiunway/tw_stock'
+)
