de66c4fe178487691e0ed47401b6f1105b28bdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package teslajsonpy/0.0.24/teslajsonpy-0.0.24.tar.gz

---
+++ packages/teslajsonpy/teslajsonpy-0.0.24.tar.gz/teslajsonpy-0.0.24/README.rst
@@ -0,0 +1,8 @@
+teslajsonpy
+===============
+
+Python module for Tesla API
+
+This is a work in progress.
+
+***** this library based on https://github.com/gglockner/teslajson code
---
+++ packages/teslajsonpy/teslajsonpy-0.0.24.tar.gz/teslajsonpy-0.0.24/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(
+    name='teslajsonpy',
+    version='0.0.24',
+    packages=['teslajsonpy'],
+    include_package_data=True,
+    python_requires='>=3',
+    license='WTFPL',
+    description='A library to work with Tesla API.',
+    long_description='A library to work with Tesla car API.',
+    url='https://github.com/zabuldon/teslajsonpy',
+    author='Sergey Isachenko',
+    author_email='sergey.isachenkol@bool.by',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet',
+    ],
+)
