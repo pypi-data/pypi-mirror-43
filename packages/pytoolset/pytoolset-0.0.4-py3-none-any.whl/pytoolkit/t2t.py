57ab4c1b7317b531933adb69a4dcbcad45ca63ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:54 +0000

    Add package pytoolset/0.0.4/pytoolset-0.0.4-py3-none-any.whl

---
+++ packages/pytoolset/pytoolset-0.0.4-py3-none-any.whl/pytoolkit/t2t.py
@@ -0,0 +1,25 @@
+ """
+ time 2 time
+
+ date: 2019.03.06
+ """
+ import pandas as pd
+ import datetime
+ import time
+
+
+ def pd_t2t(pd_time):
+     """
+     pandas Timestamp 2 datetime.datetime
+     """
+     time_stamp = pd_time.value // (1000*1000*1000)
+     return datetime.datetime.utcfromtimestamp(time_stamp)
+
+
+ def main():
+     pass
+
+
+ if __name__ == "__main__":
+     main()
+
