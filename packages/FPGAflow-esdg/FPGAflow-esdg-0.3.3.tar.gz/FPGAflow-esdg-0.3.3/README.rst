1b14169747c66fb7d092d37c058e6725dae4b5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package FPGAflow-esdg/0.3.3/FPGAflow-esdg-0.3.3.tar.gz

---
+++ packages/FPGAflow-esdg/FPGAflow-esdg-0.3.3.tar.gz/FPGAflow-esdg-0.3.3/README.rst
@@ -0,0 +1,16 @@
+######
+Readme
+######
+
+``FPGAFlow`` is a python script to handle a complete FPGA design flow where the design files, libraries and
+supplementary tools are stored in a source code repository.
+
+`Source Code Repository <https://bitbucket.org/mjroberts/fpgaflow/src/master/docs/>`_
+`Documentation <https://fpgaflow.readthedocs.io/en/latest/index.html>`_
+
+License
+-------
+
+`BSD`_ © 2018-2019 Science and Technology Facilities Council & contributors
+
+.. _BSD: LICENSE
---
+++ packages/FPGAflow-esdg/FPGAflow-esdg-0.3.3.tar.gz/FPGAflow-esdg-0.3.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+if __name__ == "__main__":
+    with open("README.rst", "r") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="FPGAflow-esdg",
+        version="0.3.3",
+        author="Electronic System Design Group",
+        author_email="fpgaflow@stfc.ac.uk",
+        description="FPGAFlow scripted tool-flow for FPGA firmware development",
+        long_description=long_description,
+        long_description_content_type="text/x-rst",
+        url="https://bitbucket.org/mjroberts/fpgaflow",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 2.7",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: POSIX :: Linux",
+        ],
+    )
