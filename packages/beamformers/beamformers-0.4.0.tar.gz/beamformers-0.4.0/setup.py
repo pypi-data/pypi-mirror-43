03620dd36e84228817a08315477545d90df9e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package beamformers/0.4.0/beamformers-0.4.0.tar.gz

---
+++ packages/beamformers/beamformers-0.4.0.tar.gz/beamformers-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='beamformers',
+      version='0.4.0',
+      description='Beamformers for audio source separation and speech enhancement',
+      url='https://github.com/Enny1991/beamformers',
+      author='Enea Ceolini (UZH Zurich)',
+      author_email='enea.ceolini@ini.uzh.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'scipy',
+            'numpy',
+            'soundfile'
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
