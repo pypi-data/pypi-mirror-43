571cb0afd8c637e30bc967e8dc7fdbcef6d11e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:35 +0000

    Add package procfile-manager/0.0.2/procfile-manager-0.0.2.tar.gz

---
+++ packages/procfile-manager/procfile-manager-0.0.2.tar.gz/procfile-manager-0.0.2/setup.py
@@ -0,0 +1,61 @@
+import os
+import re
+import setuptools
+
+NAME             = "procfile-manager"
+AUTHOR           = "Christophe VG"
+AUTHOR_EMAIL     = "contact@christophe.vg"
+DESCRIPTION      = "A Python module to manage Procfiles, running them in the first place, with as little restrictions as possible."
+LICENSE          = "MIT"
+KEYWORDS         = "procfile"
+URL              = "https://github.com/christophevg/py-" + NAME
+README           = ".github/README.md"
+CLASSIFIERS      = [
+  "Environment :: Console",
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Developers",
+  "Intended Audience :: System Administrators",
+  "Topic :: Software Development",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: Python",
+  "Programming Language :: Python :: 2.7",
+  "Programming Language :: Python :: 3.7"
+]
+INSTALL_REQUIRES = [
+  "python-dotenv"
+]
+ENTRY_POINTS     = {
+  "console_scripts": [
+    "procfile-manager=procfile_manager.command:main"
+  ]
+}
+SCRIPTS          = []
+
+HERE = os.path.dirname(__file__)
+
+def read(file):
+  with open(os.path.join(HERE, file), "r") as fh:
+    return fh.read()
+
+VERSION = re.search(
+  r'^__version__ = [\'"]([^\'"]*)[\'"]',
+  read(NAME.replace("-", "_") + "/__init__.py")
+).group(1)
+
+LONG_DESCRIPTION = read(README)
+
+if __name__ == "__main__":
+  setuptools.setup(name=NAME,
+        version=VERSION,
+        packages=setuptools.find_packages(),
+        author=AUTHOR,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        license=LICENSE,
+        keywords=KEYWORDS,
+        url=URL,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        entry_points=ENTRY_POINTS,
+        scripts=SCRIPTS)
