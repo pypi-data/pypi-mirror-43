c0c3505efad857bd6abbd68c9871c552a316f0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:01 +0000

    Add package my-module-yh-09-3-12/1.0/my_module_yh_09_3_12-1.0.tar.gz

---
+++ packages/my-module-yh-09-3-12/my_module_yh_09_3_12-1.0.tar.gz/my_module_yh_09_3_12-1.0/my_module_yh_09_3_12/setup.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+_author_ = '航少'
+from distutils.core import setup
+setup(
+name='my_module_yh_09_3_12',
+version='1.0',
+packages=['my_module_yh_09_3_12'],
+author='Adhere',
+author_email="544620552@qq.com",
+url="http://www.py2ai.com",
+)
