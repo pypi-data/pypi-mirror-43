7c91684bea6392e136b27ab708e2f428437198ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:24 +0000

    Add package mypackagebapat/0.4/mypackagebapat-0.4.tar.gz

---
+++ packages/mypackagebapat/mypackagebapat-0.4.tar.gz/mypackagebapat-0.4/mypackagebapat/mymath.py
@@ -0,0 +1,13 @@
+class one:
+<<<<<<< HEAD
+ def addition(self, a, b):
+  return a+b
+
+ def subtraction(self, a,b):
+=======
+ def addition(a, b):
+  return a+b
+
+ def subtraction(a,b):
+>>>>>>> 8162bbab90facba2d49697037af78414bd4ada39
+  return a-b
