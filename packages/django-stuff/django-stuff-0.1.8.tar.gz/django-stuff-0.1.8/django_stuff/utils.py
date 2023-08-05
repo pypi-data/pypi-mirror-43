aa97533735e6b1926f1c5ad516b5ed979ccc4343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:34 +0000

    Add package django-stuff/0.1.8/django-stuff-0.1.8.tar.gz

---
+++ packages/django-stuff/django-stuff-0.1.8.tar.gz/django-stuff-0.1.8/django_stuff/utils.py
@@ -0,0 +1,87 @@
+import hashlib
+import re
+import time
+import string
+
+from django.utils.crypto import get_random_string
+from django.core.validators import EMPTY_VALUES
+
+
+def sanitize_digits(raw_number):
+    return re.sub(r"[^0-9]", "", raw_number)
+
+
+def is_equal(words):
+    return all(c == words[i - 1] for i, c in enumerate(words) if i > 0)
+
+
+def dv_maker(v):
+    if v >= 2:
+        return 11 - v
+    return 0
+
+
+def validate_cpf(value):
+    """Value can be either a string in the format XXX.XXX.XXX-XX or an 11-digit number."""
+    if value in EMPTY_VALUES:
+        return ''
+    orig_value = value[:]
+    if not value.isdigit():
+        value = sanitize_digits(value)
+        if not value:
+            return False
+
+    if len(value) != 11:
+        return False
+    orig_dv = value[-2:]
+
+    new_1dv = sum([i * int(value[idx])
+                   for idx, i in enumerate(range(10, 1, -1))])
+    new_1dv = dv_maker(new_1dv % 11)
+    value = value[:-2] + str(new_1dv) + value[-1]
+    new_2dv = sum([i * int(value[idx])
+                   for idx, i in enumerate(range(11, 1, -1))])
+    new_2dv = dv_maker(new_2dv % 11)
+    value = value[:-1] + str(new_2dv)
+    if value[-2:] != orig_dv:
+        return False
+    if value.count(value[0]) == 11:
+        return False
+    return orig_value
+
+
+def validate_cnpj(value):
+    """Value can be either a string in the format XX.XXX.XXX/XXXX-XX or a group of 14 characters."""
+    if value in EMPTY_VALUES:
+        return ''
+    orig_value = value[:]
+    if not value.isdigit():
+        value = sanitize_digits(value)
+        if not value:
+            return False
+
+    if len(value) != 14:
+        return False
+    orig_dv = value[-2:]
+
+    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(list(range(5, 1, -1)) + list(range(9, 1, -1)))])
+    new_1dv = dv_maker(new_1dv % 11)
+    value = value[:-2] + str(new_1dv) + value[-1]
+    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(list(range(6, 1, -1)) + list(range(9, 1, -1)))])
+    new_2dv = dv_maker(new_2dv % 11)
+    value = value[:-1] + str(new_2dv)
+    if value[-2:] != orig_dv:
+        return False
+    return orig_value
+
+
+def generate_random_code(length=10):
+    allowed = string.ascii_uppercase + string.digits
+    code = get_random_string(length=length, allowed_chars=allowed)
+    return code
+
+
+def generate_md5_hashcode(key_word):
+    keyword = '{}-{}'.format(key_word, time.time())
+    hashcode = hashlib.md5(keyword.encode('utf-8')).hexdigest()
+    return hashcode
---
+++ packages/django-stuff/django-stuff-0.1.8.tar.gz/django-stuff-0.1.8/docs/source/index.rst
@@ -0,0 +1,59 @@
+Welcome to Django Stuff's documentation!
+============================================
+
+Django Stuff is a collection of tools and utilities to make your development with Django simpler.
+
+Features
+--------
+
+- TimeStamp and History models to giving you information like when your record wore created/updated and History Changes
+- UUID Model as primary key or not instead of sequence ID.
+- Serializer model to return a dict with all data of your django instance.
+- And many other stuff. For more information, see our documentation following the links below.
+
+
+Quickstart
+----------
+
+.. toctree::
+    :maxdepth: 1
+
+    quickstart/installation.rst
+
+
+User Guide
+----------
+
+.. toctree::
+    :maxdepth: 2
+
+    fields.rst
+    forms.rst
+    models/index.rst
+    utils.rst
+
+
+Development
+-----------
+
+.. toctree::
+    :maxdepth: 1
+
+    development/installation.rst
+    development/release.rst
+
+
+Downloads
+---------
+
+- `PDF <https://readthedocs.org/projects/django-stuff/downloads/pdf/latest/>`_
+
+- `Epub <https://readthedocs.org/projects/django-stuff/downloads/epub/latest/>`_
+
+
+Other
+-----
+
+.. toctree::
+
+    changelog.rst
