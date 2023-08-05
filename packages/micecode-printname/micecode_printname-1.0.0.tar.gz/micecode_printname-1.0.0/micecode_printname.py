2b9e11c1730bc052762a14e97e74e50ad4f29150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:41 +0000

    Add package micecode-printname/1.0.0/micecode_printname-1.0.0.tar.gz

---
+++ packages/micecode-printname/micecode_printname-1.0.0.tar.gz/micecode_printname-1.0.0/micecode_printname.py
@@ -0,0 +1,5 @@
+"""This is a simple test module by MiceCode wich prints "Hello {name}"."""
+def micecode_printname(name):
+    """This function takes a  name and print "Hello {name}"."""
+    print("Hello " + name)
+
---
+++ packages/micecode-printname/micecode_printname-1.0.0.tar.gz/micecode_printname-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name	 	=   'micecode_printname',
+	version		=   '1.0.0',
+	py_modules	=   ['micecode_printname'],
+	author		=   'MiceCode',
+	author_email	=   'wvmuis@gmail.com',
+	description	=   'A simple test module to print "Hello {name}".',
+    )
