90054c39ba69e280967dd87e20d92e11e7a50cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:17 +0000

    Add package tap-freshsales/0.1.1/tap-freshsales-0.1.1.tar.gz

---
+++ packages/tap-freshsales/tap-freshsales-0.1.1.tar.gz/tap-freshsales-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-freshsales",
+    version="0.1.1",
+    description="Singer.io tap for extracting FreshSales Data",
+    author="Tisham Dhar (Lori)",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_freshsales"],
+    install_requires=[
+        "singer-python==5.1.1",
+        "requests==2.20.0",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-freshsales=tap_freshsales:main
+    """,
+    packages=["tap_freshsales"],
+    package_data = {
+        "schemas": ["tap_freshsales/schemas/*.json"]
+    },
+    include_package_data=True,
+)
