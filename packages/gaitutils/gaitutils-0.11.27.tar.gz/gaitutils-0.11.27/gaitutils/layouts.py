019663694ecc8ec05587a4065f77a200b108afd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package gaitutils/0.11.27/gaitutils-0.11.27.tar.gz

---
+++ packages/gaitutils/gaitutils-0.11.27.tar.gz/gaitutils-0.11.27/gaitutils/layouts.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Handling layouts.
+
+@author: Jussi (jnu@iki.fi)
+"""
+
+from builtins import str
+from builtins import zip
+import logging
+
+from gaitutils import cfg, GaitDataError
+
+logger = logging.getLogger(__name__)
+
+
+def rm_dead_channels(emg, layout):
+    """ From EMG layout, remove rows with no valid EMG data """
+    layout_ = list()
+    for row in layout:
+        # accept channels w/ status ok, or anything that is NOT a
+        # preconfigured EMG channel
+        if any([ch not in cfg.emg.channel_labels or emg.status_ok(ch) for ch in row]):
+            layout_.append(row)
+        else:
+            logger.debug('no valid data for %s, removed row' % str(row))
+    if not layout_:
+        logger.warning('removed all - no valid EMG channels')
+    return layout_
+
+
+def rm_dead_channels_multitrial(emgs, layout):
+    """From layout, drop rows that do not have good data in any of the
+    EMG() instances given """
+    chs_ok = None
+    for i, emg in enumerate(emgs):
+        # accept channels w/ status ok, or anything that is NOT a
+        # preconfigured EMG channel
+        chs_ok_ = [ch not in cfg.emg.channel_labels or emg.status_ok(ch) for
+                   row in layout for ch in row]
+        # previously OK chs propagated as ok
+        chs_ok = ([a or b for a, b in zip(chs_ok, chs_ok_)] if i > 0 else
+                  chs_ok_)
+    if not chs_ok:
+        raise GaitDataError('No acceptable channels in any of the EMGs')
+    rowlen = len(layout[0])
+    lout = zip(*[iter(chs_ok)]*rowlen)  # grouper recipe from itertools
+    rows_ok = [any(row) for row in lout]
+    layout = [row for i, row in enumerate(layout) if rows_ok[i]]
+    return layout
+
+
+def onesided_layout(layout, side):
+    """ Add 'R' or 'L' to layout variable names """
+    if side not in ['R', 'L']:
+        raise ValueError('Invalid side')
+    return [[(side + item) if item is not None else None for item in row]
+            for row in layout]
+
+
+def filter_layout(layout, key, repl):
+    """ Replace layout items """
+    return [[repl if (item and key in item) else item for item in row]
+            for row in layout]
---
+++ packages/gaitutils/gaitutils-0.11.27.tar.gz/gaitutils-0.11.27/gaitutils/scripts/nexus_time_distance_vars.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Plot time-distance vars from single trial or as average of tagged trials
+
+@author: Jussi (jnu@iki.fi)
+"""
+
+from builtins import zip
+from builtins import range
+import logging
+import argparse
+import os.path as op
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+
+from gaitutils import (nexus, register_gui_exception_handler, analysis,
+                       GaitDataError, sessionutils, cfg)
+from gaitutils.plot import time_dist_barchart
+
+logger = logging.getLogger(__name__)
+
+
+def do_session_average_plot(tags=None, show=True, make_pdf=True):
+    """Find tagged trials from current session dir and plot average"""
+
+    sessionpath = nexus.get_sessionpath()
+    tags = tags or cfg.eclipse.tags
+    trials = sessionutils.get_c3ds(sessionpath, tags=tags,
+                                   trial_type='dynamic')
+    if not trials:
+        raise GaitDataError('No marked trials found for current Nexus session')
+    sessionpath = nexus.get_sessionpath()
+    fig = _plot_trials([trials])
+    session = op.split(sessionpath)[-1]
+    fig.suptitle('Time-distance average, session %s' % session)
+
+    if make_pdf:
+        pdf_name = op.join(sessionpath, 'time_distance_average.pdf')
+        with PdfPages(pdf_name) as pdf:
+            pdf.savefig(fig)
+
+    if show:
+        plt.show()
+
+    return fig
+
+
+def do_single_trial_plot(c3dfile, show=True, make_pdf=True):
+    """Plot a single trial time-distance. PDF goes into Nexus session dir"""
+
+    fig = _plot_trials([[c3dfile]])
+    fn = op.split(c3dfile)[1]
+    fig.suptitle('Time-distance variables, %s' % fn)
+
+    if make_pdf:
+        fn = op.split(c3dfile)[1]
+        fn_ = op.splitext(fn)[0]
+        pdf_name = op.join(nexus.get_sessionpath(),
+                           'time_distance_%s.pdf' % fn_)
+        with PdfPages(pdf_name) as pdf:
+                pdf.savefig(fig)
+
+    if show:
+        plt.show()
+
+    return fig
+
+
+def do_multitrial_plot(c3dfiles, show=True, make_pdf=True):
+    """Plot multiple trial comparison time-distance.
+    PDF goes into Nexus session dir"""
+
+    cond_labels = [op.split(c3d)[-1] for c3d in c3dfiles]
+    fig = _plot_trials([[c3d] for c3d in c3dfiles], cond_labels)
+
+    if make_pdf:
+        pdf_name = op.join(nexus.get_sessionpath(),
+                           '%representative_time_distance.pdf')
+        with PdfPages(pdf_name) as pdf:
+                pdf.savefig(fig)
+
+    if show:
+        plt.show()
+
+    return fig
+
+
+def do_comparison_plot(sessions, tags, show=True):
+    """Time-dist comparison of multiple sessions. Tagged trials from each
+    session will be picked."""
+
+    trials = list()
+    for session in sessions:
+        c3ds = sessionutils.get_c3ds(session, tags=tags, trial_type='dynamic')
+        if not c3ds:
+            raise ValueError('No tagged trials found in session %s' % session)
+        trials.append(c3ds)
+
+    cond_labels = [op.split(session)[-1] for session in sessions]
+    fig = _plot_trials(trials, cond_labels)
+
+    if show:
+        plt.show()
+
+    return fig
+
+
+def _plot_trials(trials, cond_labels=None, interactive=True):
+    """Make a time-distance variable barchart from given trials (.c3d files).
+    trials: list of lists, where inner lists represent conditions
+    and list elements represent trials.
+    Conditions is a matching list of condition labels.
+    If there are multiple trials per condition, they will be averaged.
+    plotvars specifies variables to plot (if not all) and their order
+    """
+    # XXX: hardcoded time-distance variables, to set a certain order
+    # set plotvars=None to plot all analysis variables from c3d
+    plotvars = ['Walking Speed', 'Cadence', 'Foot Off', 'Opposite Foot Off',
+                'Opposite Foot Contact', 'Double Support', 'Single Support',
+                'Stride Time', 'Stride Length', 'Step Width', 'Step Length']
+
+    if cond_labels is None:
+        cond_labels = ['Condition %d' % k for k in range(len(trials))]
+    res_avg_all = dict()
+    res_std_all = dict()
+    for cond_files, cond_label in zip(trials, cond_labels):
+        ans = list()
+        for c3dfile in cond_files:
+            an = analysis.get_analysis(c3dfile, condition=cond_label)
+            ans.append(an)
+        if len(ans) > 1:  # do average for this condition
+            res_avg = analysis.group_analysis(ans)
+            res_std = analysis.group_analysis(ans, fun=np.std)
+        else:  # do single-trial plot for this condition
+            res_avg = ans[0]
+            res_std = dict()
+            res_std[cond_label] = None
+        res_avg_all.update(res_avg)
+        res_std_all.update(res_std)
+
+    return time_dist_barchart(res_avg_all, stddev=res_std_all,
+                              stddev_bars=False, plotvars=plotvars,
+                              interactive=interactive)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tags', metavar='p', type=str, nargs='+',
+                        help='strings that must appear in trial '
+                        'description or notes')
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG)
+    register_gui_exception_handler()
+    do_session_average_plot(tags=args.tags)
