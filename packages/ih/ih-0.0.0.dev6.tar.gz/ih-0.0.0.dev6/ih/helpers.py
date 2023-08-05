5fff577c76ae66e713690b8c6128f4afd82fd1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:08 +0000

    Add package ih/0.0.0.dev6/ih-0.0.0.dev6.tar.gz

---
+++ packages/ih/ih-0.0.0.dev6.tar.gz/ih-0.0.0.dev6/ih/helpers.py
@@ -0,0 +1,37 @@
+"""
+Helpers and values
+"""
+import os
+from pathlib import Path
+
+# A series of visually distinct characters, to populate the chart
+STARS = ['✚', '✽', '※', '✷', '❐', '❄', '✔', '◀', '⊐', '♠', '★', '»', '✿', '⟘',
+         '❦', '⟒', '❦', '⚉', '♣', '▷', '♦', '×', '«', '△', '⊔', '➤', '⚑', '◼', 
+         '⬇', '✭', '⧓', '❤', '✤', '⟙', '✾', '∀', '⊕', '♥', '⬆', '⚇', '◼', '✰']
+
+
+def rgb2hex(pix):
+    """Given a tuple of r, g, b, return the hex value """
+    r, g, b = pix
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+def color_cell(rgb, icon, thread=False, center=False):
+    h = rgb2hex(rgb)
+    if thread:
+        return "<div class='color_cell cross_cell' style='background-color: {};'>&nbsp;</div>".format(h)
+
+    # Render as chart
+    if (rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 186:
+        text = "color: black"
+    else:
+        text = "color: lightgray"
+
+    if center:
+        border = "center"
+    else:
+        border = ""
+    return "<div class='color_cell {}' style='background-color: {}; {} '>{}</div>".format(border, h, text, icon)
+
+def base_path():
+    return Path(__file__).parents[1]
---
+++ packages/ih/ih-0.0.0.dev6.tar.gz/ih-0.0.0.dev6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# TODO - fix so this works
+#with open("requirements.txt", "r") as f: 
+#    requirements = f.read()
+
+setuptools.setup(
+    name="ih",
+    version="0.0.0-dev6",
+    author="katie McLaughlin",
+    author_email="katie@glasnt.com",
+    description="A very persuasive package, for creating embroidery patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glasnt/ih",
+    install_requires=['click', 'pillow'],
+    entry_points={
+        'console_scripts': [
+            "ih = ih.__main__:main"
+        ]
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
