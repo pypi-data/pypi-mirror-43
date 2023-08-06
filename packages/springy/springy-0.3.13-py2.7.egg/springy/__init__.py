ca65c19d77ea6715432d0dfd7f76b8d039d6f7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:39 +0000

    Add package springy/0.3.13/springy-0.3.13-py2.7.egg

---
+++ packages/springy/springy-0.3.13-py2.7.egg/springy/__init__.py
@@ -0,0 +1,7 @@
+from .indices import Index, registry
+from .helpers import query, parse, index, model_indices, multisearch
+from .utils import autodiscover
+from . import exceptions
+
+default_app_config = 'springy.apps.SpringyAppConfig'
+
