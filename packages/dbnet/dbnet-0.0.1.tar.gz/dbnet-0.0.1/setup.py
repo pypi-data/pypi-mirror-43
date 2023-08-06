b0986df8eb81463b62bd61c0d0b8dc05c60de750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:27 +0000

    Add package dbnet/0.0.1/dbnet-0.0.1.tar.gz

---
+++ packages/dbnet/dbnet-0.0.1.tar.gz/dbnet-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+version = '0.0.1'
+
+setup(
+  name='dbnet',
+  version=version,
+  description='DbNet.',
+  author='Fritz Larco',
+  author_email='flarco@live.com',
+  url='https://github.com/flarco/dbnet',
+  download_url='https://github.com/flarco/dbnet/archive/{}.tar.gz'.format(version),
+  keywords=['dbnet'],
+  packages=find_packages(exclude=['tests']),
+  include_package_data=True,
+  install_requires=[
+    "python-socketio",
+    "verboselogs",
+    "coloredlogs",
+    "psutil",
+    "jmespath",
+    "eventlet",
+    "xutil",
+  ],
+  entry_points={
+    'console_scripts': ['dbnet=dbnet.cli:dbnet_cli'],
+  },
+  classifiers=[
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3', 'Intended Audience :: Developers',
+    'Intended Audience :: Education', 'Intended Audience :: Science/Research',
+    'Operating System :: MacOS', 'Operating System :: Unix',
+    'Topic :: Utilities'
+  ])
