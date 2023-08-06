ad4f0bce4cabae711663724a0cfbaa08f4f03a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:29 +0000

    Add package avalon_framework/1.7.0/avalon_framework-1.7.0.tar.gz

---
+++ packages/avalon_framework/avalon_framework-1.7.0.tar.gz/avalon_framework-1.7.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='avalon_framework',
+    packages=['avalon_framework'],
+    version='1.7.0',
+    description='Avalon Standard Command Line I/O Framework',
+    author='K4YT3X',
+    author_email='k4yt3x@k4yt3x.com',
+    url='https://github.com/K4YT3X/avalon-framework',
+    download_url='https://github.com/K4YT3X/avalon-framework/archive/1.7.0.tar.gz',
+    keywords=['color', 'framework', 'standard'],  # arbitrary keywords
+    classifiers=[],
+)
