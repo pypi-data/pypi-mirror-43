2121d3e897bf3ac5ceee0dcadfb24b4059008b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:34 +0000

    Add package recompute/0.9.8/recompute-0.9.8.tar.gz

---
+++ packages/recompute/recompute-0.9.8.tar.gz/recompute-0.9.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="recompute",
+    version="0.9.8",
+    author="Suriyadeepan Ramamoorthy",
+    author_email="suriyadeepan.r@gmail.com",
+    description="Remote Computation Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suriyadeepan/recompute.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+      'console_scripts' : [ 're=recompute.recompute:main' ],
+      }
+)
