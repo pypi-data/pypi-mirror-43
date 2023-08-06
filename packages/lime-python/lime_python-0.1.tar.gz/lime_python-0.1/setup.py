8dfd5ecba15c837324f61df97209afbba409c70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:54 +0000

    Add package lime-python/0.1/lime_python-0.1.tar.gz

---
+++ packages/lime-python/lime_python-0.1.tar.gz/lime_python-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+    name='lime_python',
+    packages=['lime_python'],
+    version='0.1',
+    license='apache-2.0',
+    description='This module implements the basic models of the LIME protocol',
+    author='Gabriel Rodrigues dos Santos',
+    author_email='gabrielrsantoss@icloud.com',
+    url='https://github.com/chr0m1ng/lime-python',
+    download_url='https://github.com/chr0m1ng/lime-python/archive/v_01.tar.gz',
+    keywords=['lime', 'blip', 'chatbot'],
+    install_requires=[],
+    classifiers=[
+        # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
