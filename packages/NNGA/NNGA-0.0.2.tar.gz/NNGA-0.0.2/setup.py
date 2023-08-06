290d95dc41bab623b15121ff921247bc68e06890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package NNGA/0.0.2/NNGA-0.0.2.tar.gz

---
+++ packages/NNGA/NNGA-0.0.2.tar.gz/NNGA-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+        name='NNGA', 
+        version='0.0.2', 
+        description='A neural networks combine genetic algorithm module.',   # 简单描述
+        author='maigua', 
+        author_email='3075489925@qq.com',
+        py_modules=['neuralNetworkGA']
+)
