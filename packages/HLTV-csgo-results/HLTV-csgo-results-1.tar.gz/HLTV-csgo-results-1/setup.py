b8f9f85b149dd6b2467ee5969f0099f939fa1bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:34 +0000

    Add package HLTV-csgo-results/1/HLTV-csgo-results-1.tar.gz

---
+++ packages/HLTV-csgo-results/HLTV-csgo-results-1.tar.gz/HLTV-csgo-results-1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HLTV-csgo-results',
+    version='1',
+    description='A simple module to get pro stats from HLTV',
+    license='MIT',
+    packages=['hltv-stats'],
+    author='Benjamin Jones',
+    author_email='benj.jones7@gmail.com',
+    keywords=['hltv', 'stats', 'csgo']
+)
