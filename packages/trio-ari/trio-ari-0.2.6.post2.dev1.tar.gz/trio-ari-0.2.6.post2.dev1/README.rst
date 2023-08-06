f6c854056b756dfd495963b7913df97b81f7fa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:16 +0000

    Add package trio-ari/0.2.6.post2.dev1/trio-ari-0.2.6.post2.dev1.tar.gz

---
+++ packages/trio-ari/trio-ari-0.2.6.post2.dev1.tar.gz/trio-ari-0.2.6.post2.dev1/README.rst
@@ -0,0 +1,10 @@
+trio-ari
+========
+
+Welcome to `trio-ari <https://github.com/M-o-a-T/trio-ari>`__!
+
+Trio-ARI is a Trio-ified adapter for the Asterisk ARI interface.
+Currently, it adapts `aioari`_ to be used with Trio programs.
+
+License: Your choice of MIT or Apache License 2.0
+
---
+++ packages/trio-ari/trio-ari-0.2.6.post2.dev1.tar.gz/trio-ari-0.2.6.post2.dev1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+LONG_DESC = open("README.rst", encoding="utf-8").read()
+
+setup(
+    name="trio-ari",
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "dirty-tag"
+    },
+    description="A Trio-ified adapter for the Asterisk ARI interface",
+    url="https://github.com/M-o-a-T/trio-ari",
+    long_description=open("README.rst").read(),
+    author="Matthias Urlichs",
+    author_email="matthias@urlichs.de",
+    license="MIT -or- Apache License 2.0",
+    packages=find_packages(),
+    install_requires=[
+        "trio>=0.11",
+        "trio_swagger11",
+        "asks",
+        "attrs>=18",
+        "asyncwebsockets",
+    ],
+    keywords=[
+        "asterisk",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Trio",
+        "Topic :: Communications :: Telephony",
+        "Development Status :: 3 - Alpha",
+    ],
+)
