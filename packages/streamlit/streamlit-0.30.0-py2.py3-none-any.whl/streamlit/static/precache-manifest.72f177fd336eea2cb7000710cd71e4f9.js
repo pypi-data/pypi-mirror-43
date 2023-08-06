a8c2709bfb01c672f80d559afde8d3fcce0267b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package streamlit/0.30.0/streamlit-0.30.0-py2.py3-none-any.whl

---
+++ packages/streamlit/streamlit-0.30.0-py2.py3-none-any.whl/streamlit/static/precache-manifest.72f177fd336eea2cb7000710cd71e4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "fcf56b79f1fae5a386a6",
+    "url": "./static/js/main.ea9566bb.chunk.js"
+  },
+  {
+    "revision": "760d792b6e593370a845",
+    "url": "./static/js/2.2d54fde2.chunk.js"
+  },
+  {
+    "revision": "fcf56b79f1fae5a386a6",
+    "url": "./static/css/main.a069d013.chunk.css"
+  },
+  {
+    "revision": "760d792b6e593370a845",
+    "url": "./static/css/2.a67b2d30.chunk.css"
+  },
+  {
+    "revision": "7cae57cd968c6d651985a42ab82f613d",
+    "url": "./index.html"
+  }
+];
