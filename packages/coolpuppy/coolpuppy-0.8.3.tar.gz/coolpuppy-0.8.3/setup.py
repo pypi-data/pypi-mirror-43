9d6f11b095eee1cdb12976bccf82e5a9ef4b0cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:34 +0000

    Add package coolpuppy/0.8.3/coolpuppy-0.8.3.tar.gz

---
+++ packages/coolpuppy/coolpuppy-0.8.3.tar.gz/coolpuppy-0.8.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+      name='coolpuppy',
+      version='0.8.3',
+      scripts=['coolpup.py'],
+      install_requires=['numpy', 'cooler', 'pandas', 'natsort', 'scipy',
+                        'cooltools'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      project_urls={'Source':'https://github.com/Phlya/coolpuppy',
+                    'Issues':'https://github.com/Phlya/coolpuppy/issues'},
+      author='Ilya Flyamer',
+      author_email='flyamer@gmail.com',
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
