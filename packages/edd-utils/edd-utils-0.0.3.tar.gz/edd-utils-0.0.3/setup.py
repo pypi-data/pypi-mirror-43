a81fa06e41df717e0612a98edc54ffbbcefca820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package edd-utils/0.0.3/edd-utils-0.0.3.tar.gz

---
+++ packages/edd-utils/edd-utils-0.0.3.tar.gz/edd-utils-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edd-utils",
+    version="0.0.3",
+    author="Zak Costello",
+    author_email="zak.costello@lbl.gov",
+    description="Download Studies from an Experiment Data Depot Instance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JBEI/edd-utils",
+    packages=setuptools.find_packages(),
+    install_requires=['tqdm','pandas','requests'],
+    entry_points = {
+        'console_scripts': ['export_edd_study=edd_utils:commandline_export'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
