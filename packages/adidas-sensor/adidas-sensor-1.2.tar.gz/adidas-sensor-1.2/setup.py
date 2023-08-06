74ea387d703e04bf88911a2b8029131a196f5219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:14 +0000

    Add package adidas-sensor/1.2/adidas-sensor-1.2.tar.gz

---
+++ packages/adidas-sensor/adidas-sensor-1.2.tar.gz/adidas-sensor-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="adidas-sensor",
+    version="1.2",
+    description="Adidas Sensor Tool",
+    author="Real Python",
+    author_email="office@realpython.com",
+    license="MIT",
+    packages=["adidasSensor","adidasSensor.serial"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "adidas-sensor=adidasSensor.__main__:main",
+        ]
+    },
+)
