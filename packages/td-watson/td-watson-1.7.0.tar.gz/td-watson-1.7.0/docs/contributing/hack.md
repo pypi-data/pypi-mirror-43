1d4549c0e7125ec51adfaaef375e21e72a16e3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:53 +0000

    Add package td-watson/1.7.0/td-watson-1.7.0.tar.gz

---
+++ packages/td-watson/td-watson-1.7.0.tar.gz/td-watson-1.7.0/docs/contributing/hack.md
@@ -0,0 +1,57 @@
+# Hack
+
+Ready to contribute? Here's how to set up *Watson* for local development.
+
+## Get started!
+
+1.  Fork the [Watson repository](https://github.com/TailorDev/Watson/) on GitHub.
+2.  Clone your fork locally:
+
+        $ git clone git@github.com:your_name_here/Watson.git
+
+3.  Install Watson locally:
+
+        $ mkvirtualenv watson
+        $ cd Watson
+        $ pip install -r requirements-dev.txt
+        $ python setup.py develop
+
+4.  Create a branch for local development:
+
+        $ git checkout -b name-of-your-bugfix-or-feature
+
+    Now you can make your changes locally.
+
+5.  When you're done making changes, check that your changes pass the tests
+    (see [Run the tests](#run-the-tests)):
+
+        $ tox
+
+6. If you have added a new command or updated/fixed docstrings, please update the documentation:
+
+        $ make docs
+
+7.  Commit your changes and push your branch to GitHub:
+
+        $ git add .
+        $ git commit -m "Your detailed description of your changes."
+        $ git push -u origin name-of-your-bugfix-or-feature
+
+8.  After [reading this](./pr-guidelines.md), submit a pull request through the GitHub website.
+
+
+<a href="#run-the-tests"></a>
+## Run the tests
+
+The tests use [pytest](http://pytest.org/). To run them with the default Python
+interpreter:
+
+    $ py.test -v tests/
+
+To run the tests via [tox](http://tox.testrun.org/) with all Python versions
+which are available on your system and are defined in the `tox.ini` file,
+simply run:
+
+    $ tox
+
+This will also check the source code with [flake8](http://flake8.pycqa.org).
---
+++ packages/td-watson/td-watson-1.7.0.tar.gz/td-watson-1.7.0/tests/test_utils.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""Unit tests for the 'utils' module."""
+
+import arrow
+import functools
+import os
+import datetime
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import pytest
+from dateutil.tz import tzutc
+
+from watson.utils import (apply_weekday_offset, get_start_time_for_period,
+                          make_json_writer, safe_save, parse_tags, PY2)
+from . import mock_datetime
+
+
+_dt = functools.partial(datetime.datetime, tzinfo=tzutc())
+
+
+@pytest.mark.parametrize('now, mode, start_time', [
+    (_dt(2016, 6, 2), 'year', _dt(2016, 1, 1)),
+    (_dt(2016, 6, 2), 'month', _dt(2016, 6, 1)),
+    (_dt(2016, 6, 2), 'week', _dt(2016, 5, 30)),
+    (_dt(2016, 6, 2), 'day', _dt(2016, 6, 2)),
+    (_dt(2016, 6, 2), 'all', _dt(1970, 1, 1)),
+    (_dt(2016, 6, 2), 'luna', _dt(2016, 5, 21, 21, 16)),
+
+    (_dt(2012, 2, 24), 'year', _dt(2012, 1, 1)),
+    (_dt(2012, 2, 24), 'month', _dt(2012, 2, 1)),
+    (_dt(2012, 2, 24), 'week', _dt(2012, 2, 20)),
+    (_dt(2012, 2, 24), 'day', _dt(2012, 2, 24)),
+    (_dt(2012, 2, 24), 'all', _dt(1970, 1, 1)),
+    (_dt(2012, 2, 24), 'luna', _dt(2012, 2, 7, 21, 56)),
+])
+def test_get_start_time_for_period(now, mode, start_time):
+    with mock_datetime(now, datetime):
+        assert get_start_time_for_period(mode).datetime == start_time
+
+
+@pytest.mark.parametrize("monday_start, week_start, new_start", [
+    ("2018 12 3", "monday", "2018 12 3"),
+    ("2018 12 3", "tuesday", "2018 12 4"),
+    ("2018 12 3", "wednesday", "2018 12 5"),
+    ("2018 12 3", "thursday", "2018 12 6"),
+    ("2018 12 3", "friday", "2018 11 30"),
+    ("2018 12 3", "saturday", "2018 12 1"),
+    ("2018 12 3", "sunday", "2018 12 2"),
+    ("2018 12 3", "typo", "2018 12 3"),
+])
+def test_apply_weekday_offset(monday_start, week_start, new_start):
+    with mock_datetime(_dt(2018, 12, 6), datetime):
+        original_start = arrow.get(monday_start, "YYYY MM D")
+        result = arrow.get(new_start, "YYYY MM D")
+        assert apply_weekday_offset(original_start, week_start) == result
+
+
+def test_make_json_writer():
+    fp = StringIO()
+    writer = make_json_writer(lambda: {'foo': 42})
+    writer(fp)
+    assert fp.getvalue() == '{\n "foo": 42\n}'
+
+
+def test_make_json_writer_with_args():
+    fp = StringIO()
+    writer = make_json_writer(lambda x: {'foo': x}, 23)
+    writer(fp)
+    assert fp.getvalue() == '{\n "foo": 23\n}'
+
+
+def test_make_json_writer_with_kwargs():
+    fp = StringIO()
+    writer = make_json_writer(lambda foo=None: {'foo': foo}, foo='bar')
+    writer(fp)
+    assert fp.getvalue() == '{\n "foo": "bar"\n}'
+
+
+def test_make_json_writer_with_unicode():
+    fp = StringIO()
+    writer = make_json_writer(lambda: {u'ùñï©ôð€': u'εvεrywhεrε'})
+    writer(fp)
+    expected = u'{\n "ùñï©ôð€": "εvεrywhεrε"\n}'
+    if PY2:
+        expected = expected.encode('utf-8')
+    assert fp.getvalue() == expected
+
+
+def test_safe_save(config_dir):
+    save_file = os.path.join(config_dir, 'test')
+    backup_file = os.path.join(config_dir, 'test' + '.bak')
+
+    assert not os.path.exists(save_file)
+    safe_save(save_file, lambda f: f.write("Success"))
+    assert os.path.exists(save_file)
+    assert not os.path.exists(backup_file)
+
+    with open(save_file) as fp:
+        assert fp.read() == "Success"
+
+    safe_save(save_file, "Again")
+    assert os.path.exists(backup_file)
+
+    with open(save_file) as fp:
+        assert fp.read() == "Again"
+
+    with open(backup_file) as fp:
+        assert fp.read() == "Success"
+
+    assert os.path.getmtime(save_file) >= os.path.getmtime(backup_file)
+
+
+def test_safe_save_tmpfile_on_other_filesystem(config_dir, mock):
+    save_file = os.path.join(config_dir, 'test')
+    backup_file = os.path.join(config_dir, 'test' + '.bak')
+
+    assert not os.path.exists(save_file)
+    safe_save(save_file, lambda f: f.write("Success"))
+    assert os.path.exists(save_file)
+    assert not os.path.exists(backup_file)
+
+    with open(save_file) as fp:
+        assert fp.read() == "Success"
+
+    # simulate tmpfile being on another file-system
+    # OSError is caught and handled by shutil.move() used by save_safe()
+    mock.patch('os.rename', side_effect=OSError)
+    safe_save(save_file, "Again")
+    assert os.path.exists(backup_file)
+
+    with open(save_file) as fp:
+        assert fp.read() == "Again"
+
+
+def test_safe_save_with_exception(config_dir):
+    save_file = os.path.join(config_dir, 'test')
+    backup_file = os.path.join(config_dir, 'test' + '.bak')
+
+    def failing_writer(f):
+        raise RuntimeError("Save failed.")
+
+    assert not os.path.exists(save_file)
+
+    with pytest.raises(RuntimeError):
+        safe_save(save_file, failing_writer)
+
+    assert not os.path.exists(save_file)
+    assert not os.path.exists(backup_file)
+
+    safe_save(save_file, lambda f: f.write("Success"))
+    assert os.path.exists(save_file)
+    assert not os.path.exists(backup_file)
+
+    with pytest.raises(RuntimeError):
+        safe_save(save_file, failing_writer)
+
+    with open(save_file) as fp:
+        assert fp.read() == "Success"
+
+    assert not os.path.exists(backup_file)
+
+
+@pytest.mark.parametrize('args, parsed_tags', [
+    (['+ham', '+n', '+eggs'], ['ham', 'n', 'eggs']),
+    (['+ham', 'n', '+eggs'], ['ham n', 'eggs']),
+    (['ham', 'n', '+eggs'], ['eggs']),
+    (['ham', '+n', 'eggs'], ['n eggs']),
+    (['+ham', 'n', 'eggs'], ['ham n eggs']),
+])
+def test_parse_tags(args, parsed_tags):
+    tags = parse_tags(args)
+    assert tags == parsed_tags
