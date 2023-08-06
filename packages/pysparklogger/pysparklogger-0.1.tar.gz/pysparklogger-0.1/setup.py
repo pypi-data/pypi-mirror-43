9bc69337eef2da1a0e93bb20eda39d8bd8f3c221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:54 +0000

    Add package pysparklogger/0.1/pysparklogger-0.1.tar.gz

---
+++ packages/pysparklogger/pysparklogger-0.1.tar.gz/pysparklogger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysparklogger',
+      version='0.1',
+      description='Logger for pyspark',
+      url='https://pip.pypa.io/',
+      author='arunenquero',
+      author_email='arunkumar.rn@enquero.com',
+      license='MIT',
+      packages=['pysparklogger'],
+      zip_safe=False)
