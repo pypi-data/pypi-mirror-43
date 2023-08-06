b4e6343100312dce789e6ca847afd592025499e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:40 +0000

    Add package ubot/3.2.0/ubot-3.2.0.tar.gz

---
+++ packages/ubot/ubot-3.2.0.tar.gz/ubot-3.2.0/ubot/optional_features.py
@@ -0,0 +1,11 @@
+class OptionalFeatures:
+
+    async def check_update(self, update, update_type, update_flavor):
+        """
+        To be implemented by user, called as validator before an update is handled, if the return value is not True the
+        update is not queued
+
+        :param update: The update (a dict) received from the Telegram webhook
+        :return: True/Other value
+        """
+        return True
