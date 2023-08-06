f5c8970ebe8a46342876fc4c75e23a54d7ccdb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package emp-1zlab/0.2.7/emp-1zlab-0.2.7.tar.gz

---
+++ packages/emp-1zlab/emp-1zlab-0.2.7.tar.gz/emp-1zlab-0.2.7/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='emp-1zlab',
+    version='0.2.7',
+    py_modules=['emp_wifi', 'emp_boot', 'emp_ide', 'emp_utils', 'emp_webrepl'],
+    author='fuermohao@1zlab.com',
+    author_email='fuermohao@outlook.com',
+    url='http://emp.1zlab.com',
+    description='EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.'
+)
