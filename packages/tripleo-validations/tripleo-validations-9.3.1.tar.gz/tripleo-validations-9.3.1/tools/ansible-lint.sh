9f7212e98a4313382c450a9592a2730dc1089f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:57 +0000

    Add package tripleo-validations/9.3.1/tripleo-validations-9.3.1.tar.gz

---
+++ packages/tripleo-validations/tripleo-validations-9.3.1.tar.gz/tripleo-validations-9.3.1/tools/ansible-lint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 204: Lines should be no longer than 120 chars
+# 303: Using command rather than module
+#   we have a few use cases where we need to use curl and rsync
+# 503: Tasks that run when changed should likely be handlers
+#   this requires refactoring roles, skipping for now
+# 602: Needed in ironic boot config validation
+SKIPLIST="204,303,503,602"
+
+pushd validations
+for playbook in `find . -type f -regex '.*\.y[a]?ml'`; do
+    ansible-lint -vvv -x $SKIPLIST $playbook || lint_error=1
+done
+popd
+
+# exit with 1 if we had a least an error or warning.
+if [[ -n "$lint_error" ]]; then
+    exit 1;
+fi
+
