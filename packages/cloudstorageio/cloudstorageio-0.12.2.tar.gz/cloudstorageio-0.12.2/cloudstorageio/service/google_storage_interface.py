971f5ff8311e0503fa9594e669a1a90431d73b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package cloudstorageio/0.12.2/cloudstorageio-0.12.2.tar.gz

---
+++ packages/cloudstorageio/cloudstorageio-0.12.2.tar.gz/cloudstorageio-0.12.2/cloudstorageio/service/google_storage_interface.py
@@ -0,0 +1,95 @@
+""" Class GoogleStorageInterface handles with Google Cloud Storage files
+
+    Class GoogleStorageInterface has 'read' and 'write' methods each of them can be accessed by 'open' method
+"""
+
+import io
+from typing import Tuple, Union, Optional
+
+from google.cloud import storage
+
+
+class GoogleStorageInterface:
+    PREFIX = "gs://"
+
+    def __init__(self):
+        self._encoding = 'utf8'
+        self._storage_client = storage.Client()
+        self._mode = None
+        self._current_bucket = None
+        self._current_path = None
+        self._bucket = None
+        self._blob = None
+        self.path = None
+        self._is_open = False
+
+    @property
+    def path(self):
+        if self._current_bucket is None:
+            raise ValueError("Bucket name is not set")
+        if self._current_path is None:
+            raise ValueError("Path name is not set")
+        return f"{GoogleStorageInterface.PREFIX}{self._current_bucket}/{self._current_path}"
+
+    @path.setter
+    def path(self, value):
+        if value is None:
+            self._current_path = None
+            self._current_bucket = None
+        else:
+            self._current_bucket, self._current_path = self._parse_bucket(value)
+
+    def open(self, file: str, mode: Optional[str] = None, *args, **kwargs):
+        """Open a file from gs and return the GoogleStorageInterface object"""
+        self._mode = mode
+        self.path = file
+        return self
+
+    def read(self) -> Union[str, bytes]:
+        """ Read gs file and return the bytes
+        :return: String content of the file
+        """
+        self._bucket = self._storage_client.get_bucket(self._current_bucket)
+        self._blob = self._bucket.get_blob(self._current_path)
+        res = self._blob.download_as_string()
+
+        if self._mode is not None and 'b' not in self._mode:
+            try:
+                res = res.decode(self._encoding)
+            except UnicodeDecodeError:
+                raise ValueError(f"The content cannot be decoded into a string"
+                                 f" with encoding {self._encoding}."
+                                 f" Include 'b' on read mode to return the original bytes")
+        return res
+
+    def write(self, content: Union[str, bytes, io.IOBase]):
+        """ Write text to a file on google storage
+        :param content: The content that should be written to a file
+        :return: String content of the file specified in the filepath argument
+        """
+        if isinstance(content, str):
+            content = content.encode('utf8')
+
+        if self._mode is not None and ('w' not in self._mode and
+                                       'a' not in self._mode and
+                                       'x' not in self._mode and
+                                       '+' not in self._mode):
+            raise ValueError(f"Mode '{self._mode}' does not allow writing the file")
+        self._bucket = self._storage_client.get_bucket(self._current_bucket)
+        blob = self._bucket.blob(self._current_path)
+        blob.upload_from_string(content)
+
+    @staticmethod
+    def _parse_bucket(path: str) -> Tuple[str, str]:
+        """Given a path, return the bucket name and the filepath as a tuple"""
+        path = path.split(GoogleStorageInterface.PREFIX, 1)[-1]
+        bucket_name, path = path.split('/', 1)
+        return bucket_name, path
+
+    def __enter__(self):
+        self._is_open = True
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._is_open = False
+        self.path = None
