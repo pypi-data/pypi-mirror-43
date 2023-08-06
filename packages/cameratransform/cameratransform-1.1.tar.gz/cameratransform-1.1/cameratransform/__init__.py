0ffd7213ba6b17e651882ba928602183865f7dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:20 +0000

    Add package cameratransform/1.1/cameratransform-1.1.tar.gz

---
+++ packages/cameratransform/cameratransform-1.1.tar.gz/cameratransform-1.1/cameratransform/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __init__.py
+
+# Copyright (c) 2017-2019, Richard Gerum
+#
+# This file is part of the cameratransform package.
+#
+# cameratransform is free software: you can redistribute it and/or modify
+# it under the terms of the MIT licence.
+#
+# cameratransform is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the license
+# along with cameratransform. If not, see <https://opensource.org/licenses/MIT>
+
+from .camera import *
+from .projection import *
+from .spatial import *
+from .scene import *
+from .statistic import *
+from .lens_distortion import *
+from .parameter_set import *
+from .gps import *
+
+__version__ = "1.1"
