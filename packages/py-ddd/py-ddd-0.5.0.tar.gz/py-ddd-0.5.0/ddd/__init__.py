e079280b752df2b66d63734bdee9c0ad77f391dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:27 +0000

    Add package py-ddd/0.5.0/py-ddd-0.5.0.tar.gz

---
+++ packages/py-ddd/py-ddd-0.5.0.tar.gz/py-ddd-0.5.0/ddd/__init__.py
@@ -0,0 +1,7 @@
+from .aggregate_root import AggregateRoot
+from .domain_event import DomainEvent
+from .domain_event_collection import DomainEventCollection
+from .domain_event_handler import domain_event_handler
+from .entity import Entity
+from .process_domain_events import process_domain_events
+from .value_object import ValueObject
