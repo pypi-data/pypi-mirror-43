24a8cf80eda70d1835b38637110710eeefe148e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package oc-python-nester/1.0.0/oc_python_nester-1.0.0.tar.gz

---
+++ packages/oc-python-nester/oc_python_nester-1.0.0.tar.gz/oc_python_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='oc_python_nester',
+    version='1.0.0',
+    py_modules=['oc_python_nester'],
+    author='oc_python',
+    author_email='chenlfc@126.com',
+    url='http://www.126.com',
+    description='A simple printer of nested lists',
+)
