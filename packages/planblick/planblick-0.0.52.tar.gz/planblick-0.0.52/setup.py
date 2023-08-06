82972285648cf94412ecac44083fb0e003cf1f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:12 +0000

    Add package planblick/0.0.52/planblick-0.0.52.tar.gz

---
+++ packages/planblick/planblick-0.0.52.tar.gz/planblick-0.0.52/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+version = "0.0.52"
+setup(name='planblick',
+      version=version,
+      description='',
+      url='https://www.planblick.com',
+      author='Florian Kröber @ Planblick',
+      author_email='fk@planblick.com',
+      license='MIT',
+      packages=['planblick.httpserver', 'planblick.autorun', 'planblick.sqs', 'planblick.sns', 'planblick.logger'],
+      install_requires=[
+          'cherrypy',
+          'requests',
+          'boto3',
+          'cryptography',
+      ],
+      zip_safe=False)
