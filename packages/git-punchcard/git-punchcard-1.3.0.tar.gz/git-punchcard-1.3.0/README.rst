2c602f033ba2c502c08313da6a9d4527f8688bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:55 +0000

    Add package git-punchcard/1.3.0/git-punchcard-1.3.0.tar.gz

---
+++ packages/git-punchcard/git-punchcard-1.3.0.tar.gz/git-punchcard-1.3.0/README.rst
@@ -0,0 +1,128 @@
+git-punchcard
+=============
+
+Simple git punchcard utility, inspired by git-punchcard-plot_ but rewritten
+for python3 with matplotlib.
+
+.. _git-punchcard-plot: https://github.com/guanqun/git-punchcard-plot
+
+|Screenshot|
+
+
+Installation
+~~~~~~~~~~~~
+
+**The pragmatic way:** The script can be obtained from PyPI:
+
+.. code-block:: bash
+
+    pip install --user git-punchcard
+
+Also, make sure that ``~/.local/bin`` is in ``$PATH``. If it is not there, add
+the following lines to ``.bashrc`` or ``.zshrc``:
+
+.. code-block:: bash
+
+    PATH=$PATH:$HOME/.local/bin
+
+
+**The elegant way:** Get pipx_ and then run ``pipx install git-punchcard`` to
+install into an isolated environment.
+
+
+**The crude way:** Alternatively, simply drop the ``git_punchcard.py`` script
+into ``~/.local/bin``, rename to ``git-punchcard`` and add the folder to PATH.
+
+
+.. _pipx: https://github.com/pipxproject/pipx
+
+
+Usage
+~~~~~
+
+The syntax of the command is as follows:
+
+.. code-block:: bash
+
+    git punchcard [options] [log options] [revision range] [-- pathes]
+
+To get a list of available options, type:
+
+.. code-block:: bash
+
+    git punchcard -h            # for our own options
+
+    git help log                # for possible git log options
+
+    git help gitrevisions       # for revision range
+
+The most important options are:
+
+Show a github-like punchcard plot with grid:
+
+.. code-block:: bash
+
+    git punchcard --grid
+
+By default, the author's local timezone is used for the plot. In order to use
+a fixed timezone for all commits, you have to specify a ``--timezone``
+argument, e.g.:
+
+.. code-block:: bash
+
+    git punchcard --timezone CET
+
+Set the directory of the git repository as follows:
+
+.. code-block:: bash
+
+    git punchcard -C /path/to/repo
+
+
+Advanced examples
+~~~~~~~~~~~~~~~~~
+
+You can pass additional options understood by ``git log``. This can for
+example be used to restrict the range of commits and/or limit to commits
+performed by a certain author:
+
+.. code-block:: bash
+
+    # include only commits by specific author:
+    git punchcard --author=myself
+
+    # consider only only the 20 commits:
+    git punchcard master~20..master
+
+    # commits within a certain time frame:
+    git punchcard --since="1 year ago" --until=now
+
+    # show at which times a certain file/folder is usually edited:
+    git punchcard --follow -- src
+    git punchcard --follow -- docs
+
+    # show at which times, people like to merge:
+    git punchcard --merges
+
+You can even to restrict to certain pathes within the git repository as
+follows:
+
+.. code-block:: bash
+
+    git punchcard -- README.rst
+
+Track evolution of commit activity over the years:
+
+.. code-block:: bash
+
+    for year in {2016..2019}; do
+        git punchcard -o $year.png --title $year \
+            --since 1.1.$year --until 31.12.$year
+    done
+
+
+.. resources:
+
+.. |Screenshot| image:: https://raw.githubusercontent.com/coldfix/git-punchcard/master/screenshot.png
+   :target:             https://raw.githubusercontent.com/coldfix/git-punchcard/master/screenshot.png
+   :alt:                Screenshot
---
+++ packages/git-punchcard/git-punchcard-1.3.0.tar.gz/git-punchcard-1.3.0/git_punchcard.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+"""
+Generate a github-like punchcard for git commit activity.
+
+Usage:
+    git-punchcard [-o FILE] [-C DIR] [-t TZ]
+                  [--grid] [-w WIDTH] [--title TITLE]
+                  [<log options>] [<revision range>] [-- <pathes>]
+
+Options:
+    -C DIR, --git-dir DIR           Set path for git repository
+    -o FILE, --output FILE          Set output file
+    -t TZ, --timezone TZ            Set timezone
+    -w WIDTH, --width WIDTH         Plot width in inches
+    -g, --grid                      Enable grid
+    --title TITLE                   Set graph title
+
+    -h                              Show this help
+    --version                       Show version and exit
+
+All further options are passed directly to `git log` and can be used to
+restrict the range of commits taken into account. For more info, see `git
+help log`.
+"""
+
+__version__ = '1.3.0'
+__author__  = 'Thomas Gläßle'
+__email__   = 'thomas@coldfix.de'
+__license__ = 'Unlicense'
+__uri__     = 'https://github.com/coldfix/git-punchcard'
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import subprocess
+from datetime import datetime, timedelta
+from argparse import ArgumentParser
+
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.format_help = lambda: __doc__
+    add_argument = parser.add_argument
+    add_argument('-C', '--git-dir',  type=str, help='Path to git repository')
+    add_argument('-o', '--output',   type=str, help='Output image file name')
+    add_argument('-t', '--timezone', type=str, help='Set timezone')
+    add_argument('-w', '--width',    type=int, help='Plot width in inches')
+    add_argument('--title', help="Set graph title")
+    add_argument('-g', '--grid', action='store_true', help="Enable grid")
+    add_argument('--version', action='version', version=__version__)
+    return parser
+
+
+def main(args=None):
+    parser = argument_parser()
+    options, git_opts = parser.parse_known_args(args)
+    folder   = options.git_dir
+    output   = options.output
+    tz_name  = options.timezone
+    grid     = options.grid
+    title    = options.title
+    width    = options.width or 10
+
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    hours = ['{}'.format(x) for x in range(25)]
+
+    dates = get_commit_times(folder, git_opts)
+
+    if tz_name:
+        from pytz import timezone
+        try:
+            tz = timezone(timedelta(hours=float(tz_name)))
+        except ValueError:
+            tz = timezone(tz_name)
+        dates = [date.astimezone(tz) for date in dates]
+
+    counts = np.zeros((7, 24))
+    for date in dates:
+        counts[date.weekday()][date.hour] += 1
+
+    punchcard(
+        counts.T[:, ::-1], hours, days[::-1],
+        output=output, width=width, grid=grid, title=title)
+
+
+def get_commit_times(folder, git_opts):
+    folder = folder or '.'
+    argv = ['git', '-C', folder, 'log', '--pretty=format:%ai'] + git_opts
+    stdout = subprocess.check_output(argv).decode('utf-8')
+    return [
+        datetime.strptime(line, '%Y-%m-%d %H:%M:%S %z')
+        for line in stdout.splitlines()
+    ]
+
+
+def punchcard(counts, xlabels, ylabels, output=None, width=10, grid=False,
+              title=None):
+
+    fig, ax = plt.subplots()
+    fig.tight_layout()
+    fig.set_figwidth(width)
+
+    def set_ticks(axis, labels, size):
+        set_lim    = getattr(ax, 'set_{}lim'.format(axis))
+        set_ticks  = getattr(ax, 'set_{}ticks'.format(axis))
+        set_labels = getattr(ax, 'set_{}ticklabels'.format(axis))
+
+        set_lim(0, size)
+        set_ticks(np.arange(size + 1), minor=False)
+        set_ticks(np.arange(size) + 0.5, minor=True)
+        ax.tick_params(axis=axis, which='minor', length=0)
+
+        if len(labels) == size:
+            # labels centered in bin
+            set_labels(["" for _ in range(size+1)], minor=False)
+            set_labels(labels, minor=True)
+        else:
+            # labels at bin edges:
+            set_labels(labels, minor=False)
+            set_labels(["" for _ in range(size)], minor=True)
+
+    ax.set_title(title)
+    ax.set_aspect(1, adjustable='box')
+    set_ticks('x', xlabels, counts.shape[0])
+    set_ticks('y', ylabels, counts.shape[1])
+    ax.grid(grid)
+
+    max_radius = 0.9
+    for (x, y), value in np.ndenumerate(counts / counts.max() * max_radius):
+        if value > 0:
+            draw_circle(
+                ax, x + 0.5, y + 0.5,
+                value ** 0.50 / 2,
+                value ** 0.25)
+
+    if output:
+        fig.savefig(output, bbox_inches='tight')
+    else:
+        plt.show()
+
+
+def draw_circle(ax, x, y, radius, opacity=1):
+    color = (1-opacity, 1-opacity, 1-opacity)
+    ax.add_patch(plt.Circle(
+        (x, y), radius, color=color,
+        linestyle=None, linewidth=0))
+
+
+if __name__ == '__main__':
+    main()
