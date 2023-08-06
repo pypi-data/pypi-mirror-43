f85d675295450800a7cc4cb9458278689e899237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:29 +0000

    Add package maup/0.2/maup-0.2.tar.gz

---
+++ packages/maup/maup-0.2.tar.gz/maup-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("./README.md") as f:
+    long_description = f.read()
+
+requirements = ["numpy", "pandas", "geopandas", "shapely"]
+
+setup(
+    name="maup",
+    version="0.2",
+    description="The geospatial toolkit for redistricting data",
+    author="Max Hully",
+    author_email="max@mggg.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mggg/maup",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
