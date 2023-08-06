6d4cabe00a1120611714d2041db6df32210827db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:12 +0000

    Add package bingads/12.0.4/bingads-12.0.4.tar.gz

---
+++ packages/bingads/bingads-12.0.4.tar.gz/bingads-12.0.4/bingads/service_info.py
@@ -0,0 +1,100 @@
+class _ServiceInfo:
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def env(self):
+        return self._env
+
+    @property
+    def url(self):
+        return self._url
+
+    def __init__(self, name, env, url):
+        self._name = name
+        self._env = env
+        self._url = url
+
+_SERVICE_INFO_LIST_V12 = [
+    # ad insight service
+    _ServiceInfo(
+        "adinsight",
+        "production",
+        "https://adinsight.api.bingads.microsoft.com/Api/Advertiser/AdInsight/V12/AdInsightService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "adinsight",
+        "sandbox",
+        "https://adinsight.api.sandbox.bingads.microsoft.com/Api/Advertiser/AdInsight/V12/AdInsightService.svc?singleWsdl"
+    ),
+
+    # bulk service
+    _ServiceInfo(
+        "bulk",
+        "production",
+        "https://bulk.api.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/BulkService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "bulk",
+        "sandbox",
+        "https://bulk.api.sandbox.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/BulkService.svc?singleWsdl"
+    ),
+
+    # campaign management
+    _ServiceInfo(
+        "campaignmanagement",
+        "production",
+        "https://campaign.api.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "campaignmanagement",
+        "sandbox",
+        "https://campaign.api.sandbox.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?singleWsdl"
+    ),
+
+    # customer billing
+    _ServiceInfo(
+        "customerbilling",
+        "production",
+        "https://clientcenter.api.bingads.microsoft.com/Api/Billing/v12/CustomerBillingService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "customerbilling",
+        "sandbox",
+        "https://clientcenter.api.sandbox.bingads.microsoft.com/Api/Billing/v12/CustomerBillingService.svc?singleWsdl"
+    ),
+
+    # customer management
+    _ServiceInfo(
+        "customermanagement",
+        "production",
+        "https://clientcenter.api.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "customermanagement",
+        "sandbox",
+        "https://clientcenter.api.sandbox.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?singleWsdl"
+    ),
+
+    # reporting
+    _ServiceInfo(
+        "reporting",
+        "production",
+        "https://reporting.api.bingads.microsoft.com/Api/Advertiser/Reporting/v12/ReportingService.svc?singleWsdl"
+    ),
+    _ServiceInfo(
+        "reporting",
+        "sandbox",
+        "https://reporting.api.sandbox.bingads.microsoft.com/Api/Advertiser/Reporting/v12/ReportingService.svc?singleWsdl"
+    ),
+]
+
+
+SERVICE_INFO_DICT_V12 = {}
+
+for service_info in _SERVICE_INFO_LIST_V12:
+    SERVICE_INFO_DICT_V12[(service_info.name, service_info.env)] = service_info
+
+SERVICE_INFO_DICT = {12: SERVICE_INFO_DICT_V12}
