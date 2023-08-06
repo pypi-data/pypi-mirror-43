5c63806f2976d94df0c7d8e683d4d9d8d12599bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:30 +0000

    Add package decode/0.5.4/decode-0.5.4.tar.gz

---
+++ packages/decode/decode-0.5.4.tar.gz/decode-0.5.4/README.md
@@ -0,0 +1,22 @@
+
+![][deshima-logo-color]
+
+<br>
+
+# De:code - DESHIMA Code for data analysis
+
+De:code is a Python data analysis package.
+
+## Installation
+
+## Requirements
+
+## Links
+
++ [Repository's Wiki][repos-wiki]
++ [Package's Documentation][documentation]
+
+<!-- link URLs -->
+[repos-wiki]: https://github.com/deshima-dev/decode/wiki
+[documentation]: https://deshima-dev.github.io/decode/
+[deshima-logo-color]: https://github.com/deshima-dev/decode/wiki/Images/deshima-logo-color.png
---
+++ packages/decode/decode-0.5.4.tar.gz/decode-0.5.4/setup.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+"DESHIMA Code for data analysis"
+
+# standard library
+from setuptools import setup
+
+# module constants
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy',
+    'astropy',
+    'xarray',
+    'matplotlib',
+    'scikit-learn',
+    'pyyaml',
+    'tqdm',
+    'netCDF4',
+]
+
+PACKAGES = [
+    'decode',
+    'decode.core',
+    'decode.core.array',
+    'decode.core.cube',
+    'decode.io',
+    'decode.logging',
+    'decode.plot',
+    'decode.models',
+    'decode.joke',
+    'decode.utils',
+    'decode.utils.misc',
+    'decode.utils.ndarray'
+]
+
+
+
+# main
+setup(
+    name = 'decode',
+    description = __doc__,
+    version = '0.5.4',
+    author = 'DESHIMA software team',
+    author_email = 'tishida@ioa.s.u-tokyo.ac.jp',
+    url = 'https://github.com/deshima-dev/decode',
+    install_requires = INSTALL_REQUIRES,
+    packages = PACKAGES,
+    package_data = {'decode': ['data/*.yaml']},
+)
