4608fe0feb73448435760d1706cc59b1a491c31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:12 +0000

    Add package nester-testing/1.0.0/nester_testing-1.0.0.tar.gz

---
+++ packages/nester-testing/nester_testing-1.0.0.tar.gz/nester_testing-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'nester_testing',
+        version = '1.0.0',
+        py_modules = ['nester_testing'],
+        author = 'mnw96',
+        author_email = 'rfans29@gmail.com',
+        url = 'https://www.facebook.com/plectrum',
+        description = 'A simple printer of nested lists'
+        )
