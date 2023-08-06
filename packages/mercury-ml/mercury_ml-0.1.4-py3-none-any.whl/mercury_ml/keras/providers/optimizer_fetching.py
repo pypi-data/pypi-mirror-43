2f9e4cacebab5a6d124d3690c5c553b6b4584358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:38 +0000

    Add package mercury-ml/0.1.4/mercury_ml-0.1.4-py3-none-any.whl

---
+++ packages/mercury-ml/mercury_ml-0.1.4-py3-none-any.whl/mercury_ml/keras/providers/optimizer_fetching.py
@@ -0,0 +1,12 @@
+def get_keras_optimizer(optimizer_name, optimizer_params):
+    """
+    Fetches a vanilla Keras model optimizer
+
+    :param string optimizer_name: The name of the optimizer
+    :param dict optimizer_params: The parameters with which the optimizer should be initialized
+    :return:
+    """
+
+    from keras import optimizers
+
+    return getattr(optimizers, optimizer_name)(**optimizer_params)
