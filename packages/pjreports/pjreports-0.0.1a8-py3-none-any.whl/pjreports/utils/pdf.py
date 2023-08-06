280883414dc8d174f4c89fe56a70267018a1a84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package pjreports/0.0.1a8/pjreports-0.0.1a8-py3-none-any.whl

---
+++ packages/pjreports/pjreports-0.0.1a8-py3-none-any.whl/pjreports/utils/pdf.py
@@ -0,0 +1,25 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os
+
+def merge(input, output, reduce_file_size=False):
+    pdf_writer = PdfFileWriter()
+    for path in input:
+        pdf_reader = PdfFileReader(path)
+        for i in range(pdf_reader.getNumPages()):
+            page = pdf_reader.getPage(i)
+            if reduce_file_size:
+                page.compressContentStreams()
+            pdf_writer.addPage(page)
+
+    with open(output, 'wb') as fh:
+        pdf_writer.write(fh)
+
+def merge_ghostscript(input, output):
+    """uses postscript"""
+    cmd = " gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=%s -f" % (output, )
+
+    if not isinstance(input,list):
+        input = [input]
+    for f in input:
+        cmd += ' %s' % (f,)
+    os.system(cmd)
