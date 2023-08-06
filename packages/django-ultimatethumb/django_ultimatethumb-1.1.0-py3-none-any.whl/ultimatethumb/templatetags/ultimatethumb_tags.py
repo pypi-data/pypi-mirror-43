5ffe38c8f9978dc4e18a93a7bade268cbba84055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:41 +0000

    Add package django-ultimatethumb/1.1.0/django_ultimatethumb-1.1.0-py3-none-any.whl

---
+++ packages/django-ultimatethumb/django_ultimatethumb-1.1.0-py3-none-any.whl/ultimatethumb/templatetags/ultimatethumb_tags.py
@@ -0,0 +1,52 @@
+import os
+
+from django.template import Library
+
+from ..thumbnail import ThumbnailSet
+from ..utils import parse_source
+
+
+VALID_IMAGE_FILE_EXTENSIONS = ('jpg', 'jpeg', 'png', 'gif', 'ico')
+
+register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def ultimatethumb(
+    context,
+    as_var,
+    source,
+    sizes=None,
+    upscale=False,
+    crop=None,
+    retina=True,
+    quality=None,
+    pngquant=None
+):
+    """
+    Main template tag to generate thumbnail sourcesets.
+    """
+    source = parse_source(source)
+
+    if not source:
+        context[as_var] = None
+        return ''
+
+    source_extension = os.path.splitext(source)[1].lower().lstrip('.')
+    if source_extension not in VALID_IMAGE_FILE_EXTENSIONS:
+        context[as_var] = None
+        return ''
+
+    thumbnail_options = {'upscale': upscale, 'factor2x': retina}
+
+    if crop is not None:
+        thumbnail_options['crop'] = crop
+
+    if quality is not None:
+        thumbnail_options['quality'] = quality
+
+    if pngquant is not None:
+        thumbnail_options['pngquant'] = pngquant
+
+    context[as_var] = ThumbnailSet(source, sizes, thumbnail_options).thumbnails
+    return ''
