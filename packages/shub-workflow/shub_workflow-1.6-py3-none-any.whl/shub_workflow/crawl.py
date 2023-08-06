c5c609df444c0e809c750efcea6bd24322087271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:28 +0000

    Add package shub-workflow/1.6/shub_workflow-1.6-py3-none-any.whl

---
+++ packages/shub-workflow/shub_workflow-1.6-py3-none-any.whl/shub_workflow/crawl.py
@@ -0,0 +1,38 @@
+"""
+Base script class for spiders crawl managers.
+"""
+import json
+
+from shub_workflow.base import WorkFlowManager
+
+
+class CrawlManager(WorkFlowManager):
+
+    def add_argparser_options(self):
+        super().add_argparser_options()
+        self.argparser.add_argument('spider', help='Spider name')
+        self.argparser.add_argument('--spider-args', help='Spider arguments dict in json format', default='{}')
+        self.argparser.add_argument('--job-settings', help='Job settings dict in json format', default='{}')
+        self.argparser.add_argument('--units', help='Set number of ScrapyCloud units for each job', type=int)
+
+    def get_spider_args(self, override=None):
+        spider_args = json.loads(self.args.spider_args)
+        if override:
+            spider_args.update(override)
+        return spider_args
+
+    def get_job_settings(self, override=None):
+        job_settings = json.loads(self.args.job_settings)
+        if override:
+            job_settings.update(override)
+        return job_settings
+
+    def schedule_spider(self, spider_args_override=None, job_settings_override=None):
+        spider_args = self.get_spider_args(spider_args_override)
+        job_settings = self.get_job_settings(job_settings_override)
+        spider_args['job_settings'] = job_settings
+        return super().schedule_spider(self.args.spider, units=self.args.units, **spider_args)
+
+    def workflow_loop(self):
+        self.schedule_spider()
+        return True
