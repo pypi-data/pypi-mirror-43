64ec209106c42922f50892b1714ce1a6f1f2256c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:38 +0000

    Add package o2locktop/1.0.6/o2locktop-1.0.6.tar.gz

---
+++ packages/o2locktop/o2locktop-1.0.6.tar.gz/o2locktop-1.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import distutils.core
+from o2locktoplib import config
+import os
+
+name = 'o2locktop'
+
+dir_path = os.path.abspath(os.path.dirname(__file__))
+read_path = os.path.join(dir_path, "README.md")
+with open(read_path, "r") as fh:
+    long_description = fh.read()
+
+
+distutils.core.setup(name=name,
+    version = config.VERSION_SETUP,
+    author = "Larry Chen, Weikai Wang, Gang He",
+    author_email = "lchen@suse.com, wewang@suse.com, ghe@suse.com",
+    url = "https://github.com/ganghe/o2locktop",
+    description = "o2locktop is a top-like OCFS2 DLM lock monitor",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    license = "GPL2.0",
+    packages = ['o2locktoplib'],
+    scripts = [name],
+    include_package_data=True
+)
