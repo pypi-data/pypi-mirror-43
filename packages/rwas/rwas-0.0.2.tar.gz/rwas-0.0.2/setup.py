0d1ffd685fa602e8e4ca8296f4f1edc0ddd92baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package rwas/0.0.2/rwas-0.0.2.tar.gz

---
+++ packages/rwas/rwas-0.0.2.tar.gz/rwas-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rwas',
+    version='0.0.2',
+    description='pip install rwas',
+    license='MIT',
+    packages=['rwas'],
+    author='Erick Samaniego',
+    author_email='erickdsama@gmail.com',
+    keywords=['rwas', 'whatsapp', 'bot', 'rest'],
+    url='https://github.com/erickdsama/rwas_library'
+)
