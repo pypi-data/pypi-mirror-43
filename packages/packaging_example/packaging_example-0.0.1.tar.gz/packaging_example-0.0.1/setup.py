9cb8b6e737279faea685c98f3c3f9e9a7e893a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:28 +0000

    Add package packaging_example/0.0.1/packaging_example-0.0.1.tar.gz

---
+++ packages/packaging_example/packaging_example-0.0.1.tar.gz/packaging_example-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+setup(name='packaging_example',
+      version='0.0.1',
+      description='Example on packaging in Python3',
+      url='https://github.com/geowatson/packaging-example',
+      author='George Poputnikov',
+      author_email='g.poputnikov@innopolis.ru',
+      license='MIT',
+      packages=['packaging_example'],
+      zip_safe=False)
