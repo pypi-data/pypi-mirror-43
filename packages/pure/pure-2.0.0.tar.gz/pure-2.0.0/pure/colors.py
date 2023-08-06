c6bbb464bb9c78b69edeb5cd4d32c11ca155acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:59 +0000

    Add package pure/2.0.0/pure-2.0.0.tar.gz

---
+++ packages/pure/pure-2.0.0.tar.gz/pure-2.0.0/pure/colors.py
@@ -0,0 +1,13 @@
+import colorful
+
+colorful.use_true_colors()
+
+primary = colorful.purple1
+info = colorful.lightBlue
+mute = colorful.gray
+success = colorful.green
+normal = colorful.darkGray
+danger = colorful.red3
+light = colorful.white
+warning = colorful.orange
+dark = colorful.black
