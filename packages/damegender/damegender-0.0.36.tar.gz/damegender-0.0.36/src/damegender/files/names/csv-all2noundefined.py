c2614b864a04fa60fd2189af5f07431d8c063247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package damegender/0.0.36/damegender-0.0.36.tar.gz

---
+++ packages/damegender/damegender-0.0.36.tar.gz/damegender-0.0.36/src/damegender/files/names/csv-all2noundefined.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+import csv
+from pprint import pprint
+import re
+
+
+with open('all.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    filenou = open('allnoundefined.csv','w')
+    for row in reader:
+        if (($row[4] == 'm') | ($row[4] == 'f')):
+            filenou.write(item+"\n")
+
+filenou.close()
+
+#        print(sorted(l, key=str.lower))
+#        print(row[1])
+        # if re.search(r"'", row[1]):
+        #     result = re.sub(r"'", "", row[1])
+        # pprint(result)
---
+++ packages/damegender/damegender-0.0.36.tar.gz/damegender-0.0.36/src/damegender/pca-featuresnou.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+
+from pprint import pprint
+import pandas as pd
+from sklearn.decomposition import PCA
+
+import matplotlib.pyplot as plt
+from app.dame_sexmachine import DameSexmachine
+from app.dame_gender import Gender
+
+## LOAD DATASET
+g = Gender()
+g.features_list2csv(categorical="both", path="files/names/allnoundefined.csv")
+features = "files/features_list_no_undefined.csv"
+
+#print("STEP1: N COMPONENTS + 1 TARGET")
+
+x = pd.read_csv(features)
+print(x.columns)
+
+y = g.dataset2genderlist(dataset="files/names/all.csv")
+print(y)
+
+# # load dataset
+
+# # adding target
+
+target = pd.DataFrame(data = y, columns = ['target component'])
+print(target)
+finalDf = x.join(target)
+
+
+leftdf.index.levels[0].name = “onename”
+leftdf.index.levels[1].name = “twoname”
+rightdf.index.levels[0].name = “onename”
+
+
+
+# # normalize data
+# from sklearn import preprocessing
+# data_scaled = pd.DataFrame(preprocessing.scale(finalDf), columns = finalDf.columns)
+
+
+# # PCA
+# pca = PCA(n_components=6)
+# pca.fit_transform(data_scaled)
+
+# # Dump components relations with features:
+# print(pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2','PC-3','PC-4','PC-5','PC-6']))
