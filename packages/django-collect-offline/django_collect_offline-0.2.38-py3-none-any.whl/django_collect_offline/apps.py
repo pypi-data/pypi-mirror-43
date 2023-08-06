a885cd5c7391996bd1a2bb31c500d4eb7f397598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package django-collect-offline/0.2.38/django_collect_offline-0.2.38-py3-none-any.whl

---
+++ packages/django-collect-offline/django_collect_offline-0.2.38-py3-none-any.whl/django_collect_offline/apps.py
@@ -0,0 +1,35 @@
+import sys
+
+from django.apps import AppConfig as DjangoAppConfig
+from django.core.management.color import color_style
+
+from .site_offline_models import site_offline_models
+
+style = color_style()
+
+
+class OfflineConfigError(Exception):
+    pass
+
+
+class AppConfig(DjangoAppConfig):
+    name = "django_collect_offline"
+    verbose_name = "Offline Synchronization"
+    base_template_name = "edc_dashboard/bootstrap3/base.html"
+    custom_json_parsers = []
+    django_collect_offline_files_using = True
+
+    # see edc_device for ROLE
+
+    def ready(self):
+        from .signals import (
+            create_auth_token,
+            serialize_on_post_delete,
+            serialize_m2m_on_save,
+            serialize_on_save,
+            serialize_history_on_post_create,
+        )
+
+        sys.stdout.write(f"Loading {self.verbose_name} ...\n")
+        site_offline_models.autodiscover()
+        sys.stdout.write(f" Done loading {self.verbose_name}.\n")
