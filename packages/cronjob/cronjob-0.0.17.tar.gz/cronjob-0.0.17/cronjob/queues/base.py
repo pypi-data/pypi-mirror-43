91c1180015bcc6486a6f66675b6c6dac08b9cdab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:47 +0000

    Add package cronjob/0.0.17/cronjob-0.0.17.tar.gz

---
+++ packages/cronjob/cronjob-0.0.17.tar.gz/cronjob-0.0.17/cronjob/queues/base.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, AnyStr
+
+
+class BaseMessage(metaclass=ABCMeta):
+    """
+    封装不同的消息队列消息，以提供统一的接口, 目前没用
+    """
+
+    @abstractmethod
+    def ack(self):
+        pass
+
+    @abstractmethod
+    def payload(self):
+        pass
+
+
+class BaseQueue(metaclass=ABCMeta):
+    """
+    封装不同的消息队列，提供统一的接口，以方便支持redis/sqs/rabbitmq等
+    """
+
+    def __init__(self, config: dict) -> None:
+        self.config = config
+
+    @abstractmethod
+    def set_qname(self, qname: str) -> 'BaseQueue':
+        pass
+
+    @abstractmethod
+    def put(self, payload: bytes) -> Any:
+        pass
+
+    @abstractmethod
+    def get(self, timeout: float = 3.0) -> AnyStr:
+        pass
