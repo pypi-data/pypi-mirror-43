d90dee5a80a722f98ce0953263bb058ca6f40b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package shiboken2/5.12.2/shiboken2-5.12.2-5.12.2-cp27-cp27m-macosx_10_12_intel.whl

---
+++ packages/shiboken2/shiboken2-5.12.2-5.12.2-cp27-cp27m-macosx_10_12_intel.whl/shiboken2/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(5.12)
+
+version = "5.12.2"
+version_info = (5, 12, 2, "", "")
+
+__build_date__ = '2019-03-21T14:52:29+00:00'
+
+
+
+
+__setup_py_package_version__ = '5.12.2'
