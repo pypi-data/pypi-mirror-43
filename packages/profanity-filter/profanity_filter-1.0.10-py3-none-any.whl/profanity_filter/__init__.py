0bc44def014a241c75ffd6f421706854d9647698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:35 +0000

    Add package profanity-filter/1.0.10/profanity_filter-1.0.10-py3-none-any.whl

---
+++ packages/profanity-filter/profanity_filter-1.0.10-py3-none-any.whl/profanity_filter/__init__.py
@@ -0,0 +1,11 @@
+from .profanity_filter import (
+    Config,
+    DEEP_ANALYSIS_AVAILABLE,
+    POLYGLOT_AVAILABLE,
+    ProfanityFilter,
+    ProfanityFilterError,
+    Word,
+    __version__,
+    default_config,
+)
+from .console import main
