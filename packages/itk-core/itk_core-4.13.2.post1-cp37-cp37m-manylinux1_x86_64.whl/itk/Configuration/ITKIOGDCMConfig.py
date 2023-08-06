342c6f7655ead436338ce7e42add6b5bbac734ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:07 +0000

    Add package itk-core/4.13.2.post1/itk_core-4.13.2.post1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/itk-core/itk_core-4.13.2.post1-cp37-cp37m-manylinux1_x86_64.whl/itk/Configuration/ITKIOGDCMConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('GDCMImageIO', 'itk::GDCMImageIO', 'itkGDCMImageIO', True),
+  ('GDCMImageIOFactory', 'itk::GDCMImageIOFactory', 'itkGDCMImageIOFactory', True),
+  ('GDCMSeriesFileNames', 'itk::GDCMSeriesFileNames', 'itkGDCMSeriesFileNames', True),
+)
---
+++ packages/itk-core/itk_core-4.13.2.post1-cp37-cp37m-manylinux1_x86_64.whl/itk/Configuration/ITKIOVTKConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('VTKImageIO', 'itk::VTKImageIO', 'itkVTKImageIO', True),
+  ('VTKImageIOFactory', 'itk::VTKImageIOFactory', 'itkVTKImageIOFactory', True),
+)
