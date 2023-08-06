4c76063576d1823650f31382be0381ec2d7b4b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:05 +0000

    Add package rv-schubert-sdk/0.0.2/rv-schubert-sdk-0.0.2.tar.gz

---
+++ packages/rv-schubert-sdk/rv-schubert-sdk-0.0.2.tar.gz/rv-schubert-sdk-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name="rv-schubert-sdk",
+    author="Raphael Schubert",
+    author_email="rfswdp@gmail.com",
+    description="SKD de integracao com RV Tecnologia",
+    url="https://github.com/rfschubert/rv-schubert-sdk",
+    version='0.0.2',
+    packages=['rv_schubert_sdk'],
+    long_description=readme,
+    license=license,
+    install_requires=[
+        'xmltodict',
+        'json',
+        'requests',
+        'pendulum',
+    ],
+    include_package_data=True,
+)
