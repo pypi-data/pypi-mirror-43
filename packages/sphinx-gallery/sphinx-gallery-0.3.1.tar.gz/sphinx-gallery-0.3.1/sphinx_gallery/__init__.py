e1d8d8eda242fedd55041624b33e682eb61ff7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:28 +0000

    Add package sphinx-gallery/0.3.1/sphinx-gallery-0.3.1.tar.gz

---
+++ packages/sphinx-gallery/sphinx-gallery-0.3.1.tar.gz/sphinx-gallery-0.3.1/sphinx_gallery/__init__.py
@@ -0,0 +1,12 @@
+"""
+Sphinx Gallery
+==============
+
+"""
+import os
+__version__ = '0.3.1'
+
+
+def glr_path_static():
+    """Returns path to packaged static files"""
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
