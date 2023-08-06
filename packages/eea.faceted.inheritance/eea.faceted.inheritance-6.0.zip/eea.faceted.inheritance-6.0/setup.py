743aea2644ae0b197c633182d91206f2ac391fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:54 +0000

    Add package eea.faceted.inheritance/6.0/eea.faceted.inheritance-6.0.zip

---
+++ packages/eea.faceted.inheritance/eea.faceted.inheritance-6.0.zip/eea.faceted.inheritance-6.0/setup.py
@@ -0,0 +1,54 @@
+""" EEA Faceted Inheritance Installer
+"""
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+NAME = 'eea.faceted.inheritance'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description=("EEA Faceted Navigation extension that allow a faceted "
+                   "navigable object to inherit faceted configuration from "
+                   "another faceted navigable object."),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.1",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+      ],
+      keywords='EEA Add-ons Plone Zope',
+      author='European Environment Agency: IDM2 A-Team',
+      author_email='eea-edw-a-team-alerts@googlegroups.com',
+      url='https://github.com/eea/eea.faceted.inheritance',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea', 'eea.faceted'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'eea.facetednavigation',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
