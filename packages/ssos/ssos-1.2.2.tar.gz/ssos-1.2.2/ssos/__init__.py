0e28180df439139e1402073d55cea62d6a2bc8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:38 +0000

    Add package ssos/1.2.2/ssos-1.2.2.tar.gz

---
+++ packages/ssos/ssos-1.2.2.tar.gz/ssos-1.2.2/ssos/__init__.py
@@ -0,0 +1,17 @@
+name = 'ssos'
+__version__ = '1.2.2'
+
+
+GREETING = '''
+       *       +      ssos pipeline %s
+   +        x
+        '             https://sso-pipeline.readthedocs.io
+*   +     _.::'   |
+        :_.'    - o - ssos --default : Copy default settings to CWD
+|                 |   ssos --help    : Print detailed help
+O -     *        '    ssos [-PARAMETER] [-VALUE] path/to/images
+|
+        _..:'         max.mahlke(at)cab.inta-csic.es
+  +   (_.:
+
+        ''' % __version__
