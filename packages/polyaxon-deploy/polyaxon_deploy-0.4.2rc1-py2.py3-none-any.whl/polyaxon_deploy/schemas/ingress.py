f573c0c2d8a7962683016a72036ff815fd996913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package polyaxon-deploy/0.4.2rc1/polyaxon_deploy-0.4.2rc1-py2.py3-none-any.whl

---
+++ packages/polyaxon-deploy/polyaxon_deploy-0.4.2rc1-py2.py3-none-any.whl/polyaxon_deploy/schemas/ingress.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from marshmallow import fields
+from polyaxon_deploy.schemas.base import BaseConfig, BaseSchema
+from polyaxon_schemas.ops.environments.resources import K8SContainerResourcesSchema
+
+
+class IngressSchema(BaseSchema):
+    enabled = fields.Bool(allow_none=True)
+    tls = fields.Dict(allow_none=True)
+    annotations = fields.Dict(allow_none=True)
+    resources = fields.Nested(K8SContainerResourcesSchema, allow_none=True)
+
+    @staticmethod
+    def schema_config():
+        return IngressConfig
+
+
+class IngressConfig(BaseConfig):
+    SCHEMA = IngressSchema
+    REDUCED_ATTRIBUTES = ['enabled', 'tls', 'annotations', 'resources']
+
+    def __init__(self,
+                 enabled=None,
+                 tls=None,
+                 annotations=None,
+                 resources=None):
+        self.enabled = enabled
+        self.tls = tls
+        self.annotations = annotations
+        self.resources = resources
