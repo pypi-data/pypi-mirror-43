796ca1921e84913126c95d96c12808536d93bf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:05 +0000

    Add package planblick/0.0.48/planblick-0.0.48.tar.gz

---
+++ packages/planblick/planblick-0.0.48.tar.gz/planblick-0.0.48/planblick/sns/sns.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+from cryptography.fernet import Fernet
+import os
+import base64
+
+class Sns:
+    def publish_event(self, correlationId, eventKind, payload):
+        payload["correlationId"] = correlationId
+        sns_client = boto3.client('sns', region_name='eu-central-1')
+        key = json.loads(os.getenv("CLUSTER_CONFIG")).get("cipher-key")
+        key = base64.urlsafe_b64encode(key)
+        cipher_suite = Fernet(key)
+        sns_topic_arn = "arn:aws:sns:eu-central-1:282615081127:" + str(eventKind)
+        response = sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Message=cipher_suite.encrypt(json.dumps(payload).encode('utf-8')),
+            MessageStructure='raw')
+
+        return response
