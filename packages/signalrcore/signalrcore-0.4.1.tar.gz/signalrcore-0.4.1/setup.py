aa32e74d7c48fc4753c9bfe8fd823412da9ea456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:39 +0000

    Add package signalrcore/0.4.1/signalrcore-0.4.1.tar.gz

---
+++ packages/signalrcore/signalrcore-0.4.1.tar.gz/signalrcore-0.4.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="signalrcore",
+    version="0.4.1",
+    author="mandrewcito",
+    author_email="anbaalo@gmail.com",
+    description="A Python SignalR Core client ",
+    keywords="signalr core client, with invocation auth and streamming. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mandrewcito/signalrcore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2"
+    ],
+)
