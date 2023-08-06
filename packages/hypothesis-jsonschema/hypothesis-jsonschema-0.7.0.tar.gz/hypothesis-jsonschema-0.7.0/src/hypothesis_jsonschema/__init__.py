d703ef1aca1a724e9e4f823444bffb811cb6a498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:14 +0000

    Add package hypothesis-jsonschema/0.7.0/hypothesis-jsonschema-0.7.0.tar.gz

---
+++ packages/hypothesis-jsonschema/hypothesis-jsonschema-0.7.0.tar.gz/hypothesis-jsonschema-0.7.0/src/hypothesis_jsonschema/__init__.py
@@ -0,0 +1,10 @@
+"""A Hypothesis extension for JSON schemata.
+
+The public API contains only two functions: `from_schema` and `json_schemata`.
+Check their docstrings for details.
+"""
+
+__version__ = "0.7.0"
+__all__ = ["from_schema", "json_schemata"]
+
+from ._impl import from_schema, json_schemata
