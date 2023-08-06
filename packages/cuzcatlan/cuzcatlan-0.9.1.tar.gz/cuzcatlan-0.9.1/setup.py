10d7528fb0a19c917c884db8b1c011c0e79d9a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:12 +0000

    Add package cuzcatlan/0.9.1/cuzcatlan-0.9.1.tar.gz

---
+++ packages/cuzcatlan/cuzcatlan-0.9.1.tar.gz/cuzcatlan-0.9.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='cuzcatlan',
+      version='0.9.1',
+      description="Edwin Juarez's support library for GenePattern.",
+      url='https://github.com/edjuaro/cuzcatlan',
+      author='Edwin F. Juarez',
+      author_email='ejuarez@ucsd.edu',
+      license='MIT',
+      packages=['cuzcatlan'],
+      zip_safe=False,
+      install_requires=[
+            'numpy',
+            'scipy',
+            'pandas',
+            'matplotlib',
+            'statsmodels',
+            'seaborn',
+            'validators',
+            'IPython',
+            ],
+      )
