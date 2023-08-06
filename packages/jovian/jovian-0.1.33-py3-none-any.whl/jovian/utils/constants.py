df9670c4045d2debddb9db014a4798c0d2f66edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:25 +0000

    Add package jovian/0.1.33/jovian-0.1.33-py3-none-any.whl

---
+++ packages/jovian/jovian-0.1.33-py3-none-any.whl/jovian/utils/constants.py
@@ -0,0 +1,10 @@
+WEBAPP_URL = "https://jvn.io"
+API_URL = "https://api.jovian.ai"
+RC_FILENAME = ".jovianrc"
+FILENAME_MSG = 'Failed to detect notebook filename. Please provide the notebook filename (including .ipynb extension) as the "filename" argument to "jovian.commit".'
+
+LINUX = 'linux'
+WINDOWS = 'windows'
+MACOS = 'macos'
+
+PLATFORMS = [LINUX, WINDOWS, MACOS]
