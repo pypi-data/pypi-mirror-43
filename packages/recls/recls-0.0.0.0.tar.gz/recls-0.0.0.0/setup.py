420f1ff18277db32ccf2745f89d872e09f45cc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:36 +0000

    Add package recls/0.0.0.0/recls-0.0.0.0.tar.gz

---
+++ packages/recls/recls-0.0.0.0.tar.gz/recls-0.0.0.0/setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+
+	name='recls',
+	version='0.0.0.0',
+	license='BSD-3-Clause',
+	description='recls, for Python',
+	long_description=open('README.md').read(),
+	packages=['recls'],
+	url='https://github.com/synesissoftware/recls.Python',
+	author='Matt Wilson',
+	author_email='matthew@synesis.com.au',
+)
+
