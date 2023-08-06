cfda06526008c3e5391a8eab8a829cbabf8b3622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package RNG/0.0.2/RNG-0.0.2.tar.gz

---
+++ packages/RNG/RNG-0.0.2.tar.gz/RNG-0.0.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+
+# with open("fortuna_extras/README.md", "r") as file:
+#     long_description = file.read()
+long_description = "Comming Soon"
+
+
+setup(
+    ext_modules=cythonize(
+        Extension(
+            name="RNG",
+            sources=["RNG.pyx"],
+            language=["c++"],
+            extra_compile_args=["-std=c++17", "-Ofast", "-march=native"],
+        ),
+        compiler_directives={
+            'embedsignature': True,
+            'language_level': 3,
+        },
+    ),
+    name="RNG",
+    author="Broken aka Robert Sharp",
+    author_email="webmaster@sharpdesigndigital.com",
+    url="https://sharpdesigndigital.com",
+    requires=["Cython"],
+    version="0.0.2",
+    description="Random Number Generators",
+    long_description=long_description,
+    # long_description_content_type="text/markdown",
+    license="Free for non-commercial use",
+    platforms=["Darwin", "Linux"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Cython",
+        "Programming Language :: C++",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["RNG", ],
+    python_requires='>=3.7',
+)
