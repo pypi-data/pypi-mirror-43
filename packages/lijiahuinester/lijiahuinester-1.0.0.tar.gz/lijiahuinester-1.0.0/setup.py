b2514be0ed74894ed45e6dfd10e81fceba081dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package lijiahuinester/1.0.0/lijiahuinester-1.0.0.tar.gz

---
+++ packages/lijiahuinester/lijiahuinester-1.0.0.tar.gz/lijiahuinester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name           ="lijiahuinester",
+      version        ="1.0.0",
+      py_modules     =["lijiahuinester"],
+      author         ="lijiahui",
+      author_email   ="snljh1996@163.com",
+      url            ="http://www.headfirstlabs.com",
+      description    ="chuxuezhe",
+      )
