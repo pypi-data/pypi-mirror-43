74f8d9b086bcb8f744516614e90faa6e542e6036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:01 +0000

    Add package detools/0.14.0/detools-0.14.0.tar.gz

---
+++ packages/detools/detools-0.14.0.tar.gz/detools-0.14.0/detools/common.py
@@ -0,0 +1,45 @@
+from .errors import Error
+
+PATCH_TYPE_NORMAL    = 0
+PATCH_TYPE_IN_PLACE  = 1
+
+COMPRESSIONS = {
+    'none': 0,
+    'lzma': 1,
+    'crle': 2
+}
+
+
+def format_or(items):
+    items = [str(item) for item in items]
+
+    if len(items) == 1:
+        return items[0]
+    else:
+        return '{} or {}'.format(', '.join(items[:-1]),
+                                 items[-1])
+
+
+def format_bad_compression_string(compression):
+    return "Expected compression {}, but got {}.".format(
+        format_or(sorted(COMPRESSIONS)),
+        compression)
+
+
+def format_bad_compression_number(compression):
+    items = sorted([(n, '{}({})'.format(s, n)) for s, n in COMPRESSIONS.items()])
+
+    return "Expected compression {}, but got {}.".format(
+        format_or([v for _, v in items]),
+        compression)
+
+
+def compression_string_to_number(compression):
+    try:
+        return COMPRESSIONS[compression]
+    except KeyError:
+        raise Error(format_bad_compression_string(compression))
+
+
+def div_ceil(a, b):
+    return (a + b - 1) // b
