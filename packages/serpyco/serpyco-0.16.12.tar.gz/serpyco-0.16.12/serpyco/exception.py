05ac113fb7bdb4361b26492652d7eee0e00d4efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:32 +0000

    Add package serpyco/0.16.12/serpyco-0.16.12.tar.gz

---
+++ packages/serpyco/serpyco-0.16.12.tar.gz/serpyco-0.16.12/serpyco/exception.py
@@ -0,0 +1,22 @@
+import typing
+
+
+class BaseSerpycoError(Exception):
+    pass
+
+
+class SchemaError(BaseSerpycoError):
+    pass
+
+
+class NoEncoderError(BaseSerpycoError):
+    pass
+
+
+class ValidationError(BaseSerpycoError):
+    def __init__(self, msg: str, args: typing.Optional[typing.List[str]] = None):
+        super().__init__(msg, args)
+
+
+class NotADataClassError(BaseSerpycoError):
+    pass
