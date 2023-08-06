0d441cb73f0646d72dcfa23576e3afc78144300b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:02 +0000

    Add package gym-rl-book/0.0.4/gym_rl_book-0.0.4.tar.gz

---
+++ packages/gym-rl-book/gym_rl_book-0.0.4.tar.gz/gym_rl_book-0.0.4/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='gym_rl_book',
+      version='0.0.4',
+      install_requires=['gym', 'numpy', 'tabulate'],
+      packages=setuptools.find_packages(),
+      long_description=open('README.md').read(),
+      url='https://github.com/void-main/gym-rl-book',
+      author='void-main',
+      author_email='voidmain1313113@gmail.com',
+)
