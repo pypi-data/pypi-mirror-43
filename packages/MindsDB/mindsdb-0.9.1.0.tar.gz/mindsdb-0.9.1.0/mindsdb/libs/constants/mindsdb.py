2f3b410c38a4769ad2b0f5d31629b3f4566195fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:58 +0000

    Add package MindsDB/0.9.1.0/mindsdb-0.9.1.0.tar.gz

---
+++ packages/MindsDB/mindsdb-0.9.1.0.tar.gz/mindsdb-0.9.1.0/mindsdb/libs/constants/mindsdb.py
@@ -0,0 +1,160 @@
+
+
+DEFAULT_ORDER_BY_TYPE = 'DESC'
+TRANSACTION_LEARN = 'learn'
+TRANSACTION_PREDICT = 'predict'
+TRANSACTION_LOAD = 'load'
+TRANSACTION_CLUSTER = 'cluster'
+TRANSACTION_NORMAL_SELECT = 'normal_select'
+TRANSACTION_NORMAL_MODIFY = 'normal_modify'
+TRANSACTION_BAD_QUERY = 'bad_query'
+TRANSACTION_SHOW_DATASOURCES = 'show_datasources'
+TRANSACTION_DROP_MODEL ='drop_model'
+
+STOP_TRAINING = 'stop_training'
+KILL_TRAINING = 'kill_training'
+
+KEY_COLUMNS = 'columns'
+KEY_STATS = 'stats'
+KEY_MODEL_METADATA = 'model_metadata'
+KEY_MODEL_NAME = 'model_name'
+KEY_SUBMODEL_NAME = 'submodel_name'
+KEY_MODEL_QUERY = 'model_query'
+KEY_MODEL_TEST_QUERY = 'model_test_query'
+KEY_MODEL_GROUP_BY = 'model_group_by'
+KEY_MODEL_ORDER_BY = 'model_order_by'
+KEY_MODEL_PREDICT_COLUMNS = 'model_predict_columns'
+KEY_MODEL_WHEN_CONDITIONS = 'model_when_conditions'
+KEY_MODEL_UPDATE_FREQUENCY = 'model_upate_frequency'
+KEY_MODEL_CACHE_RESULTS = 'model_cache_results'
+KEY_MODEL_CACHE = 'model_cache'
+KEY_COLUMN_MAP = 'column_map'
+KEY_MODEL_MODEL_TYPE = 'predictor'
+KEY_MODEL_PREDICT_FROM_TABLE = 'model_predict_from_table'
+KEY_CONFIDENCE ='prediction_confidence'
+
+KEY_TRANSACTION_TYPE = 'transaction_type'
+
+LEARNING_RATE_INDEX = 0
+EPOCHS_INDEX = 1
+
+KEY_NO_GROUP_BY = 'ALL_ROWS_NO_GROUP_BY'
+
+KEY_START = 'start'
+KEY_END = 'end'
+KEY_METADATA = 'metadata'
+KEY_STATUS = 'status'
+
+FULL_TEXT_ENCODING_EXTRA_LENGTH = 4 #(len)[is_start, is_end, unfrequent_word, is_null]
+FULL_TEXT_NONE_VALUE = 0
+FULL_TEXT_IS_START = 1
+FULL_TEXT_IS_END = 2
+FULL_TEXT_UN_FREQUENT = 3
+
+EXTENSION_COLUMNS_TEMPLATE = '_extensions_.buckets.{column_name}'
+
+TEXT_ENCODING_EXTRA_LENGTH = 2 #(len)[unfrequent_word, is_null]
+
+ALL_INDEXES_LIST = ['*']
+
+class DATA_SUBTYPES:
+    # Numeric
+    INT = 'Int'
+    FLOAT = 'Float'
+    BINARY = 'Binary' # Should we have this ?
+
+    # DATETIME
+    DATE = 'Date' # YYYY-MM-DD
+    TIMESTAMP = 'Timestamp' # YYYY-MM-DD hh:mm:ss or 1852362464
+
+    # CATEGORICAL
+    SINGLE = 'Simple Category'
+    MULTIPLE = 'Complex Category' # Kind of unclear on the implementation
+
+    # FILE_PATH
+    IMAGE = 'Image'
+    VIDEO = 'Video'
+    AUDIO = 'Audio'
+
+    # URL
+    # How do we detect the tpye here... maybe setup async download for random sample an stats ?
+
+    # SEQUENTIAL
+    TEXT = 'Text'
+    ARRAY = 'Array' # Do we even want to support arrays / structs / nested ... etc ?
+
+class DATA_TYPES:
+    NUMERIC = (DATA_SUBTYPES.INT, DATA_SUBTYPES.FLOAT, DATA_SUBTYPES.BINARY)
+    DATE = (DATA_SUBTYPES.DATE, DATA_SUBTYPES.TIMESTAMP)
+    CATEGORICAL = (DATA_SUBTYPES.SINGLE, DATA_SUBTYPES.MULTIPLE)
+    FILE_PATH = (DATA_SUBTYPES.IMAGE, DATA_SUBTYPES.VIDEO, DATA_SUBTYPES.AUDIO)
+    URL = ()
+    SEQUENTIAL = (DATA_SUBTYPES.TEXT, DATA_SUBTYPES.ARRAY)
+
+
+
+class SAMPLE_TYPES:
+    TEST_SET = 'test'
+    TRAIN_SET = 'train'
+    PREDICT_SET = 'predict'
+
+class SAMPLER_MODES:
+    LEARN = 'learn'
+    PREDICT = 'predict'
+    DEFAULT = None
+
+class KEYS:
+    TEST_SET = 'test'
+    TRAIN_SET = 'train'
+    PREDICT_SET = 'predict'
+
+    ALL_SET = 'all'
+    X ='x'
+    Y ='y'
+    TS_ENCODED_ROW = 'TS_ENCODED_ROW'
+    TS_FLAT_ROW = 'TS_FLAT_ROW'
+
+class ORDER_BY_KEYS:
+    COLUMN = 0
+    ASCENDING_VALUE = 1
+
+class PREDICTORS:
+    DEFAULT = 'default'
+    CUSTOM = 'custom'
+
+class FRAMEWORKS:
+    PYTORCH = 'pytorch'
+    TENSORFLOW = 'tensorflow'
+    KERAS = 'keras'
+
+
+PHASE_START = 0
+PHASE_END = 1000
+PHASE_DATA_EXTRACTOR = 1
+PHASE_STATS_GENERATOR = 2
+PHASE_DATA_VECTORIZER = 3
+PHASE_DATA_ENCODER = 4
+PHASE_MODEL_TRAINER = 5
+PHASE_PREDICTION = 5
+PHASE_DATA_DEVECTORIZATION = 6
+PHASE_MODEL_ANALYZER = 7
+
+MODEL_STATUS_TRAINED = "Trained"
+MODEL_STATUS_PREPARING = "Preparing"
+MODEL_STATUS_TRAINING= "Training"
+MODEL_STATUS_ANALYZING = "Analyzing"
+MODEL_STATUS_ERROR = "Error"
+
+WORD_SEPARATORS = [',', "\t", ' ']
+NULL_VALUES = [None, '']
+
+SAMPLER_FLAT_TENSORS = 'flat_vectors'
+SAMPLER_COLUMN_TENSORS = 'column_tensors'
+
+MODEL_GROUP_BY_DEAFAULT_LIMIT = 80
+
+DEBUG_LOG_LEVEL = 10
+INFO_LOG_LEVEL = 20
+WARNING_LOG_LEVEL = 30
+ERROR_LOG_LEVEL = 40
+NO_LOGS_LOG_LEVEL = 50
---
+++ packages/MindsDB/mindsdb-0.9.1.0.tar.gz/mindsdb-0.9.1.0/mindsdb/libs/helpers/general_helpers.py
@@ -0,0 +1,125 @@
+
+import re
+import requests
+import platform
+import uuid
+import urllib
+
+from mindsdb.libs.data_types.mindsdb_logger import log
+from mindsdb.version import mindsdb_version as MINDSDB_VERSION
+from mindsdb.config import CONFIG
+from pathlib import Path
+
+
+def get_key_for_val(key, dict_map):
+    for orig_col in dict_map:
+        if dict_map[orig_col] == key:
+            return orig_col
+
+    return key
+
+def convert_cammelcase_to_snake_string(cammel_string):
+    """
+    Converts snake string to cammelcase
+
+    :param cammel_string: as described
+    :return: the snake string AsSaid -> as_said
+    """
+
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', cammel_string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def get_label_index_for_value(value, labels):
+    """
+    This will define what index interval does a value belong to given a list of labels
+
+    :param value: value to evaluate
+    :param labels: the labels to compare against
+    :return: an index value
+    """
+    if  value is None:
+        return 0 # Todo: Add another index for None values
+
+    if type(value) == type(''):
+        try:
+            index = labels.index(value)
+        except:
+            index = 0
+    else:
+        for index, compare_to in enumerate(labels):
+            if value < compare_to:
+                index = index
+                break
+
+    return index
+
+def convert_snake_to_cammelcase_string(snake_str, first_lower = False):
+    """
+    Converts snake to cammelcase
+
+    :param snake_str: as described
+    :param first_lower: if you want to start the string with lowercase as_said -> asSaid
+    :return: cammelcase string
+    """
+
+    components = snake_str.split('_')
+
+    if first_lower == True:
+        # We capitalize the first letter of each component except the first one
+        # with the 'title' method and join them together.
+        return components[0] + ''.join(x.title() for x in components[1:])
+    else:
+        return ''.join(x.title() for x in components)
+
+def check_for_updates():
+    """
+    Check for updates of mindsdb
+    it will ask the mindsdb server if there are new versions, if there are it will log a message
+
+    :return: None
+    """
+
+    # tmp files
+    uuid_file = CONFIG.MINDSDB_STORAGE_PATH + '/../uuid.mdb_base'
+    mdb_file = CONFIG.MINDSDB_STORAGE_PATH + '/start.mdb_base'
+
+    uuid_file_path = Path(uuid_file)
+    if uuid_file_path.is_file():
+        uuid_str = open(uuid_file).read()
+    else:
+        uuid_str = str(uuid.uuid4())
+        try:
+            open(uuid_file, 'w').write(uuid_str)
+        except:
+            log.warning('Cannot store token, Please add write permissions to file:' + uuid_file)
+            uuid_str = uuid_str + '.NO_WRITE'
+
+    file_path = Path(mdb_file)
+    if file_path.is_file():
+        token = open(mdb_file).read()
+    else:
+        token = '{system}|{version}|{uid}'.format(system=platform.system(), version=MINDSDB_VERSION, uid=uuid_str)
+        try:
+            open(mdb_file,'w').write(token)
+        except:
+            log.warning('Cannot store token, Please add write permissions to file:'+mdb_file)
+            token = token+'.NO_WRITE'
+    extra = urllib.parse.quote_plus(token)
+    try:
+        r = requests.get('http://mindsdb.com/updates/check/{extra}'.format(extra=extra), headers={'referer': 'http://check.mindsdb.com/?token={token}'.format(token=token)})
+    except:
+        log.warning('Could not check for updates')
+        return
+    try:
+        # TODO: Extract version, compare with version in version.py
+        ret = r.json()
+
+        if 'version' in ret and ret['version']!= MINDSDB_VERSION:
+            pass
+            #log.warning("There is a new version of MindsDB {version}, please do:\n    pip3 uninstall mindsdb\n    pip3 install mindsdb --user".format(version=ret['version']))
+        else:
+            log.debug('MindsDB is up to date!')
+
+    except:
+        log.warning('could not check for MindsDB updates')
