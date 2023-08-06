7728282ab05137f27be388e2e511beee5ebda258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package rbExtendedBots/0.0.2/rbExtendedBots-0.0.2.tar.gz

---
+++ packages/rbExtendedBots/rbExtendedBots-0.0.2.tar.gz/rbExtendedBots-0.0.2/setup.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+PACKAGE = "rbExtendedBots"
+VERSION = "0.0.2"
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    license='MIT',
+    description='Just another Review Bots',
+    long_description='Additional tools and checker for Review Bot',
+    author='Andre Klitzing',
+    author_email='aklitzing@gmail.com',
+    url='https://github.com/misery/ExtendedBots',
+    packages=[str('extended_bots')],
+    entry_points={
+        'reviewbot.tools': [
+            'chardetector = extended_bots.chardetector:ChardetectorTool',
+            'liquibase = extended_bots.liquibase:LiquibaseTool',
+            'regex = extended_bots.regex:RegexTool',
+            'uncrustify = extended_bots.uncrustify:UncrustifyTool',
+        ],
+    },
+    install_requires=[
+        'chardet>=2.3.0',
+        'reviewbot-worker>=1.0',
+        'RBTools>=1.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
