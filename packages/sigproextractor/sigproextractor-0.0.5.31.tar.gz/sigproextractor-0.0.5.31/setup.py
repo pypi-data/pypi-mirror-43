25a785d3c963a4a3b4ce2c5f4fbde0493dc593e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:08 +0000

    Add package sigproextractor/0.0.5.31/sigproextractor-0.0.5.31.tar.gz

---
+++ packages/sigproextractor/sigproextractor-0.0.5.31.tar.gz/sigproextractor-0.0.5.31/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md') as f:
+	long_description = f.read()
+
+setup(name='sigproextractor',
+      version='0.0.5.31',
+      description='Extracts mutational signatures from mutational catalogues',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!	
+      url="https://github.com/AlexandrovLab/SigProfilerExtractor.git",
+      author='S Mishu Ashiqul Islam',
+      author_email='m0islam@ucsd.edu',
+      license='UCSD',
+      packages=['sigproextractor'],
+      install_requires=[
+          'matplotlib>=2.2.2',
+          'scipy>=1.1.0', 
+          'numpy>=1.14.4', 
+          'pandas>=0.23.4', 
+          'nimfa>=1.1.0', 
+          'SigProfilerMatrixGenerator>=0.1.20', 
+          'sigProfilerPlotting>=0.1.17', 
+          'pillow',
+          'statsmodels>=0.9.0',
+          'scikit-learn>=0.20.2'
+           ],
+      include_package_data=True,      
+      zip_safe=False)
