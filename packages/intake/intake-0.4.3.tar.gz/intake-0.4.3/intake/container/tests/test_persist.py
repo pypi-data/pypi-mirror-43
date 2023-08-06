149ba56f60a3f75c821b834602dcb423d9030640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:38 +0000

    Add package intake/0.4.3/intake-0.4.3.tar.gz

---
+++ packages/intake/intake-0.4.3.tar.gz/intake-0.4.3/intake/container/tests/test_persist.py
@@ -0,0 +1,43 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2018, Anaconda, Inc. and Intake contributors
+# All rights reserved.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import os
+import time
+
+from intake.container.persist import store
+from intake.source.textfiles import TextFilesSource
+from intake.source.base import DataSource
+
+
+def test_store(temp_cache):
+    assert list(store) == []
+    s = DataSource(metadata={'original_name': 'blah'})
+    store.add(s._tok, s)
+    time.sleep(0.2)
+
+    store.ttl = 0
+    assert list(store) == [s._tok]
+    assert store.get_tok(s) == s._tok
+    assert store.needs_refresh(s) is False  # because it has no TTL
+
+    store.remove(s)
+    time.sleep(0.2)
+
+    assert list(store) == []
+    assert os.path.exists(store.pdir)
+    store.clear()
+    time.sleep(0.2)
+
+    assert not os.path.exists(store.pdir)
+    assert list(store) == []
+
+
+def test_backtrack(temp_cache):
+    s = TextFilesSource("*.py")
+    s2 = s.persist()
+    s3 = store.backtrack(s2)
+    assert s3 == s
