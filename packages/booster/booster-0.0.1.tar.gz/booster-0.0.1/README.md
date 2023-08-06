8990f8b15eac0e31793d6156c08dd9cf643b2b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:54 +0000

    Add package booster/0.0.1/booster-0.0.1.tar.gz

---
+++ packages/booster/booster-0.0.1.tar.gz/booster-0.0.1/README.md
@@ -0,0 +1,20 @@
+# Booster
+
+
+## Tree (Draft)
+
+
+```
+.
+├── app
+│   ├── utils
+│   │   └── preprocessing
+│   ├── 000_foo.ipynb
+│   └── 001_bar.ipynb
+├── config
+├── data
+│   └── rawdata
+├── log
+├── model
+└── tmp
+```
---
+++ packages/booster/booster-0.0.1.tar.gz/booster-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='booster',
+    version='0.0.1',
+    description='Boost your project',
+    author='Yusuke Sugomori',
+    author_email='me@yusugomori.com',
+    url='',
+    download_url='',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning'
+    ],
+    keywords='',
+    packages=find_packages()
+)
