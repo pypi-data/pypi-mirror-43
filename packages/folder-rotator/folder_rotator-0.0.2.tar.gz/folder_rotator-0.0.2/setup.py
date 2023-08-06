a07c4fec28489e0bffd25ec5a0ab7d982043ea74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:41 +0000

    Add package folder-rotator/0.0.2/folder_rotator-0.0.2.tar.gz

---
+++ packages/folder-rotator/folder_rotator-0.0.2.tar.gz/folder_rotator-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='folder_rotator',
+    version='0.0.2',
+    packages=find_packages(),
+    author='Kevin Glasson',
+    author_email='kevinglasson+folderrotator@gmail.com',
+    url='https://github.com/kevinglasson/folder_rotator',
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+    ],
+    setup_requires=[
+        "pytest-runner"
+    ],
+    tests_require=[
+        "pytest==4.3.",
+        "pytest-sugar==0.9.2"
+    ],
+    python_requires='>3.6'
+)
