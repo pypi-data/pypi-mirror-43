369d6e9fca9cf55335e8c8667de762fa7b9d658f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:36 +0000

    Add package wagtailcodeblock/1.15.0.0/wagtailcodeblock-1.15.0.0.tar.gz

---
+++ packages/wagtailcodeblock/wagtailcodeblock-1.15.0.0.tar.gz/wagtailcodeblock-1.15.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='wagtailcodeblock',
+    version="1.15.0.0",
+    description='Wagtail Code Block provides PrismJS syntax highlighting in Wagtail.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tim Allen',
+    author_email='tallen@wharton.upenn.edu',
+    url='https://github.com/FlipperPA/wagtailcodeblock',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'wagtail>=1.8',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
