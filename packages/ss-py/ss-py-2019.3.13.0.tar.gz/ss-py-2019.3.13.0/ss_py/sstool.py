26ce06a9c7f7f63191db961f1e3f674ef4cd2f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package ss-py/2019.3.13.0/ss-py-2019.3.13.0.tar.gz

---
+++ packages/ss-py/ss-py-2019.3.13.0.tar.gz/ss-py-2019.3.13.0/ss_py/sstool.py
@@ -0,0 +1,41 @@
+import sys
+import subprocess
+
+from ss_py.ssprofile import SSProfile
+
+PATH_BASE = './'
+# PATH_BASE = '/etc/ss_py/'
+FILE_PROFILE = PATH_BASE + 'profile.json'
+
+class SSTool(object):
+    def __init__(self):
+        self.profile = SSProfile(FILE_PROFILE)
+
+    def _checkPortRanges(self, port):
+        port = int(port)
+        if port > 0 and port <= 65536:
+            return True
+        return False
+
+    def startSS(self):
+        cmd = 'ssserver -qq -c ' + FILE_PROFILE
+        res = subprocess.call(cmd, shell=True)
+        return res
+
+    def stopSS(self):
+        return
+    
+    def addDelPort(self, isAdd, port, pwd):
+        if self._checkPortRanges(port) == False:
+            return False
+        if isAdd:
+            self.profile.addPort(port, pwd)
+        else:
+            self.profile.delPort(port)
+        self.profile.save(FILE_PROFILE)
+        self.stopSS()
+        self.startSS()
+    
+    
+
+
