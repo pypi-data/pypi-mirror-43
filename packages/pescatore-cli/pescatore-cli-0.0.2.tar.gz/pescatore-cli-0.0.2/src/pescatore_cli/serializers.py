07dfb04a6af7644ceb05bb7fc38f1c25650da86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:02 +0000

    Add package pescatore-cli/0.0.2/pescatore-cli-0.0.2.tar.gz

---
+++ packages/pescatore-cli/pescatore-cli-0.0.2.tar.gz/pescatore-cli-0.0.2/src/pescatore_cli/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class cerca_stringa_in_bacino__serializer(serializers.Serializer):
+    bacino = serializers.CharField(required=True)
+    stringa = serializers.CharField(required=True)
+    id_licenza = serializers.CharField(required=False)
+    note = serializers.CharField(required=False)
+    intervallo = serializers.IntegerField(required=False)
