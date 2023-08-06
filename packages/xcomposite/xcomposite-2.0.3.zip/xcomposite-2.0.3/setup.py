c491134ae39215b899873799fe1ce3748cfff6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:24 +0000

    Add package xcomposite/2.0.3/xcomposite-2.0.3.zip

---
+++ packages/xcomposite/xcomposite-2.0.3.zip/xcomposite-2.0.3/setup.py
@@ -0,0 +1,30 @@
+import os
+import setuptools
+
+if os.path.exists('README.md'):
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
+
+else:
+    long_description = 'A python package exposing the class composition design pattern'
+
+setuptools.setup(
+    name='xcomposite',
+    version='2.0.3',
+    author='Mike Malinowski',
+    author_email='mike@twisted.space',
+    description='A python package exposing the class composition design pattern',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mikemalinowski/xcomposite',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_data={
+        '': ['examples/game/data/*.json'],
+    },
+    keywords="xcomposite composite composition inheritence side combine",
+)
