f4c57bc62e48e44338358ecfefe7e217f4fe2e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:40 +0000

    Add package commented-out-code/0.0.1/commented_out_code-0.0.1.tar.gz

---
+++ packages/commented-out-code/commented_out_code-0.0.1.tar.gz/commented_out_code-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="commented_out_code",
+    version="0.0.1",
+    author="qiaowang",
+    author_email="adawq0@gmail.com",
+    description="A helloworld test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/w2qiao",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
