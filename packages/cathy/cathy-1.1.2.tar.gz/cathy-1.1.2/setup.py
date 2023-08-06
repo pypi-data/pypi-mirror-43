e13973ba079ef2fa0e2fe4816aa69e3c6067fd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:07 +0000

    Add package cathy/1.1.2/cathy-1.1.2.tar.gz

---
+++ packages/cathy/cathy-1.1.2.tar.gz/cathy-1.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+def readme_file_contents():
+    with open('README.rst') as f:
+        data = f.read()
+    return data
+
+
+setup(
+    name='cathy',
+    version='1.1.2',
+    description='Discord chat bot using AIML artificial intelligence.',
+    long_description=readme_file_contents(),
+    url='https://github.com/DevDungeon/ChattyCathy',
+    author='DevDungeon',
+    author_email='nanodano@devdungeon.com',
+    license='GPL-3.0',
+    packages=['cathy'],
+    scripts=[
+        'bin/cathy',
+        'bin/cathy.bat',
+    ],
+    package_data={
+        'cathy': [
+            'std-startup.xml',
+            'aiml/alice/*.aiml',
+            'aiml/custom/*.aiml'
+        ],
+    },
+    zip_safe=False,
+    install_requires=[
+        'docopt',
+        'python-aiml',
+        'discord.py',
+        'requests'
+    ]
+)
