f97234365ea8b469726279ded22447dfe563d21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package biblib-simple/0.1.0/biblib-simple-0.1.0.tar.gz

---
+++ packages/biblib-simple/biblib-simple-0.1.0.tar.gz/biblib-simple-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='biblib-simple',
+    version='0.1.0',
+    description='Simple, correct BibTeX parser and algorithms',
+    url='https://github.com/colour-science/biblib',
+    author='Austin Clements',
+    author_email='colour-science@googlegroups.com',
+    packages=['biblib'],
+    keywords=['bibtex', 'tex'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+        'Topic :: Text Processing',
+    ],
+    long_description=open('README.md').read(),
+)
