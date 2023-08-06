73bb410cf18d461cf50c8764c3fa043330aab9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:32 +0000

    Add package modeleval/0.1.7/modeleval-0.1.7.tar.gz

---
+++ packages/modeleval/modeleval-0.1.7.tar.gz/modeleval-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modeleval',
+    version='0.1.7',
+    url='https://github.com/tqi2/model-evaluation',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','sklearn','matplotlib','IPython'],
+    python_requires='>=3.6',
+    author='Tian (Luke) Qi',
+    author_email='tqi2@dons.usfca.edu',
+    description='A Python 3 library for a easier machine learning model evaluation',
+    keywords='machine-learning evaluation',
+    zip_safe=False
+)
