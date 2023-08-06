69da4e1a25bfd691e1d9960420515fc6cae77917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:27 +0000

    Add package pysp/0.0.5/pysp-0.0.5.tar.gz

---
+++ packages/pysp/pysp-0.0.5.tar.gz/pysp-0.0.5/pysp/serror.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import sys
+# from functools import partial
+#
+# _print = partial(print, file=sys.stdout)
+
+
+class Debug:
+    DEBUG = False
+    TAG_DEBUG = '[D] '
+    TAG_ERROR = '[E] '
+    TAG_INFO = '[I] '
+
+
+class SDebug(Debug):
+    def dprint(self, *args, **kwargs):
+        if self.DEBUG:
+            print(self.TAG_DEBUG, *args, file=sys.stderr, **kwargs)
+
+    def eprint(self, *args, **kwargs):
+        print(self.TAG_ERROR, *args, file=sys.stderr, **kwargs)
+
+    def iprint(self, *args, **kwargs):
+        print(self.TAG_INFO, *args, file=sys.stderr, **kwargs)
+
+class SCDebug(Debug):
+    @classmethod
+    def dprint(cls, *args, **kwargs):
+        if cls.DEBUG:
+            print(cls.TAG_DEBUG, *args, file=sys.stderr, **kwargs)
+
+    @classmethod
+    def eprint(cls, *args, **kwargs):
+        print(cls.TAG_ERROR, *args, file=sys.stderr, **kwargs)
+
+    @classmethod
+    def iprint(cls, *args, **kwargs):
+        print(cls.TAG_INFO, *args, file=sys.stderr, **kwargs)
+
+
+class SError(Exception):
+    pass
