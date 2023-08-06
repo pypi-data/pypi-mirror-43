b6ad23a1ebb89dc1a81a80a9433f0deed7787164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:45 +0000

    Add package magnetos/0.6.3/magnetos-0.6.3-py2.py3-none-any.whl

---
+++ packages/magnetos/magnetos-0.6.3-py2.py3-none-any.whl/magnetos/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Created by restran on 2017/8/29
+from __future__ import unicode_literals, absolute_import
+
+from . import utils as util
+
+__all__ = ['util', '__author__', '__version__']
+
+__author__ = "restran <grestran@gmail.com>"
+__version__ = '0.6.3'
---
+++ packages/magnetos/magnetos-0.6.3-py2.py3-none-any.whl/tests/test_crypto.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Created by restran on 2017/10/15
+from __future__ import unicode_literals, absolute_import
+import os
+import unittest
+import logging
+from magnetos.crypto import mobile_keyboard
+
+logger = logging.getLogger(__name__)
+
+
+class CryptoTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
