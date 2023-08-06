7e8a4b200bdce09e95eba135b1a8f2846291e887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:31 +0000

    Add package py-configfile/0.0.4/py_configfile-0.0.4.tar.gz

---
+++ packages/py-configfile/py_configfile-0.0.4.tar.gz/py_configfile-0.0.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Author: ZhangTianJie
+
+import setuptools
+
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except:
+    long_description = 'python configuration file loading package',
+
+setuptools.setup(
+    name='py_configfile',
+    version='0.0.4',
+    description='python configuration file loading package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    py_modules=["configfile"],
+
+    url='http://github.com/ztj1993/py_configfile',
+    author='ZhangTianJie',
+    author_email='ztj1993@gmail.com',
+
+    keywords='configfile config json yaml',
+    install_requires=['pyyaml'],
+
+    license='MIT',
+)
