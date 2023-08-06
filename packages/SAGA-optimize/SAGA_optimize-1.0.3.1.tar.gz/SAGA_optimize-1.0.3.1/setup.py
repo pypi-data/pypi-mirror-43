b41647c66ca0cce8a65c913c29e6d68b5ebca4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:19 +0000

    Add package SAGA-optimize/1.0.3.1/SAGA_optimize-1.0.3.1.tar.gz

---
+++ packages/SAGA-optimize/SAGA_optimize-1.0.3.1.tar.gz/SAGA_optimize-1.0.3.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+from setuptools import setup
+from SAGA_optimize import __version__
+
+
+setup(
+    name='SAGA_optimize',
+    version=__version__,
+    author='Huan Jin',
+    author_email='hji236@g.uky.edu',
+    description='Optimization method for solving boundary-value inverse problem based on a combined simulated annealing and genetic algorithm',
+    keywords="optimization inverse problem simulated annealing genetic algorithm",
+    license='BSD',
+    url='https://github.com/MoseleyBioinformaticsLab/SAGA_optimize.git',
+    py_modules=['SAGA_optimize'],
+    install_requires=['jsonpickle >= 0.9.5'],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+    ]
+
+
+)
