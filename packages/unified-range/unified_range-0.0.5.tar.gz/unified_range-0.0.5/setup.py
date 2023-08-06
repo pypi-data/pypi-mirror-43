122d3dc50c1b5bcab808a95ff2a4f862dc7c127e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:11 +0000

    Add package unified-range/0.0.5/unified_range-0.0.5.tar.gz

---
+++ packages/unified-range/unified_range-0.0.5.tar.gz/unified_range-0.0.5/setup.py
@@ -0,0 +1,22 @@
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='unified_range',
+    description="Convert between semver range and maven version range",
+    url="https://github.com/snyk/unified-range",
+    version='0.0.5',
+    # packages=['unified_range', ],
+    packages=find_packages(),
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
