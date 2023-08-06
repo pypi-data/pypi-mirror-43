06bc175d20264a5a63670e31197d2bc72d579b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package lino-cosi/19.3.0/lino-cosi-19.3.0.tar.gz

---
+++ packages/lino-cosi/lino-cosi-19.3.0.tar.gz/lino-cosi-19.3.0/lino_cosi/lib/contacts/models.py
@@ -0,0 +1,63 @@
+# Copyright 2013-2018 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+
+from lino.api import _
+from lino_xl.lib.contacts.models import *
+
+
+class PartnerDetail(PartnerDetail):
+    
+    main = "general ledger sepa.AccountsByPartner"
+
+    general = dd.Panel("""
+    address_box:60 contact_box:30 overview
+    bottom_box
+    """, label=_("General"))
+
+    ledger = dd.Panel("""
+    vat_regime purchase_account payment_term 
+    vat.VouchersByPartner
+    ledger.MovementsByPartner
+    """, label=dd.plugins.ledger.verbose_name)
+    
+    address_box = dd.Panel("""
+    name_box
+    country region city zip_code:10
+    addr1
+    #street_prefix street:25 street_no #street_box
+    addr2
+    """, label=_("Address"))
+
+    contact_box = dd.Panel("""
+    info_box
+    email:40
+    url
+    phone
+    gsm fax
+    """, label=_("Contact"))
+
+    bottom_box = """
+    remarks
+    """
+
+    # name_box = "name"
+    # info_box = "id:8 language:5"
+    #
+
+class PersonDetail(PartnerDetail, PersonDetail):
+    
+    name_box = "last_name first_name:15 gender title:10"
+    info_box = "id:5 language:10"
+    bottom_box = "remarks contacts.RolesByPerson"
+
+
+class CompanyDetail(PartnerDetail, CompanyDetail):
+
+    bottom_box = """
+    remarks contacts.RolesByCompany
+    """
+
+    name_box = "#prefix:10 name:40 type:20"
+    
+
---
+++ packages/lino-cosi/lino-cosi-19.3.0.tar.gz/lino-cosi-19.3.0/lino_cosi/lib/cosi/settings.py
@@ -0,0 +1,126 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2011-2019 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+
+"""Default settings module for a :ref:`cosi` project.
+
+"""
+
+from __future__ import unicode_literals
+
+import lino_cosi
+from lino.projects.std.settings import *
+
+
+class Site(Site):
+    """Base class for a :ref:`cosi` application.
+
+    """
+
+    verbose_name = "Lino Cosi"
+    version = lino_cosi.SETUP_INFO['version']
+    url = lino_cosi.SETUP_INFO['url']
+
+    demo_fixtures = 'std few_countries minimal_ledger \
+    furniture demo demo2'.split()
+
+    # languages = 'en de fr'
+    languages = 'en'
+
+    user_types_module = 'lino_cosi.lib.cosi.user_types'
+    custom_layouts_module = 'lino_cosi.lib.cosi.layouts'
+
+    default_build_method = 'weasy2pdf'
+
+    # textfield_format = 'html'
+
+    def get_installed_apps(self):
+        yield super(Site, self).get_installed_apps()
+        yield 'lino.modlib.gfks'
+        # yield 'lino.modlib.system'
+        yield 'lino.modlib.users'
+        yield 'lino_xl.lib.countries'
+        yield 'lino_cosi.lib.contacts'
+        #~ yield 'lino_xl.lib.households'
+
+        yield 'lino_xl.lib.excerpts'
+
+        # yield 'lino_xl.lib.outbox'
+        yield 'lino.modlib.uploads'
+        # yield 'lino.modlib.files'
+        yield 'lino.modlib.weasyprint'
+        yield 'lino.modlib.export_excel'
+        yield 'lino.modlib.tinymce'
+        # yield 'lino.modlib.wkhtmltopdf'
+
+        # ledger must come before sales because its demo fixture
+        # creates journals (?)
+
+        # yield 'lino.modlib.ledger'
+        yield 'lino_cosi.lib.products'
+        yield 'lino_xl.lib.sales'
+        # yield 'lino_xl.lib.invoicing'
+        yield 'lino_xl.lib.ledger'
+        yield 'lino_xl.lib.sepa'
+        yield 'lino_xl.lib.vat'
+        yield 'lino_xl.lib.finan'
+        # yield 'lino_xl.lib.bevat'
+        yield 'lino_xl.lib.sheets'
+        #~ 'lino.modlib.journals',
+        #~ 'lino_xl.lib.projects',
+        #~ yield 'lino_xl.lib.blogs'
+        #~ yield 'lino.modlib.tickets'
+        #~ 'lino.modlib.links',
+        #~ 'lino_xl.lib.thirds',
+        #~ yield 'lino_xl.lib.postings'
+        # yield 'lino_xl.lib.pages'
+        # yield 'lino_cosi.lib.cosi'
+
+    def setup_plugins(self):
+        """
+        Change the default value of certain plugin settings.
+
+        """
+        super(Site, self).setup_plugins()
+        self.plugins.countries.configure(hide_region=True)
+        self.plugins.ledger.configure(use_pcmn=True)
+        self.plugins.countries.configure(country_code='BE')
+        self.plugins.products.configure(menu_group='sales')
+
+    # def setup_actions(self):
+    #     super(Site, self).setup_actions()
+    #     partners = self.modules.contacts
+    #     from lino.core.merge import MergeAction
+    #     for m in (partners.Person, partners.Organisation):
+    #         m.define_action(merge_row=MergeAction(m))
+
+    # def setup_layouts(self):
+    #     super(Site, self).setup_layouts()
+
+    #     self.models.system.SiteConfigs.set_detail_layout("""
+    #     site_company next_partner_id:10
+    #     default_build_method
+    #     clients_account   sales_account
+    #     suppliers_account purchases_account tax_offices_account
+    #     """)
+
+    #     self.models.ledger.Accounts.set_detail_layout("""
+    #     ref:10 name id:5
+    #     seqno group type clearable
+    #     ledger.MovementsByAccount
+    #     """)
+
+
+# class DocsSite(Site):
+#     """A special variant used to build the docs.
+#     """
+#     def get_installed_apps(self):
+#         yield super(DocsSite, self).get_installed_apps()
+#         yield 'lino_xl.lib.courses'  # needed for Sphinx autosummary
+#         yield 'lino_xl.lib.vatless'
+
+    # def get_apps_modifiers(self, **kw):
+    #     kw = super(DocsSite, self).get_apps_modifiers(**kw)
+    #     kw.update(sales='lino_cosi.lib.auto.sales')
+    #     return kw
