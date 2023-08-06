7d1e3d88d68deee90cfa68d310e24cfd31c3f461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:54 +0000

    Add package x-aps/0.0.1/x-aps-0.0.1.tar.gz

---
+++ packages/x-aps/x-aps-0.0.1.tar.gz/x-aps-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="x-aps",
+    version="0.0.1",
+    author="landpack",
+    author_email="landpassing@gmail.com",
+    description="Using decorator way to work with APScheduler on Tornado",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/land-pack/x-aps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
