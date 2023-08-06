1b998ac430dabaf458883eec5a0e5ac326fddc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:51 +0000

    Add package siphono-notify/0.1.0/siphono_notify-0.1.0-py3-none-any.whl

---
+++ packages/siphono-notify/siphono_notify-0.1.0-py3-none-any.whl/siphono_notify/__init__.py
@@ -0,0 +1,6 @@
+"""Vanity imports for the package."""
+from siphono_notify.notifier import Notifier
+from siphono_notify.progress_bar import ProgressBar
+from siphono_notify.target import Target
+
+__version__ = '0.1.0'
