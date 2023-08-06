b30d03bc87dad75e9f78b71da0365e14c5bddba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:02 +0000

    Add package ate/0.17.0/ate-0.17.0-py3-none-any.whl

---
+++ packages/ate/ate-0.17.0-py3-none-any.whl/ate/test.py
@@ -0,0 +1,120 @@
+import logging
+
+
+class Test:
+    """
+    A test is the most basic unit of an executing test sequence.  Within
+    a test, we may have a setup(), execute(), and teardown() method.  Only the
+    `execute()` method is required to be overridden.
+
+    :param moniker: a shortcut name for this particular test
+    :param loglevel: the logging level to apply such as `logging.INFO`
+    """
+
+    def __init__(self, moniker, loglevel=logging.INFO):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.setLevel(loglevel)
+
+        self.moniker = moniker
+        self._test_is_passing = None
+        self.value = None
+        self.status = 'waiting'
+
+        self.saved_data = {}
+
+    @property
+    def is_passing(self):
+        return self._test_is_passing
+
+    def _setup(self, aborted, is_passing):
+        """
+        Pre-execution method used for logging and housekeeping.
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return:
+        """
+        self._logger.info(f'setting up "{self.moniker}"')
+
+        self._test_is_passing = True
+        self.value = None
+        self.status = 'running' if not aborted else 'aborted'
+
+        self.setup(aborted=aborted, is_passing=is_passing)
+        self.status = 'running' if not aborted else 'aborted'
+
+    def _execute(self, aborted, is_passing):
+        """
+        Pre-execution method used for logging and housekeeping.
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return:
+        """
+        self._logger.info(f'executing test "{self.moniker}"')
+
+        self.value = self.execute(aborted=aborted, is_passing=is_passing)
+        self.status = 'running' if not aborted else 'aborted'
+
+        return self.value
+
+    def _teardown(self, aborted, is_passing):
+        """
+        Pre-execution method used for logging and housekeeping.
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return:
+        """
+        self._logger.info(f'tearing down "{self.moniker}"')
+
+        self.teardown(aborted, is_passing)
+        self.status = 'complete'
+
+    def reset(self):
+        """
+        Reset the test status
+        :return: None
+        """
+        self.status = 'waiting'
+
+    def save_dict(self, data: dict):
+        self.saved_data = data.copy()
+
+    def fail(self):
+        """
+        When called, will cause the test to fail.
+
+        :return: None
+        """
+        self._test_is_passing = False
+
+    def setup(self, aborted, is_passing):
+        """
+        Abstract method intended to be overridden by subclass
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return: None
+        """
+        pass
+
+    def execute(self, aborted, is_passing):
+        """
+        Abstract method intended to be overridden by subclass
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return: value to be appended to the sequence dictionary
+        """
+        raise NotImplementedError
+
+    def teardown(self, aborted, is_passing):
+        """
+        Abstract method intended to be overridden by subclass
+
+        :param aborted: True if the test was aborted or False if not
+        :param is_passing: True if the test sequence is passing up to this point, else False
+        :return: None
+        """
+        pass
