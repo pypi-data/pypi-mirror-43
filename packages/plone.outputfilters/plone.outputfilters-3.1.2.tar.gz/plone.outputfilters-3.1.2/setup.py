6c19c53bb1d111f53b8211e62f22567204a4da2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package plone.outputfilters/3.1.2/plone.outputfilters-3.1.2.tar.gz

---
+++ packages/plone.outputfilters/plone.outputfilters-3.1.2.tar.gz/plone.outputfilters-3.1.2/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+version = '3.1.2'
+
+setup(
+    name='plone.outputfilters',
+    version=version,
+    description=(
+        "Transformations applied to HTML in "
+        "Plone text fields as they are rendered"
+    ),
+    long_description=(
+        open("README.rst").read()
+        + "\n"
+        + open(os.path.join("plone", "outputfilters", "README.rst")).read()
+        + "\n"
+        + open("CHANGES.rst").read()
+    ),
+    # Get more strings from https://pypi.org/classifiers/
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords='plone transform filter uid caption',
+    author='David Glick, Plone Foundation',
+    author_email='davidglick@groundwire.org',
+    url='http://github.com/plone/plone.outputfilters',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Products.MimetypesRegistry',
+        'Products.PortalTransforms>=2.0a1',
+        'setuptools',
+        'six',
+        'unidecode',
+        'beautifulsoup4',
+        'lxml',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.contenttypes',
+            'plone.app.testing',
+            'plone.app.robotframework',
+            'plone.namedfile',
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
