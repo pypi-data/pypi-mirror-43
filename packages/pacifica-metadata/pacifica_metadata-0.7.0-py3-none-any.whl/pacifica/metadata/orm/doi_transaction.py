5db53f830f2f8cf50a3e3bff97c1c9d56365c114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:28 +0000

    Add package pacifica-metadata/0.7.0/pacifica_metadata-0.7.0-py3-none-any.whl

---
+++ packages/pacifica-metadata/pacifica_metadata-0.7.0-py3-none-any.whl/pacifica/metadata/orm/doi_transaction.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""DOI transaction relationship."""
+from peewee import ForeignKeyField
+from pacifica.metadata.orm.utils import index_hash, unicode_type
+from pacifica.metadata.orm.transaction_release import TransactionRelease
+from pacifica.metadata.orm.doi_entries import DOIEntries
+from pacifica.metadata.rest.orm import CherryPyAPI
+
+
+class DOITransaction(CherryPyAPI):
+    """
+    Relates DOI entries with transaction release entries.
+
+    Attributes:
+        +-------------------+--------------------------------------------+
+        | Name              | Description                                |
+        +===================+============================================+
+        | doi               | Link to the DOI model                      |
+        +-------------------+--------------------------------------------+
+        | transaction       | Link to the TransactionRelease model       |
+        +-------------------+--------------------------------------------+
+    """
+
+    doi = ForeignKeyField(
+        DOIEntries, backref='transactions', field='doi', column_name='doi', primary_key=True)
+    transaction = ForeignKeyField(
+        TransactionRelease, field='transaction', backref='doi_releases')
+
+    def to_hash(self, **flags):
+        """Convert the object to a hash."""
+        obj = super(DOITransaction, self).to_hash(**flags)
+        obj['_id'] = index_hash(unicode_type(self.__data__['doi']),
+                                int(self.__data__['transaction']))
+        obj['doi'] = unicode_type(self.__data__['doi'])
+        obj['transaction'] = int(self.__data__['transaction'])
+        return obj
+
+    def from_hash(self, obj):
+        """Convert the hash into the object."""
+        super(DOITransaction, self).from_hash(obj)
+        self._set_only_if('doi', obj, 'doi',
+                          lambda: DOIEntries.get(DOIEntries.doi == obj['doi']))
+        self._set_only_if('transaction', obj, 'transaction',
+                          lambda: TransactionRelease.get(TransactionRelease.transaction == obj['transaction']))
+
+    @classmethod
+    def where_clause(cls, kwargs):
+        """Where clause for the various elements."""
+        where_clause = super(DOITransaction, cls).where_clause(kwargs)
+        return cls._where_attr_clause(where_clause, kwargs, ['doi', 'transaction'])
