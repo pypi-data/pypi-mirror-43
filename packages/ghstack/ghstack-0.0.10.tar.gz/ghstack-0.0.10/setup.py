4d004857d76ed056ecf7720116c01c53edfdfef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:37 +0000

    Add package ghstack/0.0.10/ghstack-0.0.10.tar.gz

---
+++ packages/ghstack/ghstack-0.0.10.tar.gz/ghstack-0.0.10/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ghstack",
+    version="0.0.10",
+    author="Edward Z. Yang",
+    author_email="ezyang@mit.edu",
+    description="Stack diff support for GitHub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/ghexport",
+    packages=setuptools.find_packages(exclude=("graphql", "graphql.*",)),
+    include_package_data=True,
+    package_data={
+        'ghstack': ['py.typed'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/ghstack'],
+    install_requires=[
+        'requests',
+        'typing_extensions>=3.7.2',  # need Literal
+        'dataclasses',
+    ],
+    # This is not supported by pip 10, which a lot of people have
+    # installed (because it's the conda default)
+    #
+    # entry_points={
+    #     'console_scripts': [
+    #         'ghstack = ghstack.__main__:main'
+    #     ]
+    # },
+)
