65e4e2d85748d31c93039604523d595f1bd6d419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:28 +0000

    Add package warp/0.0.1/warp-0.0.1.tar.gz

---
+++ packages/warp/warp-0.0.1.tar.gz/warp-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="warp",
+    version="0.0.1",
+    author="Willem Pienaar",
+    author_email="pypi@willem.co",
+    url="https://github.com/woop/warp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
