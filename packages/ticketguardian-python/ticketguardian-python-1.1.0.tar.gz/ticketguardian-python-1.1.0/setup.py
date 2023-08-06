6f9ed87c5a325daf850fbdc1b9349343edac50eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:09 +0000

    Add package ticketguardian-python/1.1.0/ticketguardian-python-1.1.0.tar.gz

---
+++ packages/ticketguardian-python/ticketguardian-python-1.1.0.tar.gz/ticketguardian-python-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.1.0'
+
+import os
+
+requirements = []
+
+for file in os.listdir("./requirements/dep/"):
+    with open('./requirements/dep/{}'.format(file)) as f:
+        requirements += f.read().splitlines()
+
+setup(
+    name='ticketguardian-python',
+    packages=find_packages(exclude=['test*']),
+    version=__version__,
+    description='A python SDK for interacting with the TicketGuardian API',
+    author='TicketGuardian',
+    author_email='developers@protecht.io',
+    url='https://github.com/TicketGuardian/ticketguardian-python',
+    keywords=['sdk', 'TicketGuardian', 'python'],
+    install_requires=requirements,
+    classifiers=[],
+)
