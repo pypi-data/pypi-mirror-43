407a9c785ecc92746acfd9f261425e4c418933bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:34 +0000

    Add package nameko-eventlog-dispatcher/0.4.0/nameko-eventlog-dispatcher-0.4.0.tar.gz

---
+++ packages/nameko-eventlog-dispatcher/nameko-eventlog-dispatcher-0.4.0.tar.gz/nameko-eventlog-dispatcher-0.4.0/CHANGELOG.rst
@@ -0,0 +1,52 @@
+Version History
+===============
+
+Here you can see the full list of changes between
+nameko-eventlog-dispatcher versions, where semantic versioning is used:
+*major.minor.patch*.
+
+Backwards-compatible changes increment the minor version number only.
+
+
+0.4.0
+-----
+
+Released 2019-03-15
+
+* Add support for Python 3.7 (#12)
+* Add support for older Nameko versions: 2.6, 2.7, 2.8, 2.9, 2.10, 2.11
+  (#13, #14)
+
+
+0.3.0
+-----
+
+Released 2019-01-28
+
+* Updated to support Nameko 2.11 (**this breaks compatibilty with
+  older Nameko versions**) (#8)
+* Drop support for Python 3.3 (#8)
+* Support Python 3.5 and 3.6 (#8)
+
+
+0.2.0
+-----
+
+Released 2017-06-23
+
+* Use service name as source service, instead of ``all``, when
+  dispatching Nameko events automatically.
+* Add the ability to override the default Nameko ``event_type`` (used
+  for the event logs) in the service config.
+* Add ``MANIFEST.in``
+* Add a ``metadata`` argument to the ``dispatch`` function to provide
+  extra event metadata.
+
+0.1.0
+-----
+
+Released 2017-06-19
+
+* First release of the library.
+* Add ability to manually dispatch event logs.
+* Add ability to dispatch event logs when entrypoints are fired.
---
+++ packages/nameko-eventlog-dispatcher/nameko-eventlog-dispatcher-0.4.0.tar.gz/nameko-eventlog-dispatcher-0.4.0/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+
+file_path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(file_path, 'README.rst'), 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='nameko-eventlog-dispatcher',
+    version='0.4.0',
+    description=(
+        'Nameko dependency provider that dispatches log data using Events '
+        '(Pub-Sub).'
+    ),
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Julio Trigo',
+    author_email='julio.trigo@sohonet.com',
+    url='https://github.com/sohonetlabs/nameko-eventlog-dispatcher',
+    packages=find_packages(exclude=['test', 'test.*']),
+    install_requires=['nameko>=2.6,<2.12'],
+    extras_require={
+        'dev': [
+            'pytest<=4.3.0',
+            'flake8',
+            'coverage',
+            'restructuredtext-lint',
+            'Pygments',
+        ],
+    },
+    zip_safe=True,
+    license='MIT License',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
