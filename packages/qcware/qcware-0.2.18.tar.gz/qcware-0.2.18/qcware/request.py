e734c9bb3f86c431cb1152c0eae187d6aba58d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:00 +0000

    Add package qcware/0.2.18/qcware-0.2.18.tar.gz

---
+++ packages/qcware/qcware-0.2.18.tar.gz/qcware-0.2.18/qcware/request.py
@@ -0,0 +1,32 @@
+import ast
+import json
+import pickle
+import requests
+from . import param_utils
+
+
+def pickle_json(json_object):
+    if isinstance(json_object, dict):
+        r = {}
+        for k, v in json_object.items():
+            if k != 'Q':
+                r[k] = pickle_json(v)
+            else:
+                r['Q'] = pickle.dumps(v, protocol=0)
+        return r
+    elif isinstance(json_object, list):
+        return [pickle_json(elem) for elem in json_object]
+    else:
+        return pickle.dumps(json_object, protocol=0)
+
+
+def post(api_endpoint_url, param_dictionary, endpoint_type):
+    pbuffed_params = param_utils.convert(param_dictionary, endpoint_type)
+    r = requests.post(api_endpoint_url,
+                      data=pbuffed_params.SerializeToString())
+
+    r = json.loads(r.text)
+    if r.get('solution') and endpoint_type == 'solve_binary':
+        r['solution'] = ast.literal_eval(r['solution'])
+
+    return r
