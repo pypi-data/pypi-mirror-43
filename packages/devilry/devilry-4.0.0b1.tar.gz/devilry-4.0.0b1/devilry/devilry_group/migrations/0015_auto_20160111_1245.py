decd52d1e140f481f2abc8f5b0ece6dd87ce3bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:37 +0000

    Add package devilry/4.0.0b1/devilry-4.0.0b1.tar.gz

---
+++ packages/devilry/devilry-4.0.0b1.tar.gz/devilry-4.0.0b1/devilry/devilry_group/migrations/0015_auto_20160111_1245.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('devilry_group', '0014_feedbackset_grading_status'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='feedbackset',
+            name='grading_status',
+        ),
+        migrations.AddField(
+            model_name='feedbackset',
+            name='feedbackset_type',
+            field=models.CharField(default=b'feedbackset_type_first_attempt', max_length=50, db_index=True, choices=[(b'feedbackset_type_first_attempt', b'feedbackset_type_first_attempt'), (b'feedbackset_type_new_attempt', b'feedbackset_type_new_attempt'), (b'feedbackset_type_re_edit', b'feedbackset_type_re_edit')]),
+        ),
+    ]
