84cb3a33b4827536a62b402ecb31614a5349adc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package pdbtest/1.5/pdbtest-1.5.tar.gz

---
+++ packages/pdbtest/pdbtest-1.5.tar.gz/pdbtest-1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    author='Zachary J. McCord',
+    author_email='zjmccord@gmail.com',
+    classifiers=["Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 2",
+                 "Topic :: Software Development :: Debuggers",
+                 "Topic :: Software Development :: Testing",
+                 "License :: OSI Approved :: Apache Software License",
+                 "Operating System :: OS Independent"],
+    description='A unittest shim to make PDB debugging of test cases easy.',
+    keywords=['testing', 'pdb', 'unittest'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name='pdbtest',
+    packages=setuptools.find_packages(),
+    url='https://github.com/zack112358/pdbtest/',
+    version='1.5',
+)
