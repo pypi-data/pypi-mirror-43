20f4dda1d86e3d41fd9a5dad02d2ffc411b9ced6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:46 +0000

    Add package RLDQN/0.0.1/RLDQN-0.0.1.tar.gz

---
+++ packages/RLDQN/RLDQN-0.0.1.tar.gz/RLDQN-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='RLDQN',
+      version='0.0.1',
+      description='Asynchronous DQN',
+      author='maigua',
+      author_email='396032050@qq.com',
+      license='MIT',
+      packages=['dqn'])
