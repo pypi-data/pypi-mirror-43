f3ce0734f716c4af31272ec74ef1d740cf5b40c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:52 +0000

    Add package dragonfly-opt/0.1.0/dragonfly-opt-0.1.0.tar.gz

---
+++ packages/dragonfly-opt/dragonfly-opt-0.1.0.tar.gz/dragonfly-opt-0.1.0/dragonfly/__init__.py
@@ -0,0 +1,22 @@
+"""
+  Dragonfly: An open source library for scalable and robust Bayesian optimisation.
+
+  This file contains the main APIs. See apis/opt.py and apis/moo.py for function
+  definitions and descriptions of the arguments.
+
+  Rerefernce:
+    "Tuning Hyperparameters without Grad Students: Scalable and Robust Bayesian
+    optimisation with Dragonfly", Kandasamy K, Vysyaraju K V, Neiswanger W, Paria B,
+    Collins C R, Schneider J, Poczos B, Xing E P.
+
+  -- kandasamy@cs.cmu.edu
+"""
+
+from .apis.opt import maximise_function, minimise_function, \
+                      maximise_multifidelity_function, minimise_multifidelity_function, \
+                      maximize_function, minimize_function, \
+                      maximize_multifidelity_function, minimize_multifidelity_function
+from .apis.moo import multiobjective_maximise_functions, \
+                      multiobjective_minimise_functions, \
+                      multiobjective_maximize_functions, \
+                      multiobjective_minimize_functions
