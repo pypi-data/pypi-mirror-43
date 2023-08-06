c325453731f146180097c2cdabf06bf55eff454d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:10 +0000

    Add package tag-text-extractor/0.0.17/tag-text-extractor-0.0.17.tar.gz

---
+++ packages/tag-text-extractor/tag-text-extractor-0.0.17.tar.gz/tag-text-extractor-0.0.17/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+VERSION = "0.0.17"
+
+setup(
+    name='tag-text-extractor',
+    description="Extract tag texts and word count",
+    version=VERSION,
+    url='https://github.com/KokocGroup/tag-text-extractor',
+    download_url='https://github.com/KokocGroup/tag-text-extractor/tarball/v{}'.format(VERSION),
+    packages=['tag_text_extractor'],
+    install_requires=['lxml==3.4.4']
+)
