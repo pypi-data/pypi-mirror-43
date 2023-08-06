2ff15ef400a14826eb0fd018d6087a68c5a495eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package html2docx/0.0.0/html2docx-0.0.0.tar.gz

---
+++ packages/html2docx/html2docx-0.0.0.tar.gz/html2docx-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="html2docx",
+    version="0.0.0",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)
