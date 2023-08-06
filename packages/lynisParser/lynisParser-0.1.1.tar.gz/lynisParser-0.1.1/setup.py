456c742524295ad786a44249ca88685a64fb2c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:04 +0000

    Add package lynisParser/0.1.1/lynisParser-0.1.1.tar.gz

---
+++ packages/lynisParser/lynisParser-0.1.1.tar.gz/lynisParser-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lynisParser",
+    version="0.1.1",
+    author="Shane Scott",
+    author_email="sscott@gvit.com",
+    description="Parse Lynis report issues into lists of dictionaries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GoVanguard/lynisParser",
+    packages=['lynisParser'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+    ),
+)
