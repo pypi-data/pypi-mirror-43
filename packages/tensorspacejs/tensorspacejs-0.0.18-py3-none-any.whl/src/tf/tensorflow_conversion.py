8f8970974de139beb3f2e221d8a0258abc68f9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:11 +0000

    Add package tensorspacejs/0.0.18/tensorspacejs-0.0.18-py3-none-any.whl

---
+++ packages/tensorspacejs/tensorspacejs-0.0.18-py3-none-any.whl/src/tf/tensorflow_conversion.py
@@ -0,0 +1,48 @@
+"""
+@author syt123450 / https://github.com/syt123450
+"""
+
+import sys
+
+sys.path.insert(0, "./src/tf")
+
+from saved_model import preprocess_saved_model
+from frozen_model import preprocess_frozen_model
+from hdf5_combined_model import preprocess_hdf5_combined_model
+from hdf5_separated_model import preprocess_hdf5_separated_model
+
+
+def show_tf_model_summary(path_model):
+    print(path_model)
+    print("tensorflow Model Summary...")
+
+
+def preprocess_tensorflow_model(input_format, path_model, path_output_dir, output_node_names=None):
+    if input_format == "tf_saved":
+        preprocess_saved_model(
+            path_model,
+            path_output_dir,
+            output_node_names
+        )
+    elif input_format == "tf_frozen":
+        preprocess_frozen_model(
+            path_model,
+            path_output_dir,
+            output_node_names
+        )
+    elif input_format == "tf_keras":
+        preprocess_hdf5_combined_model(
+            path_model,
+            path_output_dir,
+            output_node_names
+        )
+    elif input_format == "tf_keras_separated":
+        preprocess_hdf5_separated_model(
+            path_model,
+            path_output_dir,
+            output_node_names
+        )
+    else:
+        print("Preprocess nothing for tensorflow model.")
+
+    print("Mission Complete!!!")
