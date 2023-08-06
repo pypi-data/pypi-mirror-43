a83841c1bf86c5f56878da1a53fb7c3d80907fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:00 +0000

    Add package spotlight/0.1.2/spotlight-0.1.2.tar.gz

---
+++ packages/spotlight/spotlight-0.1.2.tar.gz/spotlight-0.1.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='spotlight',
+    version='0.1.2',
+    author='Michiel Doesburg',
+    author_email='michiel@moddix.com',
+    description='Laravel style input validation for Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mdoesburg/spotlight',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['SQLAlchemy>=1.3.1']
+)
