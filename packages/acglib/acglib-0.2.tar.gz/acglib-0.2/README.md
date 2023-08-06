f8c8c151af8e53ac8f62d3425637928ed5128d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:01 +0000

    Add package acglib/0.2/acglib-0.2.tar.gz

---
+++ packages/acglib/acglib-0.2.tar.gz/acglib-0.2/README.md
@@ -0,0 +1,2 @@
+# acglib
+A small python package of reusable code bits for AWS, Azure, and GCP.  Probably nothing you'll want to use.
---
+++ packages/acglib/acglib-0.2.tar.gz/acglib-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='acglib',
+      version='0.2',
+      description='Library for reusable cloud code',
+      long_description=long_description,
+      url='http://github.com/AustinCloudGuru/acglib',
+      author='Mark Honomichl',
+      author_email='mark@austincloud.guru',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            "boto3",
+            "awsretry"
+      ],
+      zip_safe=False,
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+      ]
+      )
