3e6cdbba52f558cbed206c68305703a908696b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:15 +0000

    Add package httpaws/0.0.4/httpaws-0.0.4.tar.gz

---
+++ packages/httpaws/httpaws-0.0.4.tar.gz/httpaws-0.0.4/httpaws/__init__.py
@@ -0,0 +1,16 @@
+"""
+httpAWS - a CLI, cURL-like tool for AWS services.
+
+"""
+__version__ = '0.0.4'
+__author__ = 'Todd Leonhardt'
+__licence__ = 'Apache 2.0'
+
+
+class ExitStatus:
+    """Program exit code constants."""
+    SUCCESS = 0
+    ERROR = 1
+
+    # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
+    ERROR_CTRL_C = 130
