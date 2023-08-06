585676a23c429ed96c20f1f14506c69cd5ba3a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:33 +0000

    Add package pexdoc/1.1.2/pexdoc-1.1.2-py35-none-any.whl

---
+++ packages/pexdoc/pexdoc-1.1.2-py35-none-any.whl/pexdoc/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+# Copyright (c) 2013-2019 Pablo Acosta-Serafini
+# See LICENSE for details
+# pylint: disable=C0111,W0401
+
+# Intra-package imports
+from .pkgdata import __version__
+from .exdoc import *
+from .exh import *
+from .pcontracts import *
+from .pinspect import *
+from .ptypes import *
