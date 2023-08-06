f41a6ceec65b2efc8a91b3c652377c89a52f0664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:42 +0000

    Add package itk-core/4.13.2.post1/itk_core-4.13.2.post1-cp27-cp27m-macosx_10_9_x86_64.whl

---
+++ packages/itk-core/itk_core-4.13.2.post1-cp27-cp27m-macosx_10_9_x86_64.whl/itk/Configuration/ITKIONIFTIConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKTransform', 'ITKIOImageBase', )
+templates = (
+  ('NiftiImageIO', 'itk::NiftiImageIO', 'itkNiftiImageIO', True),
+  ('NiftiImageIOFactory', 'itk::NiftiImageIOFactory', 'itkNiftiImageIOFactory', True),
+)
