472d4a8da68da355656ef9e8375aa5881fd04cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:58 +0000

    Add package invana-bot/0.0.0rc0/invana-bot-0.0.0rc0.tar.gz

---
+++ packages/invana-bot/invana-bot-0.0.0rc0.tar.gz/invana-bot-0.0.0rc0/invana_bot/extractors/base.py
@@ -0,0 +1,6 @@
+class ExtractorBase(object):
+    pass
+
+    def __init__(self, response=None, extractor=None):
+        self.response = response
+        self.extractor = extractor
---
+++ packages/invana-bot/invana-bot-0.0.0rc0.tar.gz/invana-bot-0.0.0rc0/invana_bot/extractors/links.py
@@ -0,0 +1,17 @@
+from invana_bot.extractors.base import ExtractorBase
+
+
+class AllLinksExtractor(ExtractorBase):
+    pass
+
+
+class SameDomainLinkExtractor(ExtractorBase):
+    pass
+
+
+class PaginationLinkExtractor(ExtractorBase):
+    pass
+
+
+class CustomLinkExtractor(ExtractorBase):
+    pass
