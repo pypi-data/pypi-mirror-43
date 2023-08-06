468b7983256603c70c4e0a76db8fa148b286e3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:36 +0000

    Add package libpath/0.0.0.0/libpath-0.0.0.0.tar.gz

---
+++ packages/libpath/libpath-0.0.0.0.tar.gz/libpath-0.0.0.0/README.md
@@ -0,0 +1,2 @@
+# libpath.Python
+
---
+++ packages/libpath/libpath-0.0.0.0.tar.gz/libpath-0.0.0.0/setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+
+	name='libpath',
+	version='0.0.0.0',
+	license='BSD-3-Clause',
+	description='libpath, for Python',
+	long_description=open('README.md').read(),
+	packages=['libpath'],
+	url='https://github.com/synesissoftware/libpath.Python',
+	author='Matt Wilson',
+	author_email='matthew@synesis.com.au',
+)
+
