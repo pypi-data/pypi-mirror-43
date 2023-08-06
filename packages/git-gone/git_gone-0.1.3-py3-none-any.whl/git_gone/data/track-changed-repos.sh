5a103b291e0f08bfc7a8993a2dcbe2243c5eeedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:57 +0000

    Add package git-gone/0.1.3/git_gone-0.1.3-py3-none-any.whl

---
+++ packages/git-gone/git_gone-0.1.3-py3-none-any.whl/git_gone/data/track-changed-repos.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+file="$HOME/.modified";
+
+# Create tracker file if it doesn't exist
+if ! [[ -r $file ]]; then
+	touch $file;
+fi;
+
+track_changed_git_repos()
+{
+	if git -C "$PWD" rev-parse; then
+		# Only append line if doesn't already exist
+		if ! $(grep -xq "$PWD" "$file"); then
+			echo $PWD >> $file;
+		fi;
+	fi;
+}
+
+chpwd() {
+	(track_changed_git_repos >/dev/null 2>&1 &)
+}
+
+
+alias leave="git-gone changes && shutdown -c now"
