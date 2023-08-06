4eaa33e9def19311d5e684df6753757908f768ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:10 +0000

    Add package apkutils-patch/0.5.4/apkutils-patch-0.5.4.tar.gz

---
+++ packages/apkutils-patch/apkutils-patch-0.5.4.tar.gz/apkutils-patch-0.5.4/setup.py
@@ -0,0 +1,40 @@
+import os.path
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="apkutils-patch",
+
+    version="0.5.4",
+
+    description=("Utils for parsing apk."),
+    # long_description=read('README.md'),
+
+    url="https://github.com/codeskyblue/apkutils",
+
+    author="mikusjelly, codeskyblue",
+    author_email="mikusjelly@gmail.com, codeskyblue@gmail.com",
+
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    keywords="apk dex axml",
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        "pyelftools",
+        "cigam",
+        "xmltodict",
+    ],
+)
