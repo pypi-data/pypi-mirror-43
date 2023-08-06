f72309ed5d4798b641cf5fa0a097942e82536c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package konstants/0.2/konstants-0.2.tar.gz

---
+++ packages/konstants/konstants-0.2.tar.gz/konstants-0.2/konstants/math.py
@@ -0,0 +1,73 @@
+class basic:
+	pi = 3.141592653589 #pi
+	e = 2.718281828459 #eulers-number
+	phi = 1.618033988749 #golden-ratio
+	arc = 3.141592653589 #pi
+	euc = 2.718281828459 #eulers-number
+	pyc = 1.414213562373 #root-two
+	aur = 1.618033988749 #golden-ratio
+	agr = 2.414213562373 #silver-ratio
+	pln = 1.324717957244 #plastic-number
+	eum = 0.577215664901 #euler-mascheroni
+	omc = 0.567143290409 #omega-const
+	gfc = 23.14069263277 #gelfond-const
+	aua = 137.5077640500 #golden-angle
+
+class piDerivatives:
+	tau = 6.283185307179 #2*pi
+	buf = 0.636619772367 #2/pi
+	rtp = 1.772453850905 #sqrt-pi
+	psq = 9.869604401089 #pi**2
+	lgi = 1.144729885849 #real-part(log(i))
+	ptp = 36.46215960720 #pi**pi
+	pip = 1.439619495847 #pi**(1/pi)
+	ipt = 0.539343498862 #infinite-power-tower-i/pi
+
+class eDerivatives:
+	twe = 5.436563656918 #2e
+	esq = 7.389056098930 #e**2
+	rte = 1.772453850905 #sqrt-e
+	ete = 15.15426224147 #e**e
+	eie = 1.444667861009 #e**(1/e)
+	ipt = 0.567143290409 #infinite-power-tower-i/e
+
+class epiCombo:
+	epi = 8.539734222673 #e*pi
+	ebp = 0.865255979432 #e/pi
+	etp = 23.14069263277 #e**pi
+	pte = 22.45915771836 #pi**e
+	eip = 1.374802227439 #e**(1/pi)
+	pie = 1.523671054858 #pi**(1/e)
+
+class named:
+	agc = 0.809394020540 #alladi-grinstead
+	ape = 1.202056903159 #apery-const
+	ber = 0.280169499023 #bernstein-const
+	buf = 0.636619772367 #2/pi-or-buffon-const
+	cat = 0.915965594177 #catalan-const
+	chm = 0.123456789101 #champernowne-const
+	cec = 0.235711131719 #copeland-erdos-const
+	con = 1.303577269034 #conway-const
+	frp = -2.502907875095 #feigenbaum-reduction-param
+	fbv = 4.669201609102 #feigenbaum-bifurcation-velocity
+	gau = 0.834626841674 #gauss-const
+	gib = 1.851937051982 #gibbs-const
+	grc = 0.783591464262 #graham-const
+	knc = 2.685452001065 #khinchin-const
+	lev = 3.275822918721 #levy-const
+	kol = 0.794507192779 #kolakoski-const
+	loc = 0.970270114392 #loch-const
+	mil = 1.306377883863 #mills-const
+	sch = 7.389056098930 #schwarzschild-const
+	sie = 0.822825249678 #sierpinskis-const
+	rab = 0.709803442861 #rabbit-const
+	tri = 1.839286755214 #tribonacci-const
+	tet = 1.839286755214 #tetranacci-const
+	hrn = 1729 #hardy-ramanujan-number
+	nof = 666 #number-of-beast
+	bep = 1000000000000066600000000000001 #belphegor-prime
+	pyc = 1.414213562373 #pythagora-const/root-two
+	thc = 1.732050807568 #theodorus-const/root-three
+	her = 1.154700538379 #hermite-const
+
+	
---
+++ packages/konstants/konstants-0.2.tar.gz/konstants-0.2/konstants/phys.py
@@ -0,0 +1,35 @@
+class fundamental:
+	avo = 6.02214*(10**23) #avogadro-const
+	far = 96485.33 #faraday-const
+	amc = 1.660538*(10**-27) #atomic-mass-const
+	mgc = 8.314462618 #molar-gas-const
+	gas = 8.250*1000 #gas-const
+	col = 8.987551*(10**9) #coloumb-const
+	c = 299792458 #speed-of-light-vacuum
+	bol = 1.38065*(10**-23) #boltsmann-const
+	gra = 9.80665 #accelaration-of-gravity
+	ryd = 1.0973731568539*(10**7) #rydberg-const
+	pnk = 6.62607004*(10**-34) #planck-const
+	sph = 4.18 #specific-heat-water
+	rpc = 1.05457182*(10**-34) #reduced-planck-const
+	muo = 4*(3.141592653589)*(10**-7) #vacuum-magnetic-perm
+	atm = 101325 #standard-atmos-in-pascal
+	fcc = 0.553960278365 #chandrasekar-first-const
+	scc = 0.541926070139 #chandrasekar-second-const
+	chg = 1.602176*(10**-19) #charge-on-proton/electron
+	erm = 9.109*(10**-31) #electron-rest-mass
+	prm = 1.6726*(10**-27) #proton-rest-mass
+	smv = 22.414*(10**-3) #std-molar-volume
+	bhr = 5.292*(10**-11) #bohr-radius
+	bhm = 9.274*(10**-24) #bohr-magneton
+	grc = 6.673*(10**-11) #gravitational-const
+	elv = 1.6022*(10**-19) #1-electron-volt
+
+class astrophysical:
+	au = 1.496*(10**11) #AU
+	par = 3.086*(10**16) #parsec
+	lum = 3.85*(10**26) #luminosity-of-sun
+	sma = 1.989*(10**30) #mass-of-sun
+	sra = 6.96*(10**8) #rad-of-sun
+	ema = 5.9742*(10**24) #mass-of-earth
+	era = 6.3781*(10**6) #rad-of-earth
