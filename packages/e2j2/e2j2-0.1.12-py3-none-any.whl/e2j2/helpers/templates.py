80ef26db3c04290d130109a9af0d9aac4bf0f184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:53 +0000

    Add package e2j2/0.1.12/e2j2-0.1.12-py3-none-any.whl

---
+++ packages/e2j2/e2j2-0.1.12-py3-none-any.whl/e2j2/helpers/templates.py
@@ -0,0 +1,39 @@
+import os
+import jinja2
+from e2j2.helpers.constants import BRIGHT_RED, RESET_ALL
+from e2j2.helpers import parsers
+
+
+def find(searchlist, j2file_ext, recurse=False):
+    if recurse:
+        return [os.path.realpath(os.path.join(dirpath, j2file)) for searchlist_item in searchlist.split(',')
+                for dirpath, dirnames, files in os.walk(searchlist_item)
+                for j2file in files if j2file.endswith(j2file_ext)]
+    else:
+        return [os.path.realpath(os.path.join(searchlist_item, j2file)) for searchlist_item in searchlist.split(',')
+                for j2file in os.listdir(searchlist_item) if j2file.endswith(j2file_ext)]
+
+
+def get_vars():
+    tags = ['json:', 'jsonfile:', 'base64:', 'consul:', 'list:', 'file:']
+    envcontext = {}
+    for envvar in os.environ:
+        envvalue = os.environ[envvar]
+        defined_tag = [tag for tag in tags if envvalue.startswith(tag)]
+        envcontext[envvar] = parsers.parse_tag(defined_tag[0], envvalue) if defined_tag else envvalue
+
+        if '** ERROR:' in envcontext[envvar]:
+            print(BRIGHT_RED + "{}='{}'".format(envvar, envcontext[envvar]) + RESET_ALL)
+
+    return envcontext
+
+
+def render(j2file, j2vars, twopass=False):
+    path, filename = os.path.split(j2file)
+    firstpass = jinja2.Environment(loader=jinja2.FileSystemLoader(path or './'),
+                             undefined=jinja2.StrictUndefined).get_template(filename).render(j2vars)
+
+    if twopass:
+        return jinja2.Environment(loader=jinja2.BaseLoader()).from_string(firstpass).render(j2vars)
+    else:
+        return firstpass
