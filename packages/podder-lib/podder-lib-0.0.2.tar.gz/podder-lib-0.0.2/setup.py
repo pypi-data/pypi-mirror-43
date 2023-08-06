3737c50fd57ffad40747bb6a4794323d6903953a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package podder-lib/0.0.2/podder-lib-0.0.2.tar.gz

---
+++ packages/podder-lib/podder-lib-0.0.2.tar.gz/podder-lib-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='podder-lib',
+    version='0.0.2',
+    description='Library for the Podder Task.',
+    packages=find_packages(),
+    author="podder-ai",
+    url='https://github.com/podder-ai/podder-lib',
+    include_package_data=True,
+    install_requires=[]
+)
