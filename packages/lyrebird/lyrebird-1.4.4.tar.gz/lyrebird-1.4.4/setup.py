ad9879ba2af4ae3303cd0031a68f78badf6239b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package lyrebird/1.4.4/lyrebird-1.4.4.tar.gz

---
+++ packages/lyrebird/lyrebird-1.4.4.tar.gz/lyrebird-1.4.4/setup.py
@@ -0,0 +1,49 @@
+import runpy
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = runpy.run_path(
+        os.path.join(here, "lyrebird", "version.py")
+    )["VERSION"]
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lyrebird',
+    version=VERSION,
+    packages=['lyrebird'],
+    url='https://github.com/meituan/lyrebird',
+    author='HBQA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ),
+    entry_points={
+        'console_scripts': [
+            'lyrebird = lyrebird.manager:main'
+        ]
+    },
+    install_requires=[
+        'mitmproxy==4.0.4',
+        'flask',
+        'requests',
+        'fire',
+        'colorama',
+        'genson', 
+        'flask-socketio', 
+        'flask-restful', 
+        'beautifulsoup4',
+        'portpicker', 
+        'colorama',
+        'packaging',
+        'Flask-SQLAlchemy'
+    ])
