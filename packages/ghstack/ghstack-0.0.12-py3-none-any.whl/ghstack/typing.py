ae5d43061dc8a94051ef6a6581f84ce0783c21cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:48 +0000

    Add package ghstack/0.0.12/ghstack-0.0.12-py3-none-any.whl

---
+++ packages/ghstack/ghstack-0.0.12-py3-none-any.whl/ghstack/typing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# A bunch of commonly used type definitions.
+
+"""
+PhabricatorDiffNumberWithD = \
+        NewType('PhabricatorDiffNumberWithD', str)  # aka "D1234567"
+GitHubNumber = NewType('GitHubNumber', int)  # aka 1234 (as in #1234)
+
+# aka MDExOlB1bGxSZXF1ZXN0MjU2NDM3MjQw
+GitHubRepositoryId = NewType('GitHubRepositoryId', str)
+"""
