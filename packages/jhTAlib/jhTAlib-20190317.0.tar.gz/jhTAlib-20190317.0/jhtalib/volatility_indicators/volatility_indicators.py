3f036bed83b6d3a6e0b886b4e4adaac3bcd1c8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:53 +0000

    Add package jhTAlib/20190317.0/jhTAlib-20190317.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20190317.0.tar.gz/jhTAlib-20190317.0/jhtalib/volatility_indicators/volatility_indicators.py
@@ -0,0 +1,42 @@
+def ATR(df, n):
+    """
+    Average True Range
+    """
+    tr_list = TRANGE(df)
+    atr_list = []
+    i = 0
+    while i < len(df['Close']):
+        if i + 1 < n:
+            atr = float('NaN')
+        else:
+            atr = ((tr_list[i - 1] * (n - 1)) + tr_list[i]) / n
+        atr_list.append(atr)
+        i += 1
+    return atr_list
+
+def NATR(df, n):
+    """
+    Normalized Average True Range
+    """
+
+def TRANGE(df):
+    """
+    True Range
+    """
+    tr_list = []
+    i = 0
+    while i < len(df['Close']):
+        if i < 1:
+            tr = float('NaN')
+        else:
+            true_high = df['High'][i]
+            if df['Close'][i - 1] > df['High'][i]:
+                true_high = df['Close'][i - 1]
+            true_low = df['Low'][i]
+            if df['Close'][i - 1] < df['Low'][i]:
+                true_low = df['Close'][i - 1]
+            tr = true_high - true_low
+        tr_list.append(tr)
+        i += 1
+    return tr_list
+
