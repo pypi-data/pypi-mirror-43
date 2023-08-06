88406205356646d7fb657d52d1fb6f7ff5348ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:11 +0000

    Add package kylileo/0.1/kylileo-0.1.tar.gz

---
+++ packages/kylileo/kylileo-0.1.tar.gz/kylileo-0.1/kylileo/__init__.py
@@ -0,0 +1,3 @@
+def joke():
+    return(u"What's a penguin's favourite food? ... "
+           u"Smokey bacon.")
---
+++ packages/kylileo/kylileo-0.1.tar.gz/kylileo-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kylileo',
+    version='0.1',
+    packages=['kylileo'],
+    url='https://github.com/kyllebirnie/kylileo',
+    license='MIT',
+    author='Kylle',
+    author_email='kyllebirnie@googlemail.com',
+    description='A python package test'
+)
