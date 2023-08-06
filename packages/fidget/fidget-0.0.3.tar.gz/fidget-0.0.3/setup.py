d3807991250898e421409dfd700c25d549039104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:19 +0000

    Add package fidget/0.0.3/fidget-0.0.3.tar.gz

---
+++ packages/fidget/fidget-0.0.3.tar.gz/fidget-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+import fidget
+
+setuptools.setup(
+    name=fidget.__name__,
+    version=fidget.__version__,
+    author=fidget.__author__,
+    packages=['fidget', 'fidget.widgets', 'fidget.backend'],
+    extras_require={
+        'PyQt': ['PyQt5'],
+        'PySide': ['PySide2']
+    },
+    python_requires='>=3.7.0',
+    include_package_data=True,
+    data_files=[
+        ('', ['README.md', 'CHANGELOG.md']),
+    ],
+)
