50395836f30d7ed71aae1c5f8b960973bcc0a456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:03 +0000

    Add package zc.catalog/3.0/zc.catalog-3.0.tar.gz

---
+++ packages/zc.catalog/zc.catalog-3.0.tar.gz/zc.catalog-3.0/src/zc/catalog/globber.py
@@ -0,0 +1,42 @@
+#############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""glob all terms at the end.
+
+
+"""
+from zope.index.text import queryparser, parsetree
+
+reconstitute = {}
+reconstitute["NOT"] = lambda nd: "not %s" % (
+    reconstitute[nd.getValue().nodeType()](nd.getValue()),)
+reconstitute["AND"] = lambda nd: "(%s)" % (" and ".join(expand(nd)),)
+reconstitute["OR"] = lambda nd: "(%s)" % (" or ".join(expand(nd)),)
+reconstitute["ATOM"] = lambda nd: '%s*' % (nd.getValue())
+reconstitute["PHRASE"] = lambda nd: '"%s"' % (
+    ' '.join((v + '*') for v in nd.getValue()),)
+reconstitute["GLOB"] = lambda nd: nd.getValue()
+
+
+def expand(nd):
+    return [reconstitute[n.nodeType()](n)
+            for n in nd.getValue()]
+
+
+def glob(query, lexicon):  # lexicon is index.lexicon
+    try:
+        tree = queryparser.QueryParser(lexicon).parseQuery(query)
+    except parsetree.ParseError:
+        return None
+    if tree is not None:
+        return reconstitute[tree.nodeType()](tree)
