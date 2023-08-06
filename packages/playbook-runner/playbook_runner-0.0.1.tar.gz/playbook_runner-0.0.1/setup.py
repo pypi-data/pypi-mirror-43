7f1a3c28a57be0ca57129a92e53302771fcd51a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:30 +0000

    Add package playbook-runner/0.0.1/playbook_runner-0.0.1.tar.gz

---
+++ packages/playbook-runner/playbook_runner-0.0.1.tar.gz/playbook_runner-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+description = 'A module that is capable of running ansible playbooks'
+
+setuptools.setup(
+    name='playbook_runner',
+    version="0.0.1",
+    author="Pavel Rogovoy",
+    author_email='pavelr@final.israel',
+    url="https://github.com/final-israel/playbook_runner",
+    description=description,
+    long_description=open('README').read(),
+    package_dir={'playbook_runner': 'playbook_runner'},
+    packages=['playbook_runner',],
+)
+
