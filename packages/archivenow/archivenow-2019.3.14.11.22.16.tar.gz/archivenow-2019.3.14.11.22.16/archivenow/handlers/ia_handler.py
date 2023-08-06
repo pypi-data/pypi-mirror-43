96d47eb15f18305a2ab53e3ec7d7d516b137142f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:55 +0000

    Add package archivenow/2019.3.14.11.22.16/archivenow-2019.3.14.11.22.16.tar.gz

---
+++ packages/archivenow/archivenow-2019.3.14.11.22.16.tar.gz/archivenow-2019.3.14.11.22.16/archivenow/handlers/ia_handler.py
@@ -0,0 +1,36 @@
+import requests
+
+class IA_handler(object):
+
+    def __init__(self):
+        self.enabled = True
+        self.name = 'The Internet Archive'
+        self.api_required = False
+
+    def push(self, uri_org, p_args=[]):
+        msg = ''
+        try:
+            uri = 'https://web.archive.org/save/' + uri_org
+            # push into the archive
+            r = requests.get(uri, timeout=120, allow_redirects=True)
+            r.raise_for_status()
+            # extract the link to the archived copy 
+            if (r != None):
+                if "Location" in r.headers:
+                    return r.headers["Location"]
+                elif "Content-Location" in r.headers:
+                    return "https://web.archive.org"+r.headers["Content-Location"]    
+                else:
+                    for r2 in r.history:
+                        if 'Location' in r2.headers:
+                            return r.url
+                            #return r2.headers['Location']
+                        if 'Content-Location' in r2.headers:
+                            return r.url
+                            #return r2.headers['Content-Location']
+            msg = "("+self.name+ "): No HTTP Location/Content-Location header is returned in the response"               
+        except Exception as e:
+            if msg == '':
+                msg = "Error (" + self.name+ "): " + str(e)
+            pass;      
+        return msg
