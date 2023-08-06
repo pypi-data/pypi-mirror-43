3c3af37aa471f7f0745f524f7e2fb896d9dae81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:17 +0000

    Add package psmon/1.1.0/psmon-1.1.0.tar.gz

---
+++ packages/psmon/psmon-1.1.0.tar.gz/psmon-1.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['psmon', 'psmon.limiters']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.2.5,<0.3.0', 'psutil>=5.5,<6.0']
+
+setup_kwargs = {
+    'name': 'psmon',
+    'version': '1.1.0',
+    'description': 'Process monitoring',
+    'long_description': '# psmon\n\n[![Travis (.com)](https://img.shields.io/travis/com/rkkautsar/psmon.svg)](https://travis-ci.com/rkkautsar/psmon)\n[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/psmon.svg)](https://pypi.org/project/psmon)\n[![Code Climate coverage](https://img.shields.io/codeclimate/coverage/rkkautsar/psmon.svg)](https://codeclimate.com/github/rkkautsar/psmon)\n[![Code Climate maintainability (percentage)](https://img.shields.io/codeclimate/maintainability-percentage/rkkautsar/psmon.svg)](https://codeclimate.com/github/rkkautsar/psmon)\n[![Dependabot Status](https://api.dependabot.com/badges/status?host=github&repo=rkkautsar/psmon)](https://dependabot.com)\n\nMonitors and limits process resource\n',
+    'author': 'Rakha Kanz Kautsar',
+    'author_email': 'rkkautsar@gmail.com',
+    'url': 'https://github.com/rkkautsar/psmon',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
