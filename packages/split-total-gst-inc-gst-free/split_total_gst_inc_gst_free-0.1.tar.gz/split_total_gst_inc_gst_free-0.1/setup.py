f4d209c03ea0fd2a6b42347a38fc1cef0c2457dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:47 +0000

    Add package split-total-gst-inc-gst-free/0.1/split_total_gst_inc_gst_free-0.1.tar.gz

---
+++ packages/split-total-gst-inc-gst-free/split_total_gst_inc_gst_free-0.1.tar.gz/split_total_gst_inc_gst_free-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="split_total_gst_inc_gst_free",
+    version="0.1",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["split_total_gst_inc_gst_free"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
