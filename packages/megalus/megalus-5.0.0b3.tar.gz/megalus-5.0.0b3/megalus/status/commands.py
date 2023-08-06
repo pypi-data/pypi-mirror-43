2f9dcbbb3346c5ef3bc4da2d4746a29ec44a3d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:40 +0000

    Add package megalus/5.0.0b3/megalus-5.0.0b3.tar.gz

---
+++ packages/megalus/megalus-5.0.0b3.tar.gz/megalus-5.0.0b3/megalus/status/commands.py
@@ -0,0 +1,38 @@
+"""Status command module."""
+import sys
+
+import click
+from blessed import Terminal
+from colorama import Style
+
+from megalus.main import Megalus
+
+
+@click.command()
+@click.option('--show-all', is_flag=True)
+@click.pass_obj
+def status(meg: Megalus, show_all: bool) -> None:
+    """Return docker services status.
+
+    :param meg: Megalus instance
+    :return: None
+    """
+    term = Terminal()
+    try:
+        with term.fullscreen():
+            with term.hidden_cursor():
+                with term.cbreak():
+                    while True:
+                        ui = meg.get_layout(term, show_all)
+                        ui.display()
+                        key_pressed = term.inkey(timeout=1)
+                        if 'q' in key_pressed.lower():
+                            raise KeyboardInterrupt
+
+    except KeyboardInterrupt:
+        print(term.color(0))
+        sys.exit(0)
+    except BaseException as exc:
+        print(term.exit_fullscreen)
+        print(Style.RESET_ALL)
+        raise exc
