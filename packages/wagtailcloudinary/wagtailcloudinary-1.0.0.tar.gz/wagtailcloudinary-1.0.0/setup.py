684c4aedf4b4c20ced32e9c2e7caff5be1455341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:31 +0000

    Add package wagtailcloudinary/1.0.0/wagtailcloudinary-1.0.0.tar.gz

---
+++ packages/wagtailcloudinary/wagtailcloudinary-1.0.0.tar.gz/wagtailcloudinary-1.0.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='wagtailcloudinary',
+    version='1.0.0',
+    packages=find_packages(include=['wagtailcloudinary'], exclude=['sandbox']),
+    install_requires=['cloudinary', 'wagtail'],
+    include_package_data=True,
+    license='BSD Licence',
+    description='Cloudinary support for Wagtail CMS',
+    long_description=README,
+    url='https://gitlab.com/thelabnyc/wagtailcloudinary',
+    author='Davor Teskera',
+    author_email='dteskera@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Framework :: Wagtail',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
