1ead1ec209cb28ba13e43744165f9fccc7ce4cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package dvc/0.32.1/dvc-0.32.1.tar.gz

---
+++ packages/dvc/dvc-0.32.1.tar.gz/dvc-0.32.1/dvc/command/root.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+import os
+
+import dvc.logger as logger
+from dvc.command.base import CmdBase
+
+
+class CmdRoot(CmdBase):
+    def run_cmd(self):
+        return self.run()
+
+    def run(self):
+        logger.info(os.path.relpath(self.repo.root_dir))
+        return 0
+
+
+def add_parser(subparsers, parent_parser):
+    ROOT_HELP = "Relative path to repo's directory."
+    root_parser = subparsers.add_parser(
+        "root", parents=[parent_parser], description=ROOT_HELP, help=ROOT_HELP
+    )
+    root_parser.set_defaults(func=CmdRoot)
