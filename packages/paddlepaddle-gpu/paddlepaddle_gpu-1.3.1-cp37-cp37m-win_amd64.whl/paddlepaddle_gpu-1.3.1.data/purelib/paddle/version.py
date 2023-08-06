644be50aaf531232de5d84193eeac0e0d4d39155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:59 +0000

    Add package paddlepaddle-gpu/1.3.1/paddlepaddle_gpu-1.3.1-cp37-cp37m-win_amd64.whl

---
+++ packages/paddlepaddle-gpu/paddlepaddle_gpu-1.3.1-cp37-cp37m-win_amd64.whl/paddlepaddle_gpu-1.3.1.data/purelib/paddle/version.py
@@ -0,0 +1,23 @@
+# THIS FILE IS GENERATED FROM PADDLEPADDLE SETUP.PY
+#
+full_version    = '1.3.1'
+major           = '1'
+minor           = '3'
+patch           = '1'
+rc              = '0'
+istaged         = True
+commit          = 'e61d7245dbe34875f66b0d6f179730f4dde647e2'
+with_mkl        = 'ON'
+
+def show():
+    if istaged:
+        print('full_version:', full_version)
+        print('major:', major)
+        print('minor:', minor)
+        print('patch:', patch)
+        print('rc:', rc)
+    else:
+        print('commit:', commit)
+
+def mkl():
+    return with_mkl
