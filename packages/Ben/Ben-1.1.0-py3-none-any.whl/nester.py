dad4980ca5b9c7df663bd0104bda70129ba6c791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:24 +0000

    Add package Ben/1.1.0/Ben-1.1.0-py3-none-any.whl

---
+++ packages/Ben/Ben-1.1.0-py3-none-any.whl/nester.py
@@ -0,0 +1,13 @@
+
+'''
+This is a test module, which can flatter a list, up to 3 levels
+'''
+
+def print_lol(the_list,level):
+    for i in the_list:
+        if isinstance(i,list):
+            print_lol(i,level+1)  # use recursive is more clear
+        else:
+            for tab in range(level):
+                print('\t',end='')
+            print(i)
