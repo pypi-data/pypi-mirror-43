333288e2deffa761d8e8e5d7cdccda38f4ecad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:04 +0000

    Add package numpy-sugar/1.4.1/numpy_sugar-1.4.1.tar.gz

---
+++ packages/numpy-sugar/numpy_sugar-1.4.1.tar.gz/numpy_sugar-1.4.1/numpy_sugar/__init__.py
@@ -0,0 +1,31 @@
+"""
+*******************
+NumPy Sugar package
+*******************
+
+Missing NumPy functionalities.
+
+"""
+from __future__ import absolute_import
+
+from . import epsilon, linalg, ma, random
+from ._array import cartesian, is_all_equal, is_all_finite, is_crescent, unique
+from ._rankdata import nanrankdata
+from ._testit import test
+
+__version__ = "1.4.1"
+
+__all__ = [
+    "__version__",
+    "test",
+    "epsilon",
+    "linalg",
+    "ma",
+    "random",
+    "cartesian",
+    "is_all_equal",
+    "is_all_finite",
+    "is_crescent",
+    "unique",
+    "nanrankdata",
+]
