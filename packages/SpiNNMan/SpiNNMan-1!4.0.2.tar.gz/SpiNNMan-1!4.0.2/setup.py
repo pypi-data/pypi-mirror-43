fb31100f12ea88c00b2b5af158625d5cf6d9db87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:35 +0000

    Add package SpiNNMan/1!4.0.2/SpiNNMan-1!4.0.2.tar.gz

---
+++ packages/SpiNNMan/SpiNNMan-1!4.0.2.tar.gz/SpiNNMan-1!4.0.2/setup.py
@@ -0,0 +1,53 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from collections import defaultdict
+import os
+
+__version__ = None
+exec(open("spinnman/_version.py").read())
+assert __version__
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "spinnman"
+data_extensions = {".aplx", ".boot", ".json", ".xml", ".xsd"}
+config_extensions = {".cfg", ".template"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    ext_set = set()
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            ext_set.add(ext)
+        if ext in config_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append(filename)
+    for ext in ext_set:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        package_data[package].append("*{}".format(ext))
+
+setup(
+    name="SpiNNMan",
+    version=__version__,
+    description="Interaction with a SpiNNaker Machine",
+    url="https://github.com/SpiNNakerManchester/SpiNNMan",
+    license="GNU GPLv3.0",
+    packages=packages,
+    package_data=package_data,
+    install_requires=[
+        'SpiNNUtilities >= 1!4.0.0, < 1!5.0.0',
+        'SpiNNMachine >= 1!4.0.0, < 1!5.0.0',
+        'SpiNNStorageHandlers >= 1!4.0.0, < 1!5.0.0',
+        'enum34',
+        'six']
+)
---
+++ packages/SpiNNMan/SpiNNMan-1!4.0.2.tar.gz/SpiNNMan-1!4.0.2/spinnman/_version.py
@@ -0,0 +1,4 @@
+__version__ = "1!4.0.2"
+__version_month__ = "September"
+__version_year__ = "2017"
+__version_name__ = "Riptalon"
