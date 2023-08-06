3f69bc97d1adf55add33e24c43dd24c974b04256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:19 +0000

    Add package QiDataLoader/1.0.1/QiDataLoader-1.0.1.tar.gz

---
+++ packages/QiDataLoader/QiDataLoader-1.0.1.tar.gz/QiDataLoader-1.0.1/src/QiDataLoader/MqDataLoader.py
@@ -0,0 +1,22 @@
+from MqTickReader import MqTickReader
+import datetime
+import os
+
+class MqDataLoader: 
+    _futureTickPath = ""
+
+    def __init__(self, futureTickPath):
+        self._futureTickPath = futureTickPath
+
+    def GetTickSeries(self, instrumentId, beginTime, endTime):
+        tickSeries = []
+        tradingDate = beginTime
+        while(tradingDate <= endTime):  
+            filePath = self._futureTickPath+"/"+ tradingDate.strftime('%Y%m%d')+"/"+instrumentId + ".tk"
+            if(os.path.exists(filePath)):
+                if (instrumentId != "index"):        
+                    mqTickReader = MqTickReader("", instrumentId, filePath)
+                    mqTickReader.Read(tickSeries, 0, 10000000000)       
+            tradingDate = tradingDate + datetime.timedelta(days=1)
+   
+        return tickSeries
