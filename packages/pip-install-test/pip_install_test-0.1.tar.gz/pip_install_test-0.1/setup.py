35d9abc2cfdc8a4fbc0b540639962c330678b4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:07 +0000

    Add package pip-install-test/0.1/pip_install_test-0.1.tar.gz

---
+++ packages/pip-install-test/pip_install_test-0.1.tar.gz/pip_install_test-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pip_install_test',
+      version='0.1',
+      description='A minimal stub package to test success of pip install',
+      url='https://github.com/lsst-sqre/sqre-pip_install_test',
+      author='Simon Krughoff',
+      author_email='krughoff@lsst.org',
+      license='MIT',
+      packages=['pip_install_test'],
+      zip_safe=False)
