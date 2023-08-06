cd0e1c753f52001251743d76233914d7597300b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:47 +0000

    Add package djangoldp-circle/0.0.11/djangoldp_circle-0.0.11-py3-none-any.whl

---
+++ packages/djangoldp-circle/djangoldp_circle-0.0.11-py3-none-any.whl/djangoldp_circle/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from djangoldp.models import Model
+
+
+class Circle(Model):
+    name = models.CharField(max_length=255, default='')
+    description = models.CharField(max_length=255, default='')
+    team = models.ManyToManyField(User, blank=True)
+    owner = models.ForeignKey(User, related_name="owned_circles", on_delete=models.DO_NOTHING)
+    jabberID = models.CharField(max_length=255, blank=True, null=True)
+    jabberRoom = models.BooleanField(default=True)
+
+    class Meta:
+        auto_author = 'owner'
+        nested_fields = ['team']
+
+    def __str__(self):
+        return self.name
