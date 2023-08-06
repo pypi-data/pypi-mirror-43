f64cbdf47a6dae8172ae9bd4b0c9158b5c3ae399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:38 +0000

    Add package django_range_slider/0.2.4/django_range_slider-0.2.4.tar.gz

---
+++ packages/django_range_slider/django_range_slider-0.2.4.tar.gz/django_range_slider-0.2.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django_range_slider',
+  packages = ['django_range_slider'], # this must be the same as the name above
+  version = '0.2.4',
+  description = 'A range-slider field for django , using jquery-ui',
+  author = 'Aleksandar Shaklev',
+  author_email = 'saklevaleksandar@gmail.com',
+  url = 'https://github.com/shakle17/django_range_slider', # use the URL to the github repo
+  download_url = 'https://github.com/shakle17/django_range_slider/tarball/0.1', # I'll explain this in a second
+  keywords = ['django', 'django-fields', 'django-forms','django-widgets'], # arbitrary keywords
+  classifiers = [],
+)
