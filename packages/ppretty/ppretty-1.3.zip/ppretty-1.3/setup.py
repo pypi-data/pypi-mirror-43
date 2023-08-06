1973b2f55b863362c42bc99e0c5cbb790085585c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:21 +0000

    Add package ppretty/1.3/ppretty-1.3.zip

---
+++ packages/ppretty/ppretty-1.3.zip/ppretty-1.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+    name='ppretty',
+    packages=['ppretty'],
+    version='1.3',
+    description='Convert python objects to a human readable format',
+    author='SymonSoft',
+    author_email='symonsoft@gmail.com',
+    url='https://github.com/symonsoft/ppretty',
+    download_url='https://github.com/symonsoft/ppretty/tarball/1.3',
+    keywords=['pretty', 'print', 'format', 'object', 'human', ''],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Utilities'
+    ],
+)
