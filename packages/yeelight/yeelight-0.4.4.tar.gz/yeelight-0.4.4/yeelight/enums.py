724091e42b3997ecd18fcd2a999b1dabf23d7dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:34 +0000

    Add package yeelight/0.4.4/yeelight-0.4.4.tar.gz

---
+++ packages/yeelight/yeelight-0.4.4.tar.gz/yeelight-0.4.4/yeelight/enums.py
@@ -0,0 +1,41 @@
+from enum import Enum, IntEnum
+
+
+class CronType(Enum):
+    """The type of event in cron."""
+
+    off = 0
+
+
+class PowerMode(IntEnum):
+    """Power mode of the light."""
+
+    LAST = 0
+    NORMAL = 1
+    RGB = 2
+    HSV = 3
+    COLOR_FLOW = 4
+    MOONLIGHT = 5
+
+
+class BulbType(Enum):
+    """
+    The bulb's type.
+
+    This is either `White` (for monochrome bulbs), `Color` (for color bulbs), `WhiteTemp` (for white bulbs with
+    configurable color temperature), `WhiteTempMood` for white bulbs with mood lighting (like the JIAOYUE 650 LED ceiling
+    light), or `Unknown` if the properties have not been fetched yet.
+    """
+
+    Unknown = -1
+    White = 0
+    Color = 1
+    WhiteTemp = 2
+    WhiteTempMood = 3
+
+
+class LightType(IntEnum):
+    """Type of light to control."""
+
+    Main = 0
+    Ambient = 1
