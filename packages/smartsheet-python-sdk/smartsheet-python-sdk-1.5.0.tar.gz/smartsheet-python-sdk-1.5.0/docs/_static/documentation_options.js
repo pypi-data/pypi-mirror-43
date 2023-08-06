171f711fece13a629b7307d26312898d06afbe54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package smartsheet-python-sdk/1.5.0/smartsheet-python-sdk-1.5.0.tar.gz

---
+++ packages/smartsheet-python-sdk/smartsheet-python-sdk-1.5.0.tar.gz/smartsheet-python-sdk-1.5.0/docs/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '1.4.1.dev10+gaba9c58.d20190319',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
