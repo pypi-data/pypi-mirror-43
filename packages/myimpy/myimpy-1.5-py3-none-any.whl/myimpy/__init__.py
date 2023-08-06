72753cf683a1949f60a41515b5708aa5a64a3af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package myimpy/1.5/myimpy-1.5-py3-none-any.whl

---
+++ packages/myimpy/myimpy-1.5-py3-none-any.whl/myimpy/__init__.py
@@ -0,0 +1,38 @@
+#H#######################################################################
+# FILENAME :        __init__.c             
+# 
+# DESIGN REF: https://python-packaging.readthedocs.io/en/latest/minimal.html
+#
+# DESCRIPTION :
+#       This script is to make Python treat the directories as containing
+#       packages; this is done to prevent directories with a common name, 
+#       such as string, from unintentionally hiding valid modules that 
+#       occur later on the module search path.
+#
+# PUBLIC FUNCTIONS :
+#       
+#
+# NOTES :
+#       This script can be simply empty and the function is just for fun.
+#
+#       Copyright: .  All rights reserved.
+# 
+# AUTHOR :          Jinhang <jinhang.d.zhu@gmail.com>
+# VERSION :         1.4    
+# START DATE :      07 Mar. 2019
+#
+# CHANGES :
+#
+# NO    VERSION     DATE        WHO     DETAIL
+# 1     1.0         07 Mar. 19  J.Z     Initialization
+# 2     1.1         08 Mar. 19  J.Z     Uploading using sdist 
+# 3     1.2         08 Mar. 19  J.Z     Uploading using twine 
+# 4     1.3         08 Mar. 19  J.Z     Morphology, transformation, basic
+# 5     1.4         12 Mar. 19  J.Z     New functions in transformation
+# 5     1.5         13 Mar. 19  J.Z     New functions in transformation
+# 
+#H#
+
+# from .text import greet
+# from .text import why
+# import myimpy.text
