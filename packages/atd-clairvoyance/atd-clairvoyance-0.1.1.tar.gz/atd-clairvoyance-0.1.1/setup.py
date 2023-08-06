17a2dfe57ef2338c86b1e06d9b294bc4b450ae14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:33 +0000

    Add package atd-clairvoyance/0.1.1/atd-clairvoyance-0.1.1.tar.gz

---
+++ packages/atd-clairvoyance/atd-clairvoyance-0.1.1.tar.gz/atd-clairvoyance-0.1.1/setup.py
@@ -0,0 +1,13 @@
+# Copyright Alan (AJ) Pryor, Jr. 2018
+
+from setuptools import setup, find_packages
+
+setup(name="atd-clairvoyance",
+      author="Alan (AJ) Pryor, Jr.",
+      author_email="apryor6@gmail.com",
+      version="0.1.1",
+      description="",
+      ext_modules=[],
+      packages=find_packages(),
+      install_requires=[],
+      )
