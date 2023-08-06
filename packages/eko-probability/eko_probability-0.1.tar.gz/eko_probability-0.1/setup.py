b9ea748e6d3ffc73c115ce076db13b685cab3621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:27 +0000

    Add package eko-probability/0.1/eko_probability-0.1.tar.gz

---
+++ packages/eko-probability/eko_probability-0.1.tar.gz/eko_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eko_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['eko_probability'],
+      author = 'Nurudeen Adesina',
+      author_email = 'access2abey@gmail.com',
+      zip_safe=False)
