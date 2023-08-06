2098b9d7656fcb9214e20974584b75d1f40374dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:34 +0000

    Add package print-MiceCode/1.0.0/print_MiceCode-1.0.0.tar.gz

---
+++ packages/print-MiceCode/print_MiceCode-1.0.0.tar.gz/print_MiceCode-1.0.0/print_MiceCode.py
@@ -0,0 +1,6 @@
+def print_MiceCode():
+
+    """A simple module to test if creating and uploading a module works"""
+
+    print("This is a test to see if creating a module works...\n"
+          "Guess what MiceCode... it works")
