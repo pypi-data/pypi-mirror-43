b813dcf465da7b4a7bd5c03e99e58a91615ce441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:37 +0000

    Add package chandra-proj1/0.1/chandra_proj1-0.1.tar.gz

---
+++ packages/chandra-proj1/chandra_proj1-0.1.tar.gz/chandra_proj1-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chandra_proj1',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['chandra_proj1'],
+      zip_safe=False)
