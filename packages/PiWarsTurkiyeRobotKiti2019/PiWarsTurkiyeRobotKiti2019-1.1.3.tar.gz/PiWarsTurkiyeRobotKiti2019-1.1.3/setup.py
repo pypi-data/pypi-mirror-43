a2a92d3e9b74d30c81b0d6929de0242e3b316e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package PiWarsTurkiyeRobotKiti2019/1.1.3/PiWarsTurkiyeRobotKiti2019-1.1.3.tar.gz

---
+++ packages/PiWarsTurkiyeRobotKiti2019/PiWarsTurkiyeRobotKiti2019-1.1.3.tar.gz/PiWarsTurkiyeRobotKiti2019-1.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setup(
+    name = "PiWarsTurkiyeRobotKiti2019",
+    version = "1.1.3",
+    author = "Yasar İdikut, Sarp Yoel Kastro",
+    author_email = "yasar.idikut@hisarschool.k12.tr, sarp.kastro@hisarschool.k12.tr",
+    description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
+    packages = ["PiWarsTurkiyeRobotKiti2019"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["Development Status :: 4 - Beta"],
+    install_requires=[
+        'picamera',
+        'pygame',
+        'pololu_drv8835_rpi',
+        'RPi.GPIO',
+        'wiringpi',
+    ]
+
+)
