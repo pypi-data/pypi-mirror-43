fc0cabd333dd12b0beb4bae481c0c4a8a4c774b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package company-matcher/0.2/company_matcher-0.2.tar.gz

---
+++ packages/company-matcher/company_matcher-0.2.tar.gz/company_matcher-0.2/company_matcher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[83]:
+
+
+import re
+from slugify import slugify
+from fuzzywuzzy import fuzz
+
+
+# In[ ]:
+
+
+def build_ngrams(line, count, stopwords, replacements):
+    normalised_line = slugify(
+        line, separator=" ", stopwords=stopwords, replacements=replacements
+    )
+    tokens = normalised_line.split()
+    sequences = [tokens[i:] for i in range(count)]
+    ngrams = zip(*sequences)
+    ngram_list = []
+    for row in ngrams:
+        ngram_list.append("_".join(row))
+    return ngram_list
+
+
+# In[89]:
+
+
+def match_name(name_to_match, list_names):
+    max_score = -1
+    max_name = ""
+    for name in list_names:
+        name = name.lower()
+        score = fuzz.ratio(name_to_match, name)
+        if score > max_score:
+            if type(list_names) == dict:
+                max_name = list_names[name]
+            else:
+                max_name = name
+            print(max_name)
+            max_score = score
+    return (max_name, max_score)
+
+
+# In[ ]:
