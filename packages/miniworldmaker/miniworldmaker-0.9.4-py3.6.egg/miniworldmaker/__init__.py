15f84730a0fbac621e04e65abcda2f620ceb5b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:21 +0000

    Add package miniworldmaker/0.9.4/miniworldmaker-0.9.4-py3.6.egg

---
+++ packages/miniworldmaker/miniworldmaker-0.9.4-py3.6.egg/miniworldmaker/__init__.py
@@ -0,0 +1,8 @@
+from miniworldmaker.actors.actor import Actor
+from miniworldmaker.boards.board import Board
+from miniworldmaker.boards.tile_based_board import TileBasedBoard
+
+__all__ = ['Actor',
+           'Board',
+           'TileBasedBoard',
+          ]
