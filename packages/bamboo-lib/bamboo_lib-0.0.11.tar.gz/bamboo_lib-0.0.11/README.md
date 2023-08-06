644f1d0ebaa29eae98338f65852051b499a9c451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package bamboo-lib/0.0.11/bamboo_lib-0.0.11.tar.gz

---
+++ packages/bamboo-lib/bamboo_lib-0.0.11.tar.gz/bamboo_lib-0.0.11/README.md
@@ -0,0 +1,28 @@
+# Installation
+
+`pip install bamboo-lib`
+
+## Additional Steps
+
+If you will need to use the distributed locking functionality, you will need to install
+some additional software. Below are the instructions for macOS
+
+### Installing Sherlock on macOS.
+
+```brew install libmemcached```
+
+```pip install pylibmc --install-option="--with-libmemcached=/usr/local/Cellar/libmemcached/1.0.18_2"```
+
+```pip install sherlock```
+
+# Running tests
+
+To run the tests, simply run:
+`pytest`
+
+Alternatively, if you would like to display all log/print statements run:
+`pytest -s`
+
+## Configuration
+
+To change the default folder where Bamboo will store HTTP downloads, set the `BAMBOO_DOWNLOAD_FOLDER` environment variable. By default, downloads will go to `/tmp`.
---
+++ packages/bamboo-lib/bamboo_lib-0.0.11.tar.gz/bamboo_lib-0.0.11/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author="Jonathan Speiser",
+    author_email="jonathan@datawheel.us",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    description="Python ETL library",
+    install_requires=[
+        "pandas==0.23.3",
+        "sqlalchemy==1.2.10",
+        "data-catapult",
+        "paramiko==2.4.1",
+        "sshtunnel==0.1.4",
+        "redis==2.10.6",
+        "sherlock==0.3.1"
+        "pytest"
+    ],
+    name="bamboo_lib",
+    license='All Rights Reserved',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    version="0.0.11",
+    url="https://github.com/Datawheel/bamboo-lib",
+)
