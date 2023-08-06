75b66d02cc2f8dc37cae9894f48b253f5685eeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:50 +0000

    Add package iparapheur-utils.beta/0.0.1.post55305/iparapheur-utils.beta-0.0.1.post55305.tar.gz

---
+++ packages/iparapheur-utils.beta/iparapheur-utils.beta-0.0.1.post55305.tar.gz/iparapheur-utils.beta-0.0.1.post55305/parapheur/scripts/ldapsearch.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding=utf-8
+from parapheur.parapheur import config
+import os
+
+query = ""
+
+
+def clean(str):
+    str = str.replace('\n', '')
+    str = str.replace('\r', '')
+    str = str.split("=", 1)
+    return str[1]
+
+
+def getConf():
+    global conf, active, url, security_principal, credentials, groupSearchBase, personDifferentialQuery
+    print("----------- Récupération des sources")
+    try:
+        conf = config.get("Ldapsearch", "conf_file")
+    except:
+        print("Fichier de conf requis: ph-init ldapsearch")
+
+    try:
+        file = open(conf, "r")
+        print("OK")
+    except:
+        print("ERREUR : Le fichier de conf " + conf + " n'existe pas")
+        exit(0)
+
+    for line in file:
+        if line.startswith("ldap.authentication.active"):
+            active = clean(line)
+        if line.startswith("ldap.authentication.java.naming.provider.url"):
+            url = clean(line)
+        if line.startswith("ldap.synchronization.java.naming.security.principal"):
+            security_principal = clean(line)
+        if line.startswith("ldap.synchronization.java.naming.security.credentials"):
+            credentials = clean(line)
+        if line.startswith("ldap.synchronization.groupSearchBase"):
+            groupSearchBase = clean(line)
+        if line.startswith("ldap.synchronization.personDifferentialQuery"):
+            personDifferentialQuery = clean(line)
+    file.close()
+    return active, url, security_principal, credentials, groupSearchBase, personDifferentialQuery
+
+
+def isSynchoEnable(authentication_active):
+    print("- Synchronisation demandée ?")
+
+    if authentication_active == "true":
+        print("Synchronisation activée")
+    else:
+        print("ERREUR : Synchronisation désactivée")
+
+
+def accessUrl(authentication_url):
+    print("- URL accessible ?")
+    # response = os.system("ping -c 1 " + authentication_url)
+    url1 = authentication_url.split("//")
+    url2 = url1[1].split(":")
+    try:
+        response = os.system("ping -c 1 " + url2[0] + " -p " + url2[1])
+    except:
+        response = os.system("ping -c 1 " + url2[0])
+
+    if response == 0:
+        print('Le serveur LDAP est accessible')
+    else:
+        print('ERREUR : Le serveur LDAP  n\'est pas accessible', authentication_url)
+
+
+def ldapRequest(url, security_principal, credentials, groupSearchBase, personDifferentialQuery):
+    print("- Requête LDAP")
+    query = "ldapsearch -LLL -H " + url + " -x -D " + security_principal + " -w '" + credentials + "' -b '" \
+            + groupSearchBase + "' '" + personDifferentialQuery + "' | grep displayName "
+    print(query)
+    return query
+
+
+def ldapSearchGrepDisplayname(query):
+    print("- Liste des utilisateurs")
+    os.system(query)
+
+
+getConf()
+isSynchoEnable(active)
+accessUrl(url)
+ldapRequest(url, security_principal, credentials, groupSearchBase, personDifferentialQuery)
+ldapSearchGrepDisplayname(query)
