7e09a1d908939c25d84d283d5cfb81dab2270a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:15 +0000

    Add package mnlp/0.1/mnlp-0.1.tar.gz

---
+++ packages/mnlp/mnlp-0.1.tar.gz/mnlp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mnlp',
+      version='0.1',
+      description='Mongolian Natural Language Processing Module.',
+      url='http://github.com/byam/mnlp',
+      author='Byambasuren Ganbaatar',
+      author_email='byambaa.mng@gmail.com',
+      license='MIT',
+      packages=['mnlp'],
+      zip_safe=False)
