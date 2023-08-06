222c68a41d863efe13607f59cd69b01ffcaba565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:24 +0000

    Add package predickter/1.0/predickter-1.0.tar.gz

---
+++ packages/predickter/predickter-1.0.tar.gz/predickter-1.0/predickter/__init__.py
@@ -0,0 +1,11 @@
+"""
+predickter
+"""
+
+__title__ = 'predickter'
+__version__ = '1.0'
+__author__ = 'Dilip Bobby'
+__license__ = 'GPLv2'
+__date__='17-03-2019'
+
+from .predickter import Predickter
