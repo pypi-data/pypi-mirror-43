0949a8a52922d364458e427d5d6ba4aaf7d1f5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:35 +0000

    Add package print-MiceCode/1.0.1/print_MiceCode-1.0.1.tar.gz

---
+++ packages/print-MiceCode/print_MiceCode-1.0.1.tar.gz/print_MiceCode-1.0.1/print_MiceCode.py
@@ -0,0 +1,8 @@
+"""A basic module te print and test for MiceCode"""
+
+def print_MiceCode():
+
+    """A simple module to test if creating and uploading a module works"""
+
+    print("This is a test to see if creating a module works...\n"
+          "Guess what MiceCode... it works(version 1.0.1.")
---
+++ packages/print-MiceCode/print_MiceCode-1.0.1.tar.gz/print_MiceCode-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name        =   "print_MiceCode",
+    version     =   "1.0.1",
+    py_modules  =   ["print_MiceCode"],
+    author      =   "MiceCode",
+    author_email=   "wvmuis@gmail.com",
+    description =   "A simple module test for and from MiceCode"
+)
