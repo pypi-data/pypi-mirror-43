8552db087d8bd2c1799fbc7a8bea331c50d73492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package chronos-cli/0.2.8/chronos-cli-0.2.8.tar.gz

---
+++ packages/chronos-cli/chronos-cli-0.2.8.tar.gz/chronos-cli-0.2.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='chronos-cli',
+    version='0.2.8',
+    author='Claude Léveillé',
+    author_email='claude-leveille@outlook.com',
+    description='A CLI tool that infers a next version number for Git repos.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/claude-leveille/chronos',
+    packages=setuptools.find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': [
+            'chronos=chronos.cli:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
