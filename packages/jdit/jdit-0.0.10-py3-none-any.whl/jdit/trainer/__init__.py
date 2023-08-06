2a1341770a802b01b6ca5248c3e54266d65cf88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:41 +0000

    Add package jdit/0.0.10/jdit-0.0.10-py3-none-any.whl

---
+++ packages/jdit/jdit-0.0.10-py3-none-any.whl/jdit/trainer/__init__.py
@@ -0,0 +1,7 @@
+from .super import SupTrainer
+from .classification import ClassificationTrainer
+from .gan import *
+# from .instances import *
+from jdit.trainer import instances
+
+__all__ = ['SupTrainer', 'ClassificationTrainer', 'instances','SupGanTrainer', 'Pix2pixGanTrainer','GenerateGanTrainer']
