1603700b50ffbd07344fdb3fb178f657a63a017f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:26 +0000

    Add package itk-numerics/4.13.2.post1/itk_numerics-4.13.2.post1-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-numerics/itk_numerics-4.13.2.post1-cp37-cp37m-win_amd64.whl/itk/Configuration/ITKIOSpatialObjectsConfig.py
@@ -0,0 +1,7 @@
+depends = ('ITKPyBase', 'ITKSpatialObjects', 'ITKMesh', )
+templates = (
+  ('SpatialObjectReader', 'itk::SpatialObjectReader', 'itkSpatialObjectReader2', True, '2'),
+  ('SpatialObjectReader', 'itk::SpatialObjectReader', 'itkSpatialObjectReader3', True, '3'),
+  ('SpatialObjectWriter', 'itk::SpatialObjectWriter', 'itkSpatialObjectWriter2', True, '2'),
+  ('SpatialObjectWriter', 'itk::SpatialObjectWriter', 'itkSpatialObjectWriter3', True, '3'),
+)
