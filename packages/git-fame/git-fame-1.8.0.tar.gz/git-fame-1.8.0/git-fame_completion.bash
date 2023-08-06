2a71f1426a8bd7ee30c6c5c8639b878a9ee5a785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:18 +0000

    Add package git-fame/1.8.0/git-fame-1.8.0.tar.gz

---
+++ packages/git-fame/git-fame-1.8.0.tar.gz/git-fame-1.8.0/git-fame_completion.bash
@@ -0,0 +1,23 @@
+_git_fame()
+{
+  local cur prv
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prv="${COMP_WORDS[COMP_CWORD-1]}"
+  case ${COMP_CWORD} in
+    1)
+      COMPREPLY=($(compgen -W "fame" ${cur}))
+      ;;
+    *)
+      case ${prv} in
+        "--sort")
+          COMPREPLY=($(compgen -W 'loc commits files' -- ${cur}))
+          ;;
+        *)
+          if [ ${COMP_WORDS[1]} == fame ]; then
+            COMPREPLY=($(compgen -dW '-h --help -v --version --cost --branch --since --sort --incl --excl -n --no-regex -s --silent-progress -t --bytype -w --ignore-whitespace -M -C --format --manpath --log' -- ${cur}))
+          fi
+          ;;
+      esac
+      ;;
+  esac
+}
