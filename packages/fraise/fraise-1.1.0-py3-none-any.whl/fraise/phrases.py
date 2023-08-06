c58d74dd86882066aaa3bf743e20e4a6ecfc9f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:11 +0000

    Add package fraise/1.1.0/fraise-1.1.0-py3-none-any.whl

---
+++ packages/fraise/fraise-1.1.0-py3-none-any.whl/fraise/phrases.py
@@ -0,0 +1,11 @@
+from . import words
+
+
+def generate(word_count=4, minimum_length=16):
+    word_list = []
+    for _ in range(word_count):
+        word_list.append(words.get_random_word())
+    passphrase = ' '.join(map(str, word_list))
+    while len(passphrase) < minimum_length:
+        passphrase += f' {words.get_random_word()}'
+    return passphrase
