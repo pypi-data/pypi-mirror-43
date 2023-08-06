4aafd679451cbc148ea6a461e1e0451741934920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:08 +0000

    Add package atomica/1.0.28/atomica-1.0.28.tar.gz

---
+++ packages/atomica/atomica-1.0.28.tar.gz/atomica-1.0.28/atomica/version.py
@@ -0,0 +1,12 @@
+"""
+Atomica version file.
+
+Standard location for module version number and date.
+"""
+
+import sciris as sc
+
+version = "1.0.28"
+versiondate = "2019-03-19"
+gitinfo = sc.gitinfo(__file__, verbose=False)
+
