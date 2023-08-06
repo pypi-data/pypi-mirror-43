c88713bac6e4549100da617860e8096bdbb83624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:21 +0000

    Add package romp/2019.3.3rc2/romp-2019.3.3rc2-py2.py3-none-any.whl

---
+++ packages/romp/romp-2019.3.3rc2-py2.py3-none-any.whl/romp/_core.py
@@ -0,0 +1,165 @@
+import glob
+import io
+import json
+import os
+import time
+import zipfile
+
+import requests
+import requests.auth
+
+
+class Build:
+    def __init__(self, id, url, human_url):
+        self.id = id
+        self.url = url
+        self.human_url = human_url
+
+    @classmethod
+    def from_response_json(cls, response_json):
+        id = response_json['id']
+        url = response_json['url']
+        human_url = (
+            'https://dev.azure.com/altendky/romp/_build/results?buildId={}'
+        ).format(id)
+
+        return cls(id=id, url=url, human_url=human_url)
+
+    def wait_for_lock_build(self, check_period=15):
+        while True:
+            print("build url:", self.url)
+            response = requests.get(self.url)
+
+            response.raise_for_status()
+            print(response.content)
+            response_json = response.json()
+
+            status = response_json['status']
+
+            if status == 'completed':
+                break
+
+            print()
+            print('Url:', self.human_url)
+            print('Build Status: ' + status)
+            print(
+                '    waiting {} seconds to check again for completion'.format(
+                    check_period,
+                ),
+            )
+            time.sleep(check_period)
+
+    def get_lock_build_artifact(self, artifact_file):
+        # 'https://dev.azure.com/altendky/a27e6706-93a8-46b1-8098-e5134713123d/_apis/build/builds/222/artifacts?artifactName=all&fileId=615BBA316A140A61F371BA354124349281B001BE9689366DA9660AC506A1ECCE01&fileName=lock.tar.gz&api-version=5.0-preview.3'
+        # url = (
+        #     'https://dev.azure.com'
+        #     '/altendky/romp/_apis/build/builds/{build_id}/artifacts'
+        # ).format(build_id=self.id)
+        url = os.path.join(self.url, 'artifacts')
+
+        print('artifact url:', url)
+
+        response = requests.get(
+            url=url,
+            params={
+                'api-version': '5.0',
+            }
+        )
+        response.raise_for_status()
+
+        artifact_name = 'lock_files'
+
+        response_json = response.json()
+        for artifact in response_json['value']:
+            if artifact['name'] != artifact_name:
+                continue
+
+            artifact_download_url = artifact['resource']['downloadUrl']
+
+            break
+        else:
+            raise Exception('artifact not found: ' + artifact_name)
+
+        response = requests.get(artifact_download_url)
+
+        artifact_file.write(response.content)
+
+
+def strip_zip_info_prefixes(prefix, zip_infos):
+    prefix = prefix.rstrip(os.sep) + os.sep
+    result = []
+
+    for zip_info in zip_infos:
+        name = zip_info.filename
+
+        if os.path.commonpath((name, prefix)) == '':
+            raise Exception('unexpected path: ' + name)
+
+        if len(name) <= len(prefix):
+            continue
+
+        if name.endswith(os.sep):
+            continue
+
+        zip_info.filename = os.path.relpath(name, prefix)
+        print(name, '->', zip_info.filename)
+        result.append(zip_info)
+
+    return result
+
+
+def post_file(data):
+    response = requests.post(
+        url='https://file.io/',
+        params={'expires': '1w'},
+        files={'file': ('archive.tar.gz', data)},
+    )
+
+    response.raise_for_status()
+    response_json = response.json()
+
+    if not response_json['success']:
+        raise Exception('failed to upload archive')
+
+    print('response_json:', json.dumps(response_json, indent=4))
+
+    return response_json['link']
+
+
+def request_remote_lock_build(
+        archive_url,
+        username,
+        personal_access_token,
+        build_request_url,
+        command,
+        environments,
+        source_branch,
+        definition_id,
+):
+    parameters = {
+        'ROMP_COMMAND': command,
+        'ROMP_ENVIRONMENTS': environments,
+    }
+
+    if archive_url is not None:
+        parameters['ROMP_ARCHIVE_URL'] = archive_url
+
+    response = requests.post(
+        url=build_request_url,
+        auth=requests.auth.HTTPBasicAuth(
+            username,
+            personal_access_token,
+        ),
+        json={
+            "definition": {"id": definition_id},
+            "sourceBranch": source_branch,
+            "parameters": json.dumps(parameters),
+        },
+    )
+
+    print(response.text)
+    response.raise_for_status()
+    response_json = response.json()
+    print(json.dumps(response_json, indent=4))
+
+    return Build.from_response_json(response_json=response_json)
---
+++ packages/romp/romp-2019.3.3rc2-py2.py3-none-any.whl/romp/cli.py
@@ -0,0 +1,186 @@
+import functools
+import getpass
+
+import click
+
+import romp._core
+
+
+@functools.wraps(click.option)
+def create_option(*args, envvar, help, show_default=True, **kwargs):
+    help = help.strip()
+    help += ' (${})'.format(envvar)
+    help = help.strip()
+
+    return click.option(
+        *args,
+        envvar=envvar,
+        help=help,
+        show_default=show_default,
+        **kwargs,
+    )
+
+
+def create_personal_access_token_option(
+        envvar='ROMP_PERSONAL_ACCESS_TOKEN',
+):
+    return create_option(
+        '--personal-access-token',
+        '--pat',
+        'personal_access_token',
+        envvar=envvar,
+        hide_input=True,
+        prompt='Personal Access Token',
+        help='A personal access token (PAT) with rights to initiate builds',
+    )
+
+
+def create_build_request_url_option(
+        envvar='ROMP_BUILD_REQUEST_URL',
+):
+    return create_option(
+        '--build-request-url',
+        default=(
+                'https://dev.azure.com'
+                '/altendky/romp/_apis/build/builds?api-version=5.0'
+        ),
+        envvar=envvar,
+        help='The URL for submitting a build request',
+    )
+
+
+def create_command_option(
+        envvar='ROMP_COMMAND',
+):
+    return create_option(
+        '--command',
+        default=(
+            "python -c 'import sys; print(sys.version); print(sys.platform)'"
+        ),
+        envvar=envvar,
+        help='The command to be run for each target',
+    )
+
+
+def create_username_option(
+        envvar='ROMP_USERNAME',
+):
+    return create_option(
+        '--username',
+        default=getpass.getuser(),
+        envvar=envvar,
+        help='Username for build URL authentication',
+    )
+
+
+def create_environments_option(
+        envvar='ROMP_ENVIRONMENTS',
+):
+    return create_option(
+        '--environments',
+        default='|Linux-3.6-x64|macOS-3.6-x64',
+        envvar=envvar,
+        help='Targets to run on',
+    )
+
+
+def create_check_period_option(
+        envvar='ROMP_CHECK_PERIOD',
+):
+    return create_option(
+        '--check-period',
+        default=15,
+        envvar=envvar,
+        help='The period used to poll the build for completion',
+    )
+
+
+def create_source_branch_option(
+        envvar='ROMP_SOURCE_BRANCH',
+):
+    return create_option(
+        '--source-branch',
+        default='develop',
+        envvar=envvar,
+        help='The romp source branch to use for the build',
+    )
+
+
+def create_definition_id_option(
+        envvar='ROMP_DEFINITION_ID',
+):
+    return create_option(
+        '--definition-id',
+        default=3,
+        envvar=envvar,
+        help='The definition id of the build to be triggered',
+    )
+
+
+def create_archive_option(
+        envvar='ROMP_ARCHIVE',
+):
+    return create_option(
+        '--archive',
+        envvar=envvar,
+        help='The archive to be uploaded to the build',
+        type=click.File('rb'),
+    )
+
+
+def create_artifact_option(
+        envvar='ROMP_ARTIFACT_PATH',
+):
+    return create_option(
+        '--artifact',
+        envvar=envvar,
+        help='The path at which to save the resulting artifact',
+        type=click.File('wb'),
+    )
+
+
+@click.command()
+@create_personal_access_token_option()
+@create_build_request_url_option()
+@create_command_option()
+@create_username_option()
+@create_environments_option()
+@create_check_period_option()
+@create_source_branch_option()
+@create_definition_id_option()
+@create_archive_option()
+@create_artifact_option()
+def main(
+        personal_access_token,
+        build_request_url,
+        command,
+        username,
+        environments,
+        check_period,
+        source_branch,
+        definition_id,
+        archive,
+        artifact,
+):
+    archive_url = None
+    if archive is not None:
+        archive_bytes = archive.read()
+        archive_url = romp._core.post_file(data=archive_bytes)
+
+    print(archive_url)
+
+    build = romp._core.request_remote_lock_build(
+        archive_url=archive_url,
+        username=username,
+        personal_access_token=personal_access_token,
+        build_request_url=build_request_url,
+        command=command,
+        environments=environments,
+        source_branch=source_branch,
+        definition_id=definition_id,
+    )
+
+    build.wait_for_lock_build(check_period=check_period)
+
+    if artifact is not None:
+        build.get_lock_build_artifact(artifact_file=artifact)
