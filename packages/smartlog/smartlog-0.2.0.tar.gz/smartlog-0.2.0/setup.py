a82eb5d9f9611b4f7f54e88e60777276dc71bb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package smartlog/0.2.0/smartlog-0.2.0.tar.gz

---
+++ packages/smartlog/smartlog-0.2.0.tar.gz/smartlog-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='smartlog',
+	version='0.2.0',
+	description='Tools to log exceptions and better interface with logging library',
+	url='https://github.com/dnut/smartlog',
+	author='Drew Nutter',
+	author_email='drew@drewnutter.com',
+	license='GPLv3',
+	package_dir={'': 'src'},
+	packages=['smartlog'],
+	zip_safe=False,
+)
