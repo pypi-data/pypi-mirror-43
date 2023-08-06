bdb190840ba81da995ef15a0aeb9e0f4f94ac7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package datacomp/0.0.1/datacomp-0.0.1.tar.gz

---
+++ packages/datacomp/datacomp-0.0.1.tar.gz/datacomp-0.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Setup.py for DataComp."""
+
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datacomp",
+    version="0.0.1",
+    author="Colin Birkenbihl",
+    author_email="colin.birken@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cojabi/datacomp",
+    packages=setuptools.find_packages("datacomp"),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'pandas',
+        'xlrd',
+        'numpy',
+        'scipy',
+        'sklearn',
+        'matplotlib_venn',
+        'pymatch',
+        'matplotlib',
+        'statsmodels',
+        'seaborn',
+    ]
+)
