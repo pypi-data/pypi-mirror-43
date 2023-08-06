b1001c922717f8057a6a7663d9d8590a6bfdc1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:22 +0000

    Add package django-schemahybrid-translation/0.1.4/django-schemahybrid-translation-0.1.4.tar.gz

---
+++ packages/django-schemahybrid-translation/django-schemahybrid-translation-0.1.4.tar.gz/django-schemahybrid-translation-0.1.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os.path
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+
+setup(
+    name='django-schemahybrid-translation',
+    version='0.1.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='WTFPL',
+    description='Translations stored as a JSONField (postgres only)',
+    long_description=README,
+    author='Unicstay',
+    author_email='dev@unicstay.com',
+    install_requires=[
+        'Django>=1.9',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
