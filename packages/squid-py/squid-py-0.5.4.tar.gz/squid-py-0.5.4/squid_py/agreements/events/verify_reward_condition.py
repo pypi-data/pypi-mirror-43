4c86a00c9f3749e780e1a1cbead59f8bb87f434b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:27 +0000

    Add package squid-py/0.5.4/squid-py-0.5.4.tar.gz

---
+++ packages/squid-py/squid-py-0.5.4.tar.gz/squid-py-0.5.4/squid_py/agreements/events/verify_reward_condition.py
@@ -0,0 +1,22 @@
+def verify_reward_tokens(event, agreement_id, did, service_agreement, price, consumer_address,
+                         publisher_account):
+    """
+
+    :param event:
+    :param agreement_id:
+    :param did:
+    :param service_agreement:
+    :param price:
+    :param consumer_address:
+    :param publisher_account:
+    :return:
+    """
+    # :TODO: verify that tokens were transfered to publisher
+    pass
+
+
+
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+verifyRewardTokens = verify_reward_tokens
