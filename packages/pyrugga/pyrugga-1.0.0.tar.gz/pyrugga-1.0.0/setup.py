123342f07a8900c5aa0bccb54e1b18af8ea03dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:42 +0000

    Add package pyrugga/1.0.0/pyrugga-1.0.0.tar.gz

---
+++ packages/pyrugga/pyrugga-1.0.0.tar.gz/pyrugga-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pyrugga',
+      version='1.0.0',
+      description='A library to analyse Rugby matches using Opta\'s SuperScout files',
+      url='https://github.com/jlondal/pyrugga',
+      author='James Londal',
+      license='GNU AFFERO GENERAL PUBLIC LICENSE',
+      install_requires=[
+         'pandas',
+         'scipy',
+         'scikit-learn',
+         'matplotlib',
+         'statsmodels',
+      ],
+      packages=['pyrugga'],
+      zip_safe=False)
