5e0177da1ed79b453613a36e6a8104c4a2c9cc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:15 +0000

    Add package lang2vec/1.0.1/lang2vec-1.0.1.tar.gz

---
+++ packages/lang2vec/lang2vec-1.0.1.tar.gz/lang2vec-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = ['numpy',
+                    'setuptools',
+                    'json']
+
+
+setuptools.setup(
+    name="lang2vec",
+    version="1.0.1",
+    author="Patrick Littell, David Mortensen, Antonis Anastasopoulos",
+    author_email="aanastas@cs.cmu.com",
+    description="A simple library for querying the URIEL typological database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonisa/lang2vec",
+    install_requires=install_requires,
+    scripts=['lang2vec/lang2vec.py'],
+    packages=['lang2vec'],
+    package_dir={'lang2vec': 'lang2vec'},
+    package_data={'lang2vec': ['data/*.npz', 'data/*.json']},
+    zip_safe=True,
+    classifiers=['Operating System :: OS Independent',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development :: Libraries :: Python Modules',
+               'Topic :: Text Processing :: Linguistic']
+)
