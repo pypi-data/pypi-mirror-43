03af8280832ef44121eb0187457160c351469c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package pescatore-cli/0.0.3/pescatore-cli-0.0.3.tar.gz

---
+++ packages/pescatore-cli/pescatore-cli-0.0.3.tar.gz/pescatore-cli-0.0.3/src/pescatore_cli/api/esche.py
@@ -0,0 +1,29 @@
+from .commons import make_request
+
+
+def list(page=None, items_per_page=None):
+    params = {}
+
+    if page:
+        params['page'] = page
+    if items_per_page:
+        params['ipp'] = items_per_page
+
+    return make_request(path='/api/v1/esche', params=params)
+
+def create(**kwargs):
+    params = {}
+
+    return make_request(path='/api/v1/esche', params=params, payload=kwargs,
+                        method='POST')
+
+def read(obj_id):
+    params = {}
+
+    return make_request(path='/api/v1/esche/{}'.format(obj_id), params=params)
+
+def delete(obj_id):
+    params = {}
+
+    return make_request(path='/api/v1/esche/{}'.format(obj_id), params=params,
+                        method='DELETE')
---
+++ packages/pescatore-cli/pescatore-cli-0.0.3.tar.gz/pescatore-cli-0.0.3/src/pescatore_cli/tasks.py
@@ -0,0 +1,36 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+from .functions import cerca_stringa_in_bacino as cerca_stringa_in_bacino_f
+
+
+logger = get_task_logger(__name__)
+
+@shared_task(bind=True)
+def cerca_stringa_in_bacino(self, stringa, bacino, id_licenza=None, note=None, intervallo=5):
+    errors = []
+    uscita = None
+
+    try:
+        uscita = cerca_stringa_in_bacino_f(stringa, bacino, id_licenza, note, intervallo)
+
+    except Exception as e:
+        if hasattr(e, 'message'):
+            msg = e.message
+        else:
+            msg = str(e)
+
+        logger.exception(msg)
+        errors.append(msg)
+
+    return {
+        'data': {
+            'uscita': uscita
+        },
+        'meta': {
+            'status': 'OK' if len(errors) == 0 else 'ERROR',
+            'errors': {
+                'cerca_stringa_in_bacino': errors
+            }
+        }
+    }
