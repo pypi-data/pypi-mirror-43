1530a69bb7c3f574f4b7de7225b234bbc2cce459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:09 +0000

    Add package sdk-dominator/0.0.1/sdk-dominator-0.0.1.tar.gz

---
+++ packages/sdk-dominator/sdk-dominator-0.0.1.tar.gz/sdk-dominator-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sdk-dominator',
+    version='0.0.1',
+    description='Reads and store user data to validate Sibyl Score',
+    long_description=readme,
+    author='Raphael Schubert',
+    author_email='rfswdp@gmail.com',
+    url='https://github.com/rfschubert/sdk-dominator',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'pycpfcnpj',
+        'requests',
+        # 'environs',
+        # 'xmltodict',
+        # 'pendulum',
+    ],
+)
