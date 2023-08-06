cb0a7b978b183f71b16031706ce3ef7e79714ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:27 +0000

    Add package adamspy/0.7.22/adamspy-0.7.22.tar.gz

---
+++ packages/adamspy/adamspy-0.7.22.tar.gz/adamspy-0.7.22/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adamspy",
+    version="0.7.22",
+    author="Ben Thornton",
+    author_email="ben.thornton@mscsoftware.com",
+    description="Python tools for working with Adams data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bthornton191/Adams_Modules",
+    packages=setuptools.find_packages(exclude=['test']),
+    package_data={'adamspy.adripy': ['templates/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires = ['thorpy', 'numpy', 'pandas', 'Jinja2', 'dripy', 'scipy']
+)
