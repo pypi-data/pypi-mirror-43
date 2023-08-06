65b97ab62d2014dc782622fa4628f25d5cbb943c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:31 +0000

    Add package diffpy.utils/3.0.0/diffpy.utils-3.0.0.tar.gz

---
+++ packages/diffpy.utils/diffpy.utils-3.0.0.tar.gz/diffpy.utils-3.0.0/src/diffpy/utils/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.utils      by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2010 The Trustees of Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Chris Farrow, Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE_DANSE.txt for license information.
+#
+##############################################################################
+
+"""Smalled shared functions for use by other diffpy packages.
+
+Subpackages:
+
+parsers  -- various utilities related to data parsing and manipulation.
+"""
+
+# package version
+from diffpy.utils.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
