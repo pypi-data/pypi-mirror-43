58831318833513c59d82035d3059cd1c6d49c128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:54 +0000

    Add package confluence-tool/0.4.1/confluence-tool-0.4.1.tar.gz

---
+++ packages/confluence-tool/confluence-tool-0.4.1.tar.gz/confluence-tool-0.4.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='confluence-tool',
+    version='0.4.1',
+    description='Confluence API and CLI',
+    author='Kay-Uwe (Kiwi) Lorenz',
+    author_email='kiwi@franka.dyndns.org',
+    install_requires=[
+            'requests',
+            'keyring',
+            'keyrings.alt',
+            'html5print',
+            'pyquery',
+            'pyaml',
+            'pystache',
+            'argdeco'
+        ],
+    url='https://github.com/klorenz/python-confluence-tool',
+    packages=['confluence_tool', 'confluence_tool.cli', ],
+    package_data = {
+        'confluence_tool': ['templates/*.mustache', 'templates/*.html']
+    },
+    include_package_data = True,
+
+    entry_points = dict(
+        console_scripts = [
+            'ct = confluence_tool:main',
+            'confluence-tool = confluence_tool:main',
+        ]
+    ),
+    license="MIT",
+    )
