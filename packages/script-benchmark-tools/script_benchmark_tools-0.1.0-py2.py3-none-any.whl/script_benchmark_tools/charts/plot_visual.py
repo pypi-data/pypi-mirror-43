840ba062c01079f3b285b22d51fc71536ff940ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package script-benchmark-tools/0.1.0/script_benchmark_tools-0.1.0-py2.py3-none-any.whl

---
+++ packages/script-benchmark-tools/script_benchmark_tools-0.1.0-py2.py3-none-any.whl/script_benchmark_tools/charts/plot_visual.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def make_plots(benchmarks):
+    plots = {}
+    for n, results in benchmarks:
+        for result in results:
+            if result.min() is -1:
+                continue
+            if not plots.get(result.name()):
+                plots[result.name()] = ([], [])
+            plots[result.name()][0].append(n)
+            plots[result.name()][1].append(result.min())
+
+    return plots.items()
+
+
+def display_benchmark_plot(benchmarks, title):
+    for name, values in make_plots(benchmarks):
+        plt.plot(values[0], values[1], label=name)
+    plt.xlabel('n')
+    plt.ylabel('min')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
