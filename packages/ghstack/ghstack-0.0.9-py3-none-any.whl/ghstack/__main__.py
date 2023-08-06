a2892e8085fa070246118181db65c17373a053a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:44 +0000

    Add package ghstack/0.0.9/ghstack-0.0.9-py3-none-any.whl

---
+++ packages/ghstack/ghstack-0.0.9-py3-none-any.whl/ghstack/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import ghstack.main
+import ghstack.github_real
+import ghstack.config
+
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description='Submit stack of diffs to GitHub.')
+    parser.add_argument(
+        '--message', '-m',
+        default='Update',
+        help='Description of change you made')
+    parser.add_argument(
+        '--update-fields', '-u', action='store_true',
+        help='Update GitHub pull request summary from the local commit')
+
+    args = parser.parse_args()
+
+    conf = ghstack.config.read_config()
+    github = ghstack.github_real.RealGitHubEndpoint(
+        oauth_token=conf.github_oauth,
+        proxy=conf.proxy
+    )
+
+    ghstack.main.main(
+        msg=args.message,
+        username=conf.github_username,
+        github=github,
+        update_fields=args.update_fields
+    )
+
+
+if __name__ == "__main__":
+    main()
