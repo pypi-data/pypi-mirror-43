f0e08c2738e5a56037f107eee5f2d4588c11528a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:29 +0000

    Add package emloop/0.2.0/emloop-0.2.0.tar.gz

---
+++ packages/emloop/emloop-0.2.0.tar.gz/emloop-0.2.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+setup(name='emloop',
+      version='0.2.0',
+      description='Smart machine learning trainer',
+      long_description='Trainer of machine learning models that automatically manages the whole process of training,'
+                       'saving and restoring models and much more',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+      ],
+      keywords='machine learning training framework',
+      url='https://github.com/iterait/emloop',
+      author='Iterait a.s.',
+      author_email='hello@iterait.com',
+      license='MIT',
+      packages=['emloop',
+                'emloop.cli',
+                'emloop.datasets',
+                'emloop.hooks',
+                'emloop.models',
+                'emloop.utils',
+                'emloop.tests'
+                ],
+      include_package_data=True,
+      zip_safe=False,
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      install_requires=[line for line in open('requirements.txt', 'r').readlines() if not line.startswith('#')],
+      entry_points={
+          'console_scripts': [
+              'emloop=emloop.entry_point:entry_point'
+          ]
+      })
