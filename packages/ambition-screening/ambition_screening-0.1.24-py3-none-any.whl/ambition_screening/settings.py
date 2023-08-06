94ab755bb49d87e9d73bf7ec6df458a86d7d480c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package ambition-screening/0.1.24/ambition_screening-0.1.24-py3-none-any.whl

---
+++ packages/ambition-screening/ambition_screening-0.1.24-py3-none-any.whl/ambition_screening/settings.py
@@ -0,0 +1,150 @@
+import os
+import sys
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APP_NAME = "ambition_screening"
+ETC_DIR = os.path.join(BASE_DIR, "etc")
+SITE_ID = 40
+REVIEWER_SITE_ID = 0
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "t3$j&v^2nbz(v2j%t+lq#&pr+uv4z)o3b5$m(o%yt9@d1(2(wi"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django_crypto_fields.apps.AppConfig",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "edc_base.apps.AppConfig",
+    "edc_identifier.apps.AppConfig",
+    "edc_protocol.apps.AppConfig",
+    "edc_registration.apps.AppConfig",
+    "edc_device.apps.AppConfig",
+    "django_collect_offline.apps.AppConfig",
+    "django_collect_offline_files.apps.AppConfig",
+    "ambition_rando.apps.AppConfig",
+    "ambition_screening.apps.EdcFacilityAppConfig",
+    "ambition_screening.apps.AppConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "ambition_screening.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+WSGI_APPLICATION = "ambition_screening.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = "/static/"
+
+
+DASHBOARD_URL_NAMES = {
+    "subject_listboard_url": "ambition_dashboard:subject_listboard_url",
+    "screening_listboard_url": "ambition_dashboard:screening_listboard_url",
+    "subject_dashboard_url": "ambition_dashboard:subject_dashboard_url",
+}
+
+RANDOMIZATION_LIST_PATH = os.path.join(
+    BASE_DIR, APP_NAME, "tests", "test_randomization_list.csv"
+)
+COUNTRY = "botswana"
+HOLIDAY_FILE = os.path.join(BASE_DIR, APP_NAME, "tests", "holidays.csv")
+
+DJANGO_COLLECT_OFFLINE_SERVER_IP = None
+DJANGO_COLLECT_OFFLINE_FILES_REMOTE_HOST = None
+DJANGO_COLLECT_OFFLINE_FILES_USER = None
+DJANGO_COLLECT_OFFLINE_FILES_USB_VOLUME = None
+
+if "test" in sys.argv:
+
+    class DisableMigrations:
+        def __contains__(self, item):
+            return True
+
+        def __getitem__(self, item):
+            return None
+
+    MIGRATION_MODULES = DisableMigrations()
+    PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
+    DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"
