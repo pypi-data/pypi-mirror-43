aab86498274dd3b5b6fe434faf71d34b03c613bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package panoptes-aggregation/3.0.0/panoptes_aggregation-3.0.0-py3-none-any.whl

---
+++ packages/panoptes-aggregation/panoptes_aggregation-3.0.0-py3-none-any.whl/panoptes_aggregation/__init__.py
@@ -0,0 +1,5 @@
+from . import extractors
+from . import reducers
+from . import scripts
+from . import version
+__version__ = version.__version__
---
+++ packages/panoptes-aggregation/panoptes_aggregation-3.0.0-py3-none-any.whl/panoptes_aggregation/extractors/__init__.py
@@ -0,0 +1,30 @@
+from .point_extractor import point_extractor
+from .point_extractor_by_frame import point_extractor_by_frame
+from .rectangle_extractor import rectangle_extractor
+from .question_extractor import question_extractor
+from .survey_extractor import survey_extractor
+from .poly_line_text_extractor import poly_line_text_extractor
+from .line_text_extractor import line_text_extractor
+from .sw_extractor import sw_extractor
+from .sw_variant_extractor import sw_variant_extractor
+from .sw_graphic_extractor import sw_graphic_extractor
+from .dropdown_extractor import dropdown_extractor
+from .shape_extractor import shape_extractor
+from .slider_extractor import slider_extractor
+
+extractors = {
+    'point_extractor': point_extractor,
+    'point_extractor_by_frame': point_extractor_by_frame,
+    'rectangle_extractor': rectangle_extractor,
+    'question_extractor': question_extractor,
+    'shortcut_extractor': question_extractor,
+    'survey_extractor': survey_extractor,
+    'poly_line_text_extractor': poly_line_text_extractor,
+    'line_text_extractor': line_text_extractor,
+    'sw_extractor': sw_extractor,
+    'sw_variant_extractor': sw_variant_extractor,
+    'sw_graphic_extractor': sw_graphic_extractor,
+    'dropdown_extractor': dropdown_extractor,
+    'shape_extractor': shape_extractor,
+    'slider_extractor': slider_extractor
+}
