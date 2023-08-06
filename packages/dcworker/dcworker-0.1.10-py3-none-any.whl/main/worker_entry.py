dc4ec3b0bb58fef97096e9e9b1cdfc4accb5c6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:02 +0000

    Add package dcworker/0.1.10/dcworker-0.1.10-py3-none-any.whl

---
+++ packages/dcworker/dcworker-0.1.10-py3-none-any.whl/main/worker_entry.py
@@ -0,0 +1,22 @@
+from worker.worker_fsm import WorkerStateMachine
+from worker.worker_start import *
+
+
+def worker_entry(runtime: WorkerRuntime,
+                 workdir: str,
+                 toplevel_datadir: str,
+                 port: int,
+                 worker_uuid: str,
+                 passcode: str,
+                 gpu_id: str):
+
+    worker = Worker(runtime.master_server(),
+                    port,
+                    workdir,
+                    toplevel_datadir,
+                    worker_uuid=worker_uuid,
+                    gpu_id=gpu_id,
+                    passcode=passcode)
+
+    worker_stm = WorkerStateMachine(worker, runtime)
+    worker_stm.start()
