3296495e0b2fb07a5767a85b5ffc192be2afbdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package apimapper/0.4/apimapper-0.4.tar.gz

---
+++ packages/apimapper/apimapper-0.4.tar.gz/apimapper-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(name='apimapper',
+      version='0.4',
+      description='API Mapper',
+      long_description='Maps API responses to desrired schema',
+      long_description_content_type='text/x-rst',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+      ],
+      keywords='API Mapper',
+      url='https://github.com/acdh-oeaw/apimapper',
+      author='Saranya Balasubramanian, Ksenia Zaytseva',
+      author_email='saranya.balasubramanian@oeaw.ac.at',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'requests',
+      ],
+      include_package_data=True,
+      zip_safe=False)
