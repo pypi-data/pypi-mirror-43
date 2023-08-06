6b1aaa8653a6591711c59b9e4ad76a03450a8f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:14 +0000

    Add package libsignal/0.0.0/libsignal-0.0.0.tar.gz

---
+++ packages/libsignal/libsignal-0.0.0.tar.gz/libsignal-0.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+README = 'README.md'
+
+
+def long_desc():
+    try:
+        import pypandoc
+    except ImportError:
+        with open(README) as f:
+            return f.read()
+    else:
+        return pypandoc.convert(README, 'rst')
+
+
+setup(
+    name='libsignal',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'python-axolotl-curve25519',
+        'protobuf>=3.6.0'
+    ],
+    license='GPLv3 License',
+    description="Python port of libsignal-protocol-java",
+    long_description=long_desc(),
+    url='https://github.com/ForstaLabs/libsignal-python',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
