f3e3ed11935845f4c2ebb76b92f1e75a205eae2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:29 +0000

    Add package enerdata/0.18.0/enerdata-0.18.0.tar.gz

---
+++ packages/enerdata/enerdata-0.18.0.tar.gz/enerdata-0.18.0/setup.py
@@ -0,0 +1,19 @@
+import sys
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = ['pytz', 'workalendar']
+
+if sys.version_info < (2, 7):
+    INSTALL_REQUIRES += ['backport_collections']
+
+setup(
+    name='enerdata',
+    version='0.18.0',
+    packages=find_packages(),
+    url='http://code.gisce.net',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    install_requires=INSTALL_REQUIRES,
+    description=''
+)
