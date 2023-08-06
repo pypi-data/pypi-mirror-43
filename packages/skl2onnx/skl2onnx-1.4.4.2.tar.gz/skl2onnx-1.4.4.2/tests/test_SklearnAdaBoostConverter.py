795c4bf6405c2cc0aa4140449b293a3619667dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:43 +0000

    Add package skl2onnx/1.4.4.2/skl2onnx-1.4.4.2.tar.gz

---
+++ packages/skl2onnx/skl2onnx-1.4.4.2.tar.gz/skl2onnx-1.4.4.2/tests/test_SklearnAdaBoostConverter.py
@@ -0,0 +1,72 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import unittest
+from sklearn.datasets import load_digits, load_iris
+from sklearn.datasets import make_regression
+from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor
+from sklearn.model_selection import train_test_split
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+from test_utils import dump_data_and_model
+
+
+class TestSklearnAdaBoostModels(unittest.TestCase):
+
+    def test_ada_boost_classifier_samme_r(self):
+        data = load_digits()
+        X, y = data.data, data.target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = AdaBoostClassifier(n_estimators=10, algorithm='SAMME.R')
+        model.fit(X_train, y_train)
+        model_onnx = convert_sklearn(model, 'AdaBoost classification',
+                                     [('input',
+                                      FloatTensorType(X_test.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X_test.astype('float32'), model, model_onnx,
+                            basename="SklearnAdaBoostClassifierSAMMER",
+                            allow_failure="StrictVersion("
+                                          "onnxruntime.__version__)"
+                                          "<= StrictVersion('0.2.1')")
+
+    def test_ada_boost_classifier_samme(self):
+        data = load_iris()
+        X, y = data.data, data.target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = AdaBoostClassifier(n_estimators=15, algorithm='SAMME')
+        model.fit(X_train, y_train)
+        model_onnx = convert_sklearn(model, 'AdaBoost classification',
+                                     [('input',
+                                      FloatTensorType(X_test.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X_test.astype('float32'), model, model_onnx,
+                            basename="SklearnAdaBoostClassifierSAMME",
+                            allow_failure="StrictVersion("
+                                          "onnxruntime.__version__)"
+                                          "<= StrictVersion('0.2.1')")
+
+    @unittest.skip(reason="CumSum op has not yet been implemented in onnx")
+    def test_ada_boost_regressor(self):
+        X, y = make_regression(n_features=4, n_samples=1000, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = AdaBoostRegressor(n_estimators=5)
+        model.fit(X_train, y_train)
+        model_onnx = convert_sklearn(model, 'AdaBoost regression',
+                                     [('input',
+                                      FloatTensorType(X_test.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X_test.astype('float32'), model, model_onnx,
+                            basename="SklearnAdaBoostRegressor",
+                            allow_failure="StrictVersion("
+                                          "onnxruntime.__version__)"
+                                          "<= StrictVersion('0.2.1')")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/skl2onnx/skl2onnx-1.4.4.2.tar.gz/skl2onnx-1.4.4.2/tests/test_SklearnOneVsRestClassifierConverter.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from skl2onnx import convert_sklearn
+from test_utils import dump_multiple_classification
+
+
+class TestOneVsRestClassifierConverter(unittest.TestCase):
+
+    def test_ova(self):
+        model = OneVsRestClassifier(LogisticRegression())
+        dump_multiple_classification(model,
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+
+    def test_ova_02(self):
+        model = OneVsRestClassifier(LogisticRegression())
+        dump_multiple_classification(model, first_class=2, suffix="F2",
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+
+    def test_ova_string(self):
+        model = OneVsRestClassifier(LogisticRegression())
+        dump_multiple_classification(model, verbose=False, label_string=True, suffix="String",
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+
+
+if __name__ == "__main__":
+    unittest.main()
