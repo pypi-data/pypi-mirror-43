c27ccc809900818e562b6f59d64a48e33d7bae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:25 +0000

    Add package py-configs-registry/0.0.2/py_configs_registry-0.0.2.tar.gz

---
+++ packages/py-configs-registry/py_configs_registry-0.0.2.tar.gz/py_configs_registry-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except:
+    long_description = 'python registry package',
+
+setup(
+    name='py_configs_registry',
+    version='0.0.2',
+    description='python registry package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    py_modules=["registry"],
+
+    url='http://github.com/ztj1993/py_configs_registry',
+    author='ZhangTianJie',
+    author_email='ztj1993@gmail.com',
+
+    keywords='registry config json yaml',
+    install_requires=['mergedict'],
+
+    license='MIT',
+)
