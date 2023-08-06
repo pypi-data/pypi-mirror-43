09092754634e8e8934a7c4475919c596b2c69fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:49 +0000

    Add package hakka-cli/0.1.1/hakka_cli-0.1.1-py3-none-any.whl

---
+++ packages/hakka-cli/hakka_cli-0.1.1-py3-none-any.whl/hakka_cli-0.1.1.data/scripts/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#install brew
+if ! type brew &> /dev/null; then
+  echo "install brew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#install giter8
+if ! type g8 &> /dev/null; then
+  echo "install giter 8..."
+  brew install giter8
+fi
