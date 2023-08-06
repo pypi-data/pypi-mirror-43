ec1938f8513f8119c9c4f5aa3da887250d8f026f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:35 +0000

    Add package aiocfscrape/0.0.8/aiocfscrape-0.0.8.tar.gz

---
+++ packages/aiocfscrape/aiocfscrape-0.0.8.tar.gz/aiocfscrape-0.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'aiocfscrape',
+    packages = ['aiocfscrape'],
+    version = '0.0.8',
+    description = 'A simple async Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/pavlodvornikov/aiocfscrape for more information.',
+    author = 'pavlodvornikov',
+    author_email = 'pavlodvornikov@gmail.com',
+    url = 'https://github.com/pavlodvornikov/aiocfscrape',
+    keywords = ['cloudflare', 'scraping', 'asyncio'],
+    install_requires = ['js2py==0.59', 'aiohttp >= 3.1.3']
+)
