7b00b99e55804fd62511e6ff86475f241849a572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:15 +0000

    Add package ovl/0.2.5.2/ovl-0.2.5.2.tar.gz

---
+++ packages/ovl/ovl-0.2.5.2.tar.gz/ovl-0.2.5.2/ovl/__init__.py
@@ -0,0 +1,24 @@
+from . import Vision
+from . import Color
+from . import General
+from . import Calibration
+from . import Drawing
+from . import Geometry
+from . import Sorters
+from . import Filters
+from . import Directions
+from . import ImageFilters
+from .Vision import *
+from .Color import *
+from .General import *
+from .Calibration import *
+from .Drawing import *
+from .Geometry import *
+from .Sorters import *
+from .Filters import *
+from .Directions import *
+from .ImageFilters import *
+
+
+__name__ = "Ovl"
+__version__ = '0.2.5.2'
