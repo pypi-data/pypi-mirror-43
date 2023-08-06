024ac0479b2129b6ae97f30f726be231b1106ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package ut2d/0.3.0/ut2d-0.3.0.tar.gz

---
+++ packages/ut2d/ut2d-0.3.0.tar.gz/ut2d-0.3.0/ut2d/lib/time.py
@@ -0,0 +1,108 @@
+import time
+from datetime import datetime, timedelta
+from typing import Union
+
+from .message import throw_msg
+from .timezone_scrapper import TimezoneScrapper
+
+
+class Time:
+    """A wrapper class providng all the functionalities supported by ut2d package.
+
+    Args:
+        ut: unix timestamp
+    """
+
+    def __init__(self, ut: Union[int, float]):
+        self.ut_now = time.time()
+        self.ut_local = self._convert_ut(ut)
+
+        self.dt_now = datetime.now()
+        self.dt_local = datetime.fromtimestamp(self.ut_local)
+        self.dt_utc = datetime.utcfromtimestamp(self.ut_local)
+    
+    def _convert_ut(self, ut: Union[int, float]) -> float:
+        """Convert input unix timstamp to 10-digit (in seconds)"""
+        ut = float(ut)
+
+        if ut <= 1e10:
+            return ut
+        elif ut > 1e12 and ut <= 1e13:
+            return ut / 1000
+
+    @staticmethod
+    def is_valid(ut: Union[int, float]) -> bool:
+        """Check if the input unix timestamp is either 10-digit (in seconds) or
+        13-digit (in milliseconds).
+
+        All other inputs are invalid.
+        """
+        try:
+            ut = float(ut)
+        except:
+            return False
+
+        if ut <= 1e10:
+            return True
+        elif ut > 1e12 and ut <= 1e13:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def fmt(dt: datetime) -> str:
+        """Format datetime to: day of week, date, time
+        
+        e.g. Sat, Mar 16, 2019 07:31PM
+        """
+        return dt.strftime('%a, %b %d, %Y %I:%M%p')
+
+    @property
+    def local(self) -> datetime:
+        return self.dt_local
+
+    @property
+    def utc(self) -> datetime:
+        return self.dt_utc
+
+    @property
+    def from_now(self) -> str:
+        """Get the time difference (timedelta) from now"""
+        if self.ut_now >= self.ut_local:
+            diff = self.dt_now - self.dt_local
+            ahead = False
+        else:
+            diff = self.dt_local - self.dt_now
+            ahead = True
+        
+        d = diff.days
+        h, _ = divmod(diff.seconds, 3600)
+        m, s = divmod(_, 60)
+
+        diff_fmt = f'Given time is {d} days, {h} hrs, {m} mins, {s} secs '
+        diff_fmt += 'ahead' if ahead else 'ago'
+
+        return diff_fmt
+
+    def in_timezone(self, timezone: str) -> datetime:
+        """Get the datetime in a given timezone"""
+        tz_sign = timezone[3]
+        tz_diff = int(timezone[4:])
+
+        if tz_sign == '-':
+            dt = self.dt_utc.timestamp() - tz_diff * 3600
+        elif tz_sign == '+':
+            dt = self.dt_utc.timestamp() + tz_diff * 3600
+        
+        return datetime.fromtimestamp(dt)
+    
+    def in_city(self, city: str) -> datetime:
+        """Get the datetime in a given city by first scrap the timezone
+        info with a scrapper, then call the in_timezone method.
+        """
+        ts = TimezoneScrapper(city)
+
+        if ts.timezone:
+            return self.in_timezone(ts.timezone)
+        else:
+            throw_msg(0, 'search_tz_failed', 1)
---
+++ packages/ut2d/ut2d-0.3.0.tar.gz/ut2d-0.3.0/ut2d/lib/timezone_scrapper.py
@@ -0,0 +1,129 @@
+import re
+from typing import Optional
+
+import requests
+from bs4 import BeautifulSoup as BS
+
+from .message import throw_msg
+
+SEARCH_ENGINES = {
+    'google': 'https://www.google.com',
+    'bing': 'https://www.bing.com',
+}
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
+
+TZ_INFO = {
+    'AST': 'UTC-4',
+    'EDT': 'UTC-4',
+    'EST': 'UTC-5',
+    'CDT': 'UTC-5',
+    'CST': 'UTC-6',
+    'MDT': 'UTC-6',
+    'MST': 'UTC-7',
+    'PDT': 'UTC-7',
+    'PST': 'UTC-8',
+    'AKDT': 'UTC-8',
+    'AKST': 'UTC-9',
+    'HADT': 'UTC-9',
+    'SDT': 'UTC-10',
+    'HST': 'UTC-10',
+    'HAST': 'UTC-10',
+    'SST': 'UTC-11',
+    'CHST': 'UTC+10',
+}
+
+
+class TimezoneScrapper:
+    """
+    Scrap the city's timezone from search engines, then conver to UTC format.
+    
+    Some popular search engines like Google or Bing supports getting the
+    time and timezone info of a given place with one simple query "xx time".
+
+    Pre-selected search engines will be tested for availability, and then
+    scrapped to collect the timezone information.
+    """
+
+    def __init__(self, city: str):
+        self.city = city
+        self.search_engines = []
+        self.timezone = None
+        
+        self._find_search_engines()
+        self._find_timezone()
+    
+    def _locate_tz(self, tz_text: str) -> str:
+        """locate timezone in raw text"""
+        return re.search('\(.*\)', tz_text).group()[1:-1]
+
+    def _to_utc_fmt(self, timezone: str) -> Optional[str]:
+        """
+        GMT-4 -> UTC-4
+        UTC-4 -> UTC-4
+        AST   -> UTC-4
+        """
+        if timezone[:3].upper() in ['UTC', 'GMT']:
+            if len(timezone) == 3:
+                return '+0'
+            else:
+                return timezone
+        else:
+            time_in_utc = TZ_INFO.get(timezone.upper())
+            if time_in_utc is None:
+                return None
+            else:
+                return time_in_utc
+
+    def _find_search_engines(self):
+        for se, se_url in SEARCH_ENGINES.items():
+            try:
+                r = requests.get(se_url, headers=HEADERS)
+                if r.status_code == 200:
+                    self.search_engines.append(se)
+            except:
+                pass
+
+        if not self.search_engines:
+            throw_msg(0, 'search_conn_failed', True)
+    
+    def _find_timezone(self):
+        for se in self.search_engines:
+            if (se == 'google') and (not self.timezone):
+                self._scrap_google()
+            if (se == 'bing') and (not self.timezone):
+                self._scrap_bing()
+
+    def _scrap_google(self):
+        city_fmt = self.city.lstrip().rstrip().replace(' ', '+')
+
+        base_url = 'https://www.google.com/search?q='
+        full_url = base_url + city_fmt + '+' + 'time'
+
+        try:
+            r = requests.get(full_url, headers=HEADERS)
+            soup = BS(r.content, 'html.parser')
+            tz_text = soup.select('span[class="KfQeJ"]')[1].text
+            
+            tz = self._locate_tz(tz_text)
+            self.timezone = self._to_utc_fmt(tz)
+        except:
+            pass
+
+    def _scrap_bing(self):
+        city_fmt = self.city.lstrip().rstrip().replace(' ', '%20')
+
+        base_url = 'https://www.bing.com/search?q='
+        full_url = base_url + city_fmt + '%20' + 'time'
+
+        try:
+            r = requests.get(full_url, headers=HEADERS)
+            soup = BS(r.content, 'html.parser')
+            tz_text = soup.select_one('div[class="baselClock"] div[class="b_focusLabel"]').text
+
+            tz_fmt = self._locate_tz(tz_text)
+            self.timezone = self._to_utc_fmt(tz_fmt)
+        except:
+            pass
