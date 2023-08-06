25b9586c54187cf7deb0ba19e2bf0878dc422eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package pyprinttree/0.0.1/pyprinttree-0.0.1.tar.gz

---
+++ packages/pyprinttree/pyprinttree-0.0.1.tar.gz/pyprinttree-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open('README.md', 'r') as file_handle:
+    long_description = file_handle.read()
+
+
+setuptools.setup(
+    name='pyprinttree',
+    version='0.0.1',
+    author='Tanner Netterville',
+    author_email='tannern@gmail.com',
+    description='A tool for printing tree structures to a console in a meaningful way.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=None,
+    packages=setuptools.find_packages(),
+    test_suite="pyprinttree.tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
