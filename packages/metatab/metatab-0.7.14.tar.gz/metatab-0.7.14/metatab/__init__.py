97ddb702e8a0485b831d83ec1a21d300f54b6c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:23 +0000

    Add package metatab/0.7.14/metatab-0.7.14.tar.gz

---
+++ packages/metatab/metatab-0.7.14.tar.gz/metatab-0.7.14/metatab/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2016 Civic Knowledge. This file is licensed under the terms of the
+# Revised BSD License, included in this distribution as LICENSE
+"""
+Record objects for the Simple Data Package format.
+"""
+
+DEFAULT_METATAB_FILE = 'metadata.csv'
+
+from .parser import *
+from .exc import *
+from .doc import MetatabDoc
+from .resolver import WebResolver
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
