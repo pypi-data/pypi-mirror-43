a957cec562f93b768aa66d435f4488da6a40f6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package doggie/0.1.4/doggie-0.1.4.tar.gz

---
+++ packages/doggie/doggie-0.1.4.tar.gz/doggie-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doggie",
+    version="0.1.4",
+    author="hujianxin",
+    author_email="hujianxincn@foxmail.com",
+    include_package_data=True,
+    install_requires=find_packages(),
+    license="Apache License",
+    description="Python library for interacting to hbase shell",
+)
