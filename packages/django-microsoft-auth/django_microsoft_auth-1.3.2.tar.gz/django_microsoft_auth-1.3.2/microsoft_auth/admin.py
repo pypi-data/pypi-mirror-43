942e4ca6bb742961cc6231fa00057869f2f92315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:29 +0000

    Add package django-microsoft-auth/1.3.2/django_microsoft_auth-1.3.2.tar.gz

---
+++ packages/django-microsoft-auth/django_microsoft_auth-1.3.2.tar.gz/django_microsoft_auth-1.3.2/microsoft_auth/admin.py
@@ -0,0 +1,51 @@
+from django.apps import apps
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import MicrosoftAccount, XboxLiveAccount
+
+User = get_user_model()
+
+# override admin site template
+admin.site.login_template = "microsoft/admin_login.html"
+
+# djangoql support
+extra_base = []
+if apps.is_installed("djangoql"):  # pragma: no branch
+    from djangoql.admin import DjangoQLSearchMixin  # pragma: no cover
+
+    extra_base = [DjangoQLSearchMixin]  # pragma: no cover
+
+base_admin = extra_base + [admin.ModelAdmin]
+base_user_admin = extra_base + [BaseUserAdmin]
+
+# unregister User mode if it is already registered
+if admin.site.is_registered(User):  # pragma: no branch
+    admin.site.unregister(User)
+
+
+@admin.register(MicrosoftAccount)
+class MicrosoftAccountAdmin(*base_admin):
+    readonly_fields = ("microsoft_id",)
+
+
+class MicrosoftAccountInlineAdmin(admin.StackedInline):
+    model = MicrosoftAccount
+    readonly_fields = ("microsoft_id",)
+
+
+@admin.register(XboxLiveAccount)
+class XboxLiveAccountAdmin(*base_admin):
+    readonly_fields = ("xbox_id", "gamertag")
+
+
+class XboxLiveAccountInlineAdmin(admin.StackedInline):
+    model = XboxLiveAccount
+    readonly_fields = ("xbox_id", "gamertag")
+
+
+@admin.register(User)
+class UserAdmin(*base_user_admin):
+    # adds MicrosoftAccount and XboxLiveAccount foreign keys to User model
+    inlines = (MicrosoftAccountInlineAdmin, XboxLiveAccountInlineAdmin)
