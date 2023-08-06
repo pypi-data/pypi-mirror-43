51301e5b7d17b934e48d1d06f97c0dfafaad3b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:24 +0000

    Add package pyOSC3/0.0.0/pyOSC3-0.0.0.tar.gz

---
+++ packages/pyOSC3/pyOSC3-0.0.0.tar.gz/pyOSC3-0.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyOSC3',
+      description='pyOSC updated for Python 3',
+      author='Ryan Kirkbride',
+      author_email='ryan@foxdot.org',
+      url='http://foxdot.org/',
+      packages=['pyOSC3'] )
+
+
