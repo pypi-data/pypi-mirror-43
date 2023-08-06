afe7e946a75669436c7340142a409500af3a1a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:00 +0000

    Add package kk592/0.1/kk592-0.1.tar.gz

---
+++ packages/kk592/kk592-0.1.tar.gz/kk592-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kk592',
+      version='0.1',
+      description='good mrng',
+      url='http://github.com/storborg/funniest',
+      author='Kamal Kumar',
+      author_email='kamalsir@ymail.com',
+      license='MIT',
+      packages=['kk592'],
+      zip_safe=False)
