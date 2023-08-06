d0be2a74aabbd7a636efccbffbff16cd419fbacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:36 +0000

    Add package jscatter/0.9.5.2/jscatter-0.9.5.2.tar.gz

---
+++ packages/jscatter/jscatter-0.9.5.2.tar.gz/jscatter-0.9.5.2/src/jscatter/examples/debug.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#  this file is intended to used in the debugger
+# write a script that calls your function to debug it
+
+import numpy as np
+
+# some arrays
+w = np.r_[-100:100]
+q = np.r_[0.001:5:0.01]
+x = np.r_[1:10]
+
