019cb891d3faacaec75c9c967c5ca6b4f8da1d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package asppack/0.0.1/asppack-0.0.1.tar.gz

---
+++ packages/asppack/asppack-0.0.1.tar.gz/asppack-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asppack",
+    version="0.0.1",
+    author="sailesh",
+    author_email="arjun_sailesh@yahoo.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aspadmanabhan/pythonrep/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
