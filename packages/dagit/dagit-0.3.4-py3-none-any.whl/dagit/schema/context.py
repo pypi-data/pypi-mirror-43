21d338cf4d19f36f4eace116cccfd1369112b478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:23 +0000

    Add package dagit/0.3.4/dagit-0.3.4-py3-none-any.whl

---
+++ packages/dagit/dagit-0.3.4-py3-none-any.whl/dagit/schema/context.py
@@ -0,0 +1,19 @@
+from dagster import check
+from dagit.pipeline_run_storage import PipelineRunStorage
+from dagit.pipeline_execution_manager import PipelineExecutionManager
+
+
+class DagsterGraphQLContext(object):
+    def __init__(
+        self, repository_container, pipeline_runs, execution_manager, throw_on_user_error=False
+    ):
+        from dagit.app import RepositoryContainer
+
+        self.repository_container = check.inst_param(
+            repository_container, 'repository_container', RepositoryContainer
+        )
+        self.pipeline_runs = check.inst_param(pipeline_runs, 'pipeline_runs', PipelineRunStorage)
+        self.execution_manager = check.inst_param(
+            execution_manager, 'pipeline_execution_manager', PipelineExecutionManager
+        )
+        self.throw_on_user_error = check.bool_param(throw_on_user_error, 'throw_on_user_error')
