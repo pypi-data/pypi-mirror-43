88dcc803f0c645dd53e639e178bee9b1f846db09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package bert-embedding/1.0.0.dev1553008814/bert_embedding-1.0.0.dev1553008814-py3-none-any.whl

---
+++ packages/bert-embedding/bert_embedding-1.0.0.dev1553008814-py3-none-any.whl/bert_embedding/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.0.0'
+version = '1.0.0'
+full_version = '1.0.0.dev1553008814'
+dev_version = '1553008814'
+release = False
+if not release:
+    version = full_version
