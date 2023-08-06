fb3fb6a5c06e2dd47013a2357c16e90ac9664ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:24 +0000

    Add package awokado/0.3b11/awokado-0.3b11.tar.gz

---
+++ packages/awokado/awokado-0.3b11.tar.gz/awokado-0.3b11/awokado/custom_fields.py
@@ -0,0 +1,51 @@
+from marshmallow import fields, validate, utils
+
+
+class Relation(fields.Field):
+    def _serialize(self, value, attr, obj):
+        return value
+
+
+class ToMany(fields.List):
+    def __init__(self, *args, **kwarg):
+        related_resource_name = kwarg.get("resource")
+        description = f"IDs of related resource ({related_resource_name})."
+
+        users_description = kwarg.pop("description", None)
+        if users_description:
+            description = f"{description} {users_description}"
+
+        super().__init__(*args, description=description, **kwarg)
+
+
+class ToOne(fields.Integer):
+    def __init__(self, *args, **kwarg):
+        related_resource_name = kwarg.get("resource")
+        description = f"ID of related resource ({related_resource_name})"
+
+        users_description = kwarg.pop("description", None)
+        if users_description:
+            description = f"{description} {users_description}"
+
+        super().__init__(*args, description=description, **kwarg)
+
+
+class Choice(fields.Str):
+    def __init__(self, *args, **kwarg):
+        allowed_values = kwarg.get("allowed_values")
+        super().__init__(
+            *args, validate=validate.OneOf(allowed_values), **kwarg
+        )
+
+
+class NotNullableList(fields.List):
+    def _serialize(self, value, attr, obj):
+        if value is None:
+            return []
+        if utils.is_collection(value):
+            return [
+                self.container._serialize(each, attr, obj)
+                for each in value
+                if each is not None
+            ]
+        return [self.container._serialize(value, attr, obj)]
