0ea29bf1b32e0a78b7262c6a1e61490abfb64ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:31 +0000

    Add package unv.web/0.1.6/unv.web-0.1.6.tar.gz

---
+++ packages/unv.web/unv.web-0.1.6.tar.gz/unv.web-0.1.6/src/unv/web/app.py
@@ -0,0 +1,38 @@
+import jinja2
+
+from aiohttp import web
+
+from unv.app.settings import SETTINGS as APP_SETTINGS
+
+from .helpers import (
+    url_for_static, url_with_domain, inline_static_from,
+    make_url_for_func, make_url_with_domain_for_func,
+)
+from .settings import SETTINGS
+
+
+def setup_jinja(app: web.Application):
+    settings = SETTINGS.get('jinja', {})
+    if not settings.get('enabled'):
+        return
+
+    settings['loader'] = jinja2.ChoiceLoader([
+        jinja2.PackageLoader(package)
+        for package in APP_SETTINGS['app']['components']
+    ])
+    if 'jinja2.ext.i18n' not in settings.setdefault('extensions', []):
+        settings['extensions'].append('jinja2.ext.i18n')
+
+    app['jinja2'] = jinja2.Environment(**settings)
+    app['jinja2'].globals.update({
+        'url_for_static': url_for_static,
+        'inline_static_from': inline_static_from,
+        'url_with_domain': url_with_domain,
+        'url_for': make_url_for_func(app),
+        'url_with_domain_for': make_url_with_domain_for_func(app),
+        'DEBUG': not APP_SETTINGS['debug'],
+    })
+
+
+def setup(app: web.Application):
+    setup_jinja(app)
---
+++ packages/unv.web/unv.web-0.1.6.tar.gz/unv.web-0.1.6/src/unv/web/helpers.py
@@ -0,0 +1,66 @@
+import pathlib
+
+from aiohttp import web
+
+from unv.utils.files import calc_crc32_for_file
+
+from .settings import SETTINGS
+
+
+async def render_template(
+        request, template_name, context=None, context_processors=None,
+        status=web.HTTPOk.status_code):
+    context = context or {}
+    context_processors = context_processors or {}
+    template = request.app['jinja2'].get_template(template_name)
+
+    for key, processor in context_processors.items():
+        if key not in context:
+            value = await processor(request)
+            context[key] = value
+
+    return web.Response(
+        text=template.render(context),
+        status=status, charset='utf-8',
+        content_type='text/html'
+    )
+
+
+def url_for_static(path):
+    real_path = pathlib.Path(SETTINGS['static']['paths']['public'])
+    real_path = real_path / path.lstrip('/')
+    hash_ = '?hash={}'.format(calc_crc32_for_file(real_path))
+    path = path.replace(SETTINGS['static']['paths']['public'], '')
+    return ''.join([SETTINGS['static']['urls']['public'], path, hash_])
+
+
+def url_for_private_static(path):
+    real_path = pathlib.Path(SETTINGS['static']['paths']['private'])
+    real_path = real_path / path.lstrip('/')
+    hash_ = '?hash={}'.format(calc_crc32_for_file(real_path))
+    path = path.replace(SETTINGS['static']['paths']['private'], '')
+    return ''.join([SETTINGS['static']['urls']['private'], path, hash_])
+
+
+def url_with_domain(path):
+    return '{}://{}{}'.format(SETTINGS['protocol'], SETTINGS['domain'], path)
+
+
+def make_url_for_func(app):
+    def url_for(route, **parts):
+        parts = {key: str(value) for key, value in parts.items()}
+        return app.router[route].url_for(**parts)
+    return url_for
+
+
+def make_url_with_domain_for_func(app):
+    def url_with_domain_for(route, **parts):
+        url = app.router[route].url_for(**parts)
+        return url_with_domain(str(url))
+    return url_with_domain_for
+
+
+def inline_static_from(path):
+    static_path = pathlib.Path(SETTINGS['static']['paths']['public'])
+    with open(str(static_path / path.lstrip('/')), 'r') as f:
+        return f.read().replace("\n", "")
