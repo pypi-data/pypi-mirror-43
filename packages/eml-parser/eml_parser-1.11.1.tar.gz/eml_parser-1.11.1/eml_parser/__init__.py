a3d5f7f1cb4e2d5395789b74a93f6589e355d352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package eml-parser/1.11.1/eml_parser-1.11.1.tar.gz

---
+++ packages/eml-parser/eml_parser-1.11.1.tar.gz/eml_parser-1.11.1/eml_parser/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+
+"""eml_parser serves as a python module for parsing eml files and returning various
+information found in the e-mail as well as computed information.
+"""
+
+from . import eml_parser
+
+__version__ = '1.11'
