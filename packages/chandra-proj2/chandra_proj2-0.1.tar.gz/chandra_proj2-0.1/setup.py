d078ca8d23d6e341c281fd0d49637c447a0abce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:18 +0000

    Add package chandra-proj2/0.1/chandra_proj2-0.1.tar.gz

---
+++ packages/chandra-proj2/chandra_proj2-0.1.tar.gz/chandra_proj2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chandra_proj2',
+      version='0.1',
+      description='The chandra_proj2 joke in the world',
+      url='http://github.com/storborg/chandra_proj2',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['chandra_proj2'],
+      zip_safe=False)
