f05db30baa64e405d832f500fb1b52c83be97a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:36 +0000

    Add package status-page/0.1.1/status-page-0.1.1.tar.gz

---
+++ packages/status-page/status-page-0.1.1.tar.gz/status-page-0.1.1/status_page/analytics/version.py
@@ -0,0 +1,48 @@
+"""LICENSE
+Copyright 2019 Hermann Krumrey <hermann@krumreyh.com>
+
+This file is part of status-page.
+
+status-page is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+status-page is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with status-page.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE"""
+
+import json
+import requests
+
+
+def get_gitlab_version(address: str, token: str) -> str:
+    """
+    Gets the version of a gitlab instance
+    :param address: The address of the gitlab instance (excluding https)
+    :param token: The API token used for authentication
+    :return: The version, minus any
+    """
+    resp = json.loads(requests.get(
+        "https://{}/api/v4/version".format(address),
+        headers={
+            "PRIVATE-TOKEN": token
+        }
+    ).text)
+    print(resp)
+    print(token)
+    return resp["version"].split("-")[0].strip()
+
+
+def get_version_from_version_file(version_file: str) -> str:
+    """
+    Retrieves the version based on a version file
+    :param version_file: The version file to read
+    :return: The version
+    """
+    return requests.get(version_file).text.strip()
