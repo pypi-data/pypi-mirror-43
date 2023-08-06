638dd721134c9e355422c79b118cc2716159f7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:25 +0000

    Add package fortiosapi/0.10.5/fortiosapi-0.10.5.tar.gz

---
+++ packages/fortiosapi/fortiosapi-0.10.5.tar.gz/fortiosapi-0.10.5/fortiosapi/__init__.py
@@ -0,0 +1,1 @@
+from .fortiosapi import FortiOSAPI
---
+++ packages/fortiosapi/fortiosapi-0.10.5.tar.gz/fortiosapi-0.10.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='fortiosapi',
+    version='0.10.5',
+    description=('Python modules to use Fortigate APIs'
+                 'full configuration, monitoring, lifecycle rest and ssh'),
+    long_description=readme(),
+    # Valid Classifiers are here:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python ',
+        'Topic :: Security',
+    ],
+    keywords='Fortinet fortigate fortios rest api',
+    install_requires=['requests', 'paramiko', 'oyaml'],
+    author='Nicolas Thomas',
+    author_email='nthomas@fortinet.com',
+    url='https://github.com/fortinet-solutions-cse/fortiosapi',
+    include_package_data=True,
+    packages=['fortiosapi'],
+)
