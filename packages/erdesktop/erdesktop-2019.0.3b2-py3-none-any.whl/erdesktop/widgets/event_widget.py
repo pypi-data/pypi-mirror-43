27e014e6dbc2e4fd924b76ab5ca6d4a7da4efa43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:10 +0000

    Add package erdesktop/2019.0.3b2/erdesktop-2019.0.3b2-py3-none-any.whl

---
+++ packages/erdesktop/erdesktop-2019.0.3b2-py3-none-any.whl/erdesktop/widgets/event_widget.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout
+
+
+class EventWidget(QWidget):
+
+	def __init__(self, parent, event_data):
+		super(EventWidget, self).__init__(parent, flags=parent.windowFlags())
+
+		self.titleLabel = QLabel()
+
+		self.parent = parent
+		self.event_data = event_data
+
+		self.setup_ui()
+
+	def setup_ui(self):
+		layout = QVBoxLayout()
+
+		self.titleLabel.setText('{} | {}'.format(self.event_data.time.strftime('%H:%M'), self.event_data.title))
+
+		# noinspection PyArgumentList
+		layout.addWidget(self.titleLabel)
+
+		self.setLayout(layout)
