8d8e4f0bc467acf58f3028bfcd0c14ff1b8be326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:20 +0000

    Add package govroam-certificate-cli/1.1/govroam-certificate-cli-1.1.tar.gz

---
+++ packages/govroam-certificate-cli/govroam-certificate-cli-1.1.tar.gz/govroam-certificate-cli-1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='govroam-certificate-cli',  
+     version='1.1',
+     author="Mike Richardson",
+     author_email="doctor@perpetual.name",
+     description="CLI certificate generator for Govroam",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     scripts=['generate_certs.py'],
+     classifiers=[
+         "Programming Language :: Python :: 2",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=[
+         "simple-roaming-certificate"
+     ],
+ )
+ 
+ 
