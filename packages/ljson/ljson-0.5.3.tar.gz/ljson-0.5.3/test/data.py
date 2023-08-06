0973334f2f1893bfd6852760c7761b6ac80b2a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:46 +0000

    Add package ljson/0.5.3/ljson-0.5.3.tar.gz

---
+++ packages/ljson/ljson-0.5.3.tar.gz/ljson-0.5.3/test/data.py
@@ -0,0 +1,49 @@
+
+data = [
+{
+	"age": 42,
+	"name": "peter",
+	"lname": "griffin"
+},
+{
+	"age": 41,
+	"name": "louis",
+	"lname": "griffin"
+},
+{
+	"age": 12,
+	"name": "chris",
+	"lname": "griffin"
+}
+]
+
+header_descriptor = {
+	"age": {"type": "int", "modifiers": []},
+	"name": {"type": "str", "modifiers": []},
+	"lname": {"type": "str", "modifiers": []}
+}
+
+item_meg = {
+	"age": 16,
+	"name": "meg",
+	"lname": "griffin"
+}
+data_slapdash = [
+{
+	"test1": "foo",
+	"test2": "bar",
+	"test3": "baz"
+},
+{
+	"test4": 4,
+},
+{
+	"test1": "foo",
+	"test2": "fool",
+	"test4": 231
+},
+{
+	"test1": True,
+	"test2": "baz"
+}
+]
