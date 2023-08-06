bb1d383413ede775d2004e9c5887cd2cbcf1e36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package planblick/0.0.80/planblick-0.0.80.tar.gz

---
+++ packages/planblick/planblick-0.0.80.tar.gz/planblick-0.0.80/planblick/encryption/encryption.py
@@ -0,0 +1,14 @@
+import base64
+from Crypto.Cipher import XOR
+
+class Encryption:
+    def __init__(self):
+        pass
+
+    def encrypt(self, message: str, password: str) -> bytes:
+        cipher = XOR.new(password)
+        return base64.b64encode(cipher.encrypt(message))
+
+    def decrypt(self, message: str, password: str) -> bytes:
+        cipher = XOR.new(password)
+        return cipher.decrypt(base64.b64decode(message))
