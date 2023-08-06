f18272cbaeadb12aeb8ecf450ecc5bb68775674c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package itk-core/4.13.2.post1/itk_core-4.13.2.post1-cp35-cp35m-win_amd64.whl

---
+++ packages/itk-core/itk_core-4.13.2.post1-cp35-cp35m-win_amd64.whl/itk/Configuration/ITKIOTIFFConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('TIFFImageIO', 'itk::TIFFImageIO', 'itkTIFFImageIO', True),
+  ('TIFFImageIOFactory', 'itk::TIFFImageIOFactory', 'itkTIFFImageIOFactory', True),
+)
