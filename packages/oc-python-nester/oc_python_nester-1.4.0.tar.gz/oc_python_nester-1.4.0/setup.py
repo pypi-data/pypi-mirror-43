125f0e5b8072257fc91d82c3a9da099b3555a213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:11 +0000

    Add package oc-python-nester/1.4.0/oc_python_nester-1.4.0.tar.gz

---
+++ packages/oc-python-nester/oc_python_nester-1.4.0.tar.gz/oc_python_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='oc_python_nester',
+    version='1.4.0',
+    py_modules=['nester'],
+    author='oc_python',
+    author_email='chenlfc@126.com',
+    url='http://ocpython.wordpress.com',
+    description='一个简单的列表内容输出模块，添加了用于输出到不同位置的选项',
+)
