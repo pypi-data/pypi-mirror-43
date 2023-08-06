7b8918d9ea519b8cd79a1b2783941da4b494c4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:05 +0000

    Add package cingta-django-sso/0.0.2/cingta-django-sso-0.0.2.tar.gz

---
+++ packages/cingta-django-sso/cingta-django-sso-0.0.2.tar.gz/cingta-django-sso-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+	name = "cingta-django-sso",
+	version = "0.0.2",
+	description = "cingta simple sso",
+	author = "Vvegetables",
+	author_email = "hardwork_fight@163.com",
+	license = "Public domain",
+	url = "https://github.com/Vvegetables/cingta-django-sso",
+	packages = find_packages(),
+	platforms = ["all"],
+	install_requires = [
+		"Django >= 1.11.0, < 2.0",
+		"PyMysql >= 0.9.2",
+		"djconnect",
+		"rsa >= 3.4.2"
+	],
+	classifiers = [
+		"Development Status :: 2 - Pre-Alpha",
+		"Environment :: Web Environment",
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+	include_package_data=True,    # 启用清单文件MANIFEST.in
+	exclude_package_date={'':['.gitignore']} #去除部分不想要的文件
+)
