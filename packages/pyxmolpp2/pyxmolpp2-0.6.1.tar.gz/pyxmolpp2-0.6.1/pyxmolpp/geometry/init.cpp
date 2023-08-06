c59de4385939873c204699b5ab2a56a8378db75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:26 +0000

    Add package pyxmolpp2/0.6.1/pyxmolpp2-0.6.1.tar.gz

---
+++ packages/pyxmolpp2/pyxmolpp2-0.6.1.tar.gz/pyxmolpp2-0.6.1/pyxmolpp/geometry/init.cpp
@@ -0,0 +1,12 @@
+#include "init.h"
+
+void pyxmolpp::init_geometry(pybind11::module& m) {
+  geometry::init_exceptions(m);
+  geometry::init_XYZ(m);
+  geometry::init_AngleValue(m);
+  geometry::init_basic(m);
+  geometry::init_Transformation3d(m);
+  geometry::init_VectorXYZ(m);
+  geometry::init_alignment(m);
+  geometry::init_autocorrelation(m);
+}
