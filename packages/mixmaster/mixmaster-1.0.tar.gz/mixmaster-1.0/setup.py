527c3dbe1a77cdb152e52664d9d2c02e09eeb371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:39 +0000

    Add package mixmaster/1.0/mixmaster-1.0.tar.gz

---
+++ packages/mixmaster/mixmaster-1.0.tar.gz/mixmaster-1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import *
+
+setup(
+	name="mixmaster",
+	packages=find_packages(),
+	version="1.0",
+	license="MIT",
+	author="Ray Speth, Bryan W. Weber",
+	author_email="yarmond@gmail.com, bryan.w.weber@gmail.com",
+	url="https://github.com/Cantera/mixmaster",
+	description="Mixmaster is a Cantera based GUI that allows you to import reaction mechanisms and to view or set the state of mixtures." \
+				  "It can carry out the thermodynamic processes adn compute the chemical equilibrium. It gives the data related to reaction and chemical species." \
+				  " It postprocess the simulation data ans shows the reaction paths.   ",
+	install_requires=[
+		"numpy",
+		"math",
+		"sys",
+		"string",
+		"os",
+		"re",
+		"types"
+		"traceback"
+	]
+)
