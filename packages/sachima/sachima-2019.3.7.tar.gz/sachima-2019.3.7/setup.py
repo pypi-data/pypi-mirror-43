7a9f3144f8f8408e47e16075371f70efba2d9101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:53 +0000

    Add package sachima/2019.3.7/sachima-2019.3.7.tar.gz

---
+++ packages/sachima/sachima-2019.3.7.tar.gz/sachima-2019.3.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sachima",
+    version="2019.3.7",
+    author="nocmk2",
+    author_email="jianye.zhang@gmail.com",
+    description="A package for Data Analyst",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DessertsLab/Sachima",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    package_data={"": ["*.css"]},
+)
