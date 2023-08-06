6a33e6fd551c5b5649287906f4d57b40b7a1230b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:07 +0000

    Add package dankcli/0.5.3/dankcli-0.5.3-py3-none-any.whl

---
+++ packages/dankcli/dankcli-0.5.3-py3-none-any.whl/dankcli/__main__.py
@@ -0,0 +1,62 @@
+from PIL import Image, ImageFont, ImageDraw
+from .functions import getFontSize, getTopLeftCorner, getWhiteSpaceHeight, textWrap, getLastIndex
+import os
+import argparse
+import sys
+
+# Parsing Arguments to get Image Path and Meme Text
+parser = argparse.ArgumentParser()
+parser.add_argument("img", help="relative path to image",
+                    type=str)
+parser.add_argument("text", help="text to put above image",
+                    type=str)
+args = parser.parse_args()
+
+# Checking for JPEG/JPG Image to make sure to later save them as RGB and not RGBA
+args_img_lower = args.img.lower()
+is_jpeg = True if ("jpg" in args_img_lower or "jpeg" in args_img_lower) else False
+
+whiteColor = "rgb(255, 255, 255)"
+blackColor = "rgb(0, 0, 0)"
+
+# Check if specified image exists
+try:
+    img = Image.open(args.img)
+except Exception as e:
+    print("Specified image doesn't exist or can't be opened")
+    sys.exit(2)
+
+Width, Height = img.size
+line = args.text.replace("\\n", "\n")
+font_path = os.path.join(os.path.dirname(__file__), "fonts", "arial.ttf")
+font = ImageFont.truetype(font_path, size=getFontSize(img))
+
+# Check if intentional line-breaks done
+if "\n" in line:
+    # If yes, individually text wrap every line
+    linesList = []
+    intentionalNewLines = line.split("\n")
+    for intentionalNewLine in intentionalNewLines:
+        linesList.append(textWrap(intentionalNewLine, font, Width))
+    lines = "\n".join(linesList)
+else:
+    # If not, just text wrap on entire thing
+    lines = textWrap(line, font, Width)
+
+imageWithWhiteSpace = Image.new("RGBA",( Width, Height + getWhiteSpaceHeight(lines, font) ),whiteColor)
+imageWithWhiteSpace.paste(img, (0, getWhiteSpaceHeight(lines, font)))
+draw = ImageDraw.Draw(imageWithWhiteSpace)
+
+draw.text(getTopLeftCorner(draw, lines, font, imageWithWhiteSpace), lines, fill=blackColor, font=font, align="left")
+
+# Get index of new meme for saving
+lastIndex = getLastIndex()
+newIndex = lastIndex + 1
+
+if is_jpeg:
+    rgbImage = imageWithWhiteSpace.convert("RGB")
+    rgbImage.save(f"dankcli-output/meme{newIndex}.jpg")
+    print("Saved as " + f"dankcli-output/meme{newIndex}.jpg")
+else:
+    imageWithWhiteSpace.save(f"dankcli-output/meme{newIndex}.png")
+    print("Saved as " + f"dankcli-output/meme{newIndex}.png")
