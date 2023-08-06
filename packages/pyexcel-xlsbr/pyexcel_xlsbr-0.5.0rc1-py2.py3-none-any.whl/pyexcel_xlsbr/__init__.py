7cbed31913f3d2bfb2286961cb6b58f335e9a4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package pyexcel-xlsbr/0.5.0rc1/pyexcel_xlsbr-0.5.0rc1-py2.py3-none-any.whl

---
+++ packages/pyexcel-xlsbr/pyexcel_xlsbr-0.5.0rc1-py2.py3-none-any.whl/pyexcel_xlsbr/__init__.py
@@ -0,0 +1,28 @@
+"""
+    pyexcel_xlsbr
+    ~~~~~~~~~~~~~~~~~~~
+    The lower level xlsb file format handler
+    :copyright: (c) 2018 by Onni Software Ltd & its contributors
+    :license: New BSD License
+"""
+# flake8: noqa
+from pyexcel_io.io import get_data as read_data
+from pyexcel_io.io import isstream
+from pyexcel_io.plugins import IOPluginInfoChain
+
+from ._version import __author__, __version__
+
+__FILE_TYPE__ = "xlsb"
+
+IOPluginInfoChain(__name__).add_a_reader(
+    relative_plugin_class_path="xlsbr.XLSBBook",
+    file_types=[__FILE_TYPE__],
+    stream_type="binary",
+)
+
+
+def get_data(afile, file_type=None, **keywords):
+    """standalone module function for reading module supported file type"""
+    if isstream(afile) and file_type is None:
+        file_type = __FILE_TYPE__
+    return read_data(afile, file_type=file_type, **keywords)
