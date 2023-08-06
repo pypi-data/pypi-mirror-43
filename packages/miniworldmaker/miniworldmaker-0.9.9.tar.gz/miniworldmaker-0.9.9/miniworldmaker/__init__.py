c957ed2b8e6afec9e6769dc01681083dc8b457cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:22 +0000

    Add package miniworldmaker/0.9.9/miniworldmaker-0.9.9.tar.gz

---
+++ packages/miniworldmaker/miniworldmaker-0.9.9.tar.gz/miniworldmaker-0.9.9/miniworldmaker/__init__.py
@@ -0,0 +1,8 @@
+from miniworldmaker.actors.actor import Actor
+from miniworldmaker.boards.board import Board
+from miniworldmaker.boards.tiled_board import TiledBoard
+
+__all__ = ['Actor',
+           'Board',
+           'TiledBoard',
+          ]
