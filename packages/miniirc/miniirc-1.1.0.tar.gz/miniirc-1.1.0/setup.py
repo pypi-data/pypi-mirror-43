0bf13583752973023acc35896accf3b3974a2922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:31 +0000

    Add package miniirc/1.1.0/miniirc-1.1.0.tar.gz

---
+++ packages/miniirc/miniirc-1.1.0.tar.gz/miniirc-1.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    desc = f.read()
+
+setup(
+    name        = 'miniirc',
+    version     = '1.1.0',
+    py_modules  = ['miniirc'],
+    author      = 'luk3yx',
+    description = 'A lightweight IRC framework.',
+    url         = 'https://github.com/luk3yx/miniirc',
+    license     = 'MIT',
+
+    long_description              = desc,
+    long_description_content_type = 'text/markdown',
+    install_requires              = ['certifi'],
+
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
