34ca4dde829584143e38e7dbdd152d4f4d34ca8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:02 +0000

    Add package avd-compose/0.3.3/avd-compose-0.3.3.tar.gz

---
+++ packages/avd-compose/avd-compose-0.3.3.tar.gz/avd-compose-0.3.3/avd_compose/androidstudio/avdmanager.py
@@ -0,0 +1,56 @@
+import sys
+
+from ..utils import shell, formatter
+
+
+class Avdmanager:
+    @staticmethod
+    def full_path():
+        return shell.get_full_path("avdmanager").strip()
+
+
+class Avd:
+    @staticmethod
+    def create(name, **kwargs):
+        # If this package is not available in the system, you should install the package with 'sdkmanager'.
+        options_string = formatter.options_as_a_string(kwargs)
+        command = """{full_path_of_tool} create avd --name "{name}" {options}""".format(
+            full_path_of_tool=Avdmanager.full_path(), name=name, options=options_string
+        )
+        return shell.run_command(command)
+
+    @staticmethod
+    def delete(name):
+        command = """{full_path_of_tool} delete avd --name "{name}" """.format(
+            full_path_of_tool=Avdmanager.full_path(), name=name
+        )
+        return shell.run_command(command)
+
+    @staticmethod
+    def move(name, rename, path):
+        pass
+
+    @staticmethod
+    def list():
+        command = "{full_path_of_tool} list avd".format(
+            full_path_of_tool=Avdmanager.full_path()
+        )
+        return shell.run_command(command)
+
+
+class Target:
+    @staticmethod
+    def list():
+        command = "{full_path_of_tool} list target".format(
+            full_path_of_tool=Avdmanager.full_path()
+        )
+        return shell.run_command(command)
+
+
+class Device:
+    @staticmethod
+    def list():
+        command = "{full_path_of_tool} list device".format(
+            full_path_of_tool=Avdmanager.full_path()
+        )
+        return shell.run_command(command)
