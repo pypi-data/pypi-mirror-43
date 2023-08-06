b4bd946de1618994e598e0c55f3c694dc7cf8d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:24 +0000

    Add package webautomators/0.2/webautomators-0.2.tar.gz

---
+++ packages/webautomators/webautomators-0.2.tar.gz/webautomators-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(name='webautomators',
+version='0.2',
+url='',
+license='MIT',
+author='Gabriela Nogueira',
+author_email='gsilvan@indracompany.com',
+description='Application Interaction Library with Web',
+packages=['webautomators'],
+install_requires=['selenium'],
+zip_safe=True)
