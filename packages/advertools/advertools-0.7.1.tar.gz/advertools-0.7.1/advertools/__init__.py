b390afe3111eb2b5071a74d69eb505b8ca363a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:43 +0000

    Add package advertools/0.7.1/advertools-0.7.1.tar.gz

---
+++ packages/advertools/advertools-0.7.1.tar.gz/advertools-0.7.1/advertools/__init__.py
@@ -0,0 +1,24 @@
+
+"""Top-level package for advertools."""
+
+__author__ = """Elias Dabbas"""
+__email__ = 'eliasdabbas@gmail.com'
+__version__ = '0.7.1'
+
+from advertools.ad_create import ad_create
+from advertools.ad_from_string import ad_from_string
+from advertools.extract import (extract, extract_currency, extract_emoji,
+                                extract_hashtags, extract_intense_words,
+                                extract_mentions, extract_questions,
+                                extract_words)
+from advertools.kw_generate import *
+from advertools.regex import *
+from advertools.stopwords import stopwords
+from advertools.url_builders import url_utm_ga
+from advertools.word_frequency import word_frequency
+from advertools.word_tokenize import word_tokenize
+from . import twitter
+from .serp import (serp_goog, serp_youtube, SERP_GOOG_VALID_VALS,
+                   youtube_channel_details, youtube_video_details,
+                   YOUTUBE_VID_CATEGORY_IDS, YOUTUBE_TOPIC_IDS,
+                   set_logging_level)
