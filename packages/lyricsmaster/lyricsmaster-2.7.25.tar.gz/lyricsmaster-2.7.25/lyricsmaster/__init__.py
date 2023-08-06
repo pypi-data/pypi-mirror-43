4da3fc8c2a046407315df8a65a1567201380030c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:47 +0000

    Add package lyricsmaster/2.7.25/lyricsmaster-2.7.25.tar.gz

---
+++ packages/lyricsmaster/lyricsmaster-2.7.25.tar.gz/lyricsmaster-2.7.25/lyricsmaster/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for LyricsMaster."""
+
+__author__ = """SekouD"""
+__email__ = 'sekoud.python@gmail.com'
+__version__ = '2.7.25'
+
+from .providers import LyricWiki, AzLyrics, Genius
+from .utils import TorController
