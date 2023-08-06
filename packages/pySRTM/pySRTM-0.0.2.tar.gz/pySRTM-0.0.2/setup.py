81fbf6d0660ec18465d41d0acc09d87ab11d2efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:47 +0000

    Add package pySRTM/0.0.2/pySRTM-0.0.2.tar.gz

---
+++ packages/pySRTM/pySRTM-0.0.2.tar.gz/pySRTM-0.0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 07 14:43:01 2018
+
+@author: tih
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pySRTM",
+    version="0.0.2",
+    author="Tim Hessels",
+    author_email="timhessels@hotmail.com",
+    description="Tool for data collecting and data processing of SRTM data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TimHessels/pySRTM",
+    packages=setuptools.find_packages(),
+	install_requires=[
+		'numpy',
+		'urllib'
+    ],
+    classifiers=(
+	    "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+	include_package_data=True
+)
