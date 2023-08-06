b21967de0b0a4d17565dea077734586d907f1a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:52 +0000

    Add package tnprofanity/0.3.3/tnprofanity-0.3.3.tar.gz

---
+++ packages/tnprofanity/tnprofanity-0.3.3.tar.gz/tnprofanity-0.3.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='tnprofanity',
+    version='0.3.3',
+    description='thai-text profanity library',
+    long_description=readme(),
+    url='',
+    author='GarKoZ',
+    author_email='gark36@gmail.com',
+    license='MIT',
+    install_requires=[
+
+    ],
+    scripts=[],
+    keywords='thai english profanity censor',
+    packages=['tnprofanity'],
+    package_dir={'tnprofanity': 'src/tnprofanity'},
+    package_data={'tnprofanity': ['*']}
+)
