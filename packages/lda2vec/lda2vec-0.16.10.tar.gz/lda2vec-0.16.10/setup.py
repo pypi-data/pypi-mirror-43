dfe5fc6d97b7d3bf74693aebb9bcecc44b10293c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:22 +0000

    Add package lda2vec/0.16.10/lda2vec-0.16.10.tar.gz

---
+++ packages/lda2vec/lda2vec-0.16.10.tar.gz/lda2vec-0.16.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(name="lda2vec",
+	  version="0.16.10",
+	  description="Tools for interpreting natural language",
+	  author="Nathan Raw",
+	  author_email="nxr9266@rit.edu",
+	  packages=find_packages(),
+	  url="")
