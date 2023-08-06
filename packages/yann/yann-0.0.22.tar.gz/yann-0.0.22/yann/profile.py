de353036ea5700390bb2c15074a07c913a2da730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:30 +0000

    Add package yann/0.0.22/yann-0.0.22.tar.gz

---
+++ packages/yann/yann-0.0.22.tar.gz/yann-0.0.22/yann/profile.py
@@ -0,0 +1,9 @@
+from torch.autograd.profiler import profile
+
+
+# TODO:
+# - https://github.com/pytorch/pytorch/issues/3749#issuecomment-374006211
+# - https://pytorch.org/docs/stable/bottleneck.html
+
+def param_count(model):
+  return sum(p.numel() for p in model.parameters())
