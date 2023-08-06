1e564df2bb033ae1176d01b313d282128771d5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:16 +0000

    Add package hotpy/0.0.3/hotpy-0.0.3.tar.gz

---
+++ packages/hotpy/hotpy-0.0.3.tar.gz/hotpy-0.0.3/hotpy/__init__.py
@@ -0,0 +1,17 @@
+"""
+hotpy
+github.com/Hotmart-Org/devops/hotpy
+"""
+
+from .cache import *
+from .csv import *
+from .datetime import *
+from .dic import *
+from .html import *
+from .int import *
+from .json import *
+from .list import *
+from .parallel import *
+from .pprint import *
+from .sh import *
+from .string import *
