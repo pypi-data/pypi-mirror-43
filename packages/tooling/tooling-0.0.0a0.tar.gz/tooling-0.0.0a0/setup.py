d71b6109d84ca533fcd9869ad69ea35321090651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:25 +0000

    Add package tooling/0.0.0a0/tooling-0.0.0a0.tar.gz

---
+++ packages/tooling/tooling-0.0.0a0.tar.gz/tooling-0.0.0a0/setup.py
@@ -0,0 +1,16 @@
+#! python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='tooling',
+    version='0.0.0a0',
+    copyright='(c) Copyright: 2019 Praveen Baburao Kulkarni',
+    description='Tools required for everyday development of Python code.',
+    url='https://github.com/SpikingNeurons/tooling.git',
+    author='Praveen Baburao Kulkarni',
+    author_email='praveenneuron@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
