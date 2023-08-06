cc796032ceabf1038d6d4915f3931811338b9c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:50 +0000

    Add package webscrapetools/0.4.3/webscrapetools-0.4.3.tar.gz

---
+++ packages/webscrapetools/webscrapetools-0.4.3.tar.gz/webscrapetools-0.4.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+__version = '0.4.3'
+
+
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+setup(
+    name='webscrapetools',
+    version=__version,
+    description='A basic but fast, persistent and threadsafe caching system',
+    long_description=readme(),
+    url='https://github.com/chris-ch/webscrapetools',
+    author='Christophe',
+    author_email='chris.perso@gmail.com',
+    packages=['webscrapetools'],
+    package_dir={'webscrapetools': 'src/webscrapetools'},
+    license='Apache',
+    download_url='https://github.com/chris-ch/webscrapetools/webscrapetools/archive/{0}.tar.gz'.format(__version),
+    install_requires=[
+        'requests',
+    ],
+    zip_safe=True
+)
