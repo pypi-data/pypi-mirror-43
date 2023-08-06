ae641c2fac04f57edd56d05645a0d63533eec39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package star-lighting/0.1/star-lighting-0.1.tar.gz

---
+++ packages/star-lighting/star-lighting-0.1.tar.gz/star-lighting-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='star-lighting',
+    version='0.1',
+    author='duxinn',
+    author_email="xiamin_1990@yahoo.com",
+    description='simplely use python',
+    long_description='simplely use python',
+    packages=find_packages('StarLighting'),
+    package_dir={'': 'StarLighting'},
+    install_requires=[],
+)
+
+
+
+
+
