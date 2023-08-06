b8dd28bede3a42954efe4223be29296b278cd06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:05 +0000

    Add package Office365-REST-Python-Client/2.1.1/Office365_REST_Python_Client-2.1.1-py3-none-any.whl

---
+++ packages/Office365-REST-Python-Client/Office365_REST_Python_Client-2.1.1-py3-none-any.whl/examples/settings.py
@@ -0,0 +1,12 @@
+settings = {
+    'url': 'https://mediadev23.sharepoint.com',
+    'user_credentials': {
+        'username': 'mattim@mediadev23.onmicrosoft.com',
+        'password': 'P@ssw0rd'
+    },
+    'client_credentials': {
+        'client_id': '',
+        'client_secret': '',
+        'redirect_url': 'https://github.com/vgrem/Office365-REST-Python-Client/'
+    }
+}
