4aef69255d3eeeecb89b0808935a5e400b9b9837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:42 +0000

    Add package suanpan/0.7.66/suanpan-0.7.66.tar.gz

---
+++ packages/suanpan/suanpan-0.7.66.tar.gz/suanpan-0.7.66/suanpan/objects.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from __future__ import print_function
+
+import copy
+import itertools
+
+from addict import Dict
+
+
+class HasName(object):
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+
+class Context(Dict, HasName):
+    pass
+
+
+class Global(Context):
+    pass
