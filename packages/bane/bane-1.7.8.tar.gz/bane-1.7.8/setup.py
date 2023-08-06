5fe7f9ab77e8d1ce698ed7499505397bab0f4ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package bane/1.7.8/bane-1.7.8.tar.gz

---
+++ packages/bane/bane-1.7.8.tar.gz/bane-1.7.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools,os,sys
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+'''
+i made this so you can use it on termux :)
+'''
+req=["requests","PySocks","bs4","pexpect","paramiko","mysql-connector","scapy","stem"]
+
+if (sys.platform == "win32") or( sys.platform == "win64"):
+ req=["requests","PySocks","bs4","paramiko","mysql-connector","scapy","stem"]
+
+if ('linux' in sys.platform):
+  req=["requests","PySocks","bs4","pexpect","mysql-connector","scapy","stem"]
+    
+setuptools.setup(
+    name="bane",
+    version="1.7.8",
+    author="AlaBouali",
+    author_email="trap.leader.123@gmail.com",
+    description="cyber security library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlaBouali/bane",
+    python_requires=">=2.7",
+    install_requires=req,
+    packages=["bane"],
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License ",
+        "Operating System :: Unix",
+    ],
+)
