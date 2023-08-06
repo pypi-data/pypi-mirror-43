d406855b5b0ef274b6048b39e48efe5288f5f30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package edc-lab/0.2.39/edc_lab-0.2.39-py3-none-any.whl

---
+++ packages/edc-lab/edc_lab-0.2.39-py3-none-any.whl/edc_lab/admin/base_model_admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_fieldsets import FieldsetsModelAdminMixin
+from edc_model_admin import (
+    ModelAdminAuditFieldsMixin,
+    ModelAdminFormInstructionsMixin,
+    ModelAdminNextUrlRedirectMixin,
+    ModelAdminFormAutoNumberMixin,
+)
+
+
+class BaseModelAdmin(
+    ModelAdminFormInstructionsMixin,
+    ModelAdminNextUrlRedirectMixin,
+    ModelAdminFormAutoNumberMixin,
+    ModelAdminRevisionMixin,
+    ModelAdminAuditFieldsMixin,
+    FieldsetsModelAdminMixin,
+    admin.ModelAdmin,
+):
+
+    list_per_page = 10
+    date_hierarchy = "modified"
+    empty_value_display = "-"
+    view_on_site = False
+    show_cancel = True
---
+++ packages/edc-lab/edc_lab-0.2.39-py3-none-any.whl/edc_lab/models/model_mixins/__init__.py
@@ -0,0 +1,6 @@
+from .aliquot import AliquotIdentifierModelMixin, AliquotLabelMixin, AliquotModelMixin
+from .aliquot import AliquotShippingMixin, AliquotTypeModelMixin
+from .panel_model_mixin import PanelModelMixin, PanelModelError, LabProfileError
+from .result import ResultItemModelMixin, ResultModelMixin
+from .requisition import RequisitionIdentifierMixin
+from .requisition import RequisitionModelMixin, RequisitionStatusMixin
