561e799d0c222e9457efb79a6428d1d33f0f7fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:36 +0000

    Add package bane/1.3.2/bane-1.3.2.tar.gz

---
+++ packages/bane/bane-1.3.2.tar.gz/bane-1.3.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bane",
+    version="1.3.2",
+    author="AlaBouali",
+    author_email="trap.leader.123@gmail.com",
+    description="cyber security library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlaBouali/bane",
+    packages=setuptools.find_packages(),
+    python_requires=">=2.7",
+    install_requires=['pexpect','paramiko','PySocks','requests','bs4','scapy','stem','mysql-connector'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License ",
+        "Operating System :: Unix",
+    ],
+)
