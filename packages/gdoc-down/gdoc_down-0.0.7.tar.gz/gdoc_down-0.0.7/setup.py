0ec1c0eefc45b900ee87256d922c73e0ec277b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:57 +0000

    Add package gdoc-down/0.0.7/gdoc_down-0.0.7.tar.gz

---
+++ packages/gdoc-down/gdoc_down-0.0.7.tar.gz/gdoc_down-0.0.7/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+try:
+    import pkg_utils
+except ImportError:
+    import pip._internal
+    pip._internal.main(['install', 'pkg_utils'])
+    import pkg_utils
+import os
+
+name = 'gdoc_down'
+dirname = os.path.dirname(__file__)
+
+# get package metadata
+md = pkg_utils.get_package_metadata(dirname, name)
+setuptools.setup(
+    name=name,
+    version=md.version,
+    description="Download Google documents to files",
+    long_description=md.long_description,
+    url="https://github.com/KarrLab/" + name,
+    download_url='https://github.com/KarrLab/' + name,
+    author="Jonathan Karr",
+    author_email="jonrkarr@gmail.com",
+    license="MIT",
+    keywords='Google documents drive download latex tex html pdf docx',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    package_data={
+        name: [
+            'VERSION',
+            'client.json',
+        ],
+    },
+    install_requires=md.install_requires,
+    extras_require=md.extras_require,
+    tests_require=md.tests_require,
+    dependency_links=md.dependency_links,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Office/Business :: Office Suites',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gdoc-down = gdoc_down.__main__:main',
+        ],
+    },
+)
