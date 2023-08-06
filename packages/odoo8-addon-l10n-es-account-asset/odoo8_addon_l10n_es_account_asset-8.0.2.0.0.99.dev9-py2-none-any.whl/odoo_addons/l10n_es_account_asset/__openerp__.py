caa25a8ee7b33055d68ac65cacca0cce6309b81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:13 +0000

    Add package odoo8-addon-l10n-es-account-asset/8.0.2.0.0.99.dev9/odoo8_addon_l10n_es_account_asset-8.0.2.0.0.99.dev9-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-es-account-asset/odoo8_addon_l10n_es_account_asset-8.0.2.0.0.99.dev9-py2-none-any.whl/odoo_addons/l10n_es_account_asset/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2012-2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Gestión de activos fijos para España",
+    "version": "8.0.2.0.0",
+    "depends": ["account_asset"],
+    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "http://www.serviciosbaeza.com",
+    "category": "Accounting & Finance",
+    "data": [
+        "views/account_asset_view.xml",
+    ],
+    "installable": True,
+}
