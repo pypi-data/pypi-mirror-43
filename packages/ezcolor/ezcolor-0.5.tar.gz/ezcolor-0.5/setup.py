5d1341737746b6af0b253f920f99e43893128afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package ezcolor/0.5/ezcolor-0.5.tar.gz

---
+++ packages/ezcolor/ezcolor-0.5.tar.gz/ezcolor-0.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='ezcolor',
+	packages=['ezcolor'],
+	version='0.5',
+	description='A lightweight lib for adding extra attributes to text',
+	author='Fardin Allahverdinazhand',
+	author_email='0x0ptim0us@gmail.com',
+	url='https://github.com/0x0ptim0us/ezcolor',
+	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.5.tar.gz',
+	keywords=['python3', 'colorize', 'ezcolor', 'output attribute', 'output colorize', 'text color', 'color'],
+	classifiers=[],
+)
