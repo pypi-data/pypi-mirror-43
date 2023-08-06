26995f6d8ab75db68a52797903a0eacbf98a9f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:39 +0000

    Add package fleetgo/0.9.3.3/fleetgo-0.9.3.3.tar.gz

---
+++ packages/fleetgo/fleetgo-0.9.3.3.tar.gz/fleetgo-0.9.3.3/fleetgo/api.py
@@ -0,0 +1,86 @@
+from .device import Device
+from .trip import Trip
+from .authentication import Authentication
+
+
+class API:
+
+    def __init__(self, client_id, client_secret, username, password):
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._username = username
+        self._password = password
+        self.authentication_info = None
+
+    def logged_in(self):
+        if (self.authentication_info is None):
+            return False
+        else:
+            return self.authentication_info.is_valid()
+
+    def login(self):
+        import requests
+
+        data_url = "https://api.fleetgo.com/api/session/login"
+
+        body = {
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
+            'username': self._username,
+            'password': self._password
+        }
+
+        response = requests.post(data_url, json=body)
+
+        self.authentication_info = Authentication()
+        self.authentication_info.set_json(response.json())
+        return self.logged_in()
+
+    def stream_devices(self, callback):
+        import json
+        import requests
+        import sseclient
+
+        if (self.authentication_info is None or
+                not self.authentication_info.is_valid()):
+            return False
+
+        data_url = "https://api.fleetgo.com/api/equipment/subscribe/?groupId=0&vehicleCategoryId=0&driverCategoryId=0"
+
+        header = self.authentication_info.create_header()
+        response = requests.get(data_url, headers=header, stream=True)
+
+        client = sseclient.SSEClient(response)
+        for event in client.events():
+            data = json.loads(event.data)
+            if (data != 'heartbeat'):
+                devices = self.parse_devices(data)
+                callback(devices)
+
+    def get_devices(self):
+        import requests
+
+        if (self.authentication_info is None or
+                not self.authentication_info.is_valid()):
+            return []
+
+        data_url = "https://api.fleetgo.com/api/equipment/Getfleet"
+        query = "?groupId=0&hasDeviceOnly=false"
+
+        header = self.authentication_info.create_header()
+        response = requests.get(data_url + query, headers=header)
+        data = response.json()
+        return self.parse_devices(data)
+
+    def parse_devices(self, json):
+        """Parse result from API."""
+        result = []
+
+        for json_device in json:
+            license_plate = json_device['EquipmentHeader']['SerialNumber']
+
+            device = Device(self, license_plate)
+            device.update_from_json(json_device)
+            result.append(device)
+
+        return result
---
+++ packages/fleetgo/fleetgo-0.9.3.3.tar.gz/fleetgo-0.9.3.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='fleetgo',
+      version='0.9.3.3',
+      description='FleetGO API Access',
+      url='http://github.com/depl0y/fleetgo-py',
+      author='Wim Haanstra',
+      author_email='wim@wim.me',
+      license='MIT',
+      packages=['fleetgo'],
+      install_requires=[
+          'requests',
+          'ciso8601',
+          'geopy',
+          'sseclient-py'
+      ],
+      python_requires='>=3',
+      zip_safe=False)
