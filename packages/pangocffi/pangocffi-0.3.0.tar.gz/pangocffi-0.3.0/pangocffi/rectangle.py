af93e8f951b61b48afef5e8f42914bb9882f6122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:44 +0000

    Add package pangocffi/0.3.0/pangocffi-0.3.0.tar.gz

---
+++ packages/pangocffi/pangocffi-0.3.0.tar.gz/pangocffi-0.3.0/pangocffi/rectangle.py
@@ -0,0 +1,55 @@
+from typing import Optional
+from . import ffi
+
+
+class Rectangle:
+    """
+    The :class:`Rectangle` structure represents a rectangle. It is
+    frequently used to represent the logical or ink extents of a single glyph
+    or section of text.
+    """
+    def __init__(
+            self,
+            pointer: Optional[ffi.CData] = None
+    ):
+        if pointer is None:
+            self.pointer = ffi.new("PangoRectangle *")
+        else:
+            self.pointer = pointer
+
+    def get_pointer(self) -> ffi.CData:
+        """
+        Returns a pointer to the :class:`Rectangle`.
+
+        :return:
+            a pointer to ``PangoRectangle``
+        """
+        return self.pointer
+
+    @staticmethod
+    def from_pointer(pointer: ffi.CData) -> 'Rectangle':
+        """
+        Returns an instance of a :class:`Rectangle` from a pointer.
+
+        :return:
+            the :class:`Rectangle`.
+        """
+        return Rectangle(
+            pointer=pointer
+        )
+
+    @property
+    def x(self) -> int:
+        return self.pointer.x
+
+    @property
+    def y(self) -> int:
+        return self.pointer.y
+
+    @property
+    def width(self) -> int:
+        return self.pointer.width
+
+    @property
+    def height(self) -> int:
+        return self.pointer.height
