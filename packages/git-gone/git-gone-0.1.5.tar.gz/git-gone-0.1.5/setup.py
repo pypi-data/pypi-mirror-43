f9fc653a8e0f73b5f114b4f7b6cfd29ba1e69bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:11 +0000

    Add package git-gone/0.1.5/git-gone-0.1.5.tar.gz

---
+++ packages/git-gone/git-gone-0.1.5.tar.gz/git-gone-0.1.5/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['git_gone', 'git_gone.data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plumbum>=1.6,<2.0']
+
+entry_points = \
+{'console_scripts': ['git-gone = git_gone.__main__:main']}
+
+setup_kwargs = {
+    'name': 'git-gone',
+    'version': '0.1.5',
+    'description': 'A simple tool to track visited Git repositories, and check them for unpushed/committed changes',
+    'long_description': None,
+    'author': 'Angus Hollands',
+    'author_email': 'goosey15@gmail.com',
+    'url': 'https://github.com/agoose77/git-gone',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
