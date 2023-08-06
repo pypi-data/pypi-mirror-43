fd39a0a55ac3237844036bcb585bb83550e75d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:43 +0000

    Add package script-benchmark-tools/0.5.8/script_benchmark_tools-0.5.8.tar.gz

---
+++ packages/script-benchmark-tools/script_benchmark_tools-0.5.8.tar.gz/script_benchmark_tools-0.5.8/script_benchmark_tools/benchmark_results.py
@@ -0,0 +1,33 @@
+
+from ansi_colours import AnsiColours as Colour
+from terminal_table import Table
+
+
+def display_benchmark_results(results):
+    headers, rows = make_benchmark_results(results)
+
+    print(Table.create(rows, headers(), column_colours=[Colour.green, Colour.yellow, Colour.red]))
+
+
+def benchmark_results(results):
+    headers, rows = make_benchmark_results(results)
+
+    return Table.create(rows, headers(), use_ansi=False)
+
+
+def make_benchmark_results(results):
+    def headers():
+        return 'min', 'avg', 'max', 'func', 'name'
+
+    def row(result):
+        return (
+            "%.9f" % result.min(),
+            "%.9f" % result.average(),
+            "%.9f" % result.max(),
+            result.name(),
+            result.user()
+        )
+
+    results.sort(key=lambda r: r.min())
+    rows = [row(result) for result in results]
+    return headers, rows
