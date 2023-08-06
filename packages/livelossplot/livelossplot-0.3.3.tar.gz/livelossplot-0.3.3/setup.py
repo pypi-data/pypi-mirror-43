85c87926e9d99420c2f952d38dec6c000590c73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:38 +0000

    Add package livelossplot/0.3.3/livelossplot-0.3.3.tar.gz

---
+++ packages/livelossplot/livelossplot-0.3.3.tar.gz/livelossplot-0.3.3/setup.py
@@ -0,0 +1,33 @@
+from io import open
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='livelossplot',
+      version='0.3.3',
+      install_requires=['matplotlib', 'notebook'],
+      description='Live training loss plot in Jupyter Notebook for Keras, PyTorch and others.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/stared/livelossplot',
+      author='Piotr Migdał',
+      author_email='pmigdal@gmail.com',
+      keywords=['keras', 'pytorch', 'plot', 'chart'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Framework :: Jupyter',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=find_packages(),
+      zip_safe=False)
