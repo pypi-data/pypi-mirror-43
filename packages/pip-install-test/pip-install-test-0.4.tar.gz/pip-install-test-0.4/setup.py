4200c254f4d515a3e9fb65d10216ac9153c27611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:16 +0000

    Add package pip-install-test/0.4/pip-install-test-0.4.tar.gz

---
+++ packages/pip-install-test/pip-install-test-0.4.tar.gz/pip-install-test-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pip-install-test',
+      version='0.4',
+      description='A minimal stub package to test success of pip install',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Simon Krughoff',
+      author_email='krughoff@lsst.org',
+      license='MIT',
+      packages=['pip_install_test'],
+      zip_safe=False)
