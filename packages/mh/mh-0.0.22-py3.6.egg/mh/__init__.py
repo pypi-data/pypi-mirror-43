2d0a7a7b643bd2d150e682813df710a1901859ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:49 +0000

    Add package mh/0.0.22/mh-0.0.22-py3.6.egg

---
+++ packages/mh/mh-0.0.22-py3.6.egg/mh/__init__.py
@@ -0,0 +1,16 @@
+# mh/__ini__.py
+'''
+eg code to start a project with.
+import mh
+To see use mh.__doc__
+Can also be seen with help(mh)
+And dir(mh)
+'''
+
+__version__ = '0.0.21.test2'
+
+import mh.mh as mh
+
+EG_VAR2=mh.EG_VAR
+testFunction = mh.testFunction
+Mh = mh.Mh
---
+++ packages/mh/mh-0.0.22-py3.6.egg/mhlib/l2/l2.py
@@ -0,0 +1,24 @@
+# l2.py
+'''
+l2 code
+'''
+
+import time
+
+L2EG_VAR='efgh'
+
+def L2Test():
+    ''' returns a test string '''
+    return 'ok'
+
+class L2Mh (object):
+    def runMh(self):
+        print ('ok run')
+        return 10
+
+    def slow(self):
+        time.sleep(5)
+        print ('In l2.slow')
+
+
+
