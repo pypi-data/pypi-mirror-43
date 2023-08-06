124b396f95c3e8d83e0c6210d00e97ec3265f572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:03 +0000

    Add package occamtools/0.0.1/occamtools-0.0.1.tar.gz

---
+++ packages/occamtools/occamtools-0.0.1.tar.gz/occamtools-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='occamtools',
+    packages=['occamtools'],
+    url='https://github.com/mortele/OccamTools',
+    license='GPL-3',
+    version='0.0.1',
+    description='Analysis and synthesis tools for OCCAM hPF simulations',
+    tags=['occammd', 'hpf', 'md'],
+    author='Morten Ledum',
+    author_email='morten.ledum@gmail.com',
+    install_requires=[
+        'pytest>=4.3.0',
+        'pytest-cov>=2.6.1',
+        'scikit-learn>=0.20.2',
+        'scipy>=1.2.1',
+        'numpy>=1.16.1',
+        'numba>=0.42.1',
+        'matplotlib>=3.0.2',
+        'flake8>=3.7.7',
+        'coverage>=4.5.2',
+        'tqdm>=4.31.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7'
+    ],
+    python_requires='>=3.7',
+    # use_scm_version=True,
+    # setup_requires=['setuptools_scm'],
+)
