2cc2f4dd9b77d523728ac852dd319e9a526a7f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:36 +0000

    Add package QiDataLoader/1.0.2/QiDataLoader-1.0.2.tar.gz

---
+++ packages/QiDataLoader/QiDataLoader-1.0.2.tar.gz/QiDataLoader-1.0.2/QiDataLoader/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from .MqDataLoader import *
+from .MqTickReader import *
+from .BinaryReader import *
+from .Quote import *
+from .Tick import *
+
+__version__ = '1.0.0'
