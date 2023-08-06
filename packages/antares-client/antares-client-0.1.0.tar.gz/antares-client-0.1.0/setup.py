722139b985e9e350dc4b113542063a3da870c5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:12 +0000

    Add package antares-client/0.1.0/antares-client-0.1.0.tar.gz

---
+++ packages/antares-client/antares-client-0.1.0.tar.gz/antares-client-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setuptools.setup(
+    author="Carl Stubens",
+    author_email="cstubens@noao.edu",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="A light-weight client for receiving alerts from ANTARES.",
+    entry_points={"console_scripts": ["antares-client=antares_client.cli:main"]},
+    install_requires=["bson", "confluent-kafka"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    name="antares-client",
+    packages=setuptools.find_packages(),
+    setup_requires=["nose>1.0"],
+    python_requires=">=3.4",
+    url="https://github.com/cstubens/antares_client",
+    version="v0.1.0",
+)
