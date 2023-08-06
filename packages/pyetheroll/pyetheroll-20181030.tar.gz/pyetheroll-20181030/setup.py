20f33e92641649144cef282b219fd8becc860b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:03 +0000

    Add package pyetheroll/20181030/pyetheroll-20181030.tar.gz

---
+++ packages/pyetheroll/pyetheroll-20181030.tar.gz/pyetheroll-20181030/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='pyetheroll',
+      version='20181030',
+      description='Python library to Etheroll smart contract',
+      author='Andre Miras',
+      url='https://github.com/AndreMiras/pyetheroll',
+      packages=['pyetheroll'],
+      install_requires=[])
