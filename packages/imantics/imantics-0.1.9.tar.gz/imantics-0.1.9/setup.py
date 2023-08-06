a5e78275739a109ff19e7508c10cf1255e68abba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package imantics/0.1.9/imantics-0.1.9.tar.gz

---
+++ packages/imantics/imantics-0.1.9.tar.gz/imantics-0.1.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='imantics',
+    version='0.1.9',
+    description='Python package for managing image annotations',
+    url='https://github.com/jsbroks/imantics',
+    author='Justin Brooks',
+    author_email='jsbroks@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    install_requires=['numpy', 'opencv-python>=3', 'lxml'],
+    packages=['imantics'],
+    python_requires='>=2.7',
+    zip_safe=False
+)
