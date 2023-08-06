14e2694c43a429d462ad58ea168eb028867de32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:58 +0000

    Add package colorlogs/0.1/colorlogs-0.1.tar.gz

---
+++ packages/colorlogs/colorlogs-0.1.tar.gz/colorlogs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colorlogs',
+      version='0.1',
+      description='Custom logger allows colored logs',
+      url='https://github.com/karthiktv90/python_utilities',
+      author='karthik',
+      author_email='karthik@mymail.com',
+      license='UNH',
+      packages=['colorlogs'],
+      zip_safe=False)
