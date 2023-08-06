dd320c2fca3f2dc65948b3cb3916352bcc03c903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package ambition-ae/0.3.19/ambition_ae-0.3.19-py3-none-any.whl

---
+++ packages/ambition-ae/ambition_ae-0.3.19-py3-none-any.whl/ambition_ae/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.urls.conf import path
+from django.views.generic.base import RedirectView
+
+from .admin_site import ambition_ae_admin
+
+app_name = "ambition_ae"
+
+urlpatterns = [
+    path("admin/", ambition_ae_admin.urls),
+    path("", RedirectView.as_view(url="admin/"), name="home_url"),
+]
+
+
+if settings.APP_NAME == "ambition_ae":
+    from django.contrib import admin
+
+    urlpatterns += [path("admin/", admin.site.urls)]
