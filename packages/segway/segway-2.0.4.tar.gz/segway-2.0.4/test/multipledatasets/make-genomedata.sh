195dea30d7ba678b331979d58367fec5266c50a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package segway/2.0.4/segway-2.0.4.tar.gz

---
+++ packages/segway/segway-2.0.4.tar.gz/segway-2.0.4/test/multipledatasets/make-genomedata.sh
@@ -0,0 +1,17 @@
+# 1A and 1B are the same!
+
+genomedata-load --sequence=chr1.fa \
+    --track=testtrack1A=testtrack1A.bedgraph \
+    --file-mode --verbose track1a.genomedata
+
+genomedata-load --sequence=chr1.fa \
+    --track=testtrack2A=testtrack2A.bedgraph \
+    --file-mode --verbose track2a.genomedata
+
+genomedata-load --sequence=chr1.fa \
+    --track=testtrack1B=testtrack1B.bedgraph \
+    --file-mode --verbose track1b.genomedata
+
+genomedata-load --sequence=chr1.fa \
+    --track=testtrack2B=testtrack2B.bedgraph \
+    --file-mode --verbose track2b.genomedata
