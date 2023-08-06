bbe2cc600575caf86598d308fef4da33008b10a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:06 +0000

    Add package haima/1.0.0/haima-1.0.0.tar.gz

---
+++ packages/haima/haima-1.0.0.tar.gz/haima-1.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  : 2019-03-21 9:38
+# @Author : haima
+# @File  : setup.py
+# @Software: PyCharm
+from distutils.core import setup
+
+setup(
+    name='haima',
+    version='1.0.0',
+    author='haima',
+    author_email='yj22sy@qq.com',
+    url= 'http://www.baidu.com',
+    description= '这是我的第一个发布安装文件'
+)
+
+# python setup.py sdist
+# python setup.py install
+# python setup.py register
+# python setup.py sdist upload
