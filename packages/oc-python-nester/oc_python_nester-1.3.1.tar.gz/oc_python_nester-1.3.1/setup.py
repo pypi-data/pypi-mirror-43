a3215a9251efc91b802d03681f66901c09e43eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:56 +0000

    Add package oc-python-nester/1.3.1/oc_python_nester-1.3.1.tar.gz

---
+++ packages/oc-python-nester/oc_python_nester-1.3.1.tar.gz/oc_python_nester-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='oc_python_nester',
+    version='1.3.1',
+    py_modules=['nester'],
+    author='oc_python',
+    author_email='chenlfc@126.com',
+    url='http://ocpython.wordpress.com',
+    description='一个简单的列表内容输出模块',
+)
