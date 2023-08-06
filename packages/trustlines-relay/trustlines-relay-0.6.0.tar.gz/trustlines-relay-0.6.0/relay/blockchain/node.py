de658d4821f0fbea82a7151e977c5b5dac27852b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:45 +0000

    Add package trustlines-relay/0.6.0/trustlines-relay-0.6.0.tar.gz

---
+++ packages/trustlines-relay/trustlines-relay-0.6.0.tar.gz/trustlines-relay-0.6.0/relay/blockchain/node.py
@@ -0,0 +1,92 @@
+import os
+import logging
+from collections import namedtuple
+
+from relay.logger import get_logger
+from relay.concurrency_utils import synchronized
+
+
+TxInfos = namedtuple("TxInfos", "balance, nonce, gas_price")
+
+
+logger = get_logger("node", logging.DEBUG)
+
+
+class Node:
+    def __init__(self, web3, is_parity=True):
+        self._web3 = web3
+        self.is_parity = is_parity
+        if is_parity:
+            logger.info(
+                "Assuming connected to parity node: Enabling parity-only rpc methods."
+            )
+
+        # the instant seal engine used in the e2e tests does not work properly,
+        # when we we relay multiple transactions at the same time.
+        # It looks like parity just does not create a new block for the
+        # second transaction it sees. When we make the relay_tx method synchronized,
+        # transactions will not end up at parity at the same time.
+        # This makes it possible to run the end2end tests.
+        # Somehow this only became an issue after the upgrade to web3 4.x
+        # Opened an upstream issue https://github.com/paritytech/parity-ethereum/issues/9660
+        if os.environ.get("TRUSTLINES_SYNC_TX_RELAY", "") == "1":
+            logger.warning(
+                "synchronizing tx relaying because TRUSTLINES_SYNC_TX_RELAY is set"
+            )
+            self._send_tx = synchronized(self._web3.eth.sendRawTransaction)
+        else:
+            self._send_tx = self._web3.eth.sendRawTransaction
+
+    def relay_tx(self, rawtxn):
+        return self._send_tx(rawtxn)
+
+    def transaction_receipt(self, txn_hash):
+        return self._web3.eth.getTransactionReceipt(txn_hash)
+
+    def get_tx_infos(self, user_address, block_identifier="pending"):
+        if self.is_parity and block_identifier == "pending":
+            nonce = int(
+                self._web3.manager.request_blocking("parity_nextNonce", [user_address]),
+                16,
+            )
+        else:
+            nonce = self._web3.eth.getTransactionCount(
+                user_address, block_identifier=block_identifier
+            )
+        return TxInfos(
+            balance=self._web3.eth.getBalance(
+                user_address, block_identifier=block_identifier
+            ),
+            nonce=nonce,
+            gas_price=self._web3.eth.gasPrice,
+        )
+
+    @property
+    def address(self):
+        return self._web3.eth.coinbase
+
+    @property
+    def blocknumber(self):
+        return self._web3.eth.blockNumber
+
+    def balance(self, address):
+        wei = self._web3.eth.getBalance(address)
+        return str(self._web3.fromWei(wei, "ether"))
+
+    def balance_wei(self, address: str) -> int:
+        return self._web3.eth.getBalance(address)
+
+    def send_ether(self, address):
+        if self._web3.eth.getBalance(address) <= 5:
+            return self._web3.eth.sendTransaction(
+                {
+                    "from": self._web3.eth.coinbase,
+                    "to": address,
+                    "value": 1000000000000000000,
+                }
+            ).hex()
+        else:
+            return None
+
+    def get_block_timestamp(self, block_number):
+        return self._web3.eth.getBlock(block_number).timestamp
---
+++ packages/trustlines-relay/trustlines-relay-0.6.0.tar.gz/trustlines-relay-0.6.0/tests/chain_integration/conftest.py
@@ -0,0 +1,272 @@
+import json
+import os
+import sys
+
+import pytest
+from tldeploy.core import deploy_networks, deploy_network
+from tldeploy.identity import MetaTransaction
+
+from relay.blockchain.currency_network_proxy import CurrencyNetworkProxy
+import eth_tester
+
+
+NETWORK_SETTINGS = [
+    {
+        "name": "Cash",
+        "symbol": "CASH",
+        "decimals": 4,
+        "fee_divisor": 1000,
+        "default_interest_rate": 0,
+        "custom_interests": True,
+    },
+    {
+        "name": "Work Hours",
+        "symbol": "HOU",
+        "decimals": 4,
+        "fee_divisor": 0,
+        "default_interest_rate": 1000,
+        "custom_interests": False,
+    },
+    {
+        "name": "Beers",
+        "symbol": "BEER",
+        "decimals": 0,
+        "fee_divisor": 0,
+        "custom_interests": False,
+    },
+]
+
+
+"""increate eth_tester's GAS_LIMIT
+Otherwise we can't deploy our contract"""
+assert eth_tester.backends.pyevm.main.GENESIS_GAS_LIMIT < 6 * 10 ** 6
+eth_tester.backends.pyevm.main.GENESIS_GAS_LIMIT = 6 * 10 ** 6
+
+
+@pytest.fixture
+def maker(accounts):
+    """checksum maker address"""
+    return accounts[0]
+
+
+@pytest.fixture
+def maker_key(account_keys):
+    """checksum maker address"""
+    return account_keys[0].to_bytes()
+
+
+@pytest.fixture
+def taker(accounts):
+    """checksum taker address"""
+    return accounts[1]
+
+
+class CurrencyNetworkProxy(CurrencyNetworkProxy):
+    def setup_trustlines(self, trustlines):
+        for (A, B, clAB, clBA) in trustlines:
+            txid = self._proxy.functions.setAccount(
+                A, B, clAB, clBA, 0, 0, 0, 0, 0, 0
+            ).transact()
+            self._web3.eth.waitForTransactionReceipt(txid)
+
+    def update_trustline(
+        self,
+        from_,
+        to,
+        creditline_given,
+        creditline_received,
+        interest_rate_given=None,
+        interest_rate_received=None,
+    ):
+        if interest_rate_given is None or interest_rate_received is None:
+            txid = self._proxy.functions.updateCreditlimits(
+                to, creditline_given, creditline_received
+            ).transact({"from": from_})
+        else:
+            txid = self._proxy.functions.updateTrustline(
+                to,
+                creditline_given,
+                creditline_received,
+                interest_rate_given,
+                interest_rate_received,
+            ).transact({"from": from_})
+        self._web3.eth.waitForTransactionReceipt(txid)
+
+    def update_trustline_with_accept(
+        self,
+        from_,
+        to,
+        creditline_given,
+        creditline_received,
+        interest_rate_given=None,
+        interest_rate_received=None,
+    ):
+        self.update_trustline(
+            from_,
+            to,
+            creditline_given,
+            creditline_received,
+            interest_rate_given,
+            interest_rate_received,
+        )
+        self.update_trustline(
+            to,
+            from_,
+            creditline_received,
+            creditline_given,
+            interest_rate_received,
+            interest_rate_given,
+        )
+
+    def transfer(self, from_, to, value, max_fee, path):
+        txid = self._proxy.functions.transfer(to, value, max_fee, path).transact(
+            {"from": from_}
+        )
+        self._web3.eth.waitForTransactionReceipt(txid)
+
+    def transfer_meta_transaction(self, to, value, max_fee, path):
+
+        function_call = self._proxy.functions.transfer(to, value, max_fee, path)
+        meta_transaction = MetaTransaction.from_function_call(
+            function_call, to=self.address
+        )
+
+        return meta_transaction
+
+    def get_balance(self, from_, to):
+        return self._proxy.functions.balance(from_, to).call()
+
+
+@pytest.fixture()
+def trustlines(accounts):
+    return [
+        (accounts[0], accounts[1], 100, 150),
+        (accounts[1], accounts[2], 200, 250),
+        (accounts[2], accounts[3], 300, 350),
+        (accounts[3], accounts[4], 400, 450),
+        (accounts[0], accounts[4], 500, 550),
+    ]  # (A, B, clAB, clBA)
+
+
+def deploy_test_network(web3):
+    return deploy_network(web3, "Trustlines", "T", 6, fee_divisor=100)
+
+
+def deploy_test_networks(web3):
+    return deploy_networks(web3, NETWORK_SETTINGS)
+
+
+@pytest.fixture(scope="session")
+def contracts():
+    with open(
+        os.path.join(sys.prefix, "trustlines-contracts", "build", "contracts.json")
+    ) as data_file:
+        return json.load(data_file)
+
+
+@pytest.fixture(scope="session")
+def currency_network_abi(contracts):
+    return contracts["CurrencyNetwork"]["abi"]
+
+
+@pytest.fixture(scope="session")
+def exchange_abi(contracts):
+    return contracts["Exchange"]["abi"]
+
+
+@pytest.fixture(scope="session")
+def token_abi(contracts):
+    return contracts["Token"]["abi"]
+
+
+@pytest.fixture()
+def testnetwork1_address(web3):
+    return deploy_test_network(web3).address
+
+
+@pytest.fixture()
+def testnetwork2_address(web3):
+    return deploy_test_network(web3).address
+
+
+@pytest.fixture()
+def testnetwork3_address(web3):
+    return deploy_test_network(web3).address
+
+
+@pytest.fixture()
+def testnetworks(web3, maker, taker):
+    currency_network_contracts, exchange_contract, unw_eth_contract = deploy_test_networks(
+        web3
+    )
+
+    unw_eth_contract.functions.deposit().transact({"from": taker, "value": 200})
+
+    currency_network = currency_network_contracts[0]
+    currency_network.functions.updateCreditlimits(maker, 300, 0).transact(
+        {"from": taker}
+    )
+    currency_network.functions.updateCreditlimits(taker, 0, 300).transact(
+        {"from": maker}
+    )
+
+    return currency_network_contracts, exchange_contract, unw_eth_contract
+
+
+@pytest.fixture()
+def exchange_address(testnetworks):
+    return testnetworks[1].address
+
+
+@pytest.fixture()
+def unw_eth_address(testnetworks):
+    return testnetworks[2].address
+
+
+@pytest.fixture()
+def network_addresses_with_exchange(testnetworks):
+    return [network.address for network in testnetworks[0]]
+
+
+@pytest.fixture()
+def currency_network(web3, currency_network_abi, testnetwork1_address):
+    currency_network = CurrencyNetworkProxy(
+        web3, currency_network_abi, testnetwork1_address
+    )
+    return currency_network
+
+
+@pytest.fixture()
+def currency_network_with_trustlines(
+    web3, currency_network_abi, testnetwork2_address, trustlines
+):
+    currency_network = CurrencyNetworkProxy(
+        web3, currency_network_abi, testnetwork2_address
+    )
+    currency_network.setup_trustlines(trustlines)
+
+    return currency_network
+
+
+@pytest.fixture()
+def currency_network_with_events(currency_network, accounts):
+    currency_network.update_trustline_with_accept(accounts[0], accounts[1], 25, 50)
+    currency_network.transfer(accounts[1], accounts[0], 10, 10, [accounts[0]])
+    currency_network.update_trustline_with_accept(accounts[0], accounts[2], 25, 50)
+    currency_network.update_trustline_with_accept(accounts[0], accounts[4], 25, 50)
+
+    return currency_network
+
+
+@pytest.fixture()
+def address_oracle(testnetworks):
+    class AddressOracle:
+        def is_currency_network(self, address):
+            return address in [network.address for network in testnetworks[0]]
+
+        def is_trusted_token(self, address):
+            print(address)
+            print(testnetworks[2].address)
+            return address == testnetworks[2].address
+
+    return AddressOracle()
