bf95eb076c8bd16bc0062eb7627e832c845149c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:49 +0000

    Add package kafka-splunk-connector/1.0.1/kafka_splunk_connector-1.0.1-py2-none-any.whl

---
+++ packages/kafka-splunk-connector/kafka_splunk_connector-1.0.1-py2-none-any.whl/kafka_splunk_connector/__init__.py
@@ -0,0 +1,45 @@
+from kafka import KafkaConsumer
+import requests
+import json
+import logging
+from splunk_hec_handler import SplunkHecHandler
+
+class KafkaSplunkConnector():
+    def __init__(self, splunk_data={}, kafka_data={}):
+	#Splunk data
+        self.splunk_host = splunk_data['host'] if 'host' in splunk_data.keys() else 'localhost'
+        self.splunk_token = splunk_data['token'] if 'token' in splunk_data.keys() else ''
+        self.splunk_port = splunk_data['port'] if 'port' in splunk_data.keys() else 8088
+        self.splunk_proto = splunk_data['proto'] if 'proto' in splunk_data.keys() else 'http'
+        self.splunk_source = splunk_data['source'] if 'source' in splunk_data.keys() else ''
+	self.splunk_ssl_verify = splunk_data['ssl_verify'] if 'ssl_verify' in splunk_data.keys() else False
+
+	#Kafka data
+	self.kafka_topic = kafka_data['topic'] 
+	self.kafka_brokers = kafka_data['brokers']
+	self.kafka_auto_offset_reset = kafka_data['auto_offset_reset'] if 'auto_offset_reset' in kafka_data.keys() else 'earliest'
+	self.kafka_enable_autocommit = kafka_data['auto_commit'] if 'auto_commit' in kafka_data.keys() else True
+    
+    def stream_to_splunk(self):
+        logger = logging.getLogger(self.splunk_source)
+	logger.setLevel(logging.INFO)
+	splunk_handler = SplunkHecHandler(self.splunk_host, self.splunk_token, port=self.splunk_port, proto=self.splunk_proto, ssl_verify = self.splunk_ssl_verify, source=self.splunk_source)
+	logger.addHandler(splunk_handler)
+	consumer = KafkaConsumer(self.kafka_topic, bootstrap_servers = self.kafka_brokers, auto_offset_reset = self.kafka_auto_offset_reset, enable_auto_commit = self.kafka_enable_autocommit)
+	for msg in consumer:
+	    if msg.value:
+                data = {"fields":{}}
+	        try:
+	            msg_value = json.loads(msg.value)
+	        except:
+	            msg_value = msg.value
+	        if type(msg_value) == dict:
+	            for k,v in msg_value.items():
+                        data["fields"].update({str(k):v})
+	        else:
+		    data = msg_value
+	        logger.info(data)
+	    else:
+	        break
+
+	    
