5b9567ab14581d809e7c3dcc342939468f129472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:23 +0000

    Add package ubot/3.3.1/ubot-3.3.1.tar.gz

---
+++ packages/ubot/ubot-3.3.1.tar.gz/ubot-3.3.1/ubot/endpoints/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidInlineKeyboardButtonType(Exception):
+    pass
+
+
+class InvalidInlineKeyboardButtonPosition(Exception):
+    pass
+
+
+class InvalidChatAction(Exception):
+    pass
+
+
+class InvalidParseMode(Exception):
+    pass
