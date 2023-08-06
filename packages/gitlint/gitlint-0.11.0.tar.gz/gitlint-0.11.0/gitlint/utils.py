b46a19458262b18cc6eda4b2056a3a35fafa1783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:18 +0000

    Add package gitlint/0.11.0/gitlint-0.11.0.tar.gz

---
+++ packages/gitlint/gitlint-0.11.0.tar.gz/gitlint-0.11.0/gitlint/utils.py
@@ -0,0 +1,33 @@
+# pylint: disable=bad-option-value,unidiomatic-typecheck,undefined-variable,no-else-return
+import sys
+
+from locale import getpreferredencoding
+
+DEFAULT_ENCODING = getpreferredencoding() or "UTF-8"
+LOG_FORMAT = '%(levelname)s: %(name)s %(message)s'
+
+
+def ustr(obj):
+    """ Python 2 and 3 utility method that converts an obj to unicode in python 2 and to a str object in python 3"""
+    if sys.version_info[0] == 2:
+        # If we are getting a string, then do an explicit decode
+        # else, just call the unicode method of the object
+        if type(obj) in [str, basestring]:  # pragma: no cover # noqa
+            return unicode(obj, DEFAULT_ENCODING)  # pragma: no cover # noqa
+        else:
+            return unicode(obj)  # pragma: no cover # noqa
+    else:
+        if type(obj) in [bytes]:
+            return obj.decode(DEFAULT_ENCODING)
+        else:
+            return str(obj)
+
+
+def sstr(obj):
+    """ Python 2 and 3 utility method that converts an obj to a DEFAULT_ENCODING encoded string in python 2
+    and to unicode in python 3.
+    Especially useful for implementing __str__ methods in python 2: http://stackoverflow.com/a/1307210/381010"""
+    if sys.version_info[0] == 2:
+        return unicode(obj).encode(DEFAULT_ENCODING)  # pragma: no cover # noqa
+    else:
+        return obj  # pragma: no cover
