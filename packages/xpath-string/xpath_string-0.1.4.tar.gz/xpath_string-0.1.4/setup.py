737a0dbca2da9ec5e6eec53070bfa3b59feffac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package xpath-string/0.1.4/xpath_string-0.1.4.tar.gz

---
+++ packages/xpath-string/xpath_string-0.1.4.tar.gz/xpath_string-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='0.1.4',
+    name='xpath_string',
+    description='Module which allows to make xpath operations over string which represent xpath',
+    author='Tomasz Jaworski',
+    author_email='jaworski.tomasz.91@gmail.com',
+    packages=find_packages(),
+    url="https://github.com/yavorek/xpath_string",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
