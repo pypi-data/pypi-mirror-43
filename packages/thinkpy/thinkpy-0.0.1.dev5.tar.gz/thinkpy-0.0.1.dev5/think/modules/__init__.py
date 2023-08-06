9c220bc13e9871d704fb7526d1040ca711b486af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package thinkpy/0.0.1.dev5/thinkpy-0.0.1.dev5.tar.gz

---
+++ packages/thinkpy/thinkpy-0.0.1.dev5.tar.gz/thinkpy-0.0.1.dev5/think/modules/__init__.py
@@ -0,0 +1,12 @@
+from .eyes import Eyes
+from .eye_tracker import Dwell, Fixation, Gaze, EyeTracker
+from .hands import Hands
+from .machine import Display, Keyboard
+from .memory import Chunk, Memory
+from .mouse import Mouse
+from .speech import Speech
+from .typing import Typing
+from .vision import Visual, Vision
+from .audition import Aural, Audition
+from .instruction import Instruction
+from .language import Language
