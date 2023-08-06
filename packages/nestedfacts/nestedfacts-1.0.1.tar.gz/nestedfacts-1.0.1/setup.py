27934ac4b0d56ccef2a0ac523710a84f5018ffc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:43 +0000

    Add package nestedfacts/1.0.1/nestedfacts-1.0.1.tar.gz

---
+++ packages/nestedfacts/nestedfacts-1.0.1.tar.gz/nestedfacts-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(name='nestedfacts',
+      description='This library enables users of Ansible to load nested'
+                  'directories as local facts. Refer to the README for'
+                  'more details.',
+      author='uberspace.de',
+      author_email='hallo@uberspace.de',
+      url='https://github.com/uberspace/nestedfacts',
+      version='1.0.1',
+      license='MIT',
+      packages=['.'],
+      zip_safe=True,
+      install_requires=[
+            'pyyaml',
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Utilities',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+      ],
+)
