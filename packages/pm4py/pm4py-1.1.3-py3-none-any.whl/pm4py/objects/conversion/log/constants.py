74a3dd5b8f6aa2592124b0a86bc5f1fe719a6d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package pm4py/1.1.3/pm4py-1.1.3-py3-none-any.whl

---
+++ packages/pm4py/pm4py-1.1.3-py3-none-any.whl/pm4py/objects/conversion/log/constants.py
@@ -0,0 +1,7 @@
+TO_TRACE_LOG = 'to_event_log'
+TO_EVENT_LOG = 'to_event_log'
+TO_EVENT_STREAM = 'to_event_stream'
+TO_DATAFRAME = 'to_dataframe'
+FROM_DATAFRAME = 'from_dataframe'
+
+DEEPCOPY = "deepcopy"
