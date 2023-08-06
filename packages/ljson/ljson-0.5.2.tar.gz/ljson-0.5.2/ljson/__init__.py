930c3d0abc7844586e877a1724fde00f95b9fb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:20 +0000

    Add package ljson/0.5.2/ljson-0.5.2.tar.gz

---
+++ packages/ljson/ljson-0.5.2.tar.gz/ljson-0.5.2/ljson/__init__.py
@@ -0,0 +1,17 @@
+"""
+The python3 module to access ljson files.
+
+``ljson.base.mem.Table`` and ``ljson.base.generic.Header``
+are imported automatically.
+
+You should see the ``ljson.base`` documentation for
+some info about Table objects.
+
+"""
+
+
+__all__ = ["base"]
+
+
+from .base.mem import Table
+from .base.generic import Header
