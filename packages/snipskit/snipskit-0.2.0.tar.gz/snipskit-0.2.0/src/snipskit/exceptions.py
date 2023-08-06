04bedec022eb8f9f1f0735fb41081c0d5912f829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:54 +0000

    Add package snipskit/0.2.0/snipskit-0.2.0.tar.gz

---
+++ packages/snipskit/snipskit-0.2.0.tar.gz/snipskit-0.2.0/src/snipskit/exceptions.py
@@ -0,0 +1,21 @@
+"""This module contains exceptions defined for the SnipsKit library."""
+
+
+class SnipsKitError(Exception):
+    """Base class for exceptions raised by SnipsKit code.
+
+    By catching this exception type, you catch all exceptions that are
+    defined by the SnipsKit library."""
+    pass
+
+
+class AssistantConfigNotFoundError(SnipsKitError):
+    """Raised when the assistant's configuration is not found in the search
+    path.
+    """
+    pass
+
+
+class SnipsConfigNotFoundError(SnipsKitError):
+    """Raised when there's no snips.toml found in the search path."""
+    pass
