e94e39af17910d903baa6f00a0c827de4cfae9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:46 +0000

    Add package py4ceedlib/0.4.23/py4ceedlib-0.4.23.tar.gz

---
+++ packages/py4ceedlib/py4ceedlib-0.4.23.tar.gz/py4ceedlib-0.4.23/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='py4ceedlib',  # This is the name of your PyPI-package.
+    version='0.4.23',  # Update the version number for new releases
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.png'],
+    },
+    packages=setuptools.find_packages()
+    #scripts=['py4ceed']  # The name of your scipt, and also the command you'll be using for calling it
+)
