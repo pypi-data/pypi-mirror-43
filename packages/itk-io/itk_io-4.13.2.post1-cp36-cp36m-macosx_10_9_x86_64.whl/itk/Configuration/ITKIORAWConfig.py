109c09acd05e37c5fc47fa98b460b569f1eabf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:47 +0000

    Add package itk-io/4.13.2.post1/itk_io-4.13.2.post1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/itk-io/itk_io-4.13.2.post1-cp36-cp36m-macosx_10_9_x86_64.whl/itk/Configuration/ITKIORAWConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('RawImageIO', 'itk::RawImageIO', 'itkRawImageIOF2', True, 'float,2'),
+  ('RawImageIO', 'itk::RawImageIO', 'itkRawImageIOF3', True, 'float,3'),
+)
