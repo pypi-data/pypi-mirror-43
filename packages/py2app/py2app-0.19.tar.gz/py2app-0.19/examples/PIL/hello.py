4aebf19f0a9428fe0d1074f180d33caecb9db9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package py2app/0.19/py2app-0.19.tar.gz

---
+++ packages/py2app/py2app-0.19.tar.gz/py2app-0.19/examples/PIL/hello.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import sys, os
+import pprint
+print("sys.path", sys.path)
+import PIL
+print("x")
+import PIL.Image
+
+PATH="/System/Library//Automator/Crop Images.action/Contents/Resources/shark-tall-no-scale.jpg"
+
+print("globals() is %r" % id(globals()))
+def somefunc():
+    print("globals() is %r" % id(globals()))
+    print("Hello from py2app")
+
+    print("frozen", repr(getattr(sys, "frozen", None)))
+
+    print("sys.path", sys.path)
+    print("sys.executable", sys.executable)
+    print("sys.prefix", sys.prefix)
+    print("sys.argv", sys.argv)
+    print("os.getcwd()", os.getcwd())
+
+
+if __name__ == '__main__':
+    somefunc()
+    img = PIL.Image.open(PATH)
+    print(type(img))
+    print(img.size)
+    print(PIL.Image.__file__)
---
+++ packages/py2app/py2app-0.19.tar.gz/py2app-0.19/examples/PyOpenSSL/README.txt
@@ -0,0 +1,1 @@
+https://github.com/msabramo/pyOpenSSL/tree/master/examples/sni
