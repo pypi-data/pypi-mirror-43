488dd90a7fc8f3b815e68cde76011ab4b25ebfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:07 +0000

    Add package django-node-assets/0.9.4/django-node-assets-0.9.4.tar.gz

---
+++ packages/django-node-assets/django-node-assets-0.9.4.tar.gz/django-node-assets-0.9.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-node-assets',
+    version='0.9.4',
+    description='The Django application that allows install and serve assets via Node.js package '
+                'manager infrastructure.',
+    author='Andrey Butenko',
+    author_email='whitespysoftware@gmail.ru',
+    url='https://github.com/whitespy/django-node-assets',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    keywords='django assets staticfiles Node.js npm',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
