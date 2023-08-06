631ab59cfd969ee4edc68ba6b9a47d6cfc0e83d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:09 +0000

    Add package minus80/0.3.0/minus80-0.3.0.tar.gz

---
+++ packages/minus80/minus80-0.3.0.tar.gz/minus80-0.3.0/minus80/CloudData.py
@@ -0,0 +1,23 @@
+
+def CloudData(engine='gcp'):
+    if engine == 's3':
+        from .S3CloudData import S3CloudData
+        return S3CloudData()
+    elif engine == 'gcp':
+        from .GCPCloudData import GCPCloudData
+        return GCPCloudData()
+    else:
+        raise ValueError(f'Cannot use {engine} as a cloud engine.')
+
+class BaseCloudData(object): #pragma: no cover
+    def __init__(self):
+        pass
+
+    def push(self, name, dtype, raw=False, compress=False):
+        raise NotImplementedError('This engine does not support pushing')
+
+    def pull(self, name, dtype, raw=False):
+        raise NotImplementedError('This engine does not support pulling')
+
+    def list(self, name=None, dtype=None, raw=None):
+        raise NotImplementedError('This engine does not support listing')
