98d0e13a74060d58550b6131e129b79b02fc8b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:48 +0000

    Add package itk-io/4.13.2.post1/itk_io-4.13.2.post1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/itk-io/itk_io-4.13.2.post1-cp36-cp36m-manylinux1_x86_64.whl/itk/Configuration/ITKIOTransformBaseConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKTransform', 'ITKDisplacementField', 'ITKCommon', )
+templates = (
+  ('TransformFileReaderTemplate', 'itk::TransformFileReaderTemplate', 'itkTransformFileReaderTemplateF', False, 'float'),
+  ('TransformFileWriterTemplate', 'itk::TransformFileWriterTemplate', 'itkTransformFileWriterTemplateF', False, 'float'),
+  ('TransformIOBaseTemplate', 'itk::TransformIOBaseTemplate', 'itkTransformIOBaseTemplateF', False, 'float'),
+)
