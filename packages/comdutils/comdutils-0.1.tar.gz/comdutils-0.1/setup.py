b602157054bc51e6f62a67101acf22ecc4d32f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:37 +0000

    Add package comdutils/0.1/comdutils-0.1.tar.gz

---
+++ packages/comdutils/comdutils-0.1.tar.gz/comdutils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comdutils',  # Required
+    version='v0.1',  # Required
+    description='a simple package for computer vision deployment',  # Required
+    long_description="a simple package for computer vision deployment",  # Optional
+    author='Chur Chur',  # Optional
+    packages=["comdutils"],  # Required
+)
