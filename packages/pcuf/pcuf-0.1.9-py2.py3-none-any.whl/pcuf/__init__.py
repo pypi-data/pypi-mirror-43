84e41ef6f2a8d05ede05196e096a6880a2ded68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:18 +0000

    Add package pcuf/0.1.9/pcuf-0.1.9-py2.py3-none-any.whl

---
+++ packages/pcuf/pcuf-0.1.9-py2.py3-none-any.whl/pcuf/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from . import file, functions, time, warnings
+
+from .containers import lookahead, assign_key, haskey, grouper
+
+__author__ = """Alberto J. Marin"""
+__email__ = "alberto@ajmar.in"
+__version__ = "0.1.9"
+
+__all__ = [
+    "file",
+    "functions",
+    "time",
+    "warnings",
+    "lookahead",
+    "assign_key",
+    "haskey",
+    "grouper",
+]
