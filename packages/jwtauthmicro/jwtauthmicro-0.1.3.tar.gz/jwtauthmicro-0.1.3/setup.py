ab9637e00d7d38460cc0712c9ceaeb67a942df4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:22 +0000

    Add package jwtauthmicro/0.1.3/jwtauthmicro-0.1.3.tar.gz

---
+++ packages/jwtauthmicro/jwtauthmicro-0.1.3.tar.gz/jwtauthmicro-0.1.3/setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='jwtauthmicro',
+    version='0.1.3',
+    description='Django microservice JWT authentication',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    author='ivan',
+    url='https://github.com/goupper/jwt-auth-micro',
+    author_email='chongwuwy@163.com',
+    license='MIT',
+    packages=find_packages('jwt_auth'),
+    include_package_data=False,
+    zip_safe=True,
+)
