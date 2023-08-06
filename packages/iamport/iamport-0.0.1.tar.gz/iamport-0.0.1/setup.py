d246c4ffab069a58c68a69e314045d1012f3561a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:04 +0000

    Add package iamport/0.0.1/iamport-0.0.1.tar.gz

---
+++ packages/iamport/iamport-0.0.1.tar.gz/iamport-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="iamport",
+    version="0.0.1",
+    author="styleshare",
+    author_email="dev@styleshare.kr",
+    description="Iamport payment client library",
+    url="https://github.com/styleshare/iamport",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ]
+)
