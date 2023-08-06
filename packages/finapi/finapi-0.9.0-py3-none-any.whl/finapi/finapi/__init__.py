e133d16a729fc02c8bfef59b7b2afce572ca41dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:27 +0000

    Add package finapi/0.9.0/finapi-0.9.0-py3-none-any.whl

---
+++ packages/finapi/finapi-0.9.0-py3-none-any.whl/finapi/finapi/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from finapi.accounts_api import AccountsApi
+from finapi.authorization_api import AuthorizationApi
+from finapi.bank_connections_api import BankConnectionsApi
+from finapi.banks_api import BanksApi
+from finapi.categories_api import CategoriesApi
+from finapi.client_configuration_api import ClientConfigurationApi
+from finapi.labels_api import LabelsApi
+from finapi.mandator_administration_api import MandatorAdministrationApi
+from finapi.mocks_and_tests_api import MocksAndTestsApi
+from finapi.notification_rules_api import NotificationRulesApi
+from finapi.payments_api import PaymentsApi
+from finapi.securities_api import SecuritiesApi
+from finapi.transactions_api import TransactionsApi
+from finapi.users_api import UsersApi
+from finapi.web_forms_api import WebFormsApi
