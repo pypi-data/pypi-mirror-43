887c6d8e51e8b5bf61f8283adb031fd02069fb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package pyteomics/4.1/pyteomics-4.1.tar.gz

---
+++ packages/pyteomics/pyteomics-4.1.tar.gz/pyteomics-4.1/tests/test_pepxml.py
@@ -0,0 +1,27 @@
+from os import path
+import pyteomics
+pyteomics.__path__ = [path.abspath(path.join(path.dirname(__file__), path.pardir, 'pyteomics'))]
+from itertools import product
+import unittest
+from pyteomics.pepxml import PepXML, read, chain, filter
+from data import pepxml_results
+
+class PepxmlTest(unittest.TestCase):
+    maxDiff = None
+    _kw = {'full_output': False, 'fdr': 1,
+        'key': lambda x: min(
+            sh['search_score'].get('expect', 1)
+            for sh in x['search_hit'])
+        }
+    def testReadPSM(self):
+        for rs, it in product([True, False], repeat=2):
+            for func in [PepXML, read, chain,
+                    lambda x, **kw: chain.from_iterable([x], **kw),
+                    lambda x, **kw: filter(x, **PepxmlTest._kw),
+                    lambda x, **kw: filter.chain(x, **PepxmlTest._kw),
+                    lambda x, **kw: filter.chain.from_iterable([x], **PepxmlTest._kw)]:
+                with func('test.pep.xml', read_schema=rs, iterative=it) as r:
+                    self.assertEqual(list(r), pepxml_results)
+
+if __name__ == '__main__':
+    unittest.main()
