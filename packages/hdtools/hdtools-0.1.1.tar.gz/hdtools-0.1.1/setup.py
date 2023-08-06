683183898edd7e4443ea50c29c2c320827de5338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:14 +0000

    Add package hdtools/0.1.1/hdtools-0.1.1.tar.gz

---
+++ packages/hdtools/hdtools-0.1.1.tar.gz/hdtools-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='hdtools',
+    version='0.1.1',
+    description='hd-tools for bitcoin and bitcoin-test',
+    author='Mahdi Fooladgar',
+    author_email='fooladgar@morvarid.io',
+    url='https://github.com/morvaridio/hdtools',
+    license='MIT',
+    packages=[
+        'hdtools',
+    ]
+)
