4e6642f7534bc77f25407fe175d23c6ad3487270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:46 +0000

    Add package zxinglight/1.1.0/zxinglight-1.1.0.tar.gz

---
+++ packages/zxinglight/zxinglight-1.1.0.tar.gz/zxinglight-1.1.0/README.rst
@@ -0,0 +1,22 @@
+ZXingLight
+==========
+
+|build-status| |docs-status| |pypi-package|
+
+A simple wrapper for `ZXing C++`_ inspired by zbarlight_.
+
+Documentation is available on <https://zxinglight.readthedocs.io/en/latest/>.
+
+.. _ZXing C++: https://github.com/glassechidna/zxing-cpp
+.. _zbarlight: https://github.com/Polyconseil/zbarlight
+
+
+.. |build-status| image:: https://travis-ci.org/lubo/zxinglight.svg?branch=master
+    :alt: Build status
+    :target: https://travis-ci.org/lubo/zxinglight
+.. |docs-status| image:: https://readthedocs.org/projects/zxinglight/badge/?version=latest
+    :alt: Documentation status
+    :target: https://zxinglight.readthedocs.io/en/latest/
+.. |pypi-package| image:: https://badge.fury.io/py/zxinglight.svg
+    :alt: PyPI package
+    :target: https://badge.fury.io/py/zxinglight
---
+++ packages/zxinglight/zxinglight-1.1.0.tar.gz/zxinglight-1.1.0/setup.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+ext_libraries = [
+    'zxing'
+]
+
+if sys.platform == 'darwin':
+    ext_libraries += [
+        'iconv'
+    ]
+
+setup(
+    name='zxinglight',
+    version='1.1.0',
+    description='A simple ZXing C++ wrapper',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+    ],
+    keywords=[
+        'zxing',
+        'barcode',
+        'QR code'
+    ],
+    author='Ľubomír Kučera',
+    author_email='lubomir.kucera.jr@gmail.com',
+    url='https://github.com/lubo/zxinglight',
+    license='MIT',
+    packages=find_packages(exclude=[
+        'tests'
+    ]),
+    ext_modules=[
+        Extension(
+            name='zxinglight._zxinglight',
+            language='c++',
+            sources=[
+                'zxinglight/_zxinglight.cpp'
+            ],
+            extra_compile_args=[
+                '-std=c++11'
+            ],
+            libraries=ext_libraries,
+        ),
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[
+        'setuptools',
+    ],
+    install_requires=[
+        'Pillow'
+    ],
+    extras_require={
+        'docs': [
+            'Sphinx',
+            'sphinx-autobuild',
+            'sphinx_rtd_theme'
+        ],
+        'test': [
+            'flake8',
+            'flake8-bugbear',
+            'nose',
+            'pep8-naming'
+        ]
+    }
+)
