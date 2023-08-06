bad1c34c3e062d242f804dc4060662d67bf37feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:17 +0000

    Add package mimic-text/0.0.1/mimic-text-0.0.1.tar.gz

---
+++ packages/mimic-text/mimic-text-0.0.1.tar.gz/mimic-text-0.0.1/setup.py
@@ -0,0 +1,33 @@
+"""Setup file for to configure distribution upload to PyPI."""
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='mimic-text',
+    version='0.0.1',
+    packages=['mimic', 'mimic.model', 'mimic.tests'],
+    url='https://github.com/ubclaunchpad/mimic',
+    license='MIT',
+    author='ubclaunchpad',
+    author_email='',
+    description='ML-powered Text Generation Library',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    include_package_data=True
+)
+
+# How to upload to pypi:
+# Run these commands in terminal:
+#   python setup.py sdist bdist_wheel
+#   tar tzf dist/mimic-text-0.0.1.tar.gz    # Checks to see if distribution file made
+#   twine check dist/*                      # Check if distribution can be uploaded
+#   twine upload dist/*                     # Uploads to PyPI
+
+
+
