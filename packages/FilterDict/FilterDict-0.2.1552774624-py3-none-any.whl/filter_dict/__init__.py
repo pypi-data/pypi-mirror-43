7028761994f86124ce5b9675b60db7ce08168143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package FilterDict/0.2.1552774624/FilterDict-0.2.1552774624-py3-none-any.whl

---
+++ packages/FilterDict/FilterDict-0.2.1552774624-py3-none-any.whl/filter_dict/__init__.py
@@ -0,0 +1,6 @@
+import inspect
+def filter_dict(dict_to_filter, thing_with_kwargs):
+    sig = inspect.signature(thing_with_kwargs)
+    filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD]
+    filtered_dict = {filter_key: dict_to_filter[filter_key] for filter_key in filter_keys if filter_key in dict_to_filter}
+    return filtered_dict
