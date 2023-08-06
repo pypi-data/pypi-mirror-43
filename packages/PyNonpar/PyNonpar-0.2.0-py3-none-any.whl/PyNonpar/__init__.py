acc38a25fa72f40d60450eb333391b4e9d9d805c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:28 +0000

    Add package PyNonpar/0.2.0/PyNonpar-0.2.0-py3-none-any.whl

---
+++ packages/PyNonpar/PyNonpar-0.2.0-py3-none-any.whl/PyNonpar/__init__.py
@@ -0,0 +1,7 @@
+""" PyNonpar: Nonparametric Statistics in Python.
+
+.. moduleauthor:: Martin Happ <martin.happ@aon.at>
+
+"""
+
+__all__ = ['pseudorank', 'multisample', 'twosample', 'twosample_paired', 'repeated_measures']
