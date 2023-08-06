30729e9ba447b6b5b292b590a1f2591e8c43c8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:24 +0000

    Add package pmast/0.1/pmast-0.1.tar.gz

---
+++ packages/pmast/pmast-0.1.tar.gz/pmast-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='pmast',
+    version='0.1',
+    description='Pattern Matching for the Python AST',
+    url='https://github.com/campadrenalin/pmast',
+    author='Philip Horger',
+    author_email='campadrenalin@gmail.com',
+    packages=['pmast'],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=True,
+)
