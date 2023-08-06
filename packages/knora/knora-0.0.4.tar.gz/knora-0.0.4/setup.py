571320370145f42042745dcc0077400ee3b342d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:40 +0000

    Add package knora/0.0.4/knora-0.0.4.tar.gz

---
+++ packages/knora/knora-0.0.4.tar.gz/knora-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='knora',
+    version='0.0.4',
+    description='A Python library and tools for the Knora-API',
+    url='https://github.com/dhlab-basel/knora-py',
+    author='Lukas Rosenthaler',
+    author_email='lukas.rosenthaler@unibas.ch',
+    license='GPLv3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'rdflib',
+        'lxml',
+        'validators'
+    ],
+    entry_points={
+          'console_scripts': ['knora-create-ontology=knora.create_ontology:main'],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
