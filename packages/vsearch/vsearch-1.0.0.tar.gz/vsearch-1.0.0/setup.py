dd0e1ceda6f7098230ca3519e2e119feef3c8e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:32 +0000

    Add package vsearch/1.0.0/vsearch-1.0.0.tar.gz

---
+++ packages/vsearch/vsearch-1.0.0.tar.gz/vsearch-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0.0',
+    description='The Head First Python Search Tools',
+    author='Zehthijs',
+    author_email    = 'hfpython@headfirstlabs.com',
+    url             = 'http://www.headfirstlabs.com',
+    py_modules=['vsearchZT'],
+)
