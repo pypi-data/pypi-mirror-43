423e3ed604857b633a2fbf295e51cde7964dbcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:39 +0000

    Add package the-refinery-toolkit/0.6.55/the_refinery_toolkit-0.6.55.tar.gz

---
+++ packages/the-refinery-toolkit/the_refinery_toolkit-0.6.55.tar.gz/the_refinery_toolkit-0.6.55/rftk/cloud_functions/services/mobile_friendly_service.py
@@ -0,0 +1,80 @@
+"""
+mobile_friendly_service.py: Utility functions used by the service that
+                            checks whether a site is mobile-friendly.
+"""
+__author__ = "Jason Wolosonovich <jason@avaland.io>"
+__license__ = "BSD 3 clause"
+import time
+
+import requests
+
+from .crawler_service import HEADERS
+
+
+# TODO: this needs more thorough investigation/testing to see how/why
+# we keep getting 502 errors
+def check_mobile_friendly(request=None):
+    """Utility function to test whether the supplied domain is mobile
+    friendly."""
+    print("check_mobile_friendly()")
+    print("url: {}".format(request["url"]))
+    # pause to avoid being rate-limited
+    time.sleep(1)
+    json_data = {"url": request["url"]}
+    api_url = \
+        "https://searchconsole.googleapis.com/v1/urlTestingTools" \
+        "/mobileFriendlyTest:run?key" \
+        "=AIzaSyCl4UW5RRovdGSf0xyjlwMpJ754n4_CcvM"
+    print("sending request for: {}".format(request["url"]))
+    success = False
+    i = 0
+    while success is False and i < 5:
+        try:
+            resp = requests.post(
+                url=api_url,
+                headers=HEADERS,
+                json=json_data
+            )
+            resp.raise_for_status()
+
+            print("response: {}".format(resp))
+            success = True
+            continue
+        except requests.exceptions.HTTPError as e:
+            print("attempt {} failed.".format(i + 1))
+            i += 1
+            resp = str(e.response.status_code)
+            continue
+
+    if success is True:
+        resp_ = {
+            "refinery_company_id": request["refinery_company_id"],
+            "refined_at": request["refined_at"],
+            "refined_date": request["refined_date"],
+            "domain": request["domain"],
+            "url": request["url"],
+            "ip_revealed": request["ip_revealed"],
+            "fuzzy_match": request["fuzzy_match"]
+        }
+
+    # we never were able to successfully test the url
+    if success is False:
+        resp_ = None
+
+    else:
+        # tests passed successfully
+        if resp.json()["testStatus"]["status"] == "COMPLETE":
+            resp_["test_results"] = resp.json()[
+                "mobileFriendliness"].lower()
+
+        # even errors seem to return a 200 status code?
+        elif resp.json()["testStatus"]["status"] != "COMPLETE":
+            if "details" in resp.json()["testStatus"].keys():
+                resp_["test_results"] = \
+                    resp.json()["testStatus"]["status"] + \
+                    " : " + \
+                    resp.json()["testStatus"]["details"]
+            else:
+                resp_["test_results"] = \
+                    resp.json()["testStatus"]["status"]
+    return resp_
---
+++ packages/the-refinery-toolkit/the_refinery_toolkit-0.6.55.tar.gz/the_refinery_toolkit-0.6.55/rftk/cloud_functions/services/wordpress_lookup_service.py
@@ -0,0 +1,87 @@
+"""
+wordpress_lookup_service.py: A collection of utilities for the wordpress plugin
+lookup service.
+"""
+__author__ = "Jason Wolosonovich <jason@avaland.io>"
+__license__ = "BSD 3 Clause"
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+from .crawler_service import HEADERS
+
+
+def get_wp_plugin_info_online(plugin=None):
+    """Takes a plugin name and returns the description, name and
+    tagshttps://www.silvabokis.com/squarespace-tips/how-to-find-out-which-squarespace-template-a-site-is-using for the plugin."""
+    print("get_wp_plugin_info_online()")
+    plugin_url = "http://plugins.svn.wordpress.org/{}/trunk/" \
+        .format(plugin)
+    try:
+        r = requests.get(url=plugin_url,
+                         headers=HEADERS)
+    except requests.exceptions.HTTPError as e:
+        print(e)
+
+    if r.status_code != 200:
+        description = None
+        tags = list()
+        name = None
+    else:
+        # check for the text file as they're potentially named
+        # differently
+        soup = BeautifulSoup(r.content,
+                             "html.parser")
+
+        # find the link with .txt and follow it
+        txt_href = soup.select_one("a[href*=.txt]")
+
+        if txt_href is None:
+            return list(), None, None
+        else:
+            txt_href = txt_href.text
+
+        # now visit that and parse the description
+        try:
+            readme_url = plugin_url + txt_href
+            r = requests.get(url=readme_url,
+                             headers=HEADERS)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+
+        if r.status_code != 200:
+            description = None
+            tags = list()
+            name = None
+        else:
+            name_re = r"(?m)^===\s*(.+?)\s*="
+            tags_re = r"(?m)^Tags:\s*(.+?)\r?$"
+            d_re = r"(?ms)^== Description ==\s*(.+?)\s+^="
+            try:
+                name = re.search(name_re,
+                                 r.text).group(1)
+                name = name.replace("\r", " ").replace("\n", " ")
+            except AttributeError as e:
+                name = None
+            try:
+                tags = re.search(tags_re,
+                                 r.text)\
+                .group(1)\
+                .split(",")
+                tags = [tag.replace("\r", "").replace("\n", "")
+                        for tag in tags]
+            except AttributeError as e:
+                tags = list()
+            try:
+                description = re.search(d_re,
+                                        r.text).group(1)
+
+                description = description.replace("\r", " ").replace(
+                    "\n", " ")
+            except AttributeError as e:
+                description = None
+    print("tags: {}".format(tags))
+    print("description: {}".format(description))
+    print("name: {}".format(name))
+    return tags, description, name
