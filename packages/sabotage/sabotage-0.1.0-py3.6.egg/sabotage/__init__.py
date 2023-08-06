7d6b70d201c49416e1a4e2f1a0f629698116a958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:28 +0000

    Add package sabotage/0.1.0/sabotage-0.1.0-py3.6.egg

---
+++ packages/sabotage/sabotage-0.1.0-py3.6.egg/sabotage/__init__.py
@@ -0,0 +1,8 @@
+from ._disconnected_network import DisconnectedNetwork
+
+
+disconnected_network = DisconnectedNetwork
+
+
+__all__ = ("disconnected_network", "DisconnectedNetwork")
+__version__ = "0.1.0"
