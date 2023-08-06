388c355c42808afcb60c6007dec50d2a744fbde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:29 +0000

    Add package os-aio-pod/0.1.0/os-aio-pod-0.1.0.tar.gz

---
+++ packages/os-aio-pod/os-aio-pod-0.1.0.tar.gz/os-aio-pod-0.1.0/src/os_aio_pod/contrib/tcp_server.py
@@ -0,0 +1,92 @@
+import asyncio
+import logging
+from multiprocessing import Process
+from socket import SO_REUSEADDR, SOL_SOCKET, socket
+from typing import Union
+
+from pydantic import BaseModel, Schema, validator
+
+from os_aio_pod.utils import model_from_string
+
+
+class Server(object):
+
+    def __init__(self, context, config):
+        self.context = context
+        self.config = config
+
+    async def on_signal(self, sig):
+        pass
+
+    async def on_setup(self):
+        pass
+
+    async def on_start(self):
+        pass
+
+    async def on_stop(self):
+        pass
+
+    async def on_cleanup(self):
+        pass
+
+
+class Config(BaseModel):
+
+    host: str = '127.0.0.1'
+    port: int = 9399
+    protocol: model_from_string(asyncio.Protocol)
+    backlog: int = 100
+    server: model_from_string(Server) = Schema(Server, validate_always=True)
+
+    class Config:
+        allow_extra = True
+
+
+class TCPServerAdapter(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    async def __call__(self, **kwargs):
+        logger = logging.getLogger(self.__class__.__name__)
+        config = Config(**kwargs)
+        loop = self.context.loop
+
+        tcp_server = config.server(self.context, config)
+        config.protocol.server = tcp_server
+
+        factory = loop.create_server(
+            config.protocol,
+            config.host,
+            config.port,
+            backlog=config.backlog,
+        )
+
+        logger.debug(f'Starting tcp server on {config.host}:{config.port}')
+
+        await tcp_server.on_setup()
+        server = await factory
+        await tcp_server.on_start()
+
+        stop_event = asyncio.Event(loop=loop)
+        stoping_lock = asyncio.Lock(loop=loop)
+
+        async def on_signal(**kwargs):
+            for sig in kwargs['keys']:
+                await tcp_server.on_signal(sig)
+
+            await stoping_lock.acquire()
+            if not stop_event.is_set():
+                await tcp_server.on_stop()
+                server.close()
+                await server.wait_closed()
+                stop_event.set()
+            stoping_lock.release()
+
+        for sig in ('SIGINT', 'SIGTERM'):
+            await self.context.add_signal_handler(sig, on_signal)
+
+        await stop_event.wait()
+        await tcp_server.on_cleanup()
+        logger.debug(f'TCP server stopped')
