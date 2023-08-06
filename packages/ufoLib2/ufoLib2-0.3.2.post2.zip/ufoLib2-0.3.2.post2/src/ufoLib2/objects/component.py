ea234e9646380d0a1ad33d4df44972a83453d7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package ufoLib2/0.3.2.post2/ufoLib2-0.3.2.post2.zip

---
+++ packages/ufoLib2/ufoLib2-0.3.2.post2.zip/ufoLib2-0.3.2.post2/src/ufoLib2/objects/component.py
@@ -0,0 +1,37 @@
+import attr
+from typing import Optional
+from fontTools.misc.transform import Identity, Transform
+from fontTools.pens.pointPen import PointToSegmentPen
+import warnings
+
+
+@attr.s(slots=True)
+class Component(object):
+    baseGlyph = attr.ib(type=str)
+    transformation = attr.ib(
+        default=Identity,
+        converter=lambda t: t if isinstance(t, Transform) else Transform(*t),
+        type=Transform,
+    )
+    identifier = attr.ib(default=None, type=Optional[str])
+
+    # -----------
+    # Pen methods
+    # -----------
+
+    def draw(self, pen):
+        pointPen = PointToSegmentPen(pen)
+        self.drawPoints(pointPen)
+
+    def drawPoints(self, pointPen):
+        try:
+            pointPen.addComponent(
+                self.baseGlyph, self.transformation, identifier=self.identifier
+            )
+        except TypeError:
+            pointPen.addComponent(self.baseGlyph, self.transformation)
+            warnings.warn(
+                "The addComponent method needs an identifier kwarg. "
+                "The component's identifier value has been discarded.",
+                UserWarning,
+            )
---
+++ packages/ufoLib2/ufoLib2-0.3.2.post2.zip/ufoLib2-0.3.2.post2/src/ufoLib2/objects/image.py
@@ -0,0 +1,62 @@
+import attr
+from typing import Optional
+from fontTools.misc.transform import Identity, Transform
+
+try:
+    from collections.abc import Mapping  # python >= 3.3
+except ImportError:
+    from collections import Mapping
+
+
+@attr.s(slots=True)
+class Image(Mapping):
+    fileName = attr.ib(default=None, type=Optional[str])
+    transformation = attr.ib(
+        default=Identity,
+        converter=lambda t: t if isinstance(t, Transform) else Transform(*t),
+        type=Transform,
+    )
+    color = attr.ib(default=None, type=Optional[str])
+
+    def clear(self):
+        self.fileName = None
+        self.transformation = Identity
+        self.color = None
+
+    def __bool__(self):
+        # Glyph.image evaluates to False if no fileName is set
+        return self.fileName is not None
+
+    # alias for python 2
+    __nonzero__ = __bool__
+
+    _transformation_keys_ = (
+        "xScale",
+        "xyScale",
+        "yxScale",
+        "yScale",
+        "xOffset",
+        "yOffset",
+    )
+    _valid_keys_ = ("fileName",) + _transformation_keys_ + ("color",)
+
+    # implementation of collections.abc.Mapping abstract methods.
+    # the fontTools.ufoLib.validators.imageValidator requires that image is a
+    # subclass of Mapping...
+
+    def __getitem__(self, key):
+        try:
+            i = self._transformation_keys_.index(key)
+        except ValueError:
+            try:
+                return getattr(self, key)
+            except AttributeError:
+                raise KeyError(key)
+        else:
+            return self.transformation[i]
+
+    def __len__(self):
+        return len(self._valid_keys_)
+
+    def __iter__(self):
+        return iter(self._valid_keys_)
