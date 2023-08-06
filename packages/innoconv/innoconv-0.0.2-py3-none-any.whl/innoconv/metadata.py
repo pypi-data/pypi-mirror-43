290df7806641bb50c6fcb3696ec645b8233cab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:15 +0000

    Add package innoconv/0.0.2/innoconv-0.0.2-py3-none-any.whl

---
+++ packages/innoconv/innoconv-0.0.2-py3-none-any.whl/innoconv/metadata.py
@@ -0,0 +1,7 @@
+"""Project metadata."""
+
+__author__ = "Mirko Dietrich, innoCampus, TU Berlin"
+__author_email__ = "dietrich@math.tu-berlin.de"
+__version__ = "0.0.2"
+__license__ = "GPLv3"
+__url__ = "https://gitlab.tu-berlin.de/innodoc/innoconv"
