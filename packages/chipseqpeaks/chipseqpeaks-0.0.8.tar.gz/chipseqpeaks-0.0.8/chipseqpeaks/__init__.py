258e5aaaf3cee418ff0bd1c52eadfeecda4fa929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:19 +0000

    Add package chipseqpeaks/0.0.8/chipseqpeaks-0.0.8.tar.gz

---
+++ packages/chipseqpeaks/chipseqpeaks-0.0.8.tar.gz/chipseqpeaks-0.0.8/chipseqpeaks/__init__.py
@@ -0,0 +1,29 @@
+"""Easy management of ChIP-seq peak calling data
+
+A mini-module for managing ChIP-seq peak calling data. The language of this
+module treats "ChIP-seq peaks" as an abstraction, but mostly handles them as 
+MACS2 output stored in memory.
+
+Blacklist data source: https://www.encodeproject.org/annotations/ENCSR636HFF/
+
+Example
+-------
+import chipseqpeaks
+with chipseqpeaks.ChIPSeqPeaks(<bytes object or path to BAM file>) as cp:
+    cp.cleans_up = False
+    cp.remove_blacklisted_peaks(<path/to/blacklist.bed>)
+    cp.write(<output prefix>)
+
+Classes
+-------
+ChIPSeqPeaks
+    object representing ChIP-seq peaks
+
+Functions
+---------
+parse_input
+    check that an input is str or bytes and return the bam file as a bytes
+    object
+"""
+
+from chipseqpeaks.chip_seq_peaks import ChIPSeqPeaks
