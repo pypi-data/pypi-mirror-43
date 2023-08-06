e672f2b1382466937217dc0889487133b306cd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package devpi-client/4.3.0/devpi-client-4.3.0.tar.gz

---
+++ packages/devpi-client/devpi-client-4.3.0.tar.gz/devpi-client-4.3.0/testing/test_quickstart.py
@@ -0,0 +1,21 @@
+import py
+import pytest
+import subprocess
+
+
+@pytest.mark.skipif("config.option.fast")
+def test_dryrun(cmd_devpi):
+    cmd_devpi("quickstart", "--dry-run")
+
+
+@pytest.mark.skipif("config.option.fast")
+def test_functional(cmd_devpi, monkeypatch, tmpdir):
+    monkeypatch.setenv("DEVPISERVER_SERVERDIR", tmpdir.join("server"))
+    monkeypatch.setenv("DEVPI_CLIENTDIR", tmpdir.join("client"))
+    cmd_devpi("quickstart")
+    try:
+        hub = cmd_devpi("quickstart", code=-2)
+        assert isinstance(hub.sysex, SystemExit)
+    finally:
+        p = py.path.local.sysfind("devpi-server")
+        subprocess.check_call([str(p), "--stop"])
