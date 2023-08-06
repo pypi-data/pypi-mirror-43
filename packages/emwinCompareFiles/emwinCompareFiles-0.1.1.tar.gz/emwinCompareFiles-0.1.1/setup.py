eb295d166673b7510cb5bcebd1ebed4879393ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:52 +0000

    Add package emwinCompareFiles/0.1.1/emwinCompareFiles-0.1.1.tar.gz

---
+++ packages/emwinCompareFiles/emwinCompareFiles-0.1.1.tar.gz/emwinCompareFiles-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='emwinCompareFiles',
+    version='0.1.1',
+    author='Katie D\'Adamo',
+    author_email='katherine.dadamo@noaa.gov',
+    description='EMWIN Latency Comparison.',
+    url='https://pypi.org/project/emwinCompareFiles/',
+    packages=setuptools.find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.1",
+        "caldav == 0.1.4",
+    ],
+)
