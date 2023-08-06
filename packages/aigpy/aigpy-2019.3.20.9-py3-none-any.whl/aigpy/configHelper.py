26c96dd637a8d9183f9b61cad96dc1c1dbfe05ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:27 +0000

    Add package aigpy/2019.3.20.9/aigpy-2019.3.20.9-py3-none-any.whl

---
+++ packages/aigpy/aigpy-2019.3.20.9-py3-none-any.whl/aigpy/configHelper.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   configHelper.py
+@Time    :   2018/12/17
+@Author  :   Yaron Huang 
+@Version :   1.0
+@Contact :   yaronhuang@qq.com
+@Desc    :   Config Tool
+'''
+import os
+import configparser
+
+def Count(fileName, section=None):
+    try:
+        ret = 0
+        cf  = configparser.ConfigParser()
+        cf.read(fileName)
+        if section == None:
+            seclist = cf.sections()
+            for sec in seclist:
+                oplist = cf.options(sec)
+                ret    = ret + len(oplist)
+        elif cf.has_section(section):
+            ret = len(cf[section])
+            
+        return ret
+    except:
+        return 0
+
+def Sections(fileName):
+    try:
+        cf = configparser.ConfigParser()
+        cf.read(fileName)
+        return cf.sections()
+    except:
+        return None
+
+def GetValue(section, key, default, fileName):
+    try:
+        cf = configparser.ConfigParser()
+        cf.read(fileName)
+        if cf.has_section(section) == False:
+            return default
+
+        for item in cf[section]:
+            if item == key:
+                str = cf.get(section, key)
+                return str
+        return default
+    except:
+        return default
+
+def SetValue(section, key, value, fileName):
+    try:
+        if os.access(fileName, 0) == False:
+            fp = open(fileName, "w")
+            fp.close()
+
+        cf = configparser.ConfigParser()
+        cf.read(fileName)
+        if cf.has_section(section) == False:
+            cf[section] = {}
+
+        cf[section][key] = value
+        with open(fileName, "w") as f:
+            cf.write(f)
+        return True
+    except:
+        return False
