f9c7eb3a92cd6553c99e1023f6f27ab6ca7e2960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package maup/0.3/maup-0.3.tar.gz

---
+++ packages/maup/maup-0.3.tar.gz/maup-0.3/maup/assign.py
@@ -0,0 +1,34 @@
+from .indexed_geometries import IndexedGeometries
+from .intersections import intersections
+from .crs import require_same_crs
+
+
+@require_same_crs
+def assign(sources, targets):
+    """Assign source geometries to targets. A source is assigned to the
+    target that covers it, or, if no target covers the entire source, the
+    target that covers the most of its area.
+    """
+    assignment = assign_by_covering(sources, targets)
+    unassigned = sources[assignment.isna()]
+    assignments_by_area = assign_by_area(unassigned, targets)
+
+    assignment.update(assignments_by_area)
+    return assignment
+
+
+def assign_by_covering(sources, targets):
+    indexed_sources = IndexedGeometries(sources)
+    return indexed_sources.assign(targets)
+
+
+def assign_by_area(sources, targets):
+    intersection_areas = intersections(sources, targets).area
+    assignment = (
+        intersection_areas.groupby(level="source").idxmax().apply(drop_source_label)
+    )
+    return assignment
+
+
+def drop_source_label(index):
+    return index[1]
