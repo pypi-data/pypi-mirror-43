0948a47024f6a3b66177b99df22084edc10a909a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:38 +0000

    Add package mh/0.0.12/mh-0.0.12.tar.gz

---
+++ packages/mh/mh-0.0.12.tar.gz/mh-0.0.12/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='mh',
+    version='0.0.12',
+    packages=['mh',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
