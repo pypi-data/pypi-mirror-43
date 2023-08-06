0b0fbc31eee2894c3de63df50870d7117f2a9e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package KEGGutils/0.2.0/KEGGutils-0.2.0.tar.gz

---
+++ packages/KEGGutils/KEGGutils-0.2.0.tar.gz/KEGGutils-0.2.0/KEGGutils/__init__.py
@@ -0,0 +1,32 @@
+from .KEGGapi import (
+        is_kegg_up,
+        delete_cached_files,
+        keggapi_list,
+        keggapi_find,
+        keggapi_get,
+        keggapi_link,
+        keggapi_info,
+        keggapi_conv,
+        keggapi_ddi,
+        get_organism_codes,
+        get_infos,
+        db_categories,
+        download_dir)
+
+from .KEGGutils import (
+        kegg_link_graph,
+        has_nodetypes,
+        linked_nodes,
+        get_nodes_by_nodetype,
+        get_unique_nodetypes,
+        connected_components,
+        projected_graph,
+        neighbor_graph,
+        graph_measures,
+        draw)
+
+from .KEGGpathway import KEGGpathway
+
+from .KEGGgraph import (KEGGgraph,
+                        KEGGlinkgraph)
+__version__ = "0.2.0"
---
+++ packages/KEGGutils/KEGGutils-0.2.0.tar.gz/KEGGutils-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+import re
+
+# view filename
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    return result.group(1)
+
+project_name = "KEGGutils"
+
+setup(
+    name = project_name,
+    version = get_property('__version__', project_name),
+    author = "Filippo Castelli",
+    author_email = "filippocastelli42@gmail.com",
+    description = ("Simple utils to work with KEGG data on NetworkX"),
+    license = "Unlicense",
+    keywords = "KEGG networkx utils",
+    url = "https://github.com/filippocastelli/KEGGutils",
+    packages=['KEGGutils'],
+    install_requires=['networkx', 'requests', 'matplotlib', 'awesome-slugify'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: Public Domain",
+    ],
+)
