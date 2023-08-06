744daef4bda4cc4f7fc36689e7c4884ee7e876c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:44 +0000

    Add package datacomp/0.0.5.dev0/datacomp-0.0.5.dev0.tar.gz

---
+++ packages/datacomp/datacomp-0.0.5.dev0.tar.gz/datacomp-0.0.5.dev0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Setup.py for DataComp."""
+
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+PACKAGES = setuptools.find_packages(where='src')
+
+setuptools.setup(
+    name="datacomp",
+    version='0.0.5-dev',
+    author="Colin Birkenbihl",
+    author_email="colin.birken@gmail.com",
+    description="Systematic comparisons of multiple datasets",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/Cojabi/datacomp",
+    packages=PACKAGES,
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas',
+        'xlrd',
+        'numpy',
+        'scipy',
+        'sklearn',
+        'matplotlib_venn',
+        'pymatch',
+        'matplotlib',
+        'statsmodels',
+        'seaborn',
+    ]
+)
