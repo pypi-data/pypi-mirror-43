704b89f1f225f9509899ecebdeea0b7f009aab7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:51 +0000

    Add package banhammer/0.1.1/banhammer-0.1.1.tar.gz

---
+++ packages/banhammer/banhammer-0.1.1.tar.gz/banhammer-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md', encoding='utf8') as f:
+        return f.read()
+
+setup(name='banhammer',
+      version='0.1.1',
+      description='Generates GIFs based on Tom Scott\'s Banhammer',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/DerpyChap/banhammer',
+      author='DerpyChap',
+      author_email='holla@derpychap.co.uk',
+      license='ISC',
+      packages=['banhammer'],
+      install_requires=['pillow', 'imageio', 'numpy'],
+      include_package_data=True,
+      zip_safe=False)
