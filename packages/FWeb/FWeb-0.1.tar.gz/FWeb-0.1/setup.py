044fec86bcfab9ff4fa94cde7684b6c2963bc2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:02 +0000

    Add package FWeb/0.1/FWeb-0.1.tar.gz

---
+++ packages/FWeb/FWeb-0.1.tar.gz/FWeb-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='FWeb',
+      version='0.1',
+      description='A simple Web Framework',
+      author='zhshch',
+      author_email='15201261945@163.com',
+      url='https://github.com/zhshch2002/TWeb',
+      packages=['TWeb'],
+      )
