3970cdab4a22e0d9d47892cc9f9f34fb67b7ef66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package aqtinstall/0.3.1/aqtinstall-0.3.1.tar.gz

---
+++ packages/aqtinstall/aqtinstall-0.3.1.tar.gz/aqtinstall-0.3.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import io
+import os
+
+from setuptools import setup
+
+
+def readme():
+    with io.open(os.path.join(os.path.dirname(__file__), 'README.rst'), mode="r", encoding="UTF-8") as readmef:
+        return readmef.read()
+
+
+setup(name='aqtinstall',
+      version='0.3.1',
+      description='Another unofficial Qt installer',
+      url='http://github.com/miurahr/aqtinstall',
+      license='MIT',
+      long_description=readme(),
+      author='Hioshi Miura',
+      author_email='miurahr@linux.com',
+      packages=["aqt"],
+      install_requires=['requests'],
+      extras_require={
+        'dev': [
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+            'flake8'
+        ]
+      },
+      scripts=["aqtinst"]
+      )
