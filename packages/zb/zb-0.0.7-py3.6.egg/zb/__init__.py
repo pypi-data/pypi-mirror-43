edfe1415f6fe342772aaedc6f0e10e3fa9b68b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:36 +0000

    Add package zb/0.0.7/zb-0.0.7-py3.6.egg

---
+++ packages/zb/zb-0.0.7-py3.6.egg/zb/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.7"
+__author__ = "zengbin93"
+
+
+from zb.utils import elapsed, create_logger
+import zb.nlp as nlp
+import zb.tools as tools
+from zb.base import OrderedAttrDict, AttrDict
+
