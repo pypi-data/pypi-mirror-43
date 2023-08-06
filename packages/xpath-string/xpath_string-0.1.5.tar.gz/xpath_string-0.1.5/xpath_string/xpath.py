34ce7e2ea2d80da431c93f1b63af217f05ea9a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package xpath-string/0.1.5/xpath_string-0.1.5.tar.gz

---
+++ packages/xpath-string/xpath_string-0.1.5.tar.gz/xpath_string-0.1.5/xpath_string/xpath.py
@@ -0,0 +1,31 @@
+from xpath_string.two_xpath_addition import TwoXpathAddition
+
+
+class Xpath(str):
+    def __init__(self, xpath_string: str):
+        super(Xpath, self).__init__()
+        self.xpath = xpath_string
+
+    def __add__(self, other):
+        """ Override the default Addition behavior"""
+        if isinstance(other, self.__class__):
+            return TwoXpathAddition(self.xpath, other.xpath).add_two_xpath_together()
+        elif isinstance(other, str):
+            return self.xpath + other
+        else:
+            raise TypeError('Only Xpath and string can be added to Xpath Object. Not {}'.format(type(other)))
+
+    def __str__(self):
+        """ Override the default str() behavior"""
+        return self.xpath
+
+    def __eq__(self, other):
+        """ Override the default Equals behavior"""
+        return self.xpath == other.xpath
+
+    def __ne__(self, other):
+        """ Override the default Unequals behavior"""
+        return self.xpath != other.xpath
+
+    def format(self, *args, **kwargs):
+        return super(Xpath, self).format(*args, **kwargs)
