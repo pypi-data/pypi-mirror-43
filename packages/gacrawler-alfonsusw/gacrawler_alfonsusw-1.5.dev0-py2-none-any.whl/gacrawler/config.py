bd52e3c1f4b63a873e2147597d069851b0d50a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package gacrawler-alfonsusw/1.5.dev0/gacrawler_alfonsusw-1.5.dev0-py2-none-any.whl

---
+++ packages/gacrawler-alfonsusw/gacrawler_alfonsusw-1.5.dev0-py2-none-any.whl/gacrawler/config.py
@@ -0,0 +1,16 @@
+from ConfigParser import SafeConfigParser
+import const
+import sys
+
+def getConfig(section, key, dataType=str):
+    parser = SafeConfigParser()
+    parser.read(const.SETTING_FILE)
+    try:
+        if dataType == int:
+            return parser.getint(section, key)
+        elif dataType == float:
+            return parser.getfloat(section, key)
+        else:
+            return parser.get(section, key)
+    except Exception as ex:
+        sys.exit('Error Reading Configuration: ' + str(ex))
