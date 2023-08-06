b488006cad1b5c8b02ea8aafe6409d529efe433d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:29 +0000

    Add package drf-multiple-settings/1.0.1/drf-multiple-settings-1.0.1.tar.gz

---
+++ packages/drf-multiple-settings/drf-multiple-settings-1.0.1.tar.gz/drf-multiple-settings-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drf-multiple-settings",
+    version="1.0.1",
+    author="Artem Vasin",
+    author_email="nonameitem@me.com",
+    description="DRF ViewSets supporting different settings for actions",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/NoNameItem/drf-multiple-settings",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'djangorestframework>=3.0'
+    ],
+)
