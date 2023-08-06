bd5eab9379a2696dd79fd40ff73b4184113b48c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:37 +0000

    Add package sundry/0.5.1/sundry-0.5.1-py3-none-any.whl

---
+++ packages/sundry/sundry-0.5.1-py3-none-any.whl/sundry/to_dynamodb.py
@@ -0,0 +1,34 @@
+
+import decimal
+from collections import OrderedDict, defaultdict
+
+# Handles Inexact error.
+decimal_context = decimal.getcontext().copy()
+decimal_context.prec = 38  # Numbers can have 38 digits precision
+
+
+def dict_to_dynamodb(input_value):
+    """
+    makes a dictionary follow boto3 item standards
+    :param input_value: input dictionary
+    :return: converted version of the original dictionary
+    """
+    if type(input_value) is dict or type(input_value) is OrderedDict or type(input_value) is defaultdict:
+        resp = {}
+        for k, v in input_value.items():
+            if type(k) is int:
+                k = str(k)  # allow int values as keys since they are unambiguous (bool, float, etc. are not)
+            resp[k] = dict_to_dynamodb(v)
+    elif type(input_value) is list:
+        resp = [dict_to_dynamodb(v) for v in input_value]
+    elif type(input_value) is str or type(input_value) is bool or input_value is None or type(input_value) is decimal.Decimal:
+        resp = input_value  # native DynamoDB types
+    elif type(input_value) is float or type(input_value) is int:
+        # boto3 uses Decimal for numbers, but handle the 'inexact error'
+        # 'casting' to str may work as well, but this should be better at maintaining precision
+        resp = decimal_context.create_decimal(input_value)
+    elif type(input_value) is tuple:
+        resp = list(input_value)  # convert tuple to a list
+    else:
+        raise NotImplementedError(type(input_value), input_value)
+    return resp
