8032f0e639cea30a0974078b746fa3f065d774f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:55 +0000

    Add package dictfier/1.0.6/dictfier-1.0.6-py2-none-any.whl

---
+++ packages/dictfier/dictfier-1.0.6-py2-none-any.whl/dictfier/api.py
@@ -0,0 +1,12 @@
+from . import factory
+
+def dictfy(obj, query, call_callable=False, not_found_create=False):
+    return factory._dict(
+        obj, 
+        query, 
+        call_callable, 
+        not_found_create
+    )
+
+def useobj(function):
+    return factory.UseObj(function)
