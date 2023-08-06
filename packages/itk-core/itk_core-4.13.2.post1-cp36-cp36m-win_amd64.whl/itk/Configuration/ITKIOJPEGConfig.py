31c50af847c548406888e1ea63b0a690bf0b476c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:03 +0000

    Add package itk-core/4.13.2.post1/itk_core-4.13.2.post1-cp36-cp36m-win_amd64.whl

---
+++ packages/itk-core/itk_core-4.13.2.post1-cp36-cp36m-win_amd64.whl/itk/Configuration/ITKIOJPEGConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('JPEGImageIO', 'itk::JPEGImageIO', 'itkJPEGImageIO', True),
+  ('JPEGImageIOFactory', 'itk::JPEGImageIOFactory', 'itkJPEGImageIOFactory', True),
+)
