ff78c82cc8993fa05ff9d80845b7a8e627b44fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package Rofi-MPD/1.0.0/Rofi-MPD-1.0.0.tar.gz

---
+++ packages/Rofi-MPD/Rofi-MPD-1.0.0.tar.gz/Rofi-MPD-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Rofi-MPD',
+    version='1.0.0',
+    install_requires=['python-mpd2'],
+    packages=['rofi', 'main'],
+    # py_modules=['rofi', 'main'],
+    url='https://github.com/JakeStanger/Rofi_MPD',
+    license='MIT',
+    author='Jake Stanger',
+    author_email='jakestanger@gmail.com',
+    description='A Rofi menu for interacting with MPD.',
+    scripts=['bin/rofi-mpd']
+)
