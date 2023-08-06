ebb9a0415a9c089ac52a349e85053a8a5d8dd8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:52 +0000

    Add package jkutils/0.8/jkutils-0.8.tar.gz

---
+++ packages/jkutils/jkutils-0.8.tar.gz/jkutils-0.8/tests/test_kafka.py
@@ -0,0 +1,9 @@
+from jkutils.jkafka import JKKafka
+
+kafka_server = "172.16.1.206:9092"
+
+
+def test_producer():
+    client = JKKafka(bootstrap_servers=kafka_server)
+    client.send(topic_name="test", data="test".encode("utf-8"))
+    assert True
