7f71c0f245e93cb178378f624cc449a3cd416ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package pyabst/0.0.2/pyabst-0.0.2.tar.gz

---
+++ packages/pyabst/pyabst-0.0.2.tar.gz/pyabst-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from os import path
+
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pyabst',
+      setup_requires=['wheel'],
+      version='0.0.2',
+      author='Joshua David Wood',
+      author_email='pyaugment@gmail.com',
+      description='A library which generates text summaries allowing users to weight significant or insignificant target words.',
+      long_description_content_type='text/markdown',
+      long_description=long_description,
+      url='https://github.com/joshuadavidwood',
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent'],
+      packages=['pyabst'])
+
