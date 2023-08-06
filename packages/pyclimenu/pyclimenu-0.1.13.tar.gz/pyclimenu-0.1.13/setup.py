adb060a7813be1b9ddfabcecb26abe63a020f824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package pyclimenu/0.1.13/pyclimenu-0.1.13.tar.gz

---
+++ packages/pyclimenu/pyclimenu-0.1.13.tar.gz/pyclimenu-0.1.13/setup.py
@@ -0,0 +1,21 @@
+from pyclimenu import __version__
+from distutils.core import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+setup(
+  name = 'pyclimenu',
+  packages = ['pyclimenu'],
+  version = __version__,
+  description = 'The easy way to create command line menus',
+  author = 'Panagiotis Simakis',
+  author_email = 'simakis@autistici.org',
+  url = 'https://github.com/sp1thas/pyclimenu',
+  download_url = 'https://github.com/sp1thas/pyclimenu/archive/master.zip',
+  keywords = ['menu'],
+  include_package_data=True,
+  classifiers = [],
+  zip_safe=False,
+  long_description=readme()
+)
