39ced7c5446e251bacbc68f696422e2342e039a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:11 +0000

    Add package itk-filtering/4.13.2.post1/itk_filtering-4.13.2.post1-cp27-cp27m-macosx_10_9_x86_64.whl

---
+++ packages/itk-filtering/itk_filtering-4.13.2.post1-cp27-cp27m-macosx_10_9_x86_64.whl/itk/Configuration/ITKAntiAliasConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKLevelSets', )
+templates = (
+  ('AntiAliasBinaryImageFilter', 'itk::AntiAliasBinaryImageFilter', 'itkAntiAliasBinaryImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('AntiAliasBinaryImageFilter', 'itk::AntiAliasBinaryImageFilter', 'itkAntiAliasBinaryImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+)
