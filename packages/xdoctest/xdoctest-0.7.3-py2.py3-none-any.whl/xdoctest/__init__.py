f8b18973fd41cb5660f302a264c447d69f99df59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:12 +0000

    Add package xdoctest/0.7.3/xdoctest-0.7.3-py2.py3-none-any.whl

---
+++ packages/xdoctest/xdoctest-0.7.3-py2.py3-none-any.whl/xdoctest/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+mkinit xdoctest --nomods
+"""
+__version__ = '0.7.3'  # nocover
+
+# Expose only select submodules
+__submodules__ = [
+    'runner',
+    'exceptions',
+]
+
+
+from xdoctest import utils
+from xdoctest import docstr
+from xdoctest.runner import (doctest_module,)
+from xdoctest.exceptions import (DoctestParseError, ExitTestException,
+                                 MalformedDocstr,)
+
+
+__all__ = ['DoctestParseError', 'ExitTestException', 'MalformedDocstr',
+           'doctest_module', 'utils', 'docstr', '__version__']
