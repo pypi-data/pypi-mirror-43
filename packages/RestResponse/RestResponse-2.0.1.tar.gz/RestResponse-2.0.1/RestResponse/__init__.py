28e9dc5ef50e29990b0729a9596708c479769526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:14 +0000

    Add package RestResponse/2.0.1/RestResponse-2.0.1.tar.gz

---
+++ packages/RestResponse/RestResponse-2.0.1.tar.gz/RestResponse-2.0.1/RestResponse/__init__.py
@@ -0,0 +1,13 @@
+from .objects import (
+    RestEncoder, RestResponse, RestObject, RestList, NoneProp, RestResponseObj
+)
+from . import orm
+
+parse = RestResponse.parse
+loads = RestResponse.loads
+__version__ = '2.0.1'
+
+__all__ = [
+    'RestEncoder', 'RestResponse', 'RestObject', 'RestList', 'NoneProp', 'parse', 'loads', 'orm',
+    'RestResponseObj'
+]
---
+++ packages/RestResponse/RestResponse-2.0.1.tar.gz/RestResponse-2.0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+from RestResponse import __version__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='RestResponse',
+    version=__version__,
+    description='A fluent python object for interfacing with RESTful JSON APIs.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tysonholub/RestResponse.git',
+    author='Tyson Holub',
+    author_email='tyson@tysonholub.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'simplejson',
+        'six',
+        'SQLAlchemy',
+        'cloudpickle'
+    ]
+)
