a35ed9da862dc19863fc8ab3fa8ecb9aebcc678d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package nic-api/0.2.0/nic_api-0.2.0.tar.gz

---
+++ packages/nic-api/nic_api-0.2.0.tar.gz/nic_api-0.2.0/setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = open(os.path.join(here, 'README.rst'), 'r').read()
+
+setup(
+    name='nic_api',
+    version='0.2.0',
+    description='NIC.RU API wrapper library',
+    long_description=long_description,
+    url='https://github.com/andr1an/nic-api',
+    author='Sergey Andrianov',
+    author_email='info@andrian.ninja',
+    license='GPLv3',
+    packages=['nic_api'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Internet :: Name Service (DNS)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=2.7, <3.8',
+    install_requires=[
+        'requests>=2.4',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/andr1an/nic-api/issues',
+        'Source': 'https://github.com/andr1an/nic-api',
+    },
+)
