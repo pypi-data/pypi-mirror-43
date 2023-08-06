d8e63288adb746e858ca9a3866ca50ab009b1fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:23 +0000

    Add package clicmod/0.7.dev0/clicmod-0.7.dev0.tar.gz

---
+++ packages/clicmod/clicmod-0.7.dev0.tar.gz/clicmod-0.7.dev0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='clicmod',
+    version='0.7dev',
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ['clicmod = impex.impex:main']
+    },
+    install_requires=[
+        'requests',
+        'urllib3',
+        'pyfiglet',
+        'tqdm'
+    ],
+    author="Nick Corso-Passaro",
+    author_email="ncpassaro@gmail.com",
+    description="A command line interface for IPsoft 1Desk content management.",
+    long_description=open('README.md').read(),
+)
