f7d2646b9555876e77c54895f391c42c25d90133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:39 +0000

    Add package rxys/0.0.1/rxys-0.0.1.tar.gz

---
+++ packages/rxys/rxys-0.0.1.tar.gz/rxys-0.0.1/README.txt
@@ -0,0 +1,3 @@
+Prepared by Nikhil Kartha on 22/2/19
+
+This needs to be passed through sdist
---
+++ packages/rxys/rxys-0.0.1.tar.gz/rxys-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+# from distutils.core import setup
+
+setup(
+    name = 'rxys',
+    version = '0.0.1',
+    py_modules = ['rxys'],
+    author = 'kartha',
+    author_email = 'kartha@vivaldi.net',
+    url = "http://github.com/NickKartha/rxys",
+    description = 'A simple printer of nested lists'
+)
+
