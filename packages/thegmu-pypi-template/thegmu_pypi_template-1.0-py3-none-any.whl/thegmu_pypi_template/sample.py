71c56ecb86485b910afea3f19d07d33aac211d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:19 +0000

    Add package thegmu-pypi-template/1.0/thegmu_pypi_template-1.0-py3-none-any.whl

---
+++ packages/thegmu-pypi-template/thegmu_pypi_template-1.0-py3-none-any.whl/thegmu_pypi_template/sample.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""The GMU Sample - sample module provided with PyPI template repo."""
+
+
+class Sample():
+    """Sample object sample provided with cloning template repo."""
+
+    @staticmethod
+    def hello(name="The GMU Project"):
+        """This function returns a string of 'Hello name'.
+
+        Accepts an optional name.
+
+        Args:
+          name: A optional string for hello testing.
+
+        Returns:
+          "Hello name"
+        """
+
+        return("Hello %s" % (name, ))
