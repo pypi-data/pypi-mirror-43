5594a7d34d030b729e4b367f198f6c8dcbce70cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:25 +0000

    Add package subunitreporter/19.3.0/subunitreporter-19.3.0.tar.gz

---
+++ packages/subunitreporter/subunitreporter-19.3.0.tar.gz/subunitreporter-19.3.0/src/subunitreporter/_metadata.py
@@ -0,0 +1,12 @@
+# Copyright Least Authority TFA GmbH
+# See LICENSE for details.
+
+from __future__ import (
+    division,
+    absolute_import,
+    print_function,
+    unicode_literals,
+)
+
+version_tuple = (19, 3, 0)
+version_string = ".".join(map(str, version_tuple))
