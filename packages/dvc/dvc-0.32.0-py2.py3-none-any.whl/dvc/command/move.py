388ee84eb07ec60a4c57d57ea9f5d1a3eb51c114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:01 +0000

    Add package dvc/0.32.0/dvc-0.32.0-py2.py3-none-any.whl

---
+++ packages/dvc/dvc-0.32.0-py2.py3-none-any.whl/dvc/command/move.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+import dvc.logger as logger
+from dvc.exceptions import DvcException
+from dvc.command.base import CmdBase
+
+
+class CmdMove(CmdBase):
+    def run(self):
+        try:
+            self.repo.move(self.args.src, self.args.dst)
+        except DvcException:
+            msg = "failed to move '{}' -> '{}'".format(
+                self.args.src, self.args.dst
+            )
+            logger.error(msg)
+            return 1
+        return 0
+
+
+def add_parser(subparsers, parent_parser):
+    description = (
+        "Rename or move a data file or a directory that "
+        "is under DVC control. It renames and modifies "
+        "the corresponding DVC file to reflect the changes."
+    )
+    help = "Rename or move a DVC controlled data file or a directory."
+    move_parser = subparsers.add_parser(
+        "move", parents=[parent_parser], description=description, help=help
+    )
+    move_parser.add_argument(
+        "src", help="Source path to a data file or directory."
+    )
+    move_parser.add_argument("dst", help="Destination path.")
+    move_parser.set_defaults(func=CmdMove)
---
+++ packages/dvc/dvc-0.32.0-py2.py3-none-any.whl/dvc/scm/__init__.py
@@ -0,0 +1,41 @@
+"""Manages source control systems(e.g. Git)."""
+
+from __future__ import unicode_literals
+
+from dvc.scm.base import Base
+from dvc.scm.git import Git
+
+import dvc
+
+
+def SCM(root_dir, repo=None):  # pylint: disable=invalid-name
+    """Returns SCM instance that corresponds to a repo at the specified
+    path.
+
+    Args:
+        root_dir (str): path to a root directory of the repo.
+        repo (dvc.repo.Repo): dvc repo instance that root_dir belongs to.
+
+    Returns:
+        dvc.scm.base.Base: SCM instance.
+    """
+    if Git.is_repo(root_dir) or Git.is_submodule(root_dir):
+        return Git(root_dir, repo=repo)
+
+    return Base(root_dir, repo=repo)
+
+
+def scm_context(method):
+    def run(*args, **kw):
+        repo = args[0]
+        assert isinstance(repo, dvc.repo.Repo)
+        try:
+            result = method(*args, **kw)
+            repo.scm.reset_ignores()
+            repo.scm.remind_to_track()
+            return result
+        except Exception as e:
+            repo.scm.cleanup_ignores()
+            raise e
+
+    return run
