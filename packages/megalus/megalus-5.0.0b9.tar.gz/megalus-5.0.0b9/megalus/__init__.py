05d8fef533f13c881ffb5123f9ede3882ff545b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:38 +0000

    Add package megalus/5.0.0b9/megalus-5.0.0b9.tar.gz

---
+++ packages/megalus/megalus-5.0.0b9.tar.gz/megalus-5.0.0b9/megalus/__init__.py
@@ -0,0 +1,12 @@
+"""Init module."""
+import json
+from os import path
+
+from megalus.utils import get_path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "..", "version.json")) as json_file:
+    version_data = json.load(json_file)
+
+__version__ = version_data['version']
