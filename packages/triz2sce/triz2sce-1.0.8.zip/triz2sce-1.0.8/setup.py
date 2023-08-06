92d8147730ce2b92e3b31aca7f2cd31a163139bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:42 +0000

    Add package triz2sce/1.0.8/triz2sce-1.0.8.zip

---
+++ packages/triz2sce/triz2sce-1.0.8.zip/triz2sce-1.0.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+f=open('README.txt', encoding='UTF-8')
+a=f.read()
+f.close()
+
+setup(name="triz2sce",
+	author="Pedro Fernández",
+	author_email="rockersuke@gmail.com",
+	version="1.0.8",
+	license="MIT",
+	url="http://www.zonafi.rockersuke.com/triz2sce/index.html",
+	description="Convierte mapas de aventuras de texto generados por Trizbort en código fuente para el DAAD.",
+	long_description=a,
+	python_requires=">=3.5",
+	scripts=['triz2sce.py', 'triz2sce_func.py'],
+)
