231385ef1ca765abde728204bc9e560410c129e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:31 +0000

    Add package rtcbot/0.0.5/rtcbot-0.0.5.tar.gz

---
+++ packages/rtcbot/rtcbot-0.0.5.tar.gz/rtcbot-0.0.5/rtcbot/__init__.py
@@ -0,0 +1,9 @@
+from .base import SubscriptionClosed
+from .connection import RTCConnection
+from .camera import CVCamera, PiCamera, CVDisplay
+from .audio import Microphone, Speaker
+from .inputs import Gamepad, Mouse, Keyboard
+from .arduino import SerialConnection
+from .subscriptions import *
+from .javascript import getRTCBotJS
+from .devices import *
