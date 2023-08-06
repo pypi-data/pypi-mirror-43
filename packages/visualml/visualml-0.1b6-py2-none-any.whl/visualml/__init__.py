56f3df670c975b7651b3c947f9297eb493f43784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package visualml/0.1b6/visualml-0.1b6-py2-none-any.whl

---
+++ packages/visualml/visualml-0.1b6-py2-none-any.whl/visualml/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for VisualML."""
+from visualml.visualml import decision_boundary_grid, plot_decision_boundary
+
+__all__ = ['decision_boundary_grid', 'plot_decision_boundary']
+
