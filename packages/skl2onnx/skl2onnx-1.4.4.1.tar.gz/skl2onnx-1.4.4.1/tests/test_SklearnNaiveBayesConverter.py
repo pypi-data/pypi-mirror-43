25756b8c3dbf698a6d2c185f21112d890909f650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:42 +0000

    Add package skl2onnx/1.4.4.1/skl2onnx-1.4.4.1.tar.gz

---
+++ packages/skl2onnx/skl2onnx-1.4.4.1.tar.gz/skl2onnx-1.4.4.1/tests/test_SklearnNaiveBayesConverter.py
@@ -0,0 +1,79 @@
+import numpy as np
+import onnx
+import unittest
+from distutils.version import StrictVersion
+from sklearn.datasets import load_digits, load_iris
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+from test_utils import dump_data_and_model
+
+class TestNaiveBayesConverter(unittest.TestCase):
+
+    def _fit_model_binary_classification(self, model, data):
+        X = data.data
+        y = data.target
+        mid_point = len(data.target_names) / 2
+        y[y < mid_point] = 0
+        y[y >= mid_point] = 1
+        model.fit(X, y)
+        return model, X.astype(np.float32)
+
+    def _fit_model_multiclass_classification(self, model, data):
+        X = data.data
+        y = data.target
+        model.fit(X, y)
+        return model, X.astype(np.float32)
+
+    def test_model_multinomial_nb_binary_classification(self):
+        model, X = self._fit_model_binary_classification(MultinomialNB(),
+                                                         load_iris())
+        model_onnx = convert_sklearn(model, 'multinomial naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnBinMultinomialNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.2.1')")
+
+    @unittest.skipIf(StrictVersion(onnx.__version__) <= StrictVersion('1.3'),
+                     'Needs greater opset 9')
+    def test_model_bernoulli_nb_binary_classification(self):
+        model, X = self._fit_model_binary_classification(BernoulliNB(),
+                                                         load_digits())
+        model_onnx = convert_sklearn(model, 'bernoulli naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnBinBernoulliNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.2.1')")
+
+    def test_model_multinomial_nb_multiclass(self):
+        model, X = self._fit_model_multiclass_classification(MultinomialNB(),
+                                                             load_iris())
+        model_onnx = convert_sklearn(model, 'multinomial naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnMclMultinomialNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.2.1')")
+
+    @unittest.skipIf(StrictVersion(onnx.__version__) <= StrictVersion('1.3'),
+                     'Needs greater opset 9')
+    def test_model_bernoulli_nb_multiclass(self):
+        model, X = self._fit_model_multiclass_classification(BernoulliNB(),
+                                                             load_digits())
+        model_onnx = convert_sklearn(model, 'bernoulli naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnMclBernoulliNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.2.1')")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/skl2onnx/skl2onnx-1.4.4.1.tar.gz/skl2onnx-1.4.4.1/tests/test_utils/utils_backend_onnxruntime.py
@@ -0,0 +1,391 @@
+"""
+Helpers to test runtimes.
+"""
+import os
+import glob
+import pickle
+import numpy
+import pandas
+import warnings
+from numpy.testing import assert_array_almost_equal, assert_array_equal
+from .utils_backend import load_data_and_model, extract_options, ExpectedAssertionError, OnnxRuntimeAssertionError, compare_outputs
+
+
+def compare_runtime(test, decimal=5, options=None, verbose=False, context=None, comparable_outputs=None):
+    """
+    The function compares the expected output (computed with
+    the model before being converted to ONNX) and the ONNX output
+    produced with module *onnxruntime*.
+    
+    :param test: dictionary with the following keys:
+        - *onnx*: onnx model (filename or object)
+        - *expected*: expected output (filename pkl or object)
+        - *data*: input data (filename pkl or object)
+    :param decimal: precision of the comparison
+    :param options: comparison options
+    :param context: specifies custom operators
+    :param verbose: in case of error, the function may print
+        more information on the standard output
+    :param comparable_outputs: compare only these outputs
+    :return: tuple (outut, lambda function to run the predictions)
+    
+    The function does not return anything but raises an error
+    if the comparison failed.
+    """
+    lambda_onnx = None
+    if context is None:
+        context = {}
+    load = load_data_and_model(test, **context)
+    if verbose:
+        print("[compare_runtime] test '{}' loaded".format(test['onnx']))
+
+    onx = test['onnx']
+    if options is None:
+        if isinstance(onx, str):
+            options = extract_options(onx)
+        else:
+            options = {}
+    elif options is None:
+        options = {}
+    elif not isinstance(options, dict):
+        raise TypeError("options must be a dictionary.")
+
+    try:
+        import onnxruntime
+    except ImportError as e:
+        warnings.warn("Unable to import onnxruntime.")
+        return None
+
+    if verbose:
+        print("[compare_runtime] InferenceSession('{}')".format(onx))
+
+    try:
+        sess = onnxruntime.InferenceSession(onx)
+    except ExpectedAssertionError as expe:
+        raise expe
+    except Exception as e:
+        if "CannotLoad" in options:
+            raise ExpectedAssertionError("Unable to load onnx '{0}' due to\n{1}".format(onx, e))
+        else:
+            if verbose:
+                import onnx
+                model = onnx.load(onx)
+                smodel = "\nJSON ONNX\n" + str(model)
+            else:
+                smodel = ""
+            raise OnnxRuntimeAssertionError("Unable to load onnx '{0}'\nONNX\n{1}".format(onx, smodel))
+    
+    input = load["data"]
+    DF = options.pop('DF', False)
+    if DF:
+        inputs = {c: input[c].values for c in input.columns}
+        for k in inputs:
+            if inputs[k].dtype == numpy.float64:
+                inputs[k] = inputs[k].astype(numpy.float32)
+            inputs[k] = inputs[k].reshape((inputs[k].shape[0], 1))
+    else:
+        if isinstance(input, dict):
+            inputs = input
+        elif isinstance(input, (list, numpy.ndarray, pandas.DataFrame)):
+            inp = sess.get_inputs()
+            if len(inp) == len(input):
+                inputs = {i.name: v for i, v in zip(inp, input)}
+            elif len(inp) == 1:
+                inputs = {inp[0].name: input}
+            elif isinstance(input, numpy.ndarray):
+                shape = sum(i.shape[1] if len(i.shape) == 2 else i.shape[0] for i in inp)
+                if shape == input.shape[1]:
+                    inputs = {n.name: input[:, i] for i, n in enumerate(inp)}
+                else:
+                    raise OnnxRuntimeAssertionError("Wrong number of inputs onnx {0} != original shape {1}, onnx='{2}'".format(len(inp), input.shape, onx))
+            elif isinstance(input, list):
+                try:
+                    array_input = numpy.array(input)
+                except Exception as e:
+                    raise OnnxRuntimeAssertionError("Wrong number of inputs onnx {0} != original {1}, onnx='{2}'".format(len(inp), len(input), onx))
+                shape = sum(i.shape[1] for i in inp)
+                if shape == array_input.shape[1]:
+                    inputs = {}
+                    c = 0
+                    for i, n in enumerate(inp):
+                        d = c + n.shape[1]
+                        inputs[n.name] = _create_column([row[c:d] for row in input], n.type)
+                        c = d
+                else:
+                    raise OnnxRuntimeAssertionError("Wrong number of inputs onnx {0} != original shape {1}, onnx='{2}'*".format(len(inp), array_input.shape, onx))
+            elif isinstance(input, pandas.DataFrame):
+                try:
+                    array_input = numpy.array(input)
+                except Exception as e:
+                    raise OnnxRuntimeAssertionError("Wrong number of inputs onnx {0} != original {1}, onnx='{2}'".format(len(inp), len(input), onx))
+                shape = sum(i.shape[1] for i in inp)
+                if shape == array_input.shape[1]:
+                    inputs = {}
+                    c = 0
+                    for i, n in enumerate(inp):
+                        d = c + n.shape[1]
+                        inputs[n.name] = _create_column(input.iloc[:, c:d], n.type)
+                        c = d
+                else:
+                    raise OnnxRuntimeAssertionError("Wrong number of inputs onnx {0}={1} columns != original shape {2}, onnx='{3}'*".format(len(inp), shape, array_input.shape, onx))
+            else:
+                raise OnnxRuntimeAssertionError("Wrong type of inputs onnx {0}, onnx='{2}'".format(type(input), onx))
+        else:
+            raise OnnxRuntimeAssertionError("Dict or list is expected, not {0}".format(type(input)))
+        
+        for k in inputs:
+            if isinstance(inputs[k], list):
+                inputs[k] = numpy.array(inputs[k])
+    
+    OneOff = options.pop('OneOff', False)
+    OneOffArray = options.pop('OneOffArray', False)
+    options.pop('SklCol', False)  # unused here but in dump_data_and_model
+    if OneOff or OneOffArray:
+        if verbose:
+            print("[compare_runtime] OneOff: type(inputs)={} len={} OneOffArray={}".format(
+                type(input), len(inputs), OneOffArray))
+        if len(inputs) == 1 and not OneOffArray:
+            name, values = list(inputs.items())[0]
+            res = []
+            for input in values:
+                try:
+                    one = sess.run(None, {name: input})
+                    if lambda_onnx is None:
+                        lambda_onnx = lambda: sess.run(None, {name: input})
+                    if verbose:
+                        import pprint
+                        pprint.pprint(one)
+                except ExpectedAssertionError as expe:
+                    raise expe
+                except Exception as e:
+                    raise OnnxRuntimeAssertionError("Unable to run onnx '{0}' due to {1}".format(onx, e))
+                res.append(one)
+            if verbose:
+                print("[compare_runtime] OneOff: _post_process_output1")
+            output = _post_process_output(res)
+        else:
+            def to_array(vv):
+                if isinstance(vv, (numpy.ndarray, numpy.int64, numpy.float32, str)):
+                    return numpy.array([vv])
+                else:
+                    return numpy.array([vv], dtype=numpy.float32)
+            t = list(inputs.items())[0]
+            res = []
+            for i in range(0, len(t[1])):
+                iii = {k: to_array(v[i]) for k, v in inputs.items()}
+                try:
+                    one = sess.run(None, iii)
+                    if lambda_onnx is None:
+                        lambda_onnx = lambda: sess.run(None, iii)
+                    if verbose:
+                        import pprint
+                        pprint.pprint(one)
+                except ExpectedAssertionError as expe:
+                    raise expe
+                except Exception as e:
+                    if verbose:
+                        import onnx
+                        model = onnx.load(onx)
+                        smodel = "\nJSON ONNX\n" + str(model)
+                    else:
+                        smodel = ""
+                    raise OnnxRuntimeAssertionError("Unable to run onnx '{0}' due to {1}{2}".format(onx, e, smodel))
+                res.append(one)
+            if verbose:
+                print("[compare_runtime] OneOff: _post_process_output2")
+            output = _post_process_output(res)
+            
+            if OneOffArray:
+                if isinstance(output, list):
+                    pass
+                elif not isinstance(output, numpy.ndarray):
+                    raise TypeError("output must be an array, not {}".format(type(output)))
+                else:
+                    output = [output]
+    else:
+        if verbose:
+            print("[compare_runtime] type(inputs)={} len={} names={}".format(
+                type(input), len(inputs), list(sorted(inputs))))
+        if verbose:
+            run_options = onnxruntime.RunOptions()
+            run_options.run_log_verbosity_level = 5
+        else:
+            run_options = None
+        try:
+            output = sess.run(None, inputs, run_options)
+            lambda_onnx = lambda: sess.run(None, inputs)
+            if verbose:
+                import pprint
+                pprint.pprint(output)
+        except ExpectedAssertionError as expe:
+            raise expe
+        except RuntimeError as e:
+            if "-Fail" in onx:
+                raise ExpectedAssertionError("onnxruntime cannot compute the prediction for '{0}'".format(onx))
+            else:
+                if verbose:
+                    import onnx
+                    model = onnx.load(onx)
+                    smodel = "\nJSON ONNX\n" + str(model)
+                else:
+                    smodel = ""
+                raise OnnxRuntimeAssertionError("onnxruntime cannot compute the prediction for '{0}' due to {1}{2}".format(onx, e, smodel))
+        except Exception as e:
+            raise OnnxRuntimeAssertionError("Unable to run onnx '{0}' due to {1}".format(onx, e))
+        if verbose:
+            print("[compare_runtime] done type={}".format(type(output)))
+    
+    output0 = output.copy()
+
+    if comparable_outputs:
+        cmp_exp = [load["expected"][o] for o in comparable_outputs]
+        cmp_out = [output[o] for o in comparable_outputs]
+    else:
+        cmp_exp = load["expected"]
+        cmp_out = output
+    
+    try:
+        _compare_expected(cmp_exp, cmp_out, sess, onx, decimal=decimal,
+                          verbose=verbose, **options)
+    except ExpectedAssertionError as expe:
+        raise expe
+    except Exception as e:
+        if verbose:
+            import onnx
+            model = onnx.load(onx)
+            smodel = "\nJSON ONNX\n" + str(model)
+        else:
+            smodel = ""
+        raise OnnxRuntimeAssertionError("Model '{0}' has discrepencies.\n{1}: {2}{3}".format(onx, type(e), e, smodel))
+        
+    return output0, lambda_onnx
+    
+        
+def _post_process_output(res):
+    """
+    Applies post processings before running the comparison
+    such as changing type from list to arrays.
+    """
+    if isinstance(res, list):
+        if len(res) == 0:
+            return res
+        elif len(res) == 1:
+            return _post_process_output(res[0])
+        elif isinstance(res[0], numpy.ndarray):
+            return numpy.array(res)
+        elif isinstance(res[0], dict):
+            import pandas
+            return pandas.DataFrame(res).values
+        else:
+            ls = [len(r) for r in res]
+            mi = min(ls)
+            if mi != max(ls):
+                raise NotImplementedError("Unable to postprocess various number of outputs in [{0}, {1}]".format(min(ls), max(ls)))
+            if mi > 1:
+                output = []
+                for i in range(mi):
+                    output.append(_post_process_output([r[i] for r in res]))
+                return output
+            elif isinstance(res[0], list):
+                # list of lists
+                if isinstance(res[0][0], list):
+                    return numpy.array(res)
+                elif len(res[0]) == 1 and isinstance(res[0][0], dict):
+                    return _post_process_output([r[0] for r in res])
+                elif len(res) == 1:
+                    return res
+                else:
+                    if len(res[0]) != 1:
+                        raise NotImplementedError("Not conversion implemented for {0}".format(res))
+                    st = [r[0] for r in res]
+                    return numpy.vstack(st)
+            else:
+                return res
+    else:
+        return res
+
+def _create_column(values, dtype):
+    "Creates a column from values with dtype"
+    if str(dtype) == "tensor(int64)":
+        return numpy.array(values, dtype=numpy.int64)
+    elif str(dtype) == "tensor(float)":
+        return numpy.array(values, dtype=numpy.float32)
+    elif str(dtype) == "tensor(string)":
+        return numpy.array(values, dtype=numpy.str)
+    else:
+        raise OnnxRuntimeAssertionError("Unable to create one column from dtype '{0}'".format(dtype))
+
+
+def _compare_expected(expected, output, sess, onnx, decimal=5, verbose=False, **kwargs):
+    """
+    Compares the expected output against the runtime outputs.
+    This is specific to *onnxruntime* due to variable *sess*
+    of type *onnxruntime.InferenceSession*.
+    """
+    tested = 0
+    if isinstance(expected, list):
+        if isinstance(output, list):
+            if 'Out0' in kwargs:
+                expected = expected[:1]
+                output = output[:1]
+                del kwargs['Out0']
+            if 'Reshape' in kwargs:
+                del kwargs['Reshape']
+                output = numpy.hstack(output).ravel()
+                output = output.reshape((len(expected),
+                                         len(output.ravel()) // len(expected)))
+            if len(expected) != len(output):
+                raise OnnxRuntimeAssertionError("Unexpected number of outputs '{0}', expected={1}, got={2}".format(onnx, len(expected), len(output)))
+            for exp, out in zip(expected, output):
+                _compare_expected(exp, out, sess, onnx, decimal=5, verbose=verbose, **kwargs)
+                tested += 1
+        else:
+            raise OnnxRuntimeAssertionError("Type mismatch for '{0}', output type is {1}".format(onnx, type(output)))
+    elif isinstance(expected, dict):
+        if not isinstance(output, dict):
+            raise OnnxRuntimeAssertionError("Type mismatch for '{0}'".format(onnx))                
+        for k, v in output.items():
+            if k not in expected:
+                continue
+            msg = compare_outputs(expected[k], v, decimal=decimal, verbose=verbose, **kwargs)
+            if msg:
+                raise OnnxRuntimeAssertionError("Unexpected output '{0}' in model '{1}'\n{2}".format(k, onnx, msg))
+            tested += 1
+    elif isinstance(expected, numpy.ndarray):
+        if isinstance(output, list):
+            if expected.shape[0] == len(output) and isinstance(output[0], dict):
+                import pandas
+                output = pandas.DataFrame(output)
+                output = output[list(sorted(output.columns))]
+                output = output.values
+        if isinstance(output, (dict, list)):
+            if len(output) != 1:
+                ex = str(output)
+                if len(ex) > 170:
+                    ex = ex[:170] + "..."
+                raise OnnxRuntimeAssertionError("More than one output when 1 is expected for onnx '{0}'\n{1}".format(onnx, ex))
+            output = output[-1]
+        if not isinstance(output, numpy.ndarray):
+            raise OnnxRuntimeAssertionError("output must be an array for onnx '{0}' not {1}".format(onnx, type(output)))
+        msg = compare_outputs(expected, output, decimal=decimal, verbose=verbose, **kwargs)
+        if isinstance(msg, ExpectedAssertionError):
+            raise msg
+        if msg:
+            raise OnnxRuntimeAssertionError("Unexpected output in model '{0}'\n{1}".format(onnx, msg))
+        tested += 1
+    else:
+        from scipy.sparse.csr import csr_matrix
+        if isinstance(expected, csr_matrix):
+            # DictVectorizer
+            one_array = numpy.array(output)
+            dense = numpy.asarray(expected.todense())
+            msg = compare_outputs(dense, one_array, decimal=decimal, verbose=verbose, **kwargs)
+            if msg:
+                raise OnnxRuntimeAssertionError("Unexpected output in model '{0}'\n{1}".format(onnx, msg))
+            tested += 1
+        else:
+            raise OnnxRuntimeAssertionError("Unexpected type for expected output ({1}) and onnx '{0}'".format(onnx, type(expected)))
+    if tested == 0:
+        raise OnnxRuntimeAssertionError("No test for onnx '{0}'".format(onnx))        
+    
+
