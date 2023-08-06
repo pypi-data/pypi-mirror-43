80be671f6c5708971fccd5b37fb7decd806ab1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:46 +0000

    Add package haima/1.2.0/haima-1.2.0.tar.gz

---
+++ packages/haima/haima-1.2.0.tar.gz/haima-1.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  : 2019-03-21 9:38
+# @Author : haima
+# @File  : setup.py
+# @Software: PyCharm
+from distutils.core import setup
+
+setup(
+    name='haima',
+    version='1.2.0',
+    author='haima',
+    author_email='yj22sy@qq.com',
+    url= 'http://www.haima.com',
+    description= '列表嵌套循环改进,开启缩进功能'
+)
+
+# python setup.py sdist
+# python setup.py install
+# python setup.py register
+# python setup.py sdist upload
