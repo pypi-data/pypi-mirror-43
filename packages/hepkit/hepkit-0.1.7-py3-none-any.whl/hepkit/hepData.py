51b168eeec9c2285165eda9d0d59431c01c3a4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:24 +0000

    Add package hepkit/0.1.7/hepkit-0.1.7-py3-none-any.whl

---
+++ packages/hepkit/hepkit-0.1.7-py3-none-any.whl/hepkit/hepData.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def PrintPdColumns(df):
+	cols = [col for col in df.columns]
+	print(cols)
+
+def FlattenList(notflat_list):
+	flat_list = [item for sublist in notflat_list for item in sublist]
+	return flat_list
