1fc9410dc58d423600f9f42cf9f4a7398f72d812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:23 +0000

    Add package remhind/0.1.1/remhind-0.1.1-py3-none-any.whl

---
+++ packages/remhind/remhind-0.1.1-py3-none-any.whl/remhind/__main__.py
@@ -0,0 +1,45 @@
+import asyncio
+import argparse
+import logging
+import pathlib
+
+import gi
+import toml
+from xdg import XDG_CONFIG_HOME, XDG_CACHE_HOME
+
+from .monitor import monitor_calendars
+from .events import check_events, CalendarStore
+
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify  # noqa
+
+
+async def monitor_file_events(args):
+    with args.config.open() as fd:
+        config = toml.load(fd)
+
+    log_level = max(logging.CRITICAL - args.verbose * 10, logging.NOTSET)
+    logging.basicConfig(
+        format='%(asctime)s:%(levelname)s:%(message)s', level=log_level)
+    Notify.init('remhind')
+
+    calendars = CalendarStore(config['calendars'].values(), args.database)
+
+    events_checker = check_events(calendars)
+    calendars_monitor = monitor_calendars(config['calendars'], calendars)
+    await asyncio.gather(events_checker, calendars_monitor)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="remind event from vdirs")
+    parser.add_argument('-c', '--config', type=pathlib.Path,
+        default=XDG_CONFIG_HOME / 'remhind' / 'config')
+    parser.add_argument('-d', '--database', type=pathlib.Path,
+        default=XDG_CACHE_HOME / 'remhind.db')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+
+    asyncio.run(monitor_file_events(parser.parse_args()))
+
+
+if __name__ == '__main__':
+    main()
