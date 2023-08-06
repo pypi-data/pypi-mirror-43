3d95a9580633e3f1f9b418f141d50edcb3781390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:27 +0000

    Add package pactly/0.1/pactly-0.1.tar.gz

---
+++ packages/pactly/pactly-0.1.tar.gz/pactly-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='pactly',
+      version='0.1',
+      description='fastText prototype',
+      url='http://pactly.ai',
+      author='Alara Dirik',
+      author_email='alara@pactly.ai',
+      license='MIT',
+      packages=['pactly'],
+      zip_safe=False)
