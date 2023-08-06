6c6955fc71d2c95b4d65b0c2f4bed1bef3f14e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:27 +0000

    Add package suanpan/0.7.68/suanpan-0.7.68-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.7.68-py3-none-any.whl/suanpan/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from __future__ import print_function
+
+from suanpan import objects
+
+__version__ = "0.7.68"
+
+g = objects.Global()
---
+++ packages/suanpan/suanpan-0.7.68-py3-none-any.whl/suanpan/services/arguments.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from __future__ import print_function
+
+from suanpan.arguments import *  # pylint: disable-msg=w0614
+from suanpan.dw.arguments import *  # pylint: disable-msg=w0614
+from suanpan.storage.arguments import *  # pylint: disable-msg=w0614
