f965d543797c0b41e9003c4974b8d7a36f69a3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:48 +0000

    Add package edkahe-distributions/0.1/edkahe_distributions-0.1.tar.gz

---
+++ packages/edkahe-distributions/edkahe_distributions-0.1.tar.gz/edkahe_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='edkahe_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['edkahe_distributions'],
+      author='Ediz Herkert',
+      author_email='ediz-h@web.de',
+      zip_safe=False)
