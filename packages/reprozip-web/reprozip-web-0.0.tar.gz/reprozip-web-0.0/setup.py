17f37f9c5a0fa700b80db10e1adc40559debe94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:36 +0000

    Add package reprozip-web/0.0/reprozip-web-0.0.tar.gz

---
+++ packages/reprozip-web/reprozip-web-0.0.tar.gz/reprozip-web-0.0/setup.py
@@ -0,0 +1,8 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+setup(name='reprozip-web',
+      version='0.0',
+      py_modules=['reprozip_web'],
+      description="Allows the ReproZip unpacker to record and replay data journalism websites packaged as .rpz files")
