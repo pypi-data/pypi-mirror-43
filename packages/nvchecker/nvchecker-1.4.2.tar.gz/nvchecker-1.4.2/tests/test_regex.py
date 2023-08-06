7e6ecee6b9c2410da84fde3c85b4c2d86067f94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:35 +0000

    Add package nvchecker/1.4.2/nvchecker-1.4.2.tar.gz

---
+++ packages/nvchecker/nvchecker-1.4.2.tar.gz/nvchecker-1.4.2/tests/test_regex.py
@@ -0,0 +1,27 @@
+# MIT licensed
+# Copyright (c) 2013-2017 lilydjwg <lilydjwg@gmail.com>, et al.
+
+import pytest
+pytestmark = pytest.mark.asyncio
+
+@pytest.mark.skipif(True,
+                    reason='httpbin is overloaded?')
+async def test_regex_httpbin(get_version):
+    assert await get_version("example", {
+        "url": "https://httpbin.org/get",
+        "regex": '"User-Agent": "(\w+)"',
+        "user_agent": "Meow",
+    }) == "Meow"
+
+async def test_regex(get_version):
+    assert await get_version("example", {
+        "url": "http://example.net/",
+        "regex": 'for (\w+) examples',
+    }) == "illustrative"
+
+async def test_missing_ok(get_version):
+    assert await get_version("example", {
+        "url": "http://example.net/",
+        "regex": "foobar",
+        "missing_ok": True,
+    }) is None
