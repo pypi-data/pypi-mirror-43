cc0f6f749701c7e464990d634dcefbe96c5d7e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package liwc-analysis/1.2.2/liwc-analysis-1.2.2.tar.gz

---
+++ packages/liwc-analysis/liwc-analysis-1.2.2.tar.gz/liwc-analysis-1.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="liwc-analysis",
+    version="1.2.2",
+    author="Eric Wiener",
+    author_email="ericwiener3@gmail.com",
+    description="Driver for LIWC2015 analysis. LIWC2015 dictionary not included.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EricWiener/liwc-analysis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas',
+    ],
+    packages=["liwcanalysis"],
+)
