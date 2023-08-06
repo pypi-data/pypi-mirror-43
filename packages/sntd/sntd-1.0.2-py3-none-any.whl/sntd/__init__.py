032cf8eab9aab89b0977e7bfac1b4d029ccb664a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:38 +0000

    Add package sntd/1.0.2/sntd-1.0.2-py3-none-any.whl

---
+++ packages/sntd/sntd-1.0.2-py3-none-any.whl/sntd/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+from .curve_io import *
+from .simulation import *
+from .fitting import *
+from .ml import *
+
+import sncosmo
+sncosmo.Model._flux=_mlFlux
+sncosmo.PropagationEffect.propagate=_mlProp
+sncosmo.CCM89Dust=_CCM89Dust
+sncosmo.OD94Dust=_OD94Dust
+sncosmo.F99Dust=_F99Dust
