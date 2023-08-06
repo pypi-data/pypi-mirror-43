22e3319f503e817ec1e9b1c2c9cf78186cfe9a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:36 +0000

    Add package suanpan/0.7.70/suanpan-0.7.70.tar.gz

---
+++ packages/suanpan/suanpan-0.7.70.tar.gz/suanpan-0.7.70/suanpan/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import print_function
+
+from suanpan import objects
+
+<<<<<<< HEAD
+__version__ = "0.7.70"
+=======
+__version__ = "0.7.69"
+>>>>>>> develop
+
+g = objects.Global()
