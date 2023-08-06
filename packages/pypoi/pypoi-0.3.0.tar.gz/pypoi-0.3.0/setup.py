732050d41830af53f5fc728800f6448b9fe9ed3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:26 +0000

    Add package pypoi/0.3.0/pypoi-0.3.0.tar.gz

---
+++ packages/pypoi/pypoi-0.3.0.tar.gz/pypoi-0.3.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+desciption = '"Py"thon program for "Poi"sson Image Editing'
+long_description = open('README.md').read()
+
+setup(
+    name='pypoi',
+    version='0.3.0',
+    description=desciption,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+
+    author='Fumihiro Bessho (Ben)',
+    author_email='fumihiro.bessho@gmail.com',
+    url='https://github.com/fbessho/PyPoi',
+    packages=['pypoi', 'pypoi.testimages'],
+    package_data={'pypoi': ['testimages/*.png']},
+    scripts=['scripts/pypoi'],
+    install_requires=['Pillow', 'numpy', 'pyamg', 'scipy', 'future']
+)
+
