8503790f22164e41c18a31cfb208bbf278be5766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:21 +0000

    Add package Lugger-Framework/0.0.1/Lugger%20Framework-0.0.1.tar.gz

---
+++ packages/Lugger-Framework/Lugger%20Framework-0.0.1.tar.gz/Lugger Framework-0.0.1/README.rst
@@ -0,0 +1,5 @@
+================
+Lugger Framework
+================
+
+Faster, small and easy web framework
---
+++ packages/Lugger-Framework/Lugger%20Framework-0.0.1.tar.gz/Lugger Framework-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='Lugger Framework',
+    version='0.0.1',
+    author='Oleksandr Popov',
+    author_email='oleksandr.popov@outlook.com',
+    description='Faster, small and easy web framework',
+    long_description=long_description,
+    long_description_content_type=' text/x-rst',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)
