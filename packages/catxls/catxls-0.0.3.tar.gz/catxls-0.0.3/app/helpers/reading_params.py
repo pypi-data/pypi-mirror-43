101df9d9a95cc1162b38192d6e726b3f0fbd2479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:33 +0000

    Add package catxls/0.0.3/catxls-0.0.3.tar.gz

---
+++ packages/catxls/catxls-0.0.3.tar.gz/catxls-0.0.3/app/helpers/reading_params.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#! -*- coding:utf-8 -*-
+""" Helper get argument of scripts
+"""
+
+import argparse
+import os
+
+
+def _check_file_exist(file_arg):
+  """ Check file is exits """
+  if not os.path.exist(file_arg):
+    raise argparse.ArgumentError('File %s doest exist', file_arg)
+  return file_arg
+
+
+__parser__ = argparse.ArgumentParser(description='input')
+__parser__.add_argument(
+    action='store',
+    dest='file_input',
+    type=open,
+    metavar="FILE",
+    help="The input FILE xls"
+)
+# __parser__.add_argument(
+#     '-i', '--input',
+#     action='store',
+#     dest='file_input',
+#     type=open,
+#     metavar="FILE",
+#     help="The input FILE"
+# )
+# __parser__.add_argument(
+#     '-o', '--output',
+#     action='store',
+#     dest='file_output',
+#     metavar="FILE",
+#     help="The output FILE "
+# )
+
+
+def get_argument():
+    """return argument  """
+    args = __parser__.parse_args()
+    return args
---
+++ packages/catxls/catxls-0.0.3.tar.gz/catxls-0.0.3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#!-*-coding:utf8-*-
+"""File setup.py"""
+from setuptools import setup, find_packages
+setup(
+    name='catxls',
+    version='0.0.3',
+    packages=find_packages(
+        '.',
+        exclude=[
+            '*.tests', '*.tests.*', 'tests.*', 'tests'
+        ]
+    ),
+    license='MIT',
+    description='read xls, xlsx on server',
+    author='huongnhd',
+    author_email = 'huong.nhdh@gmail.com',
+    url = 'https://github.com/huongnhdh/catxls.git',
+    download_url = 'https://github.com/huongnhdh/catxls/archive/v_003.tar.gz',
+    keywords = ['xls', 'xlsx', 'cat xlsx', 'server'],
+    install_requires=[
+      'pyexcel==0.5.13',
+      'pyexcel-xls==0.5.8'
+    ],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: System Administrators',
+    'Topic :: System :: System Shells',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+    entry_points="""
+    [console_scripts]
+    catxls=app.scripts.catxlsx:_main
+    """
+)
