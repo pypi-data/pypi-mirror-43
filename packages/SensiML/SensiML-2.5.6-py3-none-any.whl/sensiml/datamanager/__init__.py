8f6d613f331a13b50ad171171da270bac005fbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:34 +0000

    Add package SensiML/2.5.6/SensiML-2.5.6-py3-none-any.whl

---
+++ packages/SensiML/SensiML-2.5.6-py3-none-any.whl/sensiml/datamanager/__init__.py
@@ -0,0 +1,11 @@
+
+from sensiml.datamanager.functions import Functions
+from sensiml.datamanager.platformdescriptions import PlatformDescriptions
+from sensiml.datamanager.pipelineseeds import  PipelineSeeds
+from sensiml.datamanager.featurefiles import FeatureFiles
+from sensiml.datamanager.projects import Projects
+from sensiml.datamanager.segmenter import Segmenters
+
+
+
+__all__ = ['Functions', 'FeatureFiles', 'Projects', 'Segmenters', 'PipelineSeeds','PlatformDescriptions']
