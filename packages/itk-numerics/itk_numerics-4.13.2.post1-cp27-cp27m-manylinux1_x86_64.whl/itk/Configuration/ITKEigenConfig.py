2b0e7b02ccb3b5548b7a9ce133c65d87556cc962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:57 +0000

    Add package itk-numerics/4.13.2.post1/itk_numerics-4.13.2.post1-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/itk-numerics/itk_numerics-4.13.2.post1-cp27-cp27m-manylinux1_x86_64.whl/itk/Configuration/ITKEigenConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKImageFilterBase', )
+templates = (
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF22', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,2 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF32', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,3 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF42', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,4 >,2 >'),
+)
