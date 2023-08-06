5efe38a552faf77cea00713d91352cf0f3249572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:36 +0000

    Add package listentoeverything/0.3.3/listentoeverything-0.3.3-py2.py3-none-any.whl

---
+++ packages/listentoeverything/listentoeverything-0.3.3-py2.py3-none-any.whl/listentoeverything/cli.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for listentoeverything."""
+import sys
+import click
+from listentoeverything import listen
+from os import path
+sys.path.append('..')
+
+
+@click.command()
+@click.option("--config_file", default=path.join(path.expanduser("~"), ".listen.yml"), help="Path of config file")
+def main(config_file):
+    """Console script for listentoeverything."""
+    listen.main(config_file)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
