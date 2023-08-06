4ab5263a1f50b69ad5306e696486aae139b780d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:42 +0000

    Add package refinery/2.1.1/refinery-2.1.1-py3.5.egg

---
+++ packages/refinery/refinery-2.1.1-py3.5.egg/refinery/__init__.py
@@ -0,0 +1,10 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#           YYYY.MM.DD
+__date__ = "2019.03.25"
+__version__ = (2, 1, 1)
+__all__ = (
+    'hashcacher', 'hashcacher_window', 'main', 'meta_window', 'widgets', 'crc_functions'
+    )
