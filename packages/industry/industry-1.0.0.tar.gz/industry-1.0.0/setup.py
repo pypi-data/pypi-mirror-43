5aebff2f69e6b933432a317834ed8a9520e925ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:25 +0000

    Add package industry/1.0.0/industry-1.0.0.tar.gz

---
+++ packages/industry/industry-1.0.0.tar.gz/industry-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name	=	'industry',
+	version	=	'1.0.0',
+	py_modules	=	['industry11_090319'],
+	author	=	'shm',
+	url	=	'https://www.python.org/sigs/distutils-sig/',
+	description = 'first dist',
+)
