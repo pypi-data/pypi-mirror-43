16cec6d5e903d5d34f354ed2a9e1c0028b857025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:43 +0000

    Add package lifelines/0.20.1/lifelines-0.20.1.tar.gz

---
+++ packages/lifelines/lifelines-0.20.1.tar.gz/lifelines-0.20.1/lifelines/fitters/exponential_fitter.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from lifelines.fitters import KnownModelParametericUnivariateFitter
+
+
+class ExponentialFitter(KnownModelParametericUnivariateFitter):
+    r"""
+    This class implements an Exponential model for univariate data. The model has parameterized
+    form:
+
+    .. math::  S(t) = \exp\left(\frac{-t}{\lambda}\right),   \lambda >0
+
+    which implies the cumulative hazard rate is
+
+    .. math::  H(t) = \frac{t}{\lambda}
+
+    and the hazard rate is:
+
+    .. math::  h(t) = \frac{1}{\lambda}
+
+    After calling the `.fit` method, you have access to properties like: ``survival_function_``, ``lambda_``, ``cumulative_hazard_``
+    A summary of the fit is available with the method ``print_summary()``
+
+    Important
+    ----------
+    The parameterization of this model changed in lifelines 0.19.0. Previously, the cumulative hazard looked like
+    :math:`\lambda t`. The parameterization is now the reciprocal of :math:`\lambda`.
+
+
+    Notes
+    -----
+    Reference: https://www4.stat.ncsu.edu/~dzhang2/st745/chap3.pdf
+
+    Attributes
+    ----------
+    cumulative_hazard_ : DataFrame
+        The estimated cumulative hazard (with custom timeline if provided)
+    confidence_interval_cumulative_hazard_ : DataFrame
+        The lower and upper confidence intervals for the cumulative hazard
+    hazard_ : DataFrame
+        The estimated hazard (with custom timeline if provided)
+    confidence_interval_hazard_ : DataFrame
+        The lower and upper confidence intervals for the hazard
+    survival_function_ : DataFrame
+        The estimated survival function (with custom timeline if provided)
+    confidence_interval_survival_function_ : DataFrame
+        The lower and upper confidence intervals for the survival function
+    variance_matrix_ : numpy array
+        The variance matrix of the coefficients
+    median_: float
+        The median time to event
+    lambda_: float
+        The fitted parameter in the model
+    durations: array
+        The durations provided
+    event_observed: array
+        The event_observed variable provided
+    timeline: array
+        The time line to use for plotting and indexing
+    entry: array or None
+        The entry array provided, or None
+    """
+
+    _fitted_parameter_names = ["lambda_"]
+
+    @property
+    def median_(self):
+        return np.log(2) / self.lambda_
+
+    def _fit_model(self, T, E, entry, show_progress=False):
+        T = T - entry
+        lambda_ = T.sum() / E.sum()
+        lambda_variance_ = T.sum() / lambda_
+        log_likelihood = -np.log(lambda_) * E.sum() - T.sum() / lambda_
+        return [lambda_], log_likelihood, np.array([[1.0 / lambda_variance_]])
+
+    def _cumulative_hazard(self, params, times):
+        lambda_ = params[0]
+        return times / lambda_
