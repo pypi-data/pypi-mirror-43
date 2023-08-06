ec9db6f1ecb41e2f7e2f2e672cd8361f8e9e323e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:29 +0000

    Add package metasdk/1.0.14/metasdk-1.0.14-py3-none-any.whl

---
+++ packages/metasdk/metasdk-1.0.14-py3-none-any.whl/metasdk/exceptions.py
@@ -0,0 +1,99 @@
+# Более подробное описание в документации по ошибкам что в каком случае использовать
+
+
+class SDKError(Exception):
+    """
+    Корневая ошибка для SDK
+    """
+    pass
+
+
+class BadRequestError(SDKError):
+    """
+    Запрос принципиально неправильный (неверная версия api, не передан обязательный параметр и пр.) (HTTP 400)
+    """
+    pass
+
+
+class AuthError(SDKError):
+    """
+    Невозможно авторизоваться (HTTP 401)
+    """
+    pass
+
+
+class ForbiddenError(SDKError):
+    """
+    Нет прав для соверщения операции (HTTP 403)
+    """
+    pass
+
+
+class ServerError(SDKError):
+    """
+    Сервер ответил ошибкой (HTTP >500)
+    """
+    pass
+
+
+class NoContentError(SDKError):
+    """
+    Нет содержимого (HTTP 204)
+    """
+    pass
+
+
+class RequestError(SDKError):
+    """
+    Ошибка исполнения запроса(HTTP >400)
+    """
+    pass
+
+
+class UnexpectedError(SDKError):
+    """
+    Непредвиденная ошибка (HTTP other)
+    """
+    pass
+
+
+class DbQueryError(SDKError):
+    """
+    Ошибка работы с базой данных
+    """
+    pass
+
+
+class RetryHttpRequestError(SDKError):
+    """
+    Невозможность повторного запроса (HTTP 502, 503, 504)
+    """
+    pass
+
+
+class ApiProxyError(SDKError):
+    """
+    Ошибка работы с прокси
+    """
+    pass
+
+
+class EndOfTriesError(SDKError):
+    """
+    Достигнут лимит количества повторов запроса
+    """
+    pass
+
+
+class LockServiceError(SDKError):
+    """
+    Ошибка при работе с сервисом блокировки очереди (LockService)
+    """
+    pass
+
+
+class BadParametersError(SDKError):
+    """
+    Ошибка неправильной настройки параметров для запроса апи или фидов
+    """
+    pass
