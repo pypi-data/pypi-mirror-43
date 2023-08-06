a489510881afa59d480a68244487fcc2634f000a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package xcovlib/0.7.0/xcovlib-0.7.0.tar.gz

---
+++ packages/xcovlib/xcovlib-0.7.0.tar.gz/xcovlib-0.7.0/xcovlib/http/auth.py
@@ -0,0 +1,47 @@
+"""
+Authentication related module.
+"""
+from __future__ import unicode_literals
+
+import hmac
+import re
+from base64 import b64encode
+from hashlib import sha1
+from urllib.parse import quote
+
+from ..utils.dates import http_date
+
+SIGNATURE_RE = re.compile('signature="(.+?)"')
+
+
+class SignatureAuth(object):
+    """Class for basic authentication support."""
+
+    def __init__(self, key, secret):
+        self._key = key
+        self._secret = secret
+        self._headers = None
+
+    def create_signature(self, date):
+        raw = 'date: {date}'.format(date=date)
+        hashed = hmac.digest(self._secret.encode('utf-8'), raw.encode('utf-8'), sha1)
+        return quote(b64encode(hashed))
+
+    def build_signature(self, signature, key):
+        template = ('Signature keyId="%(key)s",algorithm="hmac-sha1",'
+                    'signature="%(signature)s"')
+
+        return template % {
+            'key': key,
+            'signature': signature
+        }
+
+    def sign(self):
+        date = http_date()
+        auth = self.build_signature(signature=self.create_signature(date), key=self._key)
+
+        return {
+            'Date': date,
+            'Authorization': auth,
+            'X-Api-Key': self._key,
+        }
