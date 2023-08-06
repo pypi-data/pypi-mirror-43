16adaaf17941081185b81e7855b6d32d703e5214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:52 +0000

    Add package packt-calc/0.3.0/packt-calc-0.3.0.tar.gz

---
+++ packages/packt-calc/packt-calc-0.3.0.tar.gz/packt-calc-0.3.0/packt_calc/cli.py
@@ -0,0 +1,4 @@
+from packt_calc.calc import start_server
+
+def main():
+    start_server()
---
+++ packages/packt-calc/packt-calc-0.3.0.tar.gz/packt-calc-0.3.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ 
+    'aiohttp>=3.5',
+]
+
+setup_requirements = [ 
+    'pytest-runner'
+]
+
+test_requirements = [ 
+    'pytest'
+]
+
+setup(
+    author="Ada Lovelace",
+    author_email='myself@mail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+    description="My cool python project",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    name='packt-calc',
+    packages=find_packages(include=['packt_calc']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/PacktPublishing/Migrating-from-Python-2-to-Python-3/chapter08/packt-calc',
+    version='0.3.0',
+    zip_safe=True,
+    entry_points={
+        'console_scripts': ['packt-calc=packt_calc.cli:main'],
+    }
+)
