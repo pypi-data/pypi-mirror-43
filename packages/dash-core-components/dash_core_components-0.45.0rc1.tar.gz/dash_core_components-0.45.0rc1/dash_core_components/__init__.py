da251e632e60d0d8c7d2dd770a343b9d5f29b8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:42 +0000

    Add package dash-core-components/0.45.0rc1/dash_core_components-0.45.0rc1.tar.gz

---
+++ packages/dash-core-components/dash_core_components-0.45.0rc1.tar.gz/dash_core_components-0.45.0rc1/dash_core_components/__init__.py
@@ -0,0 +1,48 @@
+"""This package provides the core React component suite for Dash."""
+
+from __future__ import print_function as _
+
+import os as _os
+import sys as _sys
+
+from .version import __version__
+
+from ._imports_ import *
+from ._imports_ import __all__
+
+
+_current_path = _os.path.dirname(_os.path.abspath(__file__))
+
+
+_this_module = _sys.modules[__name__]
+
+
+_js_dist = [
+    {
+        'external_url': 'https://cdn.plot.ly/plotly-1.45.0.min.js',
+        'relative_package_path': 'plotly-1.45.0.min.js',
+        'namespace': 'dash_core_components'
+    },
+    {
+        'relative_package_path': '{}.min.js'.format(__name__),
+        'dev_package_path': '{}.dev.js'.format(__name__),
+        'external_url': (
+            'https://unpkg.com/dash-core-components@{}'
+            '/dash_core_components/dash_core_components.min.js'
+        ).format('0.45.0-rc1'),
+        'namespace': 'dash_core_components'
+    },
+    {
+        'relative_package_path': '{}.min.js.map'.format(__name__),
+        'dev_package_path': '{}.dev.js.map'.format(__name__),
+        'external_url': (
+            'https://unpkg.com/dash-core-components@{}'
+            '/dash_core_components/dash_core_components.min.js.map'
+        ).format('0.45.0-rc1'),
+        'namespace': 'dash_core_components',
+        'dynamic': True
+    }
+]
+
+for _component in __all__:
+    setattr(locals()[_component], '_js_dist', _js_dist)
---
+++ packages/dash-core-components/dash_core_components-0.45.0rc1.tar.gz/dash_core_components-0.45.0rc1/dash_core_components/_imports_.py
@@ -0,0 +1,49 @@
+from ._checklist import Checklist
+from ._confirm_dialog import ConfirmDialog
+from ._confirm_dialog_provider import ConfirmDialogProvider
+from ._date_picker_range import DatePickerRange
+from ._date_picker_single import DatePickerSingle
+from ._dropdown import Dropdown
+from ._graph import Graph
+from ._input import Input
+from ._interval import Interval
+from ._link import Link
+from ._loading import Loading
+from ._location import Location
+from ._logout_button import LogoutButton
+from ._markdown import Markdown
+from ._radio_items import RadioItems
+from ._range_slider import RangeSlider
+from ._slider import Slider
+from ._store import Store
+from ._syntax_highlighter import SyntaxHighlighter
+from ._tab import Tab
+from ._tabs import Tabs
+from ._textarea import Textarea
+from ._upload import Upload
+
+__all__ = [
+    "Checklist",
+    "ConfirmDialog",
+    "ConfirmDialogProvider",
+    "DatePickerRange",
+    "DatePickerSingle",
+    "Dropdown",
+    "Graph",
+    "Input",
+    "Interval",
+    "Link",
+    "Loading",
+    "Location",
+    "LogoutButton",
+    "Markdown",
+    "RadioItems",
+    "RangeSlider",
+    "Slider",
+    "Store",
+    "SyntaxHighlighter",
+    "Tab",
+    "Tabs",
+    "Textarea",
+    "Upload"
+]
