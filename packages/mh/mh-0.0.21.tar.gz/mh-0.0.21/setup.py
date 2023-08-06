c3df8746fc92db444293046016f1b2b829810346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:08 +0000

    Add package mh/0.0.21/mh-0.0.21.tar.gz

---
+++ packages/mh/mh-0.0.21.tar.gz/mh-0.0.21/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mh",
+    version="0.0.21",
+    author="Mark Hallett",
+    author_email="mh@markhallett.co.uk",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markhallett/mh",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
