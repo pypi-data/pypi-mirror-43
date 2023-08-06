d078313853ab3381a5ed86ff6c41319e8eb01eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package vigilancemeteo/2.0.0/vigilancemeteo-2.0.0-py2.py3-none-any.whl

---
+++ packages/vigilancemeteo/vigilancemeteo-2.0.0-py2.py3-none-any.whl/vigilancemeteo/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""Vigilancemeteo provide an API to fetch France weather alerts from Météo
+France website.
+
+
+ZoneAlerte class allows to fetch active weather alerts for a french department.
+"""
+
+from .vigilance import ZoneAlerte
+from .__version__ import __version__, VERSION
