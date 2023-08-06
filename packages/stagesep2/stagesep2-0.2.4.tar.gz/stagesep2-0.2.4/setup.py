c6acae6bebadc0939880f33c28564d8a8693cc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:19 +0000

    Add package stagesep2/0.2.4/stagesep2-0.2.4.tar.gz

---
+++ packages/stagesep2/stagesep2-0.2.4.tar.gz/stagesep2-0.2.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='stagesep2',
+    version='0.2.4',
+    description='Analyse, and convert video into useful data.',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/stagesep2',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'opencv-python',
+        'structlog',
+        'numpy',
+        'jieba',
+        'scikit-image',
+        'pyecharts',
+        'pyecharts_snapshot',
+        'findit',
+        'tesserocr',
+        'Pillow',
+    ]
+)
