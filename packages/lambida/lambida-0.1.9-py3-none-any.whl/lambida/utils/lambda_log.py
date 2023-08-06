1dc345d4944f3ac9ebc7513353532b706268afc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:04 +0000

    Add package lambida/0.1.9/lambida-0.1.9-py3-none-any.whl

---
+++ packages/lambida/lambida-0.1.9-py3-none-any.whl/lambida/utils/lambda_log.py
@@ -0,0 +1,35 @@
+"""AWS Lambda Logging."""
+import logging
+import sys
+import os
+import functools
+import traceback
+
+
+def logging_setup(aws_request_id):
+    """Basic config."""
+    root = logging.getLogger(os.path.basename(__file__))
+    if not root.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "%(levelname)s RequestId: "+aws_request_id+" %(message)s")
+        handler.setFormatter(formatter)
+        root.addHandler(handler)
+        root.setLevel(logging.INFO)
+        root.propagate = False
+        return root
+    return root
+
+
+def log_event_on_error(handler):
+    """Basic Error Logger."""
+    @functools.wraps(handler)
+    def wrapper(event, context):
+        try:
+            return handler(event, context)
+        except Exception:
+            log = logging_setup(context.aws_request_id)
+            log.error('ERROR EVENT: {}'
+                      .format(traceback.format_exc().replace('\n', "").replace('  ', ".")))
+            raise
+    return wrapper
