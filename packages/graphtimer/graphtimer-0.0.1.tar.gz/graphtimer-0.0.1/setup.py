98af383160cb0d3b308b9643e022ffdc6677a02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package graphtimer/0.0.1/graphtimer-0.0.1.tar.gz

---
+++ packages/graphtimer/graphtimer-0.0.1.tar.gz/graphtimer-0.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+description = 'Time code execution and graph results'
+long_description = ''''''
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+]
+
+install_requires = []
+
+test_requires = []
+
+extra_requires = {
+    'base': [
+        'matplotlib',
+    ]
+}
+
+setup(
+    name='graphtimer',
+    version=version,
+    description=description,
+    long_description=long_description,
+    author='Peilonrayz',
+    author_email='peilonrayz@gmail.com',
+    url='https://github.com/Peilonrayz/graphtimer',
+    license='MIT',
+    keywords='timer graph performance',
+    packages=find_packages(),
+    classifiers=classifiers,
+    install_requires=install_requires,
+    tests_require=test_requires,
+    extras_require=extra_requires,
+)
