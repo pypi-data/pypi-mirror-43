87b6929c29450a91c1d7aabf1ed2362ab237ddce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:18 +0000

    Add package gitlint/0.11.0/gitlint-0.11.0-py2.py3-none-any.whl

---
+++ packages/gitlint/gitlint-0.11.0-py2.py3-none-any.whl/gitlint/tests/test_hooks.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+try:
+    # python 2.x
+    from mock import patch, ANY, mock_open
+except ImportError:
+    # python 3.x
+    from unittest.mock import patch, ANY, mock_open  # pylint: disable=no-name-in-module, import-error
+
+from gitlint.tests.base import BaseTestCase
+from gitlint.config import LintConfig
+from gitlint.hooks import GitHookInstaller, GitHookInstallerError, COMMIT_MSG_HOOK_SRC_PATH, COMMIT_MSG_HOOK_DST_PATH, \
+    GITLINT_HOOK_IDENTIFIER
+
+
+class HookTests(BaseTestCase):
+    def test_commit_msg_hook_path(self):
+        lint_config = LintConfig()
+        lint_config.target = self.SAMPLES_DIR
+        expected_path = os.path.join(self.SAMPLES_DIR, COMMIT_MSG_HOOK_DST_PATH)
+        path = GitHookInstaller.commit_msg_hook_path(lint_config)
+        self.assertEqual(path, expected_path)
+
+    @staticmethod
+    @patch('os.chmod')
+    @patch('os.stat')
+    @patch('gitlint.hooks.shutil.copy')
+    @patch('os.path.exists', return_value=False)
+    @patch('os.path.isdir', return_value=True)
+    def test_install_commit_msg_hook(isdir, path_exists, copy, stat, chmod):
+        lint_config = LintConfig()
+        lint_config.target = u"/foo/bår"
+        expected_dst = os.path.join(u"/foo/bår", COMMIT_MSG_HOOK_DST_PATH)
+        GitHookInstaller.install_commit_msg_hook(lint_config)
+        isdir.assert_called_with(u"/foo/bår" + '/.git/hooks')
+        path_exists.assert_called_once_with(expected_dst)
+        copy.assert_called_once_with(COMMIT_MSG_HOOK_SRC_PATH, expected_dst)
+        stat.assert_called_once_with(expected_dst)
+        chmod.assert_called_once_with(expected_dst, ANY)
+
+    @patch('gitlint.hooks.shutil.copy')
+    @patch('os.path.exists', return_value=False)
+    @patch('os.path.isdir', return_value=True)
+    def test_install_commit_msg_hook_negative(self, isdir, path_exists, copy):
+        lint_config = LintConfig()
+        lint_config.target = u"/foo/bår"
+        # mock that current dir is not a git repo
+        isdir.return_value = False
+        expected_msg = u"{0} is not a git repository".format(u"/foo/bår")
+        with self.assertRaisesRegex(GitHookInstallerError, expected_msg):
+            GitHookInstaller.install_commit_msg_hook(lint_config)
+            isdir.assert_called_with(os.path.join(u"/foo/bår", '.git/hooks'))
+            path_exists.assert_not_called()
+            copy.assert_not_called()
+
+        # mock that there is already a commit hook present
+        isdir.return_value = True
+        path_exists.return_value = True
+        expected_dst = os.path.join(u"/foo/bår", COMMIT_MSG_HOOK_DST_PATH)
+        expected_msg = u"There is already a commit-msg hook file present in {0}.\n".format(expected_dst) + \
+                       "gitlint currently does not support appending to an existing commit-msg file."
+        with self.assertRaisesRegex(GitHookInstallerError, expected_msg):
+            GitHookInstaller.install_commit_msg_hook(lint_config)
+
+    @staticmethod
+    @patch('os.remove')
+    @patch('os.path.exists', return_value=True)
+    @patch('os.path.isdir', return_value=True)
+    def test_uninstall_commit_msg_hook(isdir, path_exists, remove):
+        lint_config = LintConfig()
+        lint_config.target = u"/foo/bår"
+        read_data = "#!/bin/sh\n" + GITLINT_HOOK_IDENTIFIER
+        with patch('gitlint.hooks.open', mock_open(read_data=read_data), create=True):
+            GitHookInstaller.uninstall_commit_msg_hook(lint_config)
+
+        expected_dst = os.path.join(u"/foo/bår", COMMIT_MSG_HOOK_DST_PATH)
+        isdir.assert_called_with(os.path.join(u"/foo/bår", '.git/hooks'))
+        path_exists.assert_called_once_with(expected_dst)
+        remove.assert_called_with(expected_dst)
+
+    @patch('os.remove')
+    @patch('os.path.exists', return_value=True)
+    @patch('os.path.isdir', return_value=True)
+    def test_uninstall_commit_msg_hook_negative(self, isdir, path_exists, remove):
+        lint_config = LintConfig()
+        lint_config.target = u"/foo/bår"
+        # mock that the current directory is not a git repo
+        isdir.return_value = False
+        expected_msg = u"{0} is not a git repository".format(u"/foo/bår")
+        with self.assertRaisesRegex(GitHookInstallerError, expected_msg):
+            GitHookInstaller.uninstall_commit_msg_hook(lint_config)
+            isdir.assert_called_with(os.path.join(u"/foo/bår", '.git/hooks'))
+            path_exists.assert_not_called()
+            remove.assert_not_called()
+
+        # mock that there is no commit hook present
+        isdir.return_value = True
+        path_exists.return_value = False
+        expected_dst = os.path.join(u"/foo/bår", COMMIT_MSG_HOOK_DST_PATH)
+        expected_msg = u"There is no commit-msg hook present in {0}.".format(expected_dst)
+        with self.assertRaisesRegex(GitHookInstallerError, expected_msg):
+            GitHookInstaller.uninstall_commit_msg_hook(lint_config)
+            isdir.assert_called_with(os.path.join(u"/foo/bår", '.git/hooks'))
+            path_exists.assert_called_once_with(expected_dst)
+            remove.assert_not_called()
+
+        # mock that there is a different (=not gitlint) commit hook
+        isdir.return_value = True
+        path_exists.return_value = True
+        read_data = "#!/bin/sh\nfoo"
+        expected_dst = os.path.join(u"/foo/bår", COMMIT_MSG_HOOK_DST_PATH)
+        expected_msg = u"The commit-msg hook in {0} was not installed by gitlint ".format(expected_dst) + \
+                       r"\(or it was modified\).\nUninstallation of 3th party or modified gitlint hooks " + \
+                       "is not supported."
+        with patch('gitlint.hooks.open', mock_open(read_data=read_data), create=True):
+            with self.assertRaisesRegex(GitHookInstallerError, expected_msg):
+                GitHookInstaller.uninstall_commit_msg_hook(lint_config)
+            remove.assert_not_called()
---
+++ packages/gitlint/gitlint-0.11.0-py2.py3-none-any.whl/qa/test_hooks.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=too-many-function-args,unexpected-keyword-arg
+from sh import git, gitlint  # pylint: disable=no-name-in-module
+from qa.base import BaseTestCase
+
+
+class HookTests(BaseTestCase):
+    """ Integration tests for gitlint commitmsg hooks"""
+
+    VIOLATIONS = ['gitlint: checking commit message...\n',
+                  u'1: T3 Title has trailing punctuation (.): "WIP: This ïs a title."\n',
+                  u'1: T5 Title contains the word \'WIP\' (case-insensitive): "WIP: This ïs a title."\n',
+                  u'2: B4 Second line is not empty: "Contënt on the second line"\n',
+                  '3: B6 Body message is missing\n',
+                  '-----------------------------------------------\n',
+                  'gitlint: \x1b[31mYour commit message contains the above violations.\x1b[0m\n']
+
+    def setUp(self):
+        self.responses = []
+        self.response_index = 0
+        self.githook_output = []
+
+        # install git commit-msg hook and assert output
+        output_installed = gitlint("install-hook", _cwd=self.tmp_git_repo)
+        expected_installed = u"Successfully installed gitlint commit-msg hook in %s/.git/hooks/commit-msg\n" % \
+                             self.tmp_git_repo
+        self.assertEqualStdout(output_installed, expected_installed)
+
+    def tearDown(self):
+        # uninstall git commit-msg hook and assert output
+        output_uninstalled = gitlint("uninstall-hook", _cwd=self.tmp_git_repo)
+        expected_uninstalled = u"Successfully uninstalled gitlint commit-msg hook from %s/.git/hooks/commit-msg\n" % \
+                               self.tmp_git_repo
+        self.assertEqualStdout(output_uninstalled, expected_uninstalled)
+
+    def _violations(self):
+        # Make a copy of the violations array so that we don't inadvertently edit it in the test (like I did :D)
+        return list(self.VIOLATIONS)
+
+        # callback function that captures git commit-msg hook output
+
+    def _interact(self, line, stdin):
+        self.githook_output.append(line)
+        # Answer 'yes' to question to keep violating commit-msg
+        if "Your commit message contains the above violations" in line:
+            response = self.responses[self.response_index]
+            stdin.put("{0}\n".format(response))
+            self.response_index = (self.response_index + 1) % len(self.responses)
+
+    def test_commit_hook_continue(self):
+        self.responses = ["y"]
+        test_filename = self._create_simple_commit(u"WIP: This ïs a title.\nContënt on the second line",
+                                                   out=self._interact, tty_in=True)
+
+        # Determine short commit-msg hash, needed to determine expected output
+        short_hash = git("rev-parse", "--short", "HEAD", _cwd=self.tmp_git_repo, _tty_in=True).replace("\n", "")
+
+        expected_output = self._violations()
+        expected_output += ["Continue with commit anyways (this keeps the current commit message)? " +
+                            "[y(es)/n(no)/e(dit)] " +
+                            u"[master (root-commit) %s] WIP: This ïs a title. Contënt on the second line\n"
+                            % short_hash,
+                            " 1 file changed, 0 insertions(+), 0 deletions(-)\n",
+                            u" create mode 100644 %s\n" % test_filename]
+
+        assert len(self.githook_output) == len(expected_output)
+        for output, expected in zip(self.githook_output, expected_output):
+            self.assertMultiLineEqual(
+                output.replace('\r', ''),
+                expected.replace('\r', ''))
+
+    def test_commit_hook_abort(self):
+        self.responses = ["n"]
+        test_filename = self._create_simple_commit(u"WIP: This ïs a title.\nContënt on the second line",
+                                                   out=self._interact, ok_code=1, tty_in=True)
+        git("rm", "-f", test_filename, _cwd=self.tmp_git_repo)
+
+        # Determine short commit-msg hash, needed to determine expected output
+
+        expected_output = self._violations()
+        expected_output += ["Continue with commit anyways (this keeps the current commit message)? " +
+                            "[y(es)/n(no)/e(dit)] " +
+                            "Commit aborted.\n",
+                            "Your commit message: \n",
+                            "-----------------------------------------------\n",
+                            u"WIP: This ïs a title.\n",
+                            u"Contënt on the second line\n",
+                            "-----------------------------------------------\n"]
+
+        self.assertListEqual(expected_output, self.githook_output)
+
+    def test_commit_hook_edit(self):
+        self.responses = ["e", "y"]
+        env = {"EDITOR": ":"}
+        test_filename = self._create_simple_commit(u"WIP: This ïs a title.\nContënt on the second line",
+                                                   out=self._interact, env=env, tty_in=True)
+        git("rm", "-f", test_filename, _cwd=self.tmp_git_repo)
+
+        short_hash = git("rev-parse", "--short", "HEAD", _cwd=self.tmp_git_repo, _tty_in=True).replace("\n", "")
+
+        # Determine short commit-msg hash, needed to determine expected output
+
+        expected_output = self._violations()
+        expected_output += ['Continue with commit anyways (this keeps the current commit message)? ' +
+                            '[y(es)/n(no)/e(dit)] ' + self._violations()[0]]
+        expected_output += self._violations()[1:]
+        expected_output += ['Continue with commit anyways (this keeps the current commit message)? ' +
+                            "[y(es)/n(no)/e(dit)] " +
+                            u"[master %s] WIP: This ïs a title. Contënt on the second line\n" % short_hash,
+                            " 1 file changed, 0 insertions(+), 0 deletions(-)\n",
+                            u" create mode 100644 %s\n" % test_filename]
+
+        assert len(self.githook_output) == len(expected_output)
+        for output, expected in zip(self.githook_output, expected_output):
+            self.assertMultiLineEqual(
+                output.replace('\r', ''),
+                expected.replace('\r', ''))
