ba050a8f0047dbca2725af8ac5dbe5adb8cc0edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:05 +0000

    Add package ventana/v0.1.3/ventana-v0.1.3.tar.gz

---
+++ packages/ventana/ventana-v0.1.3.tar.gz/ventana-v0.1.3/ventana/cutpoints.py
@@ -0,0 +1,87 @@
+"""
+Cutpoints
+=============
+Defines methods for classifying activity levels based on a second-by-second input.
+
+Provides 3 different methods for validation purposes
+"""
+
+import numpy as np
+import ventana.settings as settings
+from ventana.utils import chunks
+
+def freedson_cut(val):
+    if val < settings.FREEDSON_SED:
+        return "sedentary"
+    elif val < settings.FREEDSON_LIGHT:
+        return "light"
+    elif val < settings.FREEDSON_MOD:
+        return "moderate"
+    else:
+        return "vigourous"
+
+def freedson(counts, time_freq = 60):
+    """
+    Freedson second-by-second classification of activity level based on second-by-second vertical counts
+
+    :param counts: Second-by-second vertical counts (numbers)
+    :type counts: list
+    :return: Second-by-second classification of activity levels (strings)
+    :rtype: list
+    """
+    minute_est = [freedson_cut(sum(x)) for x in chunks(counts, time_freq)]
+    second_est = []
+    length = min(len(counts), time_freq)
+    [second_est.extend([x] * length) for x in minute_est]
+    return np.array(second_est)
+
+def sasaki_cut(val):
+    if val < settings.SASAKI_LIGHT:
+        return "light"
+    elif val < settings.SASAKI_MOD:
+        return "moderate"
+    elif val < settings.SASAKI_VIG:
+        return "vigourous"
+    else:
+        return "very vigourous"
+
+def sasaki(counts, time_freq = 60):
+    """
+    Sasaki second-by-second classification of activity level based on second-by-second vertical counts
+
+    :param counts: Second-by-second vertical counts (numbers)
+    :type counts: list
+    :return: Second-by-second classification of activity levels (strings)
+    :rtype: list
+    """
+    minute_est = [sasaki_cut(sum(x)) for x in chunks(counts, time_freq)]
+    second_est = []
+    length = min(len(counts), time_freq)
+    [second_est.extend([x] * length) for x in minute_est]
+    return np.array(second_est)
+
+def nhanes_cut(val):
+    if val < settings.NHANES_SED:
+        return "sedentary"
+    elif val < settings.NHANES_LIGHT:
+        return "light"
+    elif val < settings.NHANES_MOD:
+        return "moderate"
+    else:
+        return "vigourous"
+
+def nhanes(counts, time_freq = 60):
+    """
+    Nhanes second-by-second classification of activity level based on second-by-second vertical counts
+
+    :param counts: Second-by-second vertical counts (numbers)
+    :type counts: list
+    :return: Second-by-second classification of activity levels (strings)
+    :rtype: list
+    """
+    minute_est = [nhanes_cut(sum(x)) for x in chunks(counts, time_freq)]
+    second_est = []
+    length = min(len(counts), time_freq)
+    [second_est.extend([x] * length) for x in minute_est]
+    return np.array(second_est)
+    
---
+++ packages/ventana/ventana-v0.1.3.tar.gz/ventana-v0.1.3/ventana/sojourn.py
@@ -0,0 +1,106 @@
+"""
+Sojourn
+=============
+Defines methods for classifying activity levels based on a second-by-second input using sojourn methods.
+
+Provides 1x and 3x methods for validation purposes
+"""
+
+import numpy as np
+import ventana.settings as const
+from ventana.METs import cr2_mets
+
+def yield_sojourns(values):
+    running = []
+    flag = True if values[0] > 0. else False
+    for value in values:
+        if (value > 0) == flag:
+            running.append(value)
+        else:
+            yield running
+            running = [value]
+            flag = not flag
+
+def is_too_short_undet(ident, length):
+    return (ident == "undetermined") and (length < const.ACTIVITY_CUT)
+
+def combine_sojourns(clean_s, clean_id):
+    skip = False
+    for i, soj in enumerate(clean_s):
+        if skip:
+            skip = False
+            continue
+        if is_too_short_undet(clean_id[i], len(soj)):
+            if (i == 0) or (i + 1 == len(clean_id)):
+                yield soj, "undetermined"
+            elif clean_id[i - 1] == clean_id[i + 1]:
+                yield clean_s[i - 1] + soj + clean_s[i + 1], clean_id[i - 1]
+                skip = True
+            else:
+                yield clean_s[i - 1] + soj, clean_id[i - 1]
+        else:
+            if (i + 1 < len(clean_id)) and is_too_short_undet(clean_id[i + 1], len(clean_s[i + 1])):
+                continue
+            yield soj, clean_id[i]
+
+
+
+def clean_sojourns(sojourns, identity):
+    j = 0
+    clean_s = []
+    clean_id = []
+    for i in range(len((identity))):
+        if j > 0:
+            j -= 1
+            continue
+        if (i + 1 < len(identity)) and (identity[i] == identity[i + 1]):
+            x = sojourns[i]
+            j = 0
+            while (i + j + 1 < len(identity)) and (identity[i + j] == identity[i + j + 1]):
+                x.extend(sojourns[i + j + 1])
+                j += 1
+            clean_s.append(x)
+            clean_id.append(identity[i])
+        else:
+            clean_s.append(sojourns[i])
+            clean_id.append(identity[i])
+    return combine_sojourns(clean_s, clean_id)
+
+def sojourn_1x(counts, met_method = cr2_mets):
+    """
+    Sojourn second-by-second estimation of METs based on second-by-second vertical counts
+
+    :param counts: Second-by-second vertical counts (numbers)
+    :type counts: list
+    :return: Second-by-second estimation of METs (floats)
+    :rtype: list
+    """
+    base_identity = []
+    sojourns = []
+    for sojourn in yield_sojourns(counts):
+        if (sojourn[0] == 0) and (len(sojourn) > const.SIT_CUT):
+            base_identity.append("sedentary")
+        elif (sojourn[0] != 0) and (len(sojourn) > const.ACTIVITY_CUT):
+            base_identity.append("activity")
+        else:
+            base_identity.append("undetermined")
+        sojourns.append(sojourn)
+    pred = []
+    for sojourn, level in clean_sojourns(sojourns, base_identity):
+        mets = []
+        if level == "activity":
+            mets = met_method(sojourn)
+        else:
+            val_cut = np.mean(sojourn)
+            if val_cut > const.CUT_HIGH:
+                mets = met_method(sojourn)
+            elif (val_cut > const.CUT_MIN) and (val_cut <= const.CUT_MED) and (len(sojourn) > const.SIT_CUT):
+                mets = [const.SOJ_METS_MED] * len(sojourn)
+            elif (val_cut > const.CUT_MIN) and (val_cut <= const.CUT_MED) and (len(sojourn) <= const.SIT_CUT):
+                mets = [const.SOJ_METS_HIGH] * len(sojourn)
+            elif (val_cut > const.CUT_MED) and (val_cut <= const.CUT_HIGH):
+                mets = [const.SOJ_METS_VIG] * len(sojourn)
+            else:
+                mets = [const.SOJ_METS_MIN] * len(sojourn)
+        pred.extend(mets)
+    return pred
