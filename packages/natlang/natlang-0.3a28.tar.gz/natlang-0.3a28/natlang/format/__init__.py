ff5642442f6397db3fc273e31570c6755e6f538c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package natlang/0.3a28/natlang-0.3a28.tar.gz

---
+++ packages/natlang/natlang-0.3a28.tar.gz/natlang-0.3a28/natlang/format/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+import unittest
+from natlang.format import AMR
+from natlang.format import astTree
+from natlang.format import pyCode
+from natlang.format import semanticFrame
+from natlang.format import tree
+from natlang.format import txt
+from natlang.format import txtFiles
+from natlang.format import txtOrTree
+from natlang.format import conll
+from natlang.format import django
+from natlang.format import intent
+from natlang.format import intentJSON
+from natlang.format import pyCodeJSON
+from natlang.format import bashJSON
