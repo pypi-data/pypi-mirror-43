ff55d1e6c5b9783149ec686c36fa960e6a4d656d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:32 +0000

    Add package skanetrafiken/2.2.0/skanetrafiken-2.2.0.tar.gz

---
+++ packages/skanetrafiken/skanetrafiken-2.2.0.tar.gz/skanetrafiken-2.2.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='skanetrafiken',
+      version='2.2.0',
+      py_modules=['skanetrafiken'],
+      author='Peter Stark',
+      author_email='peterstark72@gmail.com',
+      url='https://github.com/peterstark72/skanetrafiken',
+      install_requires=['lxml'],
+      description="Python wrapper for Skanetrafiken Open API",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: Free for non-commercial use',
+                   'Natural Language :: Swedish',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Internet']
+      )
