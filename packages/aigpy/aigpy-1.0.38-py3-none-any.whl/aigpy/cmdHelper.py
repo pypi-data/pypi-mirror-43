1f0ba1f9759829ebdaa9b848b0cd0a49774c1d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:17 +0000

    Add package aigpy/1.0.38/aigpy-1.0.38-py3-none-any.whl

---
+++ packages/aigpy/aigpy-1.0.38-py3-none-any.whl/aigpy/cmdHelper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   cmdHelper.py
+@Time    :   2019/02/27
+@Author  :   Yaron Huang 
+@Version :   1.0
+@Contact :   yaronhuang@qq.com
+@Desc    :   
+'''
+
+import sys
+
+# Compatible with python 2.x version
+def myinput(desc):
+    v = sys.version_info
+    if v[0] > 2:
+        return input(desc)
+    else:
+        return raw_input(desc)
+
+def myinputInt(desc, default):
+    try:
+        str = myinput(desc)
+        ret = int(str)
+        return ret
+    except:
+        return default
+
+def myinputFloat(desc, default):
+    try:
+        str = myinput(desc)
+        ret = float(str)
+        return ret
+    except:
+        return default
+
+def findInArgv(str):
+    if sys.argv == None or len(sys.argv) == 0:
+        return None
+    
+    for item in sys.argv:
+        if item == sys.argv[0]:
+            continue
+        if item.find(str) >= 0:
+            return item
+    return None
+
+def converArgvToStr(list):
+    str = ''
+    for item in list:
+        if str != '':
+            str = str + ' '
+        str = str + '"' + item + '"'
+    return str
