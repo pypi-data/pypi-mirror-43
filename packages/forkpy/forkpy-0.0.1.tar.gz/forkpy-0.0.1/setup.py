ce36e2f926810092507b3e52687520333619f1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:21 +0000

    Add package forkpy/0.0.1/forkpy-0.0.1.tar.gz

---
+++ packages/forkpy/forkpy-0.0.1.tar.gz/forkpy-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='forkpy',
+    version='0.0.1',
+    author='Yohan Chalier',
+    author_email='yohan.chalier@gmail.com',
+    packages=['forkpy',],
+    scripts=[],
+    url='http://pypi.python.org/pypi/forkpy/',
+    license='GNU GPLv3',
+    description='Multiprocessing framework.',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
