98a22d7fbc1c32e361f40e85ab786f180f060325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package tango-simlib/0.3.0/tango_simlib-0.3.0.tar.gz

---
+++ packages/tango-simlib/tango_simlib-0.3.0.tar.gz/tango_simlib-0.3.0/tango_simlib/examples/weather2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from PyTango.server import server_run
+from tango_simlib.tango_sim_generator import (configure_device_models, get_tango_device_server)
+
+
+def main():
+    sim_data_files = ['../tests/config_files/Weather_SimDD.json']
+    model = configure_device_models(sim_data_files)
+    TangoDeviceServers = get_tango_device_server(model, sim_data_files)
+    server_run(TangoDeviceServers)
+
+if __name__ == "__main__":
+    main()
+
