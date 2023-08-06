4389e73b4adccc4e257682a2e87a60a9bd28a0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:19 +0000

    Add package amlfbp/0.36/amlfbp-0.36.tar.gz

---
+++ packages/amlfbp/amlfbp-0.36.tar.gz/amlfbp-0.36/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.dirname(__file__)
+packages = find_packages(where=here)
+package_dir = {k: os.path.join(here, k.replace(".", "/")) for k in packages}
+
+setup(name='amlfbp',
+      version='0.36',
+      description="Azure Machine Learning service compute for Busy People. See more in the Github",
+      author='Aleksander Callebat',
+      author_email='aleks_callebat@hotmail.fr',
+      url='https://github.com/alekscallebat/amlfbp',
+      packages=["amlfbp"],
+      entry_point={
+            'console_scripts': ['amlfbp = amlfbp.__main__:main']
+            }
+      )
