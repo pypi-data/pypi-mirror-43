433ccbae63e760f36da409bd2b8df807845f932d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:24 +0000

    Add package openpifpaf/0.2.1/openpifpaf-0.2.1.tar.gz

---
+++ packages/openpifpaf/openpifpaf-0.2.1.tar.gz/openpifpaf-0.2.1/openpifpaf/datasets.py
@@ -0,0 +1,146 @@
+import logging
+import os
+import torch.utils.data
+import torchvision
+from PIL import Image
+
+from . import transforms, utils
+
+
+def collate_images_anns_meta(batch):
+    images = torch.utils.data.dataloader.default_collate([b[0] for b in batch])
+    anns = [b[1] for b in batch]
+    metas = [b[2] for b in batch]
+    return images, anns, metas
+
+
+def collate_images_targets_meta(batch):
+    images = torch.utils.data.dataloader.default_collate([b[0] for b in batch])
+    targets = torch.utils.data.dataloader.default_collate([b[1] for b in batch])
+    metas = [b[2] for b in batch]
+    return images, targets, metas
+
+
+class CocoKeypoints(torch.utils.data.Dataset):
+    """`MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>`_ Dataset.
+
+    Based on `torchvision.dataset.CocoDetection`.
+
+    Caches preprocessing.
+
+    Args:
+        root (string): Root directory where images are downloaded to.
+        annFile (string): Path to json annotation file.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+
+    def __init__(self, root, annFile, image_transform=None, target_transforms=None,
+                 n_images=None, preprocess=None, all_images=False):
+        from pycocotools.coco import COCO
+        self.root = root
+        self.coco = COCO(annFile)
+
+        self.cat_ids = self.coco.getCatIds(catNms=['person'])
+        if not all_images:
+            self.ids = self.coco.getImgIds(catIds=self.cat_ids)
+            self.filter_for_keypoint_annotations()
+        else:
+            self.ids = self.coco.getImgIds()
+        if n_images:
+            self.ids = self.ids[:n_images]
+        print('Images: {}'.format(len(self.ids)))
+
+        self.preprocess = preprocess or transforms.Preprocess()
+        self.image_transform = image_transform or transforms.image_transform
+        self.target_transforms = target_transforms
+
+        self.log = logging.getLogger(self.__class__.__name__)
+
+    def filter_for_keypoint_annotations(self):
+        print('filter for keypoint annotations ...')
+        def has_keypoint_annotation(image_id):
+            ann_ids = self.coco.getAnnIds(imgIds=image_id, catIds=self.cat_ids)
+            anns = self.coco.loadAnns(ann_ids)
+            for ann in anns:
+                if 'keypoints' not in ann:
+                    continue
+                if any(v > 0.0 for v in ann['keypoints'][2::3]):
+                    return True
+            return False
+
+        self.ids = [image_id for image_id in self.ids
+                    if has_keypoint_annotation(image_id)]
+        print('... done.')
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        image_id = self.ids[index]
+        ann_ids = self.coco.getAnnIds(imgIds=image_id, catIds=self.cat_ids)
+        anns = self.coco.loadAnns(ann_ids)
+
+        image_info = self.coco.loadImgs(image_id)[0]
+        self.log.debug(image_info)
+        with open(os.path.join(self.root, image_info['file_name']), 'rb') as f:
+            image = Image.open(f).convert('RGB')
+
+        meta = {
+            'image_id': image_id,
+            'file_name': image_info['file_name'],
+        }
+
+        if 'flickr_url' in image_info:
+            _, flickr_file_name = image_info['flickr_url'].rsplit('/', maxsplit=1)
+            flickr_id, _ = flickr_file_name.split('_', maxsplit=1)
+            meta['flickr_full_page'] = 'http://flickr.com/photo.gne?id={}'.format(flickr_id)
+
+        # preprocess image and annotations
+        image, anns, preprocess_meta = self.preprocess(image, anns)
+        meta.update(preprocess_meta)
+
+        # transform image
+        original_size = image.size
+        image = self.image_transform(image)
+
+        # mask valid
+        valid_area = meta['valid_area']
+        utils.mask_valid_image(image, valid_area)
+
+        # if there are not target transforms, done here
+        self.log.debug(meta)
+        if self.target_transforms is None:
+            return image, anns, meta
+
+        # transform targets
+        targets = [t(anns, original_size) for t in self.target_transforms]
+        return image, targets, meta
+
+    def __len__(self):
+        return len(self.ids)
+
+
+class ImageList(torch.utils.data.Dataset):
+    def __init__(self, image_paths, image_transform=None):
+        self.image_paths = image_paths
+        self.image_transform = image_transform or transforms.image_transform
+
+    def __getitem__(self, index):
+        image_path = self.image_paths[index]
+        with open(image_path, 'rb') as f:
+            image = Image.open(f).convert('RGB')
+
+        original_image = torchvision.transforms.functional.to_tensor(image)
+        image = self.image_transform(image)
+
+        return image_path, original_image, image
+
+    def __len__(self):
+        return len(self.image_paths)
