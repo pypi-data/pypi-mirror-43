b2fc36965301d8d29e9ba06ff529dbc84b015e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:58 +0000

    Add package fern-labextension/0.0.6/fern_labextension-0.0.6.tar.gz

---
+++ packages/fern-labextension/fern_labextension-0.0.6.tar.gz/fern_labextension-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fern_labextension',
+    version='0.0.6',
+    author='amie',
+    author_email='hej@amie.ai',
+    license="private",
+    description='Fern Jupyter lab Extension',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/disraptoer/fern-next',
+    python_requires='>=3.5',
+    install_requires=[
+        'ipython', 'pandas', 'numpy', 'jupyterlab', 'amieci>=0.0.7'
+    ])
