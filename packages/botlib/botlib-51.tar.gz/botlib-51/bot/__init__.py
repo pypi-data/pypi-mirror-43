1c593c1aabf7fb2615ef815cea6cc027976e87f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:07 +0000

    Add package botlib/51/botlib-51.tar.gz

---
+++ packages/botlib/botlib-51.tar.gz/botlib-51/bot/__init__.py
@@ -0,0 +1,74 @@
+# bot/__init__.py
+#
+#
+
+""" bot package. """
+
+__version__ = 51
+__txt__ = "Framework to program bots"
+
+## imports
+
+import bot
+import bot.handler
+import logging
+import sys
+
+from bot.tasks import launch
+from bot.utils import hd
+
+## classes
+
+class Bot(bot.handler.Handler):
+
+    def __init__(self):
+        super().__init__()
+        self.channels = []
+        self.prompt = False
+        self.verbose = True
+
+    def announce(self, txt):
+        for channel in self.channels:
+            self.say(channel, txt)
+
+    def cmd(self, txt, options="", origin=""):
+        if not cfg.workdir:
+            cfg.workdir = hd(".bot")
+            bot.base.cdir(cfg.workdir)
+        self.start()
+        txt = self.get_aliased(txt)
+        e = bot.event.Event(txt)
+        e.orig = repr(self)
+        e.options = options
+        e.origin = origin or "root@shell"
+        self.dispatch(e)
+        self.ready()
+        return e
+
+    def join(self):
+        pass
+
+    def raw(self, txt):
+        print(txt)
+
+    def resume(self):
+        pass
+
+    def say(self, botid, channel, txt):
+        if self.verbose:
+            self.raw(txt)
+
+    def show_prompt(self):
+        pass
+
+    def work(self):
+        while not self._stopped:
+            event = self._queue.get()
+            if not event:
+                break
+            self.dispatch(event)
+        self.ready()
+
+cfg = bot.base.Cfg(_default="")
+cfg.workdir = "data"
+cfg.logdir = "logs"
