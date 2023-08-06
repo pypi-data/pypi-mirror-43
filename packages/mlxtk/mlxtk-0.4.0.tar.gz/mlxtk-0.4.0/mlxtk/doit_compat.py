3ae323d752aa6cf1f2f7b060b72f07a69ed764c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:16 +0000

    Add package mlxtk/0.4.0/mlxtk-0.4.0.tar.gz

---
+++ packages/mlxtk/mlxtk-0.4.0.tar.gz/mlxtk-0.4.0/mlxtk/doit_compat.py
@@ -0,0 +1,73 @@
+"""Compatibility layer for the DoIt library.
+
+This module provides various helper functions and classes to use the
+`DoIt <http://pydoit.org/>`_ library in a way that is adequate for mlxtk.
+"""
+from doit.cmd_base import TaskLoader
+from doit.doit_cmd import DoitMain
+from doit.task import dict_to_task
+
+from .timing import Timer
+
+
+class CustomTaskLoader(TaskLoader):
+    def __init__(self, task_generators):
+        self.task_generators = task_generators
+
+        super().__init__()
+
+    def load_tasks(self, cmd, opt_values, pos_args):
+        del cmd
+        del opt_values
+        del pos_args
+        tasks = []
+        for task_generator in self.task_generators:
+            tasks.append(dict_to_task(task_generator()))
+        return tasks, {}
+
+
+def run_doit(task_generators, arguments=None) -> int:
+    if arguments is None:
+        raise ValueError("No arguments passed to DoitMain")
+    return DoitMain(CustomTaskLoader(task_generators)).run(arguments)
+
+
+class DoitAction:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, targets, *args, **kwargs):
+        timer = Timer()
+        ret = self.func(targets, *args, **kwargs)
+        timer.stop()
+
+        if ret is None:
+            return {
+                self.func.__name__: {
+                    "monotonic_time": timer.get_monotonic_time(),
+                    "perf_time": timer.get_perf_time(),
+                    "process_time": timer.get_process_time(),
+                }
+            }
+
+        if isinstance(ret, dict):
+            ret[self.func.__name__] = ret.get(self.func.__name__, {})
+            ret[self.func.
+                __name__]["monotonic_time"] = timer.get_monotonic_time()
+            ret[self.func.__name__]["perf_time"] = timer.get_perf_time()
+            ret[self.func.__name__]["process_time"] = timer.get_process_time()
+            return ret
+
+        if isinstance(ret, bool):
+            if not ret:
+                return ret
+            return {
+                self.func.__name__: {
+                    "monotonic_time": timer.get_monotonic_time(),
+                    "perf_time": timer.get_perf_time(),
+                    "process_time": timer.get_process_time(),
+                }
+            }
+
+        raise NotImplementedError(
+            "The return type {} is not supported for Doit actions")
---
+++ packages/mlxtk/mlxtk-0.4.0.tar.gz/mlxtk-0.4.0/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="mlxtk",
+    version="0.4.0",
+    author="Fabian Köhler",
+    author_email="fkoehler@physnet.uni-hamburg.de",
+    description="Toolkit to design, run and analyze ML-MCTDH(X) simulations",
+    long_description="""
+mlxtk gives the user a simple interface to setup physical systems and provides
+common simulation tasks to be used as building blocks to set up rather complex
+simulations. Data is automatically stored in efficient formats (i.e. HDF5 and
+gzipped files).
+
+Simulations can also be used in the context of parameter scans where each
+simulation is executed for each specified parameter combination. Submission
+of simulation jobs to computing clusters is easily achieved from the command
+line.
+
+Furthermore, analysis and plotting tools are provided to interpret the
+simulation outcome.
+""",
+    url="https://github.com/f-koehler/mlxtk",
+    license="MIT",
+    install_requires=[
+        "colorama",
+        "doit",
+        "h5py",
+        "jinja2",
+        "matplotlib",
+        "numpy",
+        "numpy-stl",
+        "pandas",
+        "scipy",
+        "tabulate",
+    ],
+    packages=["mlxtk"],
+    entry_points={
+        "console_scripts": [
+            "gpop_model = mlxtk.scripts.gpop_model:main",
+            "plot_energy = mlxtk.scripts.plot_energy:main",
+            "plot_energy_diff = mlxtk.scripts.plot_energy_diff:main",
+            "plot_entropy = mlxtk.scripts.plot_entropy:main",
+            "plot_entropy_diff = mlxtk.scripts.plot_entropy_diff:main",
+            "plot_expval = mlxtk.scripts.plot_expval:main",
+            "plot_gpop = mlxtk.scripts.plot_gpop:main",
+            "plot_natpop = mlxtk.scripts.plot_natpop:main",
+            "spectrum_1b = mlxtk.scripts.spectrum_1b:main",
+        ]
+    },
+    keywords=["physics quantum dynamics ML-MCTDHX MCTDHB MCTDHF dataflow"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: MacOS X",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    python_requires=">=3.5",
+    zip_safe=True,
+)
